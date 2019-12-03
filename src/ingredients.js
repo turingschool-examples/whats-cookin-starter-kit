89b23b73d706267361bcf4f20c448d7583f0a6c8
Cody Smith <codysmith2814@gmail.com>
2019-12-03 15:33:26 -0700

    Starts test

---
+++ src/ingredients.js
@@ -0,0 +1,9 @@
+class Ingredients {
+  constructor(id, name, estimatedCostInCents) {
+    this.id = id;
+    this.name = name;
+    this.estimatedCostInCents = estimatedCostInCents;
+  }
+}
+
+module.exports = Ingredients;
