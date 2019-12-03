16085725897c9c03520de59b29d16283258d9ff8
Virginia Ladd <virginialadd2@gmail.com>
2019-12-03 14:20:11 -0700

    Create user class with 2 methofs

---
+++ src/user.js
@@ -0,0 +1,16 @@
+class User = {
+  constructor(id, name, pantry) {
+    this.id = id;
+    this.name = name;
+    this.pantry = pantry;
+  }
+
+  saveToFavorites() {
+
+  }
+
+  recipesToCook() {
+
+  }
+  
+}
