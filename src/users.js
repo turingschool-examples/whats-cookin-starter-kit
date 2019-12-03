5d5034b23a32a12bc54d1c469e7617884cc9fde1
Cody Smith <codysmith2814@gmail.com>
2019-12-03 15:34:21 -0700

    Starts test

---
+++ src/users.js
@@ -0,0 +1,11 @@
+class Users {
+  constructor(id, name, pantry) {
+    this.id = id;
+    this.name = name;
+    this.pantry = pantry;
+    this.favoriteRecipes = [];
+    this.weeklyMenu = [];
+  }
+}
+
+module.exports = Users
