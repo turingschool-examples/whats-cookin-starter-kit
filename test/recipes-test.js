bb54c0fc0398879c9bfabd48ee9df350bbd35400
Karen Nguyen <knguyen4184@yahoo.com>
2019-12-03 16:37:48 -0700

    Write test to instantiate Recipes

--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,2 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Recipes = require('../src/recipes-class');
+
+describe('recipes', function() {
+
+  it('should be a function', function() {
+    expect(Recipes).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Recipes();
+    expect(turn).to.be.an.instanceof(Recipes);
+  });
+});
