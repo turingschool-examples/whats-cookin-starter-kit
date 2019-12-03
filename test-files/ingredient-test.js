bc575c03644656e7bc81e5576b98f45abafc3700
Grayson Palmer <gpalmer1385@gmail.com>
2019-12-03 16:54:09 -0700

    Create tests for the ingredient class

--- test-files/ingredient-test.js
+++ test-files/ingredient-test.js
@@ -1,3 +1,38 @@
 const chai = require("chai");
 const expect = chai.expect;
+let ingredient;
+
 const Ingredients = ("../src/scripts/Ingredients.js");
+
+beforeEach(() => {
+  // Will need to input arguments to match the data file
+  ingredient = new Ingredients(2080, 'kimchi', 710);
+})
+
+describe ('Ingredients', () => {
+
+  it('should be a function', () => {
+    expect(Ingredient).to.be.a('function');
+  })
+
+  it('should be an instance of Ingredient', () => {
+    expect(ingredient).to.be.an.instanceof(Ingredient);
+  })
+
+  it('should have a unique id', () => {
+    expect(ingredient.id).to.equal(2080);
+  })
+
+  it('should have the name of the ingredient', () => {
+    expect(ingredient.name).to.equal('kimchi');
+  })
+
+  it('should have a price in cents', () => {
+    expect(ingredient.estimatedCostInCents).to.equal(710);
+  })
+
+  it('should be able to show the price in dollars', () => {
+    expect(ingredient.priceInDollars()).to.equal(7.10);
+  })
+
+})
