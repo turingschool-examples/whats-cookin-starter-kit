a2660b726b3b2c38e2b4bba537c08b3765133d72
Cody Smith <codysmith2814@gmail.com>
2019-12-03 19:07:59 -0700

    adds instatiation function

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,28 @@
 //add event listener on recipe button
 //when clicked, reassign pantry.selectedRecipe to recipe that was clicked.
 
-let recipe = document.querySelector('.recipe-container');
 
-recipe.addEventListener('click', pantry.moveToSelectedRecipes)
+
+//appending in the iterator
+function instantiateRecipeCards() {
+  recipeData.forEach(recipe => {
+    document.querySelector('.recipe-card-area').insertAdjacentHTML('afterbegin',
+      `<div class="recipe-container">
+<div class="image-container">
+<img src="https://spoonacular.com/recipeImages/${recipe.id}-556x370.jpg" class="image-container">
+</div>
+<div class="recipe-text">
+  <h2>${recipe.name}</h2>
+  <div class="card-button-containter">
+    <button type="button" class="card-buttons">View Recipe</button>
+    <button type="button" class="card-buttons">Add to Menu</button>
+    <button type="button" class="card-buttons">Add to Favorites</button>
+  </div>
+</div>
+</div>`)
+  })
+  
+}
+
+instantiateRecipeCards();
 
