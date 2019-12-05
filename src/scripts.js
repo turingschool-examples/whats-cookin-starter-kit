5dc57cf624d6c3e5661868e4199776a642509e16
“Greg_Grimes” <cggrimes@yahoo.com>
2019-12-04 17:40:35 -0700

    Start transition from welcome page to chosen page

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,34 @@
+// const Recipes = require('../data/recipes')
+
 const navBar = document.querySelector('nav');
 const next = document.querySelector('button');
-const welcomeBoxes = document.querySelectorAll('.welcome')
-next.addEventListener("click", animateNavBar)
+const welcomeBoxes = document.querySelectorAll('.menu-box');
+const cardHolder = document.querySelector('.recipe-holder');
+
+next.addEventListener("click", animateNavBar);
 
 function animateNavBar (){
   for(i = 0; i < welcomeBoxes.length; i++){
-    welcomeBoxes[i].classList.add('faded')
+    welcomeBoxes[i].classList.add('faded');
   }
   navBar.style.animationDuration = '1.5s';
   navBar.style.animationName = 'slidein';
   navBar.style.animationFillMode = 'forwards';
+  //insert new nav bar here and new divs for the 4 pages
+  setTimeout(function(){
+    console.log('hi');
+    navBar.removeAttribute("style");
+    navBar.classList.add("main-nav");
+    welcomeBoxes.classList.remove("welcome");
+    welcomeBoxes.classList.add("nav-popup");
+  }, 1500);
 }
 
+// Instantiating new cards???
+
+// for(var i = 0; i < recipeData; i++){
+//   let recipe = new Recipes(recipeData[i]);
+//   cardHolder.innerHTML += `
+//   <div class ="recipes">${recipe}</div>
+//   `
+// }
