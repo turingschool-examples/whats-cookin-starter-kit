0c16b4e0cb9bae8577d2b6d1cc38c4feee1dd375
“Greg_Grimes” <cggrimes@yahoo.com>
2019-12-05 13:47:36 -0700

    Animate buttons

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
 // const Recipes = require('../data/recipes')
 
+const body = document.querySelector('body');
 const navBar = document.querySelector('nav');
 const next = document.querySelector('button');
 const welcomeBoxes = document.querySelectorAll('.menu-box');
@@ -12,18 +13,24 @@ function animateNavBar (){
     welcomeBoxes[i].classList.add('faded');
   }
   navBar.style.animationDuration = '1.5s';
-  navBar.style.animationName = 'slidein';
-  navBar.style.animationFillMode = 'forwards';
+  navBar.style.animationName = 'nav-animation';
   //insert new nav bar here and new divs for the 4 pages
   setTimeout(function(){
     console.log('hi');
     navBar.removeAttribute("style");
     navBar.classList.add("main-nav");
-    welcomeBoxes.classList.remove("welcome");
-    welcomeBoxes.classList.add("nav-popup");
+    for(i = 0; i < welcomeBoxes.length; i++){
+      welcomeBoxes[i].classList.remove("welcome");
+      welcomeBoxes[i].classList.remove("faded");
+      welcomeBoxes[i].classList.add("nav-popup");
+      welcomeBoxes[i].classList.add("popup-animate");
+    }
   }, 1500);
 }
 
+function animateButton(){
+
+}
 // Instantiating new cards???
 
 // for(var i = 0; i < recipeData; i++){
