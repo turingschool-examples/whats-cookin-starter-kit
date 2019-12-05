9d39d6648b8fef0e9573e7605919ce129e6b6a59
Kim McCaskill <kimmccaskill92@gmail.com>
2019-12-05 14:14:13 -0700

    Clean up recipe class

--- src/recipe.js
+++ src/recipe.js
@@ -1,5 +1,5 @@
-const recipeData = require('../sub-data/recipes-sub-data.js')
-const ingredientData = require('../sub-data/ingredients-sub-data.js')
+// const recipeData = require('../data/recipes.js')
+// const ingredientData = require('../data/ingredients.js')
 
 class Recipe {
   constructor(name, id, image, ingredients, instructions, tags) {
@@ -11,32 +11,32 @@ class Recipe {
     this.tags = tags;
   }
 
-  calculateTotalCost(name) {
-    let ingredients = []
-    let findIngredients = recipeData.find(recipe => {
-      if (recipe.name === name) {
-        recipe.ingredients.map(ingredient => {
-          ingredients.push(ingredient.id);
-        })
-      }
-    })
-    let findEachCost = ingredients.reduce((acc, ingredient) => {
-      ingredientData.forEach(dataIngredient => {
-        if(dataIngredient.id === ingredient) {
-          acc += (dataIngredient.estimatedCostInCents*
-        }
-      })
-      return acc;
-    }, 0)
-    return findEachCost;
-    // let recipeCost = ingredientData.reduce((acc, ingredient) => {
-    //   if (ingredient.name === recipeName) {
-    //     acc += ingredient.estimatedCostInCents;
-    //   }
-    //   return acc;
-    // }, 0)
-    // return recipeCost;
-  }
+  // calculateTotalCost(name) {
+  //   let ingredients = []
+  //   let findIngredients = recipeData.find(recipe => {
+  //     if (recipe.name === name) {
+  //       recipe.ingredients.map(ingredient => {
+  //         ingredients.push(ingredient.id);
+  //       })
+  //     }
+  //   })
+  //   let findEachCost = ingredients.reduce((acc, ingredient) => {
+  //     ingredientData.forEach(dataIngredient => {
+  //       if(dataIngredient.id === ingredient) {
+  //         acc += (dataIngredient.estimatedCostInCents*
+  //       }
+  //     })
+  //     return acc;
+  //   }, 0)
+  //   return findEachCost;
+  //   // let recipeCost = ingredientData.reduce((acc, ingredient) => {
+  //   //   if (ingredient.name === recipeName) {
+  //   //     acc += ingredient.estimatedCostInCents;
+  //   //   }
+  //   //   return acc;
+  //   // }, 0)
+  //   // return recipeCost;
+  // }
 }
 
 if (typeof module !== 'undefined') {
