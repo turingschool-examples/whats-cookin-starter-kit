41044c81d7cc09bef3b7ec1b578456a27213e24d
Virginia Ladd <virginialadd2@gmail.com>
2019-12-05 16:30:31 -0700

    Build out available meals based off pantry function

--- src/user.js
+++ src/user.js
@@ -1,12 +1,11 @@
-const Recipe = require('../src/recipe.js');
-
 class User {
   constructor(id, name, pantry) {
     this.id = id;
     this.name = name;
     this.pantry = pantry;
-    // this.recipe = recipe;
     this.favorites = [];
+    this.pantryItemsId = [];
+    this.recipeIngId = [];
   }
 
   saveToFavorites() {
@@ -14,13 +13,58 @@ class User {
     return this.favorites;
   }
 
-  recipesToCook() {
+  condenseUserIngredientId() {
+    this.pantryItemsId.push(this.pantry[0].ingredient);
+    return this.pantryItemsId;
+  }
 
+  condenseRecipeIngredientId() {
+    this.recipeIngId.push(recipe.ingredients.id);
+    return this.recipeIngId;
+
+  }
+
+  recipesToCook() {
+    this.condenseUserIngredientId();
+    this.condenseRecipeIngredientId();
+    //filter through recipes
+    //conditional if the user has the ingredients
+    //return a new array of the available recipes
+    // console.log(this.pantry[0].ingredient);
+    // console.log(recipe.ingredients.id);
+
+    //filter for all id's in recipe needed
+    //filter users pantry id's into a single array
+    //recipes contained within the users data returns true
+
+    let availableRecipes = [];
+    // if(recipe.ingredients.id === this.pantry[0].ingredient) {
+    //   availableRecipes.push(recipe);
+    // }
+    this.recipeIngId.forEach(ingredient => {
+
+      // if(this.pantryItemsId.includes(ingredient)) {
+      //   let recipeRequiredIng = [];
+      //   recipeRequiredIng.push(ingredient);
+      // };
+      //[12, 5, 8, 130, 44].every(x => x >= 10); // false
+      //[12, 54, 18, 130, 44].every(x => x >= 10); // true​
+
+      this.pantryItemsId.every((ingredient) => {
+        availableRecipes.push(recipe);
+        // return availableRecipes;
+        console.log(availableRecipes);
+      });
+
+
+    });
+    return availableRecipes;
   }
 
   searchByTags() {
 
   }
+
 }
 
 module.exports = User;
--- test/user-test.js
+++ test/user-test.js
@@ -2,12 +2,17 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const User = require('../src/user.js');
+const Recipe = require('../src/recipe.js');
 
 describe('Users', function() {
-
+  let user, recipe;
   beforeEach(() => {
-      user = new User(1,'Saige O\'Kon', [11477, 1]);
-    });
+    recipe = new Recipe(595736, 'Loaded Chocolate Chip Pudding Cups', 'https://spoonacular.com/recipeImages/595736-556x370.jpg', ['antipasti', 'starter', 'snack'], ['Add egg and vanilla and mix until combined.'])
+    user = new User(1,'Saige O\'Kon', [{'ingredient': 20081, 'amount': 2}]);
+
+
+    // recipe = new Recipe(541288, 'Sesame Cookies', 'https://spoonacular.com/recipeImages/541288-556x370.jpg', ['antipasti', 'starter', 'snack',], ['Place them on two baking sheets lined with parchment paper.'])
+  });
 
     it('should be a function', function() {
       expect(User).to.be.a('function');
@@ -23,29 +28,54 @@ describe('Users', function() {
       });
 
       it('should know what it has in the pantry', function() {
-        expect(user.pantry).to.deep.equal([11477, 1]);
+        expect(user.pantry[0]).to.deep.equal({'ingredient': 20081, 'amount': 2});
       });
 
       it('should know the correct name', function() {
         expect(user.name).to.equal('Saige O\'Kon');
       });
 
-      it.skip('should know what it can cook', function() {
-        expect(user.saveToFavorites()).to.equal();
-      });
-
       it.skip('should be able to sort by recipes can cook', function() {
         expect(user.recipesToCook()).to.deep.equal();
       });
+    });
 
-      it.skip('should be able to search by tags', function() {
-        expect(user.searchByTags()).to.deep.equal();
-      });
+    it.skip('should be able to search by tags', function() {
+      expect(user.searchByTags()).to.deep.equal();
+    });
 
+    it.skip('should know what it can cook', function() {
+      expect(user.saveToFavorites()).to.equal();
     });
 
     it('should store favorite recipe by id numbers', function() {
       user.saveToFavorites();
       expect(user.favorites[0].id).to.equal(595736);
+    });
+
+    describe('should sort through available recipes', function() {
+
+      it('should store pantry ingredients by id', function() {
+        expect(user.condenseUserIngredientId()).to.deep.equal([20081]);
+      });
+
+      it('should store recipe ingredients by id', function() {
+        expect(user.condenseRecipeIngredientId()).to.deep.equal([20081]);
+      });
+
+      it('should instantiate a new recipe', function() {
+        let recipe2 = new Recipe(595736, 'Loaded Chocolate Chip Pudding Cookie Cups', 'https://spoonacular.com/recipeImages/595736-556x370.jpg', ['antipasti', 'starter', 'snack',], ['Add egg and vanilla and mix until combined.']);
+        expect(recipe2).to.be.an.instanceof(Recipe);
+      });
+
+      it('should be able to find available recipes based on pantry', function() {
+        let recipe1 = new Recipe(541288, 'Sesame Cookies', 'https://spoonacular.com/recipeImages/541288-556x370.jpg', ['antipasti', 'starter', 'snack',], ['Place them on two baking sheets lined with parchment paper.']);
+        let recipe2 = new Recipe(595736, 'Loaded Chocolate Chip Pudding Cookie Cups', 'https://spoonacular.com/recipeImages/595736-556x370.jpg', ['antipasti', 'starter', 'snack',], ['Add egg and vanilla and mix until combined.']);
+        let recipe3 = new Recipe(741603, 'Elvis Pancakes', 'https://spoonacular.com/recipeImages/741603-556x370.jpeg', ['side dish',], ['Watch how to make this recipe.']);
+        user.recipesToCook();
+        console.log(user.availableRecipes);
+
+        expect(user.recipesToCook()).to.deep.equal([recipe3, recipe2, recipe1])
+      });
     });
 });
