94e0f4c0d3471dfbdeb5a0d2c05da10ecbc12ed1
Jesse Maxim <jessmaxim303@gmail.com>
2019-12-05 12:25:43 -0700

    File name change

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// const User = require('../src/User');
+// const Cookbook = require('../src/Cookbook');
+// const Pantry = require('../src/Pantry');
+
+// const users = require('../data/users');
+// const ingredients = require('../data/ingredients');
+// const recipes = require('../data/recipes');
+
+// const chai = require('chai');
+// const expect = chai.expect;
+
+let user = new User(users)
+let cookbook = new Cookbook(recipeData)
+let pantry = new Pantry()
+
+let recipeName = document.querySelector('.recipe_title');
+let cooksName = document.querySelector('.user_title');
+let mainRecipeArea = document.querySelector('.main_recipe-area');
+let addFavoriteButton;
+let favoriteButton;
+
+function insertRecipeTitle(recipeData) {
+	recipeName.innerHTML = user.displayRecipeName(recipeData)
+}
+
+function addFavoritesArray(e) {
+	console.log(e)
+}
+
+function displayRecipes() {
+	cookbook.recipes.forEach(recipe => {
+    mainRecipeArea.insertAdjacentHTML('afterbegin',
+      `<section class="recipe_info-box a">
+            <img src="${recipe.image}">
+            <h1 class="recipe_title">${recipe.name}</h1>
+            <h4 id="ingredients_list">Ingredients:</h4>
+            <button class="add_to-favorites">Favorite</button>
+            <button class="add_to-cook">Save for later</button>
+       </section>`);
+  });
+}
+
+cooksName.innerHTML = user.makeUserName()
+
+displayRecipes()
