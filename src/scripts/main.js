67fcaed3d09598ad3dec4d42d346327145fdb954
Trond Makonese <trondation@gmail.com>
2019-12-05 13:59:58 -0700

    Functions for instatiation has been started.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,25 @@
+// const recipeData = require('../data/Recipes');
+
+
 let navBarToggle = document.querySelector('.navbar-toggle');
 let mainNav = document.querySelector('.main-nav');
+let suggestedRecipes = document.querySelector('.injected-suggested-recipes');
+let allRecipes = null;
 
+window.onload = loadSuggestedRecipesFunction();
+console.log();
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+function loadSuggestedRecipesFunction() {
+
+allRecipes = suggestedRecipes.querySelectorAll('.recipe-card');
+allRecipes.forEach(recipe => recipe.addEventListener('click', recipeHandler));
+};
+
+function instantiateRecipes() {
+  let recipes = [];
+
+
+};
