55daae0c8c7d6e11a801b542853eb1fe8f36e018
Trond Makonese <trondation@gmail.com>
2019-12-05 15:31:24 -0700

    Creating functions to display recipe instructions.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,6 +19,12 @@ suggestedRecipes.insertAdjacentHTML('beforeend',
 `<div class="recipe-card">
   <img src="${allRecipes[i].image}" alt="fork and knife logo">
   <p>${allRecipes[i].name}</p>
+  <div class="recipe-container hidden">
+    <ul class="recipe-ingredients">
+    </ul>
+    <ol class="recipe-directions">
+    </ol>
+  </div>
 </div>`
 )}
 recipeSelector = suggestedRecipes.querySelectorAll('.recipe-card');
