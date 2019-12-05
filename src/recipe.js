689e96f7d0ba845c0831ea47ce1d40a6850fb908
Jim Weist <jp_weist@yahoo.com>
2019-12-05 14:57:55 -0700

    most of the recipe tests are passing reworking the difficult ones

--- src/recipe.js
+++ src/recipe.js
@@ -1,15 +1,16 @@
 const Ingredient = require('../src/ingredients.js');
-ingredients = new Ingredient(20081, 'wheat flour', 142);
+// ingredients = new Ingredient(20081, 'wheat flour', 142);
 
 
 class Recipe {
-  constructor(id, name, image, tags, instructions) {
+  constructor(id, name, image, tags, instructions, ingredients ) {
     this.id = id;
     this.tags = tags;
     this.name = name;
     this.image = image;
-    this.instructions = instructions;
     this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.costPerRecipe = 0;
     // console.log(this.ingredients);
   }
 
@@ -18,12 +19,13 @@ class Recipe {
   filterByIngredients() {
   }
 
-  findCostPerRecipe() {
-    return this.ingredients.reduce((acc, ingredient) => {
-      return ingredient.forEach(ingredient => {
-        console.log('something')
-      })
-    }, 0);
+  findCostPerRecipe(recipeId) {
+    // take in id of ingredient
+    // return cost of all ingredients
+    this.ingredients.reduce((acc, val) => {
+      // console.log(val);
+      return acc;
+    },0)
   }
 
   retrieveInstructions() {
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -2,12 +2,24 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Recipe = require('../src/recipe.js');
+const Ingredient = require('../src/ingredients.js');
 
 describe('Recipe', function() {
+  let recipe, ingredients;
 
   beforeEach(() => {
-      recipe = new Recipe(595736, 'Loaded Chocolate Chip Pudding Cookie Cups', 'https://spoonacular.com/recipeImages/595736-556x370.jpg', ['antipasti', 'starter', 'snack'], ['Add egg and vanilla and mix until combined.']);
-    });
+    recipe = new Recipe(595736,
+      'Loaded Chocolate Chip Pudding Cookie Cups',
+      'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+      ['antipasti', 'starter', 'snack'],
+      ['Add egg and vanilla and mix until combined.'],
+      [1,"In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy."],
+      [ "all purpose flour", 20081, [ 1.5, "c" ] ]
+    );
+
+      ingredients = new Ingredient(20081, "wheat flour", 539);
+  });
+
 
     it('should be a function', function() {
       expect(Recipe).to.be.a('function');
@@ -16,6 +28,7 @@ describe('Recipe', function() {
     it('should instantiate a new recipe', function() {
       expect(recipe).to.be.an.instanceof(Recipe);
     });
+
 
     describe('stored data', function() {
       it('should know the correct ID number', function() {
@@ -41,8 +54,8 @@ describe('Recipe', function() {
     });
 
     it('should know the cost of the recipe per ingredient', function() {
-
-      expect(recipe.findCostPerRecipe()).to.equal(570.85)
+      recipe.findCostPerRecipe(595736)
+      expect(recipe.findCostPerRecipe()).to.equal(539)
     });
 
 });
