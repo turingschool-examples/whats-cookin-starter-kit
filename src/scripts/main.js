dc49953ab13fed91967e352f328ec07fe42cc081
Trond Makonese <trondation@gmail.com>
2019-12-05 16:13:26 -0700

    Finished functionality to add the list of ingredienmts.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-console.log(recipeData);
+// console.log(recipeData);
 // const recipeData = require('../data/recipes');
 
 
@@ -19,14 +19,17 @@ suggestedRecipes.insertAdjacentHTML('beforeend',
 `<div class="recipe-card">
   <img src="${allRecipes[i].image}" alt="fork and knife logo">
   <p>${allRecipes[i].name}</p>
-  <div class="recipe-container hidden">
+  <div class="recipe-container">
     <ul class="recipe-ingredients">
+      ${recipeIngredients(allRecipes[i].ingredients)}
     </ul>
     <ol class="recipe-directions">
     </ol>
   </div>
 </div>`
 )}
+// console.log('Obj Array', allRecipes[0].ingredients);
+// console.log('Function Ary', recipeIngredients(allRecipes[0].ingredients));
 recipeSelector = suggestedRecipes.querySelectorAll('.recipe-card');
 recipeSelector.forEach(recipe => recipe.addEventListener('click', recipeHandler));
 };
@@ -39,3 +42,9 @@ function instantiateRecipes() {
   }
   return recipes;
 };
+
+function recipeIngredients(ingredientList) {
+  const ingredientMap = ingredientList.map(x => `<li>${x.name}: ${(Math.floor(x.quanitity.amount * 100) / 100) + ' ' + x.quanitity.unit}</li>`);
+  // console.log(ingredientMap.join('\n'));
+  return ingredientMap.join('\n');
+}
