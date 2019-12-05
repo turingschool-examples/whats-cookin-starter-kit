228d73922ba6b8a95c4dfd43343be4065b00cb01
Kim McCaskill <kimmccaskill92@gmail.com>
2019-12-05 14:14:46 -0700

    Add functionality to populate card template

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+const recipeSection = document.querySelector('.recipes-section');
+
+populateCards();
+
+function populateCards() {
+  return recipeData.forEach(element => {
+    let recipe = new Recipe(element.name, element.id, element.image, element.ingredients, element.instructions, element.tags)
+    recipeSection.innerHTML += `
+      <div class="recipe-card">
+        <div class="recipe-img"></div>
+        <div class="recipe-card-bar">
+          <p class="recipe-card-name">${recipe.name}</p>
+          <div class='btns-container'>
+            <button class="heart-btn"></button>
+            <button class="cook-btn"></button>
+          </div>
+        </div>
+      </div>`
+  }
+  )
+}
