b9c893ff9b64ad9c2aaec70ffd0c233bcfb63bec
Noah Gambee <noah@gambee.dev>
2019-12-04 17:40:38 -0700

    Add default properties and methods for User

---
+++ src/users.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.namel
+    this.pantry = [];
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+  }
+  addTofavoriteRecipes() {
+
+  }
+  addRecipeToCook() {
+
+  }
+  
+}
