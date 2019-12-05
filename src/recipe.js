f024384c1277bea3cc5fb447bd054c89d3ba8597
Kim McCaskill <kimmccaskill92@gmail.com>
2019-12-05 08:39:40 -0700

    Add updated function

--- src/recipe.js
+++ src/recipe.js
@@ -12,14 +12,23 @@ class Recipe {
   }
 
   calculateTotalCost(name) {
-    let recipeName = recipeData.find(recipe => {
+    let ingredients = []
+    let findIngredients = recipeData.find(recipe => {
       if (recipe.name === name) {
         recipe.ingredients.map(ingredient => {
-          return ingredient.name;
+          ingredients.push(ingredient.id);
         })
       }
     })
-    return recipeName;
+    let findEachCost = ingredients.reduce((acc, ingredient) => {
+      ingredientData.forEach(dataIngredient => {
+        if(dataIngredient.id === ingredient) {
+          acc += (dataIngredient.estimatedCostInCents*
+        }
+      })
+      return acc;
+    }, 0)
+    return findEachCost;
     // let recipeCost = ingredientData.reduce((acc, ingredient) => {
     //   if (ingredient.name === recipeName) {
     //     acc += ingredient.estimatedCostInCents;
