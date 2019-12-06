0317f7f21c17df93af722aa2bc1e82a379608c29
Jesse Maxim <jessmaxim303@gmail.com>
2019-12-06 11:02:04 -0700

    Push to master

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
-// const User = require('../src/User');
-// const Cookbook = require('../src/Cookbook');
-// const Pantry = require('../src/Pantry');
 
-// const users = require('../data/users');
-// const ingredients = require('../data/ingredients');
-// const recipes = require('../data/recipes');
-
-// const generateRandomIndex = (source) => {
-// 	return Math.floor(source.length * Math.random())
-// }
-
-// console.log(generateRandomIndex(users))
-// window.onload()
-
+const generateRandomIndex = (source) => {
+	return Math.floor(source.length * Math.random())
+}
 
 let user = new User(users[generateRandomIndex(users)])
-// let cookbook = new Cookbook()
-// console.log(user)
-
-// let user = new User(users[generateRandomIndex(users)])
-
 
 let currentUser = users.filter(card => card.id === Math.floor(users.length * Math.random()))
 
-// let cookbook = new Cookbook()
-
-// const chai = require('chai');
-// const expect = chai.expect;
-
-// let user = new User(users)
 let cookbook = new Cookbook(recipeData)
 let pantry = new Pantry()
 
@@ -38,34 +16,44 @@ let mainRecipeArea = document.querySelector('.main_recipe-area');
 let addFavoriteButton;
 let favoriteButton;
 
+mainRecipeArea.addEventListener("click", addtoFavoriteRecipes);
+
+function addtoFavoriteRecipes(e) {
+  // console.log("add 2 favorites");
+  // console.log(e);
+  if (e.target.classList.contains("add_to-favorites")){
+    console.log(e.target.classList)
+  }
+}
 
 function kickOff() {
-	console.log(users.name);
+	// console.log(users.name);
 	cooksName.innerHTML = user.name;
 }
 
-// function displayName() {
-// 	recipeName.innerHTML = user.displayName(recipeData)
-// 	user.displayName(users);
-// }
+function displayName() {
+	recipeName.innerHTML = user.displayName(recipeData)
+	user.displayName(users);
+}
 
 function insertRecipeTitle(recipeData) {
 	// recipeName.innerHTML = user.displayRecipeName(recipeData)
 }
 
-function addFavoritesArray(e) {
-	console.log(e)
-}
+// function addFavoritesArray(e) {
+// 	console.log(e)
+// }
 
 function displayRecipes() {
 	cookbook.recipes.forEach(recipe => {
-    mainRecipeArea.insertAdjacentHTML('afterbegin',
+    mainRecipeArea.insertAdjacentHTML('beforeend',
       `<section class="recipe_info-box">
             <img src="${recipe.image}">
             <h1 class="recipe_title">${recipe.name}</h1>
-            <h4 id="ingredients_list">Ingredients:</h4>
-            <button class="add_to-favorites"></button>
+            <section id="ingredients_display-area"></section>
+            <button class="add_to-favorites">Favorites</button>
             <button class="add_to-cook">Save for later</button>
+            <button class="display_ingredients">Ingredients</button>
        </section>`);
   });
 }
