1c3ce84cd51f72c361c4798f936704759c3c82f0
Cody Smith <codysmith2814@gmail.com>
2019-12-05 17:33:51 -0700

    Adds functionality to run the user.addToFavorite() method

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,8 @@
+/* eslint-disable max-len */
+
 let allRecipes = [];
-
-
+let randomNum = genRanNum();
+let user;
 
 const instantiateRecipeCards = () => {
   recipeData.forEach(recipe => {
@@ -12,8 +14,10 @@ const instantiateRecipeCards = () => {
       recipeData.instructions,
       recipeData.tags)
     allRecipes.push(recipe)
-  })
-  allRecipes.forEach(recipe => {
+  })   
+
+   allRecipes.forEach(recipe => {
+    recipeCounter++;
     document.querySelector('.recipe-card-area').insertAdjacentHTML('afterbegin', `
     <div class="recipe-container">
       <div class="image-container">
@@ -25,7 +29,7 @@ const instantiateRecipeCards = () => {
        <div class="card-button-containter">
          <button type="button" class="card-buttons view-recipe">View Recipe</button>
          <button type="button" class="card-buttons add-to-menu">Add to Menu</button>
-         <button type="button" class="card-buttons add-to-favorites">Add to Favorites</button>
+         <button type="button" class="card-buttons add-to-favorites" id=${recipeCounter}>Add to Favorites</button>
        </div>
      </div>`);
   })
@@ -35,56 +39,57 @@ function genRanNum() {
   return Math.floor(Math.random() * 50)
 }
 
-let randomNum = genRanNum();
 
-const instantiateUsers = () => {
+
+const instantiateUser = () => {
   var selectedUser = users.find(person => {
     return person.id === randomNum;
   })
-  let user = new User (
+    user = new User (
     selectedUser.id,
     selectedUser.name,
-    selectedUser.pantry)  
+    selectedUser.pantry) 
+    console.log(user); 
 }
 
 
 
-const selectUser = () => {
-
-}
-
-// const selectUser = () => {
-//   allUsers.forEach(person => {
-//     if (allUsers[id].includes(genRanNum))
-//     console.log('hi')
-//   })
-// }
-
-// const instantiateNewPantry = () => {
-
-//   })
-// }
-
-const getEvent = (event) => {
-  if (event.target.classList.contains('add-to-menu')) {
-    console.log(event)
-    pantry.moveToSelectedRecipe();
+const addToFavorites = (event) => {
+  if (event.target.classList.contains('add-to-favorites')) {
+    var parsedId = parseInt(event.target.id);
+    var selectedFaveRecipe = allRecipes[parsedId];
+    user.addToFavorites(selectedFaveRecipe);
   }
 }
 
+const displayFavorites = (event) => {
+  if (event.target.classList.contains('display-fav-button')) {
+   document.querySelector('.recipe-card-area').innerText = '';
+   user.favoriteRecipes.forEach(recipe => {
+    recipeCounter++;
+    document.querySelector('.recipe-card-area').insertAdjacentHTML('afterbegin', `
+    <div class="recipe-container">
+      <div class="image-container">
+         <img src="https://spoonacular.com/recipeImages/${recipe.id}-556x370.jpg" class="image-container">
+       </div>
+       <div class="recipe-text">
+         <h2>${recipe.name}</h2>
+       </div>
+       <div class="card-button-containter">
+         <button type="button" class="card-buttons view-recipe">View Recipe</button>
+         <button type="button" class="card-buttons add-to-menu">Add to Menu</button>
+         <button type="button" class="card-buttons add-to-favorites" id=${recipeCounter}>Add to Favorites</button>
+       </div>
+     </div>`);
+    })
+  }
+}
+
+
+
 let addToMenuBtns = document.querySelectorAll('.add-to-menu');
-
-addToMenuBtns.forEach(menuBtn => {
-  addEventListener('click', getEvent)
-});
-
-instantiateRecipeCards();
-instantiateUsers();
-selectUser();
-
-document.querySelector('.enter-button').addEventListener('click', loadDashboard);
-
-
+let addToFavoriteBtns = document.querySelectorAll('.add-to-favorite');
+ 
 
 function loadDashboard() {
   document.querySelector('.splash-container').classList.add('hidden');
@@ -92,3 +97,18 @@ function loadDashboard() {
   document.querySelector('.recipe-card-area').classList.remove('hidden');
   instantiateRecipeCards();
 }
+
+instantiateRecipeCards();
+instantiateUser();
+
+document.querySelector('.enter-button').addEventListener('click', loadDashboard)
+document.querySelector('.recipe-card-area').addEventListener('click', addToFavorites)
+document.querySelector('nav').addEventListener('click', displayFavorites)
+
+// addToFavoriteBtns.forEach(faveBtn => {
+//   addEventListener('click', console.log('turing'))
+// });
+
+
+
+var recipeCounter = -1;
