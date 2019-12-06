62d623947ca41b0ff7af779b33bd0f702c0c6eb4
Melissa Steele <melissa.steele09@gmail.com>
2019-12-06 13:55:55 -0700

    Implement function to display favorites

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+var favorites = document.querySelector("#favorites");
+var homeRecipes = document.querySelector(".home-recipes");
+
+favorites.addEventListener("click", displayFavorites);
+
+function displayFavorites() {
+  console.log("Hello");
+  event.preventDefault();
+  homeRecipes.classList.add("hidden");
+}
