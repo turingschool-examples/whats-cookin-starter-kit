d99da5e21ad8526336e1cee7f14f538b14fc5f1d
Jim Weist <jp_weist@yahoo.com>
2019-12-07 12:17:58 -0700

    added jquery to the buttons on page and for user

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,23 @@
 $( document ).ready(function() {
-  console.log("yea")
-});
+
+  $('.login').on( "click", function() {
+    let userName = $('#user-login').val() || users[0].name;
+    window.location = 'index.html';
+  });
+
+
+  $('#user-name').html(users[0].name);
+
+  $('.heart').on( "click", function() {
+    console.log("heart")
+  });
+
+  $('.chef').on( "click", function() {
+    console.log('chef')
+  });
+
+  $('.recipe-book').on( "click", function() {
+    console.log("recipe book");
+  });
+
+}); // end jQuery
