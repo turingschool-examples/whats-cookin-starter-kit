27d88a53c59b605df1cff7a2b8380328206c3188
Jesse Maxim <jessmaxim303@gmail.com>
2019-12-07 12:20:01 -0700

    Added new function to save favorite recipe

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-
-const generateRandomIndex = (source) => {
-	return Math.floor(source.length * Math.random())
-}
-
-let user = new User(users[generateRandomIndex(users)])
-
-let currentUser = users.filter(card => card.id === Math.floor(users.length * Math.random()))
+let user = new User(users)
+let currentUser = [];
+currentUser = users.filter(card => card.id === Math.floor(users.length * Math.random()))
 
 let cookbook = new Cookbook(recipeData)
 let pantry = new Pantry()
@@ -13,36 +8,18 @@ let pantry = new Pantry()
 let recipeName = document.querySelector('.recipe_title');
 let cooksName = document.querySelector('.user_title');
 let mainRecipeArea = document.querySelector('.main_recipe-area');
+let jsFavoriteArea = document.querySelector('.js_favorite-area');
 let addFavoriteButton;
 let favoriteButton;
 
-mainRecipeArea.addEventListener("click", addtoFavoriteRecipes);
+mainRecipeArea.addEventListener("click", recipeCardEvent);
 
-function addtoFavoriteRecipes(e) {
-  // console.log("add 2 favorites");
-  // console.log(e);
+function recipeCardEvent(e) {
   if (e.target.classList.contains("add_to-favorites")){
-    console.log(e.target.classList)
+    user.addToFavorites(e.target.parentElement.children[1].innerHTML)
+    displayFavoriteRecipes()
   }
 }
-
-function kickOff() {
-	// console.log(users.name);
-	cooksName.innerHTML = user.name;
-}
-
-function displayName() {
-	recipeName.innerHTML = user.displayName(recipeData)
-	user.displayName(users);
-}
-
-function insertRecipeTitle(recipeData) {
-	// recipeName.innerHTML = user.displayRecipeName(recipeData)
-}
-
-// function addFavoritesArray(e) {
-// 	console.log(e)
-// }
 
 function displayRecipes() {
 	cookbook.recipes.forEach(recipe => {
@@ -58,7 +35,17 @@ function displayRecipes() {
   });
 }
 
+function displayFavoriteRecipes() {
+  jsFavoriteArea.innerHTML = ''
+  let sum = user.displayFavorites()
+  sum.forEach(recipe => {
+    jsFavoriteArea.insertAdjacentHTML('beforeend',
+      `<section class="user_favorite-container">
+            <h1 class="user_recipe-title">${recipe}</h1>
+       </section>`);
+  });
+}
+
 cooksName.innerHTML = currentUser[0].name
 
 displayRecipes()
-kickOff()
