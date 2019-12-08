a7b2a07533938ddbe7b0fcf91dc1abd38cc0a671
Melissa Steele <melissa.steele09@gmail.com>
2019-12-08 09:50:14 -0700

    Add func to add recipe button

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,18 @@
 var favorites = document.querySelector("#favorites");
 var homeRecipes = document.querySelector(".home-recipes");
 var favoriteRecipes = document.querySelector("#favorite-recipes-main");
+var addButton = document.querySelector("#add-button");
 
 favorites.addEventListener("click", displayFavorites);
+addButton.addEventListener("click", displayRecipeForm);
 
 function displayFavorites() {
   event.preventDefault();
   homeRecipes.classList.add("hidden");
   favoriteRecipes.classList.remove("hidden");
+}
+
+function displayRecipeForm() {
+  favoriteRecipes.classList.add("hidden");
+
 }
