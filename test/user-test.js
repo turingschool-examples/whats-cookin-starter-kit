b2887ad0644d39a7c06d42260187328eae9c1095
Virginia Ladd <virginialadd2@gmail.com>
2019-12-09 13:41:36 -0700

    Allow users to search by tags

--- test/user-test.js
+++ test/user-test.js
@@ -48,17 +48,32 @@ describe('Users', function() {
       it('should know the correct name', function() {
         expect(user.name).to.equal('Saige O\'Kon');
       });
-
-      it.skip('should be able to sort by recipes can cook', function() {
-        expect(user.recipesToCook()).to.deep.equal();
-      });
     });
 
-    it.skip('should be able to search by tags', function() {
-      expect(user.searchByTags()).to.deep.equal();
+    it.only('should be able to search by tags', function() {
+      let ing = [{
+        "name": "all purpose flour",
+        "id": 20081,
+        "quanitity": {
+          "amount": 1,
+          "unit": "cup"
+        }
+      }, {
+        "name": "baking soda",
+        "id": 18372,
+        "quanitity": {
+          "amount": 0.5,
+          "unit": "tsp"
+        }
+      }];
+      let recipe1 = new Recipe(541288, 'Sesame Cookies', 'https://spoonacular.com/recipeImages/541288-556x370.jpg', ['antipasti', 'starter', 'snack',], ['Place them on two baking sheets lined with parchment paper.'], ing);
+      let recipe2 = new Recipe(595736, 'Loaded Chocolate Chip Pudding Cookie Cups', 'https://spoonacular.com/recipeImages/595736-556x370.jpg', ['antipasti', 'starter', 'snack',], ['Add egg and vanilla and mix until combined.'], ing);
+      let recipe3 = new Recipe(741603, 'Elvis Pancakes', 'https://spoonacular.com/recipeImages/741603-556x370.jpeg', ['side dish',], ['Watch how to make this recipe.'], ing);
+      let cookBook = [recipe1, recipe2, recipe3];
+      expect(user.searchByTags('snack', cookBook)).to.deep.equal([recipe1, recipe2]);
     });
 
-    it.skip('should know what it can cook', function() {
+    it('should save favorite recipes', function() {
       expect(user.saveToFavorites()).to.equal();
     });
 
