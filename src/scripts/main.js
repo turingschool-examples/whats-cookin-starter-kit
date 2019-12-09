8f21680046967a734f06211542963bfe93876df4
Grayson Palmer <gpalmer1385@gmail.com>
2019-12-09 15:15:37 -0700

    Add function to reveal/collapse recipe

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,39 @@
-// console.log(recipeData);
-// const recipeData = require('../data/recipes');
-
-
 let navBarToggle = document.querySelector('.navbar-toggle');
 let mainNav = document.querySelector('.main-nav');
 let suggestedRecipes = document.querySelector('.injected-suggested-recipes');
 let allRecipes = [];
 
-window.onload = loadSuggestedRecipesFunction();
+window.onload = loadSuggestedRecipesFunction;
 navBarToggle.addEventListener('click', function () {
+  console.log('blah');
   mainNav.classList.toggle('active');
 });
 
 function loadSuggestedRecipesFunction() {
-allRecipes = instantiateRecipes();
-for(let i = 0; i <  allRecipes.length; i++) {
-suggestedRecipes.insertAdjacentHTML('beforeend',
-`<div class="recipe-card">
-  <img src="${allRecipes[i].image}" alt="fork and knife logo">
-  <p>${allRecipes[i].name}</p>
-  <div class="recipe-container">
-    <ul class="recipe-ingredients">
-      ${recipeIngredients(allRecipes[i].ingredients)}
-    </ul>
-    <ol class="recipe-directions">
-      ${recipeDirections(allRecipes[i].instructions)}
-    </ol>
-  </div>
-</div>`
-)}
-// console.log('Obj Array', allRecipes[0].ingredients);
-// console.log('Function Ary', recipeIngredients(allRecipes[0].ingredients));
-recipeSelector = suggestedRecipes.querySelectorAll('.recipe-card');
-recipeSelector.forEach(recipe => recipe.addEventListener('click', recipeHandler));
+  allRecipes = instantiateRecipes();
+  for(let i = 0; i <  allRecipes.length; i++) {
+    suggestedRecipes.insertAdjacentHTML('beforeend',
+    `<div class="recipe-card">
+      <div class="recipe-header">
+        <img src="${allRecipes[i].image}" alt="Picture of ${allRecipes[i].name}">
+        <h3>${allRecipes[i].name}</h3>
+      </div>
+      <div class="recipe-content hidden">
+        <ul class="recipe-ingredients">
+          ${recipeIngredients(allRecipes[i].ingredients)}
+        </ul>
+        <ol class="recipe-directions">
+          ${recipeDirections(allRecipes[i].instructions)}
+        </ol>
+      </div>
+    </div>`
+    )}
+  recipeSelector = suggestedRecipes.querySelectorAll('.recipe-card');
+  recipeSelector.forEach(recipe => recipe.addEventListener('click', recipeHandler));
 };
 
 function instantiateRecipes() {
   let recipes = [];
-
   for (let i = 0; i < recipeData.length; i++) {
     recipes.push(new Recipes(recipeData[i]))
   }
@@ -45,13 +41,16 @@ function instantiateRecipes() {
 };
 
 function recipeIngredients(ingredientList) {
-  const ingredientMap = ingredientList.map(x => `<li>${x.name}: ${(Math.floor(x.quanitity.amount * 100) / 100) + ' ' + x.quanitity.unit}</li>`);
-  // console.log(ingredientMap.join('\n'));
-  return ingredientMap.join('\n');
-}
+  return ingredientList.map(x => `<li data-id='${x.id}'><i>${x.name}</i>: ${(Math.floor(x.quanitity.amount * 100) / 100) + ' ' + x.quanitity.unit}</li>`).join('\n');
+};
 
 function recipeDirections(directionsList) {
-  const directionsMap = directionsList.map(x => `<li>${x.instruction}</li>`);
-  // console.log(ingredientMap.join('\n'));
-  return directionsMap.join('\n');
+  return directionsList.map(x => `<li>${x.instruction}</li>`).join('\n');
+};
+
+function recipeHandler(event) {
+  if (event.target.parentNode.classList.contains('recipe-header')) {
+    event.target.parentNode.parentNode.classList.toggle('recipe-card-active');
+    event.target.parentNode.parentNode.children[1].classList.toggle('hidden');
+  }
 }
