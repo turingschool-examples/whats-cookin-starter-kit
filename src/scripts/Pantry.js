be30046482c2b4ed3d45dbcd014f2cdf8f1dbaa2
Grayson Palmer <gpalmer1385@gmail.com>
2019-12-10 15:57:37 -0700

    Constructor and missing ingredient function

--- src/scripts/Pantry.js
+++ src/scripts/Pantry.js
@@ -1,9 +1,15 @@
-class UserPantry {
-  constructor() {
+class Pantry {
+  constructor(ary) {
+    this.pantryList = ary;
+  }
 
+  missingIngredients(recipeIngredients) {
+    let missingIngredients = [];
+    let query = this.pantryList.filter(pantryItem => recipeIngredients.map(x => x.id).includes(pantryItem.ingredient))
+    console.log(query);
   }
 }
 
 if (typeof module !== 'undefined') {
-  module.exports = UserPantry;
+  module.exports = Pantry;
 }
