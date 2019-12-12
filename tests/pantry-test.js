69b8820cc0812bbb302fba9e1b2c04878841acae
kevin johnson <kevinj902302@gmail.com>
2019-12-12 01:52:53 -0700

    updated tests

---
+++ tests/pantry-test.js
@@ -0,0 +1,21 @@
+// 
+// const chai = require('chai');
+// const expect = chai.expect;
+// const Pantry = require('../src/pantry');
+// let cookbook;
+//
+// beforeEach(() => {
+//   cookbook = new Pantry({ ingredients: [], ingredientsNeeded: []});
+// });
+//
+// describe('Pantry', function() {
+//
+//   it('should be a function', function() {
+//     expect(Pantry).to.be.a('function');
+//   });
+//
+//   it('should be an instance of Pantry', function() {
+//     expect(cookbook.ingredients).to.equal([]);
+//     expect(cookbook).to.be.an.instanceof(Pantry);
+//   });
+// })
