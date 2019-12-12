639ddaaa9cd0e97770916abd154718a6af6a07d7
kevin johnson <kevinj902302@gmail.com>
2019-12-12 01:52:24 -0700

    updated tests

---
+++ tests/cookbook-test.js
@@ -0,0 +1,19 @@
+// const chai = require('chai');
+// const expect = chai.expect;
+// const Cookbook = require('../src/cookbook');
+// let cookbook;
+//
+// beforeEach(() => {
+//   cookbook = new Cookbook({ recipes: recipeData, costOfRecipe: 0});
+// });
+//
+// describe('Cookbook', function() {
+//
+//   it('should be a function', function() {
+//     expect(Cookbook).to.be.a('function');
+//   });
+//
+//   it('should be an instance of Cookbook', function() {
+//     expect(cookbook.recipes).to.equal('');
+//     expect(cookbook).to.be.an.instanceof(Cookbook);
+//   });
