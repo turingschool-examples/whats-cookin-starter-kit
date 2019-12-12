64d15d4e89a8b7b831cb913b6199e9c4ebde124f
kevin johnson <kevinj902302@gmail.com>
2019-12-12 01:53:12 -0700

    updated tests

--- tests/user-test.js
+++ tests/user-test.js
@@ -1,22 +1,18 @@
 // const chai = require('chai');
 // const expect = chai.expect;
-
-// // const User = require('../src/User.js');
-
+//
+// const User = require('../src/User.js');
+//
 // describe('User', function() {
-// //   it('should be a function', function() {
-// //   	const user = new User();
-// //     expect(user).to.be.a('function');
-// //      // expect(turn.giveFeedback()).to.equal('correct!');
-// //   });
-
-// //   it('should be a function', function() {
-// //   	const user = new User();
-    
-// //     expect(user.stock).to.equal(0);
-// //   });
 //   it('should be a function', function() {
+//   	const user = new User();
 //     expect(user).to.be.a('function');
 //   });
-
+//
+//   it('should be a function', function() {
+//   	const user = new User();
+//
+//     expect(user.stock).to.equal(0);
+//   });
+//
 // })
