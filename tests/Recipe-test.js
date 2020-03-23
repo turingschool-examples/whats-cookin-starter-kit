b16fc0bbafb53afb874e2e33c1ad9abf6b8ac28e
carflor <cfdeclet@gmail.com>
2020-03-23 16:59:59 -0600

    Add test data variables

--- tests/Recipe-test.js
+++ tests/Recipe-test.js
@@ -1,2 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Recipe = require('../src/Recipe');
+const Pantry = require('../src/Pantry');
+const ingredientsTestData = require('./test/Ingredients-test-data');
+const recipeTestData = require('./test/Recipe-test-data');
+
 const User = require('../src/User');
-const 
+
+describe('Recipe', function() {
+  
+})
