80c57e799e70f7a2fedfa9e18449b8c018a4767b
carflor <cfdeclet@gmail.com>
2020-03-23 17:00:09 -0600

    Initial Commit

---
+++ tests/Recipe-test-data.js
@@ -0,0 +1,176 @@
+let recipeDataTest = [
+  {
+    "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+    "id": 595736,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [
+      {
+        "id": 20081,
+        "quantity": {
+          "amount": 1.5,
+          "unit": "c"
+        }
+      },
+      {
+        "id": 18372,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "tsp"
+        }
+      },
+      {
+        "id": 1123,
+        "quantity": {
+          "amount": 1,
+          "unit": "large"
+        }
+      },
+      {
+        "id": 19335,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "c"
+        }
+      },
+      {
+        "id": 19206,
+        "quantity": {
+          "amount": 3,
+          "unit": "Tbsp"
+        }
+      },
+      {
+        "id": 19334,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "c"
+        }
+      },
+      {
+        "id": 2047,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "tsp"
+        }
+      },
+      {
+        "id": 1012047,
+        "quantity": {
+          "amount": 24,
+          "unit": "servings"
+        }
+      },
+      {
+        "id": 10019903,
+        "quantity": {
+          "amount": 2,
+          "unit": "c"
+        }
+      },
+      {
+        "id": 1145,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "c"
+        }
+      },
+      {
+        "id": 2050,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "tsp"
+        }
+      }
+    ]
+  },
+  {
+    "name": "Maple Dijon Apple Cider Grilled Pork Chops",
+    "id": 678353,
+    "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
+    "ingredients": [
+      {
+        "id": 1009016,
+        "quantity": {
+          "amount": 1.5,
+          "unit": "cups"
+        }
+      },
+      {
+        "id": 9003,
+        "quantity": {
+          "amount": 2,
+          "unit": ""
+        }
+      },
+      {
+        "id": 20027,
+        "quantity": {
+          "amount": 1,
+          "unit": "tablespoon"
+        }
+      },
+      {
+        "id": 1002046,
+        "quantity": {
+          "amount": 1,
+          "unit": "tablespoon"
+        }
+      },
+      {
+        "id": 11215,
+        "quantity": {
+          "amount": 1,
+          "unit": "clove"
+        }
+      },
+      {
+        "id": 1012046,
+        "quantity": {
+          "amount": 1,
+          "unit": "tablespoon"
+        }
+      },
+      {
+        "id": 19911,
+        "quantity": {
+          "amount": 0.25,
+          "unit": "cup"
+        }
+      },
+      {
+        "id": 16112,
+        "quantity": {
+          "amount": 1,
+          "unit": "tablespoon"
+        }
+      },
+      {
+        "id": 10010062,
+        "quantity": {
+          "amount": 24,
+          "unit": "ounce"
+        }
+      },
+      {
+        "id": 1102047,
+        "quantity": {
+          "amount": 4,
+          "unit": "servings"
+        }
+      },
+      {
+        "id": 16124,
+        "quantity": {
+          "amount": 1,
+          "unit": "tablespoon"
+        }
+      },
+      {
+        "id": 1016168,
+        "quantity": {
+          "amount": 1,
+          "unit": "tablespoon"
+        }
+      }
+    ]
+  }];
