ded101f8546fabd4f9fe2ee686c1c40ec20f5c90
Charlie Bandstra <bandstracharlie@gmail.com>
2020-03-23 16:23:50 -0600

    Add constructor to User class

---
+++ src/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(id, name, pantry) {
+    this.id = id;
+    this.name = name;
+    this.pantry = pantry;
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+  }
+}
+
+module.exports = User;
