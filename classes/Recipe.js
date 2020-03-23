2374b906914a2a592db6eb63a5537f20b078d8d2
Quinn Elder <qre.turing@gmail.com>
2020-03-23 17:35:12 -0600

    Add new JS files for testing and code base.

---
+++ classes/Recipe.js
@@ -0,0 +1,8 @@
+class Recipe {
+  constructor(dish) {
+    this.name = dish.name; 
+    this.id = dish.id;
+    this.image = dish.image;
+    this.ingredients = dish.ingredients;
+  }
+}
---
+++ classes/User.js
@@ -0,0 +1,29 @@
+class User {
+  constructor(person) {
+    this.id = person.id;
+    this.name = person.name;
+    this.pantry = person.pantry;
+    this.favRecipes = [];
+    this.cookBook = [];
+  }
+
+  createPantry() {
+    // instantiates a pantry class and the this.pantry will be the argument of the new pantry object instance.
+  }
+
+  addFavRecipe() {
+    // add the newly clicked on favRecipe to this.favRecipe array
+  }
+
+  removeFavRecipe() {
+    // remove the clicked on favRecipe from this.favRecipe array
+  }
+
+  addToCookBook() {
+    // add the newly clicked on recipe to this.cookBook array
+  }
+
+  removeFromCookBook() {
+    // remove the clicked on recipe from this.cookBook array
+  }
+}
