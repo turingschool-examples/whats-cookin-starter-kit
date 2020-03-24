dc08a0ca4ec5b976e008ba77f97fbe4a1fc9894a
carflor <cfdeclet@gmail.com>
2020-03-23 19:16:00 -0600

    Got new data from turing repo to fit spec

--- tests/Recipe-test-data.js
+++ tests/Recipe-test-data.js
@@ -1,6 +1,5 @@
-let recipeDataTest = [
+const recipeDataTest = [
   {
-    "name": "Loaded Chocolate Chip Pudding Cookie Cups",
     "id": 595736,
     "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
     "ingredients": [
@@ -81,10 +80,44 @@ let recipeDataTest = [
           "unit": "tsp"
         }
       }
+    ],
+    "instructions": [
+      {
+        "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+        "number": 1
+      },
+      {
+        "instruction": "Add egg and vanilla and mix until combined.",
+        "number": 2
+      },
+      {
+        "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+        "number": 3
+      },
+      {
+        "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
+        "number": 4
+      },
+      {
+        "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
+        "number": 5
+      },
+      {
+        "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
+        "number": 6
+      }
+    ],
+    "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+    "tags": [
+      "antipasti",
+      "starter",
+      "snack",
+      "appetizer",
+      "antipasto",
+      "hor d'oeuvre"
     ]
   },
   {
-    "name": "Maple Dijon Apple Cider Grilled Pork Chops",
     "id": 678353,
     "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
     "ingredients": [
@@ -172,6 +205,19 @@ let recipeDataTest = [
           "unit": "tablespoon"
         }
       }
+    ],
+    "instructions": [
+      {
+        "instruction": "Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!",
+        "number": 1
+      }
+    ],
+    "name": "Maple Dijon Apple Cider Grilled Pork Chops",
+    "tags": [
+      "lunch",
+      "main course",
+      "main dish",
+      "dinner"
     ]
   }];
 
