14d3b436c6563199d04c3b0d93ee3db60ee2c05e
carflor <cfdeclet@gmail.com>
2020-03-23 19:16:49 -0600

    Further develope recipe class test

--- tests/Recipe-test.js
+++ tests/Recipe-test.js
@@ -4,16 +4,16 @@ const expect = chai.expect;
 const Recipe = require('../src/Recipe');
 const Pantry = require('../src/Pantry');
 
+let recipeDataTest = require('../tests/Recipe-test-data');
 let ingredientsData = require('../tests/Ingredients-test-data');
-let recipeData = require('../tests/Recipe-test-data');
 
 // const User = require('../src/User');
 
 describe('Recipe', function() {
-  let recipe, pantry;
+  let recipe1, pantry;
   beforeEach(function() {
-    recipe = new Recipe();
-    pantry = new Pantry();
+    recipe1 = new Recipe(recipeDataTest[0]);
+    pantry = new Pantry(ingredientsData);
   })
 
   it('should be a function', function () {
@@ -21,21 +21,26 @@ describe('Recipe', function() {
   })
 
   it('should instantiate recipe', function() {
-    expect(recipe).to.be.an.instanceOf(Recipe);
+    expect(recipe1).to.be.an.instanceOf(Recipe);
   })
 
-  it('should have a name', function () {
-
-    expect(recipeData[0].name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
+  it('should have a name', function() {
+    expect(recipe1.name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups')
   })
 
-  // it.skip('should have an id', function () {
+  it('should have an id', function() {
+    expect(recipe1.id).to.equal(595736);
+  })
 
-  //   expect(recipe.id).to.be.a('function');
-  // })
+  it('should have an image', function() {
+    expect(recipe1.image).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg');
+  })
 
-  // it.skip('should be a function', function () {
+  it('should have an array of ingredients', function() {
+    console.log(pantry);
+    
+    expect(recipe1.ingredients).to.deep.equal(pantry[0].ingredients)
+  })
 
-  //   expect(Recipe).to.be.a('function');
-  // })
+  
 })
