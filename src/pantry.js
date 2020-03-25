dc64203f27c361b680c497ef8471a8840589e508
Jeremiah Black <jeremiahblackol@gmail.com>
2020-03-24 21:17:56 -0600

    get quantity function works

--- src/pantry.js
+++ src/pantry.js
@@ -1,5 +1,58 @@
+const pantryIngredients = [
+  {
+    "ingredient": 1017,
+    "amount": 2
+  },
+  {
+    "ingredient": 18371,
+    "amount": 7
+  },
+  {
+    "ingredient": 1001,
+    "amount": 6
+  },
+  {
+    "ingredient": 99223,
+    "amount": 2
+  },
+  {
+    "ingredient": 1230,
+    "amount": 2
+  },
+  {
+    "ingredient": 9152,
+    "amount": 4
+  },
+  {
+    "ingredient": 20081,
+    "amount": 10
+  },
+  {
+    "ingredient": 18372,
+    "amount": 3
+  },
+];
+
 class Pantry {
-  constructor() {
+  constructor(ingredients) {
+    this.ingredients = ingredients || [];
+  }
+
+  getQuantity(ingredient) {
+    let amount;
+    pantryIngredients.map((ingred) => {
+      if (ingred.ingredient === ingredient.id) {
+      amount = ingred.amount;
+      }
+    });
+    return amount
+  }
+
+  measureIngredients(recipe) {
+    
+  }
+
+  removeIngredients() {
 
   }
 }
@@ -7,3 +60,22 @@ class Pantry {
 if (typeof module !== 'undefined') {
   module.exports = Pantry;
 }
+
+/*Pantries
+Every User should have a pantry.
+A Pantry holds on to all the ingredients
+its owner has stocked, and the amount
+of each ingredient they have. As a user,
+I should be able to:
+
+Determine whether my pantry has enough
+ingredients to cook a given meal
+Determine the amount of ingredients still
+needed to cook a given meal, based on
+what’s in my pantry
+Remove the ingredients used for a given meal
+from my pantry, once that meal has been cooked
+(only applicable if users have a list of mealsToCook;
+can be considered a stretch goal)
+A Note:
+When you test a JavaScript class file, you need to use module.exports in your class file so the test can use the code - just like Mythical Creatures. However, in this project when you call that same class in the browser, the browser has no idea what module is. So your code crashes. The module object is something your tests know how to use, but the browser has no idea what it is. To fix this, you’ll want to include something like this in your class file (for each class file that you create):*/
--- tests/pantry-test.js
+++ tests/pantry-test.js
@@ -7,5 +7,85 @@ const Recipe = require('../src/Recipe');
 const User = require('../src/User');
 
 describe ('Pantry', function() {
+  let recipe;
+  let ingredient1;
+  let ingredient2;
+
+  beforeEach(function() {
+    ingredient1 = new Ingredient(20081, 'wheat flour', 142);
+    ingredient2 = new Ingredient(18372, "bicarbonate of soda", 582);
+    pantry = new Pantry([ingredient1, ingredient2])
+
+  })
+
+  it('should be an instance of pantry', function() {
+    expect(pantry).to.be.an.instanceOf(Pantry);
+  });
+
+  it('should be able to have multiple ingredients', function() {
+    expect(pantry.ingredients).to.be.an('array');
+  });
+
+  it('should have ingredients', function() {
+    expect(pantry.ingredients).to.deep.equal([ingredient1, ingredient2]);
+  });
+
+  it('should return an ingredient\'s quantity', function() {
+    expect(pantry.getQuantity).to.be.a('function');
+    expect(pantry.getQuantity(ingredient1)).to.equal(10);
+  });
+
+  it('should be able to check pantry for ingredients', function() {
+    expect(pantry.measureIngredients).to.be.a('function');
+  });
+
+  it('should return whether there are enough ingredients in pantry', function() {
+    let recipe = new Recipe(
+      1001,
+      'https://images.com',
+      [{ingredient1}, {ingredient2}],
+      [{'instruction': 'open', 'number': 1}, {'instruction': 'heat'}, {'number': 2}],
+      'Basic Food',
+      ['dinner', 'lunch']
+    );
+
+    expect(pantry.measureIngredients(recipe)).to.equal('Nope, need to shop!');
+  })
 
 })
+
+/*{
+  "id": 1017,
+  "name": "cream cheese",
+  "estimatedCostInCents": 955
+},
+
+{
+  "id": 18371,
+  "name": "baking powder",
+  "estimatedCostInCents": 346
+},
+
+{
+  "id": 1001,
+  "name": "butter",
+  "estimatedCostInCents": 618
+},
+
+{
+  "id": 99223,
+  "name": "canned chipotle chilies in adobo",
+  "estimatedCostInCents": 299
+},
+
+{
+  "id": 1230,
+  "name": "buttermilk",
+  "estimatedCostInCents": 773
+},
+
+{
+  "id": 9152,
+  "name": "lemon juice",
+  "estimatedCostInCents": 274
+}, */
