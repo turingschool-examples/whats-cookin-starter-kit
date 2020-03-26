8bbe3168a068d8546d7a2a6cd89911be6ab33d16
Kyle Wong <kylewong2510@gmail.com>
2020-03-26 14:56:25 -0600

    Add methods to recipe class

--- src/recipe.js
+++ src/recipe.js
@@ -6,6 +6,26 @@ class Recipe {
     this.instructions = instructions;
     this.name = name;
     this.tags = tags;
+    this.isFavorite = false;
+    this.cookNext = false;
+  }
+
+  getIngredientsCost() {
+    let ingredientsCost = this.ingredients
+      .map(ingredient => ingredient.estimatedCostInCents)
+      .reduce((sum, cost) => {
+        sum += cost;
+        return sum
+      }, 0);
+    return `$${(ingredientsCost * .01).toFixed(2)}`
+  }
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite
+  }
+
+  toggleCookNext() {
+    this.cookNext = !this.cookNext
   }
 }
 
