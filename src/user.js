72fccbd1eb10fffa934abb006f4ee94704f23c93
Justin Corbin <jcorbin2290@gmail.com>
2020-03-26 16:28:34 -0600

    shuffleled around methods on USER and Pantry classes

--- src/user.js
+++ src/user.js
@@ -1,5 +1,5 @@
-const Pantry = require('../src/Pantry');
-const Recipe = require('../src/Recipe');
+const Pantry = require('../src/pantry');
+const Recipe = require('../src/recipe');
 
 class User {
   constructor(obj) {
@@ -7,7 +7,6 @@ class User {
     this.id = obj.id;
     this.pantry = new Pantry(obj.pantry)
     this.favorites = [];
-    this.mealToCook = []
   }
 
   addToFavorites() {
@@ -17,20 +16,6 @@ class User {
 
   removeFromFavorites() {
     // removes recipe from favorites array
-  }
-
-
-  stageMealToCook() {
-    //take current meal selection
-    //pass it into pantry.validateMeal
-    //^ if above equates to true, move meal to meal to cook on user
-    //^ if aboce equates to false. run pantry required for meal and return required items.
-    //add meal user wants to cook to meal que.
-  }
-
-  cookMeal() {
-    //run remove item(s) from pantry
-    //set meal to cook to an empty []
   }
 
 }
--- tests/Pantry-test.js
+++ tests/Pantry-test.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Recipe = require('../src/Recipe');
-const Pantry = require('../src/Pantry');
+const Recipe = require('../src/recipe');
+const Pantry = require('../src/pantry');
+const User = require('../src/user');
 
 let recipeDataTest = require('../tests/Recipe-test-data');
 let ingredientsTestData = require('../tests/Ingredients-test-data');
@@ -13,8 +14,8 @@ describe('Pantry', function() {
 
   beforeEach(function() {
     recipe1 = new Recipe(recipeDataTest[0]);
-    user = userTestData;
-    pantry = new Pantry(user.pantry);
+    user = new User(userTestData)
+    pantry = user.pantry
   })
 
   it('should be a function', function () {
@@ -22,16 +23,17 @@ describe('Pantry', function() {
   })
 
   it('should instantiate pantry', function() {
-    expect(pantry).to.be.an.instanceOf(Pantry);
+    expect(user.pantry).to.be.an.instanceOf(Pantry);
   })
 
   it('should have all users ingredients', function() {
-    expect(pantry.pantry).to.deep.equal(user.pantry)
+    expect(user.pantry).to.equal(pantry)
   })
 
-  it.only('should be able to check if it contains all ingridients needed for a meal', function() {
+  it('should be able to check if it contains all ingridients needed for a meal', function() {
     pantry.validateMealToCook();
-    // expect(pantry.pantry).to.include(recipe1.ingredients)
+
+
   })
 
 
