0c7f5ebbc277568069039286b1123dc8ec4fc63c
Justin Corbin <jcorbin2290@gmail.com>
2020-03-27 20:27:28 -0600

    created tests for getIngredientsDetails method on Pantry class

--- tests/Pantry-test.js
+++ tests/Pantry-test.js
@@ -6,8 +6,9 @@ const Pantry = require('../src/pantry');
 const User = require('../src/user');
 
 let recipeDataTest = require('../tests/Recipe-test-data');
-let ingredientsTestData = require('../tests/Ingredients-test-data');
 let userTestData = require('../tests/user-test-data');
+let ingredientsData = require('../data/ingredients.js')
+
 
 describe('Pantry', function() {
   let recipe1, pantry, user;
@@ -18,25 +19,21 @@ describe('Pantry', function() {
     pantry = user.pantry
   })
 
-  it('should be a function', function () {
+  it.skip('should be a function', function () {
     expect(Pantry).to.be.a('function');
   })
 
-  it('should instantiate pantry', function() {
+  it.skip('should instantiate pantry', function() {
     expect(user.pantry).to.be.an.instanceOf(Pantry);
   })
 
-  it('should have all users ingredients', function() {
+  it.skip('should have all users ingredients', function() {
     expect(user.pantry).to.equal(pantry)
   })
 
-  it.only('should be able to check if it contains all ingridients needed for a meal', function() {
-
-    // console.log('user test data',user.pantry.pantry);
-    // console.log('recipe test data', recipeDataTest[0]);
-    pantry.validateMeal(recipeDataTest[0]);
-
-
+  it.skip('it should be able to gain the properties of ingredients', function() {
+    pantry.getIngredientDetails(ingredientsData);
+    expect(pantry.pantry[0]).to.have.property('name')
   })
 
 
