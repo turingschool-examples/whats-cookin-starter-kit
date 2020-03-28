471f8e83bc557f0ddd59176a48742fcfcf947bfc
Justin Corbin <jcorbin2290@gmail.com>
2020-03-27 19:53:17 -0600

    Created a getIngredientDetails method

--- src/pantry.js
+++ src/pantry.js
@@ -1,27 +1,27 @@
 const Recipe = require('../src/recipe');
 const recipeData = require('../data/recipes');
 
+
 class Pantry {
   constructor(userPantry) {
     this.pantry = userPantry;
-    this.mealToCook = []
   };
 
-  validateMeal(currentRecipe) {
-    
-    //checkes ingridients and quantity required
-    //if above equals true, return true (enable cook button)
-    //if above equals false return false. (run requiredForMeal)
+  getIngredientDetails(ingredientsData) {
+    this.pantry = this.pantry.map(ingredient => {
+      let ingredientData = ingredientsData.find(data => data.id === ingredient.ingredient)
+      return {...ingredient, ...ingredientData}
+    })
   };
 
-  requiredForMeal() {
-    //return values and required amount
+  requiredForMeal(recipe) {
+
   };
 
   cookMeal() {
     /// set mealToCook to empty []
     //run remove item(s) from pantry
-  }
+  };
 
   removeUsedIngredient() {
     //after user clicks cook mean removes items and quantities from pantry.
