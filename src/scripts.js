9cb70c648f8e7bba8d8cc955ef157d0068bce1aa
Jeremiah Black <jeremiahblackol@gmail.com>
2020-03-29 11:23:47 -0600

    recipe names populate on load

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,12 @@
 console.log('Hello world');
 
+// const Recipe = require('./Recipe');
+
+
+let recipeSection = document.querySelector('.all-recipes');
+
+recipeSection.addEventListener('click', addRecipesToDOM);
+
 var expanded = false;
 
 function showCheckboxes() {
@@ -14,9 +21,31 @@ function showCheckboxes() {
 }
 
 function addRecipesToDOM() {
+  console.log(recipeData);
+  recipeData.forEach((recipe) => {
+    recipe = new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags);
+    console.log(recipe);
+    recipeSection.insertAdjacentHTML('afterbegin', `<div><h1>${recipe.name}</h1></div>`)
+  });
+  // recipeData.forEach(recipe => {
+  //   console.log(recipe);
+  // })
   //loop through recipes, instantiate each recipe; create a card
   //forEach recipe.
   //
+}
+
+function generateUser() {
+  //on Load
+  //from user usersData
+  //random index
+  //use let user hold the value
+}
+
+function greetUser() {
+  //takes current user
+  //uses current user name at top of page
+  //let user
 }
 
   //  DOM
