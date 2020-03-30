46b82ee400f75c5acc0429f6bf7ccd963ee61f4d
December G <dgarnetsmith@gmail.com>
2020-03-29 21:03:22 -0600

    Instantiate recipes, add recipes to search page, begin adding functionality to add to meals to cook and favorite recipes

--- src/scripts.js
+++ src/scripts.js
@@ -5,10 +5,12 @@ const favorites = document.querySelector('.favorites');
 const mealPlan = document.querySelector('.meal-plan');
 const searchResults = document.querySelector('.search-results');
 const pantry = document.querySelector('.pantry');
+const allRecipesSection = document.querySelector('.all-recipes-group');
+
 //want to create navbar functionality
 
 document.addEventListener('click', changePageView);
-
+allRecipesSection.addEventListener('click', selectCards);
 
 function changePageView(event) {
   const header = document.getElementsByTagName('header')[0]
@@ -51,10 +53,63 @@ function changePageView(event) {
   }
 }
 
-function addUserToPage() {
-  console.log(usersData);
-  const user0 = new User(usersData[0]);
-  console.log(user0);
+function selectCards(event) {
+  console.log(event.target)
+  if (event.target.classList.contains('unselected-heart')) {
+    favoriteRecipe(event.target.id);
+  } else if (event.target.classList.contains('unselected-chef-hat')) {
+    addRecipeToMealPlan();
+  }
 }
+
+function addUserToPage() {
+  const user0 = new User(usersData[0]);
+  instantiateAllRecipes();
+}
+
+//need to create function that instantiates all recipes
+//display all recipes in the search page in a function -- all should be unchecked for now
+//create function that is invoked when "favorite" icon is clicked
+// after favorite icon is clicked, should now become selected icons
+//the function should also make sure that the favorite meal function from the class is invoked with recipe as the argument
+//favorite meal should then be added to the DOM using interpolation
+//create click function that executes if you've clicked one of the unselected or selected buttons;
+
+function instantiateAllRecipes() {
+  let instantiatedRecipes = recipeData.map((recipe, index) => {
+  let eachRecipe = new Recipe(recipe);
+  return eachRecipe;
+});
+  addRecipesToSearchPage(instantiatedRecipes)
+}
+
+function addRecipesToSearchPage(instantiatedRecipes) {
+  //add functionality to add each recipe to the javascript
+  //create constant for the div that holds all of the searches in it
+  instantiatedRecipes.forEach(recipe => {
+  allRecipesSection.innerHTML += `<div class="recipe-card">
+    <img class="recipe-card-image" src=${recipe.image} alt=${recipe.name}>
+    <p class="recipe-card-title">${recipe.name}</p>
+    <img class="unselected-heart" src="../assets/heart-regular.svg" alt="unselected heart icon">
+    <img class="unselected-chef-hat" src="../assets/unselected-chef-hat.svg" alt="unselected recipe to cook">
+  </div>`
+});
+
+}
+
+function favoriteRecipe() {
+//should add recipe to favorited recipe array
+//should also display recipe in favorites section
+  // user.favoriteMeal()
+  console.log('in favorite recipe function!')
+}
+
+function addRecipeToMealPlan() {
+  console.log('in add recipe to meal plan  function!')
+
+}
+
+
+
 
 window.onload = addUserToPage();
