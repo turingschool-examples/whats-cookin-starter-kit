4a43de4426da9204572c8dbf84913ed40a2d8fa9
Kyle Wong <kylewong2510@gmail.com>
2020-03-30 16:44:45 -0600

    Card styling and addRemoveFavorite function

--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,7 @@ console.log('Hello world');
 
 
 let allRecipesDisplay = document.querySelector('.all-recipes-display');
-let recipeSection = document.querySelector('.all-recipes');
+let recipeSection = document.querySelector('.recipe-section');
 let welcomeUser = document.querySelector('.welcome');
 var recipeScreenCount = 0;
 var closeButton;
@@ -37,25 +37,26 @@ function showCheckboxes() {
 function clickHandler() {
   let retrievedRecipe;
   if (recipeScreenCount < 1) {
-  if (event.target.classList.contains('card-image')) {
+    if (event.target.classList.contains('card-image')) {
       recipeData.forEach((recipe) => {
         if (recipe.id == event.target.id) {
           retrievedRecipe = recipe;
           retrievedRecipe = new Recipe(retrievedRecipe.id, retrievedRecipe.image, retrievedRecipe.ingredients, retrievedRecipe.instructions, retrievedRecipe.name, retrievedRecipe.tags)
-      }
-    });
-    displayRecipe(retrievedRecipe);
-    recipeScreenCount++;
+        }
+      });
+      displayRecipe(retrievedRecipe);
+      recipeScreenCount++;
+    }
   }
-}
-
   if (event.target.classList.contains('close-button')) {
-    event.target.parentNode.remove()
+    event.target.parentNode.parentNode.remove()
     recipeScreenCount--
   }
+  if (event.target.classList.contains('favorite')) {
+    console.log(retrievedRecipe)
+    addRemoveFavorite(retrievedRecipe, user)
+  }
 }
-
-// }
 
 function addRecipesToDOM() {
   recipeData.forEach((recipe) => {
@@ -65,34 +66,52 @@ function addRecipesToDOM() {
       <div class='recipe-card-header'>
         <p>${recipe.name}</p>
         <div class="card-btns">
-          <button id='favorite'>
+          <button class="favorite">
+            F
           </button>
-          <button id='cook-next'>
+          <button class="cook-next">
+            C
           </button>
         </div>
       </div>
       <div class="recipe-img">
-        <img id=${recipe.id} class="card-image"src="${recipe.image}" alt="">
+        <img id=${recipe.id} class="card-image" src="${recipe.image}" alt="">
       </div>
       <footer></footer>
     </div>`
     })
   };
 
+  let user;
+
   function generateAndGreetRandomUser() {
     let randomIndex = Math.floor(Math.random() * 50)
     let newUser = usersData[randomIndex]
-    let user = new User(newUser.name, newUser.id, newUser.pantry);
+    user = new User(newUser.name, newUser.id, newUser.pantry);
+    console.log(user)
     welcomeUser.innerText = `Welcome ${newUser.name}`
   }
 
-
   function displayRecipe(recipe) {
-    recipeSection.insertAdjacentHTML('afterbegin',
-    `<div class="display-recipe"><button class="close-button" type="button" name="button">Close
-    </button><h2>${recipe.name}</h2><h1>COST:${recipe.getIngredientsCost(recipe)}</h1>
-    <h1>(${recipe.tags})</h1><img class="card-image"src="${recipe.image}" alt="">
-    <p>${getRecipeInstructions(recipe)}</p></div>`)
+    recipeSection.innerHTML=
+    `<div class="display-recipe">
+      <header id="displayed-recipe-header">
+        <div id="recipe-header-title-section">
+          <h1 id="recipe-title">${recipe.name}</h1>
+          <button class="close-button" type="button" name="button">
+            X
+          </button>
+        </div>
+        <div id="recipe-header-info-section">
+          <h2 id="recipe-cost">COST:${recipe.getIngredientsCost(recipe)}</h2>
+          <h3 id="recipe-tags">(${recipe.tags})</h3>
+        </div>
+      </header>
+      <div id="recipe-body">
+        <img class="card-image" src="${recipe.image}" alt="">
+        <p id="recipe-instructions">${getRecipeInstructions(recipe)}</p>
+      </div>
+    </div>`
   }
 
   function getRecipeInstructions(recipe) {
@@ -104,11 +123,49 @@ function addRecipesToDOM() {
     return instructions
   }
 
-  /*
+  let favIcon = document.querySelector('favorite');
+  let cookNextIcon = document.getElementsByClassName('.cook-next');
+  let myRecipesDisplay = document.getElementById('my-recipes-display');
+  console.log('fav', favIcon)
+  favIcon.addEventListener('click', addRemoveFavorite); 
 
+  function addRemoveFavorite(recipe, user) {
+    console.log(user)
+    if (recipe.isFavorite) {
+      user.removeFavoriteRecipe()
+    } else {
+      user.addFavoriteRecipe()
+    }
+    displayMyRecipes()
+  }
+
+  function displayMyRecipes() {
+    user.favoriteRecipes.forEach(recipe => {
+      recipe = new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags);
+      myRecipesDisplay.innerHTML += 
+      `<div class='recipe-card'>
+      <div class='recipe-card-header'>
+      <p>${recipe.name}</p>
+      <div class="card-btns">
+      <button class="favorite">
+      F
+      </button>
+      <button class="cook-next">
+      C
+      </button>
+      </div>
+      </div>
+      <div class="recipe-img">
+      <img id=${recipe.id} class="card-image" src="${recipe.image}" alt="">
+      </div>
+      <footer></footer>
+      </div>`
+    })
+  }
+  /*
   function addToFavorites() {
     on click
-    takes recipe adds it to user davorite recipes array.
+    takes recipe adds it to user Favorite recipes array.
     then displays the title in the favorite recipe section.
     the title is a link to the whole recipe.
     if it is already favorited; it will not show up again.
