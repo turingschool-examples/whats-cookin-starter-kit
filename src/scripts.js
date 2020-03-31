b8e403afeb1e609240ae907f116387698abf79e0
Jeremiah Black <jeremiahblackol@gmail.com>
2020-03-31 07:48:40 -0600

    fix merge conflicts

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,7 @@
 console.log('Hello world');
 
-// const Recipe = require('./Recipe');
-
-
 let allRecipesDisplay = document.querySelector('.all-recipes-display');
-let recipeSection = document.querySelector('.recipe-section');
+let recipeSection = document.querySelector('.all-recipes');
 let welcomeUser = document.querySelector('.welcome');
 let tagsMenu = document.querySelector('.tags-menu');
 // let pageTitleButton = document.getElementById('page-title');
@@ -23,9 +20,7 @@ window.onload = function() {
 recipeSection.addEventListener('click', clickHandler);
 tagsMenu.addEventListener('change', filterRecipesByTag);
 // pageTitleButton.addEventListener('click', addRecipesToDOM);
-// console.log(tagsMenu);
-//   removeRecipeDisplay();
-// );
+
 
 
 var expanded = false;
@@ -44,24 +39,22 @@ function showCheckboxes() {
 function clickHandler() {
   let retrievedRecipe;
   if (recipeScreenCount < 1) {
-    if (event.target.classList.contains('card-image')) {
+  if (event.target.classList.contains('card-image')) {
       recipeData.forEach((recipe) => {
         if (recipe.id == event.target.id) {
           retrievedRecipe = recipe;
           retrievedRecipe = new Recipe(retrievedRecipe.id, retrievedRecipe.image, retrievedRecipe.ingredients, retrievedRecipe.instructions, retrievedRecipe.name, retrievedRecipe.tags)
-        }
-      });
-      displayRecipe(retrievedRecipe);
-      recipeScreenCount++;
-    }
+      }
+    });
+    displayRecipe(retrievedRecipe);
+    recipeScreenCount++;
   }
+}
+
   if (event.target.classList.contains('close-button')) {
-    event.target.parentNode.parentNode.remove()
+    event.target.parentNode.remove()
     recipeScreenCount--
   }
-  // if (event.target.classList.contains('favorite')) {
-  //   console.log(retrievedRecipe)
-  //   addRemoveFavorite(retrievedRecipe, user)
 
   if (event.target.classList.contains('favorite')) {
     let favoriteRecipeID = event.target.closest('.recipe-card').id;
@@ -76,14 +69,8 @@ function clickHandler() {
     //stopping before i can toggle isFavorite to false; and remove from array
     recipeToAdd = new Recipe(recipeToAdd.id, recipeToAdd.image, recipeToAdd.ingredients, recipeToAdd.instructions, recipeToAdd.name, recipeToAdd.tags);
     addRemoveFavorite(recipeToAdd, user);
-    console.log(recipeToAdd);
-    console.log(user);
-    // console.log(favoriteRecipeID);
-    }
-    // console.log(recipeToAdd);
-    // addRemoveFavorite(recipeToAdd, user)
-    // console.log(user.favoriteRecipes);
-}
+  }
+ }
 
 
 function addRecipesToDOM() {
@@ -96,51 +83,34 @@ function addRecipesToDOM() {
       <div class='recipe-card-header'>
         <p>${recipe.name}</p>
         <div class="card-btns">
- feature/dom-manipulation
           <button class='favorite'>F
           </button>
           <button class='cook-next'>C
-
           </button>
         </div>
       </div>
       <div class="recipe-img">
-        <img id=${recipe.id} class="card-image" src="${recipe.image}" alt="">
+        <img id=${recipe.id} class="card-image"src="${recipe.image}" alt="">
       </div>
       <footer></footer>
     </div>`
     })
   };
 
-
   function generateAndGreetRandomUser() {
     let randomIndex = Math.floor(Math.random() * 50)
     let newUser = usersData[randomIndex]
     user = new User(newUser.name, newUser.id, newUser.pantry);
-
     welcomeUser.innerText = `Welcome ${newUser.name}`
   }
 
+
   function displayRecipe(recipe) {
-    recipeSection.innerHTML=
-    `<div class="display-recipe">
-      <header id="displayed-recipe-header">
-        <div id="recipe-header-title-section">
-          <h1 id="recipe-title">${recipe.name}</h1>
-          <button class="close-button" type="button" name="button">
-            X
-          </button>
-        </div>
-        <div id="recipe-header-info-section">
-          <h2 id="recipe-cost">COST:${recipe.getIngredientsCost(recipe)}</h2>
-          <h3 id="recipe-tags">(${recipe.tags})</h3>
-        </div>
-      </header>
-      <div id="recipe-body">
-        <img class="card-image" src="${recipe.image}" alt="">
-        <p id="recipe-instructions">${getRecipeInstructions(recipe)}</p>
-      </div>
-    </div>`
+    recipeSection.insertAdjacentHTML('afterbegin',
+    `<div class="display-recipe"><button class="close-button" type="button" name="button">Close
+    </button><h2>${recipe.name}</h2><h1>COST:${recipe.getIngredientsCost(recipe)}</h1>
+    <h1>(${recipe.tags})</h1><img class="card-image"src="${recipe.image}" alt="">
+    <p>${getRecipeInstructions(recipe)}</p></div>`)
   }
 
   function getRecipeInstructions(recipe) {
@@ -151,46 +121,6 @@ function addRecipesToDOM() {
     })
     return instructions
 }
-
-  // let favIcon = document.querySelector('favorite');
-  // let cookNextIcon = document.getElementsByClassName('.cook-next');
-  let myRecipesDisplay = document.getElementById('my-recipes-display');
-  // console.log('fav', favIcon)
-  // favIcon.addEventListener('click', addRemoveFavorite);
-
-  function addRemoveFavorite(recipe, user) {
-    console.log(user)
-    if (recipe.isFavorite) {
-      user.removeFavoriteRecipe()
-    } else {
-      user.addFavoriteRecipe()
-    }
-    displayMyRecipes()
-  }
-
-  function displayMyRecipes() {
-    user.favoriteRecipes.forEach(recipe => {
-      recipe = new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags);
-      myRecipesDisplay.innerHTML +=
-      `<div class='recipe-card'>
-      <div class='recipe-card-header'>
-      <p>${recipe.name}</p>
-      <div class="card-btns">
-      <button class="favorite">
-      F
-      </button>
-      <button class="cook-next">
-      C
-      </button>
-      </div>
-      </div>
-      <div class="recipe-img">
-      <img id=${recipe.id} class="card-image" src="${recipe.image}" alt="">
-      </div>
-      <footer></footer>
-      </div>`
-    })
-  }
 
   function filterRecipesByTag() {
     allRecipesDisplay.innerHTML = '';
@@ -222,11 +152,8 @@ function addRecipesToDOM() {
     });
   }
 
-  // let favIcon = document.querySelector('favorite');
-  // let cookNextIcon = document.getElementsByClassName('.cook-next');
-  // let myRecipesDisplay = document.getElementById('my-recipes-display');
-  // console.log('fav', favIcon)
-  // favIcon.addEventListener('click', addRemoveFavorite);
+
+  let myRecipesDisplay = document.getElementById('my-recipes-display');
 
   function addRemoveFavorite(recipe, user) {
     if (recipe.isFavorite) {
@@ -261,34 +188,7 @@ function addRecipesToDOM() {
     })
 }
 
-  /*
 
-  function filterRecipesByType() {
-    takes an argument of type;
-    loops through all recipes, returns an array of only the recipes
-    that match.
-    clears current recipe section;
-    pulls the recipe info out.
-    recreates the cards.
-}
-
-  function addToFavorites() {
-    on click
-    takes recipe adds it to user Favorite recipes array.
-    then displays the title in the favorite recipe section.
-    the title is a link to the whole recipe.
-    if it is already favorited; it will not show up again.
-    leaves a visual indication on the card that it is favorited.
-    if clicked a second time visual indication goes away
-}
-
-*/
-
-  //  filterRecipes(/*type or input.value*/) {
-  //   //should be able to filter recipes by type
-  //   //we should have buttons that correspond to
-  //   //the types of recipes
-  // }
 
   // searchAllSavedRecipes(/*input.value*/) {
   //   //wherever we put the search input the input
