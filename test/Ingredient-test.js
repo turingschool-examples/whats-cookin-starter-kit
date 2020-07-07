eca939a93a7b326f1f84542eee6bef9f2f910385
Amy Karnaze <akarnaze@gmail.com>
2020-07-06 19:30:47 -0600

    Add instance of Recipe test

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -1,19 +1,24 @@
+/* eslint-disable */
 const chai = require("chai");
 
 const expect = chai.expect;
 var Ingredient = require('../src/Ingredient')
 
 describe('Ingredient', function () {
-            let ingredient;
-            beforeEach(function () {
-                var ingredientData = {
-                    "id": 20081,
-                    "name": "wheat flour",
-                    "estimatedCostInCents": 142
-                };
-                ingredient = new Ingredient(ingredientData);
-            });
+    let ingredient;
+    beforeEach(function () {
+        var ingredientData = {
+            "id": 20081,
+            "name": "wheat flour",
+            "estimatedCostInCents": 142
+        };
+        ingredient = new Ingredient(ingredientData);
+    });
 
-            it('should be a function', function () {
-                expect(Ingredient).to.be.a('function');
-            });
+    it('should be a function', function () {
+        expect(Ingredient).to.be.a('function');
+    });
+    it('should be an instance of Recipe', function () {
+        expect(recipe).to.be.an.instanceof(Recipe);
+    });
+    });
