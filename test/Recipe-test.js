d00f7c01d6b7798a081fb3d0bc874cf3b3d896e1
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-07 14:14:12 -0600

    Add Recipe tests for all properties and update class accordingly

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -29,7 +29,7 @@ describe('Recipe', function() {
     name = 'Funfetti Cookies';
     tags = ['dessert', 'breakfast']; 
     recipe = new Recipe(id, image, ingredients, instructions, name, tags);
-  })
+  });
   it('should be a function', function() {
     expect(Recipe).to.be.a('function');
   });
@@ -37,5 +37,33 @@ describe('Recipe', function() {
   it('should be an instance of Recipe', function() {
     expect(recipe).to.be.an.instanceof(Recipe);
   });
+
+  it('should have an id', function() {
+    expect(recipe.id).to.equal(id);
+  });
+
+//Add sad path test for if non-number is passed in as id
+//should function return 'Please enter a valid id'?
+//same for all other properties 
+
+  it('should have an image', function() {
+    expect(recipe.image).to.equal(image); 
+  });
+
+  it('should have ingredients', function() {
+    expect(recipe.ingredients).to.deep.equal(ingredients);
+  });
+
+  it('should have instructions', function() {
+    expect(recipe.instructions).to.deep.equal(instructions)
+  });
+
+  it('should have a name', function() {
+    expect(recipe.name).to.equal(name);
+  });
+
+  it('should have category tags', function() {
+    expect(recipe.tags).to.deep.equal(tags); 
+  }); 
 
 })
