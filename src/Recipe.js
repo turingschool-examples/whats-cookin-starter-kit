15664eacc31f75241e18e7e9ef60a238966246f9
Amy Karnaze <akarnaze@gmail.com>
2020-07-06 20:14:26 -0600

    Add ingredients property to constructor

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,6 +4,8 @@ class Recipe {
         constructor(recipeData) {
             this.id = recipeData.id;
             this.image = recipeData.image;
+            this.ingredients = recipeData.ingredients;
+
     }
 }
 
