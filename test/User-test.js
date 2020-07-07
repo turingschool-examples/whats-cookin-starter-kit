fd6a002f5ffcdb9f2b175b6fc212025a5b272d8c
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-07 13:47:24 -0600

    Wrote function and instantiation for User class

---
+++ test/User-test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('../src/User');
+
+describe('User', function() {
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
+
+  it('should be an instance of User', function() {
+    const user = new User();
+    expect(user).to.be.an.instanceof(User);
+  });
+});
