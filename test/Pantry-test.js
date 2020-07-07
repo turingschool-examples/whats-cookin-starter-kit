92e52d87e969a038223f6c8f96f4216b27bbf7da
Amy Karnaze <akarnaze@gmail.com>
2020-07-06 20:42:51 -0600

    Add intial import and beforeEach to Class

---
+++ test/Pantry-test.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const expect = chai.expect;
+var Pantry = require('../src/Pantry')
+
+describe('Pantry', function () {
+  let pantry;
+  beforeEach(function()) {
+    user = new Pantry();
+  });
+  
+  });
