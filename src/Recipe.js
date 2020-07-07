16885cdecb34683afab4c5fb1f845dfaeec0ee41
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-07 14:50:00 -0600

    Create recipe js file and create recipe class

---
+++ src/Recipe.js
@@ -0,0 +1,30 @@
+class Recipe {
+  constructor(recipe, ingredientsData) {
+    this.id = recipe.id;
+    this.name = recipe.name;
+    this.instructions = recipe.instructions;
+    this.tags = recipe.tags;
+    this.image = recipe.image;
+    this.ingredients = recipe.ingredients
+    this.ingredientsData = ingredientsData;
+  }
+
+  getCost() { 
+  //   console.log(this.ingredientsData)
+  //   let costCounter = 0;
+  //   this.ingredients.forEach(ingredient => {
+  //     this.ingredientsData.find(item => {
+  //       if (item.id === ingredient.id) {
+  //         costCounter += item.estimatedCostInCents * ingredient.quantity.amount;
+  //       }
+  //     })
+  //   })
+  //   return costCounter;
+  // }
+
+}
+
+
+if (typeof module !== 'undefined') {
+  module.exports = Recipe;
+}
