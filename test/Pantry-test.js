f092345e053e99d73b77854ebf356c3c8d689bf6
Amy Karnaze <akarnaze@gmail.com>
2020-07-06 20:55:11 -0600

    Attempts to try and fix bug

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
+const chai = require('chai');
+
 const expect = chai.expect;
 var Pantry = require('../src/Pantry')
+// var User = require('../src/User')
 
 describe('Pantry', function () {
   let pantry;
-  beforeEach(function()) {
-    user = new Pantry();
+  beforeEach(function() {
+    // user = new Pantry();
   });
 
   it('should be a function', function () {
@@ -16,8 +19,8 @@ it('should be an instance of User', function() {
     expect(pantry).to.be.an.instanceof(Pantry);
 });
 
-it('should have a property of ingredients', function () {
+it('should have a property of ingredients', function() {
     expect(pantry.ingredients).to.deep.equal([]);
   });
 
-  });
+});
