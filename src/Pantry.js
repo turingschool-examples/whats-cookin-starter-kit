57039e997fe74336fdd6740ac05786aa5cb36216
Joshua Sevy <joshuasevy@outlook.com>
2020-07-07 14:35:34 -0600

    Add class structure, and test file for pantry class

---
+++ src/Pantry.js
@@ -0,0 +1,12 @@
+class Pantry {
+  constructor() {
+    /*
+    Determine whether my pantry has enough ingredients to cook a given meal
+    Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
+    Remove the ingredients used for a given meal from my pantry, once that meal has been cooked
+    (only applicable if users have a list of mealsToCook; can be considered a stretch goal)
+    */
+  }
+}
+
+module.exports = Pantry;
