413c8aa634a84b2355d6f51bab1f727231a094e1
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-07 15:14:10 -0600

    Add methods to User class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,25 @@
 class User {
-  constructor() {
+  constructor(id, name, pantry) {
+    this.id = id;
+    this.name = name;
+    this.pantry = pantry;
+    this.recipesToCook = [];
+    this.favoriteRecipes = [];
+  }
+
+  addFavoriteRecipes() {
+    //add to / remove from the user's favoriteRecipes (an array)
+  }
+
+  removeFavoriteRecipes() {
+
+  }
+
+  addRecipeToCook() {
+
+  }
+  
+  searchFavoriteRecipes() {
 
   }
 }
