004f18dc7b7b158b8ce72cd45af9c1e558ade0a6
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-07 14:21:47 -0600

    Change test file to include beforeEach

--- test/User-test.js
+++ test/User-test.js
@@ -4,13 +4,16 @@ const expect = chai.expect;
 const User = require('../src/User');
 
 describe('User', function() {
+  let user;
+  beforeEach(function() {
+    user = new User();
+  });
 
   it('should be a function', function() {
     expect(User).to.be.a('function');
   });
 
   it('should be an instance of User', function() {
-    const user = new User();
     expect(user).to.be.an.instanceof(User);
   });
 });
