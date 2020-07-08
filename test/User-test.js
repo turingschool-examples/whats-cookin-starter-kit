9f21c93cd1724adc840eb4bec6a116df539444a6
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-08 13:40:57 -0600

    Change test for object structure

--- test/User-test.js
+++ test/User-test.js
@@ -2,29 +2,36 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const User = require('../src/User');
+const Recipe = require('../src/Recipe');
 
 describe('User', function() {
   let user;
+  let recipe;
+
   beforeEach(function() {
-    user = new User('Josh', 1, [
+    user = new User(
       {
-        "ingredient": 11477,
-        "amount": 4
-      },
-      {
-        "ingredient": 11297,
-        "amount": 4
-      },
-      {
-        "ingredient": 1082047,
-        "amount": 10
-      },
-      {
-        "ingredient": 20081,
-        "amount": 5
-      }
-    ]);
-  });
+        'name': 'Josh',
+        'id': 1,
+        'pantry': [
+          {
+            "ingredient": 11477,
+            "amount": 4
+          },
+          {
+            "ingredient": 11297,
+            "amount": 4
+          },
+          {
+            "ingredient": 1082047,
+            "amount": 10
+          },
+          {
+            "ingredient": 20081,
+            "amount": 5
+          }
+        ]
+      });
 
   it('should be a function', function() {
     expect(User).to.be.a('function');
@@ -63,8 +70,12 @@ describe('User', function() {
     ]);
   })
 
-  it('should start with no favorite recipes or recipes to cook', function() {
+  it('should start with no favorite recipes', function() {
     expect(user.favoriteRecipes.length).to.equal(0);
+  })
+
+  it('should start with no recipes to cook', function() {
     expect(user.recipesToCook.length).to.equal(0);
   })
+});
 });
