11f890caea6cbe538ebc8e319e0c0bc0e6cd6bd8
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-08 08:18:10 -0600

    Change User to a User object, add to favorite recipes array, add to recipes to cook array

--- src/User.js
+++ src/User.js
@@ -1,5 +1,5 @@
 class User {
-  constructor(name, id, pantry) {
+  constructor({name, id, pantry}) {
     this.name = name;
     this.id = id;
     this.pantry = pantry;
@@ -9,20 +9,32 @@ class User {
 
   addFavoriteRecipes(recipe) {
     //add to / remove from the user's favoriteRecipes (an array)
-    // this.addFavoriteRecipes.push(recipe);
+    this.favoriteRecipes.push(recipe);
+    console.log(this.favoriteRecipes)
   }
 
   removeFavoriteRecipes() {
-    //possibly use filter to return an array that doesn't include recipe
+    //possibly use findIndex to return an array that doesn't include recipe
     //reassign favorite recipes to that array
   }
 
-  addRecipeToCook() {
-
+  addRecipeToCook(recipe) {
+    this.recipesToCook.push(recipe)
   }
 
   searchFavoriteRecipes() {
+    // This function is used for the search functionality--being able to search through the recipes that exist
+    //Loop(using something besides a for loop) through our favoriteRecipes array to see if the input (which will be the parameter) includes recipe info
+    //If so push into the searchRecipes array and return it
+    //Uses showInputFinder function to accomplish this
+  }
 
+  // showInputFinder() {
+  //   var searchBarInput = searchbar.value;
+  //   var foundRecipes = searchFavoriteRecipes(searchBarInput);
+  //   updatePageHtml(foundRecipes);
+
+    //DOM manipulation for later down the road
   }
 }
 
