0da2565d1cbec4348b18d4e31a653aeeec575f84
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-08 16:01:16 -0600

    Ran lint for test file to fix spacing

--- test/User-test.js
+++ test/User-test.js
@@ -35,57 +35,57 @@ describe('User', function() {
 
     recipe1 = new Recipe(
       {
-      "id": 595736,
-      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-      "ingredients": [
-        {
-          "id": 20081,
-          "quantity": {
-            "amount": 1.5,
-            "unit": "c"
+        "id": 595736,
+        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+        "ingredients": [
+          {
+            "id": 20081,
+            "quantity": {
+              "amount": 1.5,
+              "unit": "c"
+            }
+          },
+          {
+            "id": 18372,
+            "quantity": {
+              "amount": 0.5,
+              "unit": "tsp"
+            }
+          },
+          {
+            "id": 1123,
+            "quantity": {
+              "amount": 1,
+              "unit": "large"
+            }
           }
-        },
-        {
-          "id": 18372,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "tsp"
-          }
-        },
-        {
-          "id": 1123,
-          "quantity": {
-            "amount": 1,
-            "unit": "large"
-          }
-        }
-      ],
-    "instructions": [
-      {
-        "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
-        "number": 1
-      },
-      {
-        "instruction": "Add egg and vanilla and mix until combined.",
-        "number": 2
-      },
-      {
-        "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
-        "number": 3
-      },
-    ],
-  "name": "Loaded Chocolate Chip Pudding Cookie Cups",
-  "tags": [
-        "antipasti",
-        "starter",
-        "snack",
-    ]
-  });
+        ],
+        "instructions": [
+          {
+            "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+            "number": 1
+          },
+          {
+            "instruction": "Add egg and vanilla and mix until combined.",
+            "number": 2
+          },
+          {
+            "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+            "number": 3
+          },
+        ],
+        "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+        "tags": [
+          "antipasti",
+          "starter",
+          "snack",
+        ]
+      });
 
-  recipe2 = new Recipe({
-  "id": 678353,
-  "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
-  "ingredients": [
+    recipe2 = new Recipe({
+      "id": 678353,
+      "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
+      "ingredients": [
         {
           "id": 1009016,
           "quantity": {
@@ -120,84 +120,84 @@ describe('User', function() {
         "main course",
         "main dish",
       ]
-  }),
+    }),
 
-  recipe3 = new Recipe({
-    "id": 412309,
-    "image": "https://spoonacular.com/recipeImages/412309-556x370.jpeg",
-    "ingredients": [
-      {
-        "id": 1002030,
-        "quantity": {
-          "amount": 4,
-          "unit": "teaspoons"
+    recipe3 = new Recipe({
+      "id": 412309,
+      "image": "https://spoonacular.com/recipeImages/412309-556x370.jpeg",
+      "ingredients": [
+        {
+          "id": 1002030,
+          "quantity": {
+            "amount": 4,
+            "unit": "teaspoons"
+          }
+        },
+        {
+          "id": 19334,
+          "quantity": {
+            "amount": 8,
+            "unit": "tablespoons"
+          }
+        },
+        {
+          "id": 1001,
+          "quantity": {
+            "amount": 2,
+            "unit": "cups"
+          }
         }
-      },
-      {
-        "id": 19334,
-        "quantity": {
-          "amount": 8,
-          "unit": "tablespoons"
+      ],
+      "instructions": [
+        {
+          "instruction": "Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.",
+          "number": 1
         }
-      },
-      {
-        "id": 1001,
-        "quantity": {
-          "amount": 2,
-          "unit": "cups"
-        }
-      }
-    ],
-  "instructions": [
-    {
-      "instruction": "Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.",
-      "number": 1
-    }
-  ],
-  "name": "Dirty Steve's Original Wing Sauce",
-  "tags": [
-    "sauce"
-    ]
+      ],
+      "name": "Dirty Steve's Original Wing Sauce",
+      "tags": [
+        "sauce"
+      ]
+    });
   });
-});
 
 
-it('should be a function', function() {
-  expect(User).to.be.a('function');
-});
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
 
-it('should be an instance of User', function() {
-  expect(user).to.be.an.instanceof(User);
-});
+  it('should be an instance of User', function() {
+    expect(user).to.be.an.instanceof(User);
+  });
 
-it('should have an name', function() {
-  expect(user.name).to.equal('Josh');
-})
+  it('should have an name', function() {
+    expect(user.name).to.equal('Josh');
+  })
 
-it('should have an id', function() {
-  expect(user.id).to.equal(1);
-})
+  it('should have an id', function() {
+    expect(user.id).to.equal(1);
+  })
 
-it('should have an pantry', function() {
-  expect(user.pantry).to.deep.equal([
-    {
-      "ingredient": 11477,
-      "amount": 4
-    },
-    {
-      "ingredient": 11297,
-      "amount": 4
-    },
-    {
-      "ingredient": 1082047,
-      "amount": 10
-    },
-    {
-      "ingredient": 20081,
-      "amount": 5
-    }
-  ]);
-})
+  it('should have an pantry', function() {
+    expect(user.pantry).to.deep.equal([
+      {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 10
+      },
+      {
+        "ingredient": 20081,
+        "amount": 5
+      }
+    ]);
+  })
 
   it('should start with no favorite recipes', function() {
     expect(user.favoriteRecipes.length).to.equal(0);
