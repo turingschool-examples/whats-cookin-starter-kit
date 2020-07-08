90dcc123ae9d1d79cbaac59a77299384cc3822ea
Greyson Elkins <greysonelkins@gmail.com>
2020-07-08 17:28:10 -0600

    Propagate recipe data on-load

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,16 @@
+const cardDisplay = document.querySelector('.card-display');
+
+window.onload = propagateCards(recipeData);
+
+propagateCards = (recipeCards) => {
+  recipeCards.forEach((recipe) => {
+    cardDisplay.innerHTML += 
+    `<div class="recipe-card" style="background-image: url(${recipe.image})">
+    <div class="card-info">
+    <img class="fav-icon" src="https://www.clipartmax.com/png/middle/175-1753277_free-image-on-pixabay-star-icon-png.png" />
+    <div class="recipe-title">${recipe.name}</div>
+    </div>
+    </div>`
+  })
+}
 
