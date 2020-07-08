5738f3c99d8bc645046f1ae6c91b07bf2b302d3b
Carlyle Douglas <cdouglascorp@gmail.com>
2020-07-07 21:34:31 -0600

    Create User class with properties and toggleFavoriteRecipes method

---
+++ src/User.js
@@ -0,0 +1,30 @@
+class User {
+  constructor(usersData) {
+    this.name = usersData.name
+    this.id = usersData.id
+    this.pantry = usersData.pantry
+    this.favoriteRecipes = []
+    this.recipesToCook = []
+    this.groceryList = []
+  }
+
+  toggleFavoriteRecipe(recipe) {
+    let existingRecipe = this.favoriteRecipes.find(faveRecipe => {
+      if(faveRecipe.id === recipe.id) {
+        return faveRecipe;
+      }
+    })
+    if(existingRecipe) {
+      let index = this.favoriteRecipes.indexOf(existingRecipe)
+      this.favoriteRecipes.splice(index, 1);
+    } else {
+      this.favoriteRecipes.push(recipe)
+    }
+  }
+
+
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}
