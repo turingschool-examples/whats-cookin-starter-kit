c75065a8fd6c22e0df9ca11b510773aa21773fe6
Katy St Sauveur <katyrogowski@yahoo.com>
2020-07-08 13:40:13 -0600

    Wrote implemenation code for User file

--- src/User.js
+++ src/User.js
@@ -1,5 +1,5 @@
 class User {
-  constructor(name, id, pantry) {
+  constructor({name, id, pantry}) {
     this.name = name;
     this.id = id;
     this.pantry = pantry;
@@ -9,21 +9,38 @@ class User {
 
   addFavoriteRecipes(recipe) {
     //add to / remove from the user's favoriteRecipes (an array)
-    // this.addFavoriteRecipes.push(recipe);
+    this.favoriteRecipes.push(recipe);
   }
 
-  removeFavoriteRecipes() {
-    //possibly use filter to return an array that doesn't include recipe
-    //reassign favorite recipes to that array
+  removeFavoriteRecipes(recipe) {
+    let removeRecipe = this.favoriteRecipes.indexOf(recipe);
+    this.favoriteRecipes.splice(removeRecipe, 1);
+    //possibly use indexOf to return an array that doesn't include recipe
+    //The indexOf() method returns the first index at which a given element can be found in the array
+    //This method returns -1 if the value to search for never occurs.
+    //Identifying using indexOf()
+    //Returns the removed items---the removedRecipe and how many as arguments
+
   }
 
-  addRecipeToCook() {
-
+  addRecipeToCook(recipe) {
+    this.recipesToCook.push(recipe)
   }
 
   searchFavoriteRecipes() {
-
+    // This function is used for the search functionality--being able to search through the recipes that exist
+    //Loop(using something besides a for loop) through our favoriteRecipes array to see if the input (which will be the parameter) includes recipe info
+    //If so push into the searchRecipes array and return it
+    //Uses showInputFinder function to accomplish this
   }
+
+  // showInputFinder() {
+  //   var searchBarInput = searchbar.value;
+  //   var foundRecipes = searchFavoriteRecipes(searchBarInput);
+  //   updatePageHtml(foundRecipes);
+
+    //DOM manipulation for later down the road
 }
+
 
 module.exports = User;
