46ba6cbf8349391b6ff772f659619ef3935e7e48
Jordy Shryock <jordy1611@gmail.com>
2020-07-08 16:28:11 -0600

    Add multiple sad path tests to recipe test file

--- src/Recipe.js
+++ src/Recipe.js
@@ -11,16 +11,13 @@ class Recipe {
   }
 
   getIngredientName(ingredient) { //returns name of ingredient
-    let name = '';
+    let name;
     ingredientsData.forEach(ingredientData => {
       if (ingredient.id === ingredientData.id) {
         name = ingredientData.name;
       }
     })
     return name;
-    // sort through ingredient data file
-    // if id of ingredient equals id in data filled
-    // return name of ingredient
   }
 
   getIngredientCost(ingredient) { //returns cost of ingredient in dollar amount
@@ -40,34 +37,25 @@ class Recipe {
     });
     let totalCost = costs.reduce((sum, num) => sum += num, 0);
     return totalCost; //changes to dollar amount
-    // access ingredients for total of each ingredient
-    // access recipes.js for cost of ingredient using id. .find(id === id) return cost
-    // multiply these two
-    // combine all ingredients in array
-    // return cost of all combined ingredients
   }
 
   returnInstructions() { //returns recipe's instructions as an array of objects
-    console.log(this.instructions);
     return this.instructions;
-    //  return this.ingredients
-    // maybe only return
-  }
+  };
 
-  returnIngredients() { //returns recipe's ingredients as an array of objects 
-    console.log(this.ingredients);
+  returnIngredients() { //returns recipe's ingredients as an array of objects only including id and amount
     return this.ingredients
-  }
+  };
 
   checkForIngredient(ingredient) { //returns true if recipe contains ingredient
     let test = this.ingredients.find(ingredientData => ingredientData.id === ingredient.id);
     return (test !== undefined); //allows for both types of ingredient objects to be compared
     // return this.ingredients.includes(ingredient); //simpler but may be limited to what's passed in
-    }
+  };
 
   checkForTag(tag) { //returns true if recipe contains tag, pass tag in as string
     return this.tags.includes(tag)
-  }
+  };
 }
 
 module.exports = Recipe;
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -23,76 +23,129 @@ describe("Recipe", function () {
     );
   });
 
-  it("should be a function", function () {
+  it("should be a function", function() {
     expect(Recipe).to.be.a("function");
   });
 
-  it("should be an instance of Recipe", function () {
+  it("should be an instance of Recipe", function() {
     expect(recipe).to.be.an.instanceOf(Recipe);
   });
 
-  // create sad path id for non number id's
-  it('should have an id', function () {
+  it('should have an id', function() {
     expect(recipe.id).to.equal(595736);
   });
 
-  it('should have an id that is a number', function () {
-
+  it('should have an id that is a number', function() {
+    expect(recipe.id).to.be.a('number');
   });
 
-  it('should have an image', function () {
+  it('should have an image link', function() {
     expect(recipe.image).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg');
   });
 
-  it('should have a list of ingredients', function () { //refactor
+  it('should have an image link that is a string', function() {
+    expect(recipe.image).to.be.a('string');
+  });
+
+  it('should have a list of ingredients', function() {
     expect(recipe.ingredients).to.deep.equal([ingredient1, ingredient2, ingredient3]);
+    expect(recipe.ingredients.length).to.equal(3);
   });
 
-  it('should have a list of instructions', function () {//refactor
+  it('should have a list of ingredients that is an array of objects', function() {
+    expect(recipe.ingredients).to.be.an('array');
+    expect(recipe.ingredients[0]).to.be.an('object');
+  });
+
+  it('should have a list of instructions', function() {
     expect(recipe.instructions).to.deep.equal([instruction1, instruction2, instruction3]);
+    expect(recipe.instructions.length).to.equal(3);
   });
 
-  it('should have a name', function () {
+  it('should have a list of instructions that is an array of objects', function() {
+    expect(recipe.instructions).to.be.an('array');
+    expect(recipe.instructions[0]).to.be.an('object');
+  });
+
+  it('should have a name', function() {
     expect(recipe.name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
   });
 
-  it('should have a list of tags', function () {
+  it('should have a name that is a string', function() {
+    expect(recipe.name).to.be.a('string');
+  });
+
+  it('should have a list of tags', function() {
     expect(recipe.tags).to.deep.equal(['antipasti', 'starter', 'snack', 'appetizer']);
+  });
+
+  it('should have a list of tags that is an array of strings', function() {
+    expect(recipe.tags).to.be.an('array');
+    expect(recipe.tags[0]).to.be.a('string');
   });
 
   it('should return an ingredient\'s name', function(){
     expect(recipe.getIngredientName(ingredient1)).to.equal('wheat flour');
   });
 
-  // SHOULD BE ABLE to take multiple
+  it('should return a different ingredient\'s name', function(){
+    expect(recipe.getIngredientName(ingredient3)).to.equal('eggs');
+  });
+
+  it('should return an ingredient\'s name as a string', function() {
+    expect(recipe.getIngredientName(ingredient1)).to.be.a('string');
+  });
+
   it('should return an ingredient\'s cost', function(){
     expect(recipe.getIngredientCost(ingredient1)).to.equal(1.42);
+  });
+
+  it('should return an ingredient\'s cost as a number', function() {
+    expect(recipe.getIngredientCost(ingredient1)).to.be.a('number');
   });
 
   it('should calculate total cost of the recipe', function() { //need to change if price display is changed
     expect(recipe.calculateTotalCost()).to.equal(11.96);
   });
 
-  it('should return its own instructions', function() { //need to change if price display is changed
+  it('should return total cost as a number', function() {
+    expect(recipe.calculateTotalCost()).to.be.a('number');
+  });
+
+  it('should return its own instructions', function() {
     expect(recipe.returnInstructions()).to.deep.equal([instruction1, instruction2, instruction3]);
     expect(recipe.returnInstructions().length).to.equal(3);
   });
 
-  it('should return its own ingredients', function() { //need to change if price display is changed
+  it('should return its own instructions as an array of objects', function() {
+    expect(recipe.returnInstructions()).to.be.an('array');
+
+    var instructions = recipe.returnInstructions();
+    expect(instructions[0]).to.be.an('object');
+  });
+
+  it('should return its own ingredients', function() {
     expect(recipe.returnIngredients()).to.deep.equal([ingredient1, ingredient2, ingredient3]);
     expect(recipe.returnIngredients().length).to.equal(3);
   });
 
-  it('should check if it includes a specific ingredient', function() { //need to change if price display is changed
+  it('should return its own ingredients as an array of objects', function() {
+    expect(recipe.returnIngredients()).to.be.an('array');
+
+    var ingredients = recipe.returnIngredients();
+    expect(ingredients[0]).to.be.an('object');
+  });
+
+  it('should check if it includes a specific ingredient', function() {
     expect(recipe.checkForIngredient(ingredient1)).to.equal(true);
   });
 
-  it('should check if it does not include a specific ingredient', function() { //need to change if price display is changed
+  it('should check if it does not include a specific ingredient', function() {
     const ingredient4 = {"id": 19335, "name": "sucrose", "estimatedCostInCents": 902};
     expect(recipe.checkForIngredient(ingredient4)).to.equal(false);
   });
 
-  it('should check if it includes a specific tag', function() { //need to change if price display is changed
+  it('should check if it includes a specific tag', function() {
     let tag = 'antipasti';
     expect(recipe.checkForTag(tag)).to.equal(true);
   });
