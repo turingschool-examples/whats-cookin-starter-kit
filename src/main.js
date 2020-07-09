c1a5c6522307107aff014334a03bd5ae01d06606
Greyson Elkins <greysonelkins@gmail.com>
2020-07-09 14:02:55 -0600

    Create event lisenters and handlers

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
 const cardDisplay = document.querySelector('.card-display');
+const nav = document.querySelector('nav');
 const currentUser = new User(generateRandomUser());
+//event listening
+cardDisplay.addEventListener('click', cardEventHandler);
+nav.addEventListener('click', navEventHandler);
+
+function cardEventHandler(event) {
+  if (event.target.classList.contains('star-icon')) {
+    console.log(`Oh you think recipe ${event.path[2].id} looks good?`)
+  } else {
+  console.log(`I see recipe ${event.target.id}`)
+  }
+}
+
+function navEventHandler(event) {
+  if(event.target.id === "recipe-page-button" ) {
+    console.log('You\'re already looking at the recipe page dangus');
+  } else if (event.target.id === "user-page-button") {
+    console.log(`Oh, typical ${currentUser.name}, always clicking on their self.`)
+  }
+}
+// page manipulation
 const propagateCards = (recipeCards) => {
   recipeCards.forEach((recipe) => {
     cardDisplay.innerHTML += 
     `<div class="recipe-card" id="${recipe.id}" style="background-image: url(${recipe.image})">
     <div class="card-info">
-    <img class="fav-icon" src="https://www.clipartmax.com/png/middle/175-1753277_free-image-on-pixabay-star-icon-png.png" />
-    <div class="recipe-title">${recipe.name}</div>
+    <img class="star-icon id="${recipe.id}"" src="https://www.clipartmax.com/png/middle/175-1753277_free-image-on-pixabay-star-icon-png.png" />
+    <div class="recipe-title" id="${recipe.id}">${recipe.name}</div>
     </div>
     </div>`
   })
 }
-
+// user functions
 function generateRandomUser() {
   return usersData[Math.round(Math.random() * usersData.length)];
 }
