6c747bbce7fe07de3a98deb3f1e9f08d3081a5ea
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-08 20:00:03 -0600

    Add Recipe class tests and methods to check for ingredient stock

--- src/Pantry.js
+++ src/Pantry.js
@@ -2,27 +2,26 @@ class Pantry {
   constructor(ingredients) {
     this.ingredients = ingredients || [];
   }
+
+  checkForRecipeIngredients = recipe => {
+    return recipe.ingredients.every(recipeIngredient => {
+      if (this.checkIngredientStockInPantry(recipeIngredient) > 0) {
+        return false;
+      } else {
+        return true;
+      };
+    });
+  }
+    
+  checkIngredientStockInPantry(recipeIngredient) {
+    let matchingIngredient = this.ingredients.find(pantryIngredient => pantryIngredient.ingredient === recipeIngredient.id);
+    //returns how much of ingredient is still needed
+    //If > 0, pantry is not stocked for recipe
+    //If <= 0, pantry is stocked for recipe
+    return recipeIngredient.quantity.amount - matchingIngredient.amount; 
+  } 
 }
 
 if (typeof module !== 'undefined') {
   module.exports = Pantry;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//Every User should have a pantry. A Pantry holds on to all the ingredients its owner has stocked, and the amount of each ingredient they have. As a user, I should be able to:
-
-//Determine whether my pantry has enough ingredients to cook a given meal
-
-//Create Property: pantryItems (array of ingredient objects that have ingredient id & amount properties)
-
-// Create Method: checkIfPantryStocked(recipe): Iterates through ingredients in given recipe & checks if either ingredient is NOT in pantry, or if it is, if the amount needed is GREATER than what is currently in pantry; as soon as any of these conditions are true, return false(i.e.no, pantry is not fully stocked for this recipe); otherwise, return true
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Pantry = require('../src/Pantry');
+const Recipe = require('../src/Recipe');
 
 describe('Pantry', function() {
   let ingredient1, ingredient2, ingredient3, ingredients, pantry;
@@ -29,5 +30,57 @@ describe('Pantry', function() {
     pantry2 = new Pantry(); 
 
     expect(pantry2.ingredients).to.deep.equal([]);
-  })
+  });
+
+  it('should be able to calculate how much of a recipe ingredient is missing from the pantry', function() {
+    const ingredient = {
+      id: 1,
+      quantity: {
+        amount: 12,
+        unit: "cans"
+      }
+    };
+ 
+    const missingIngredientStock = pantry.checkIngredientStockInPantry(ingredient);
+
+    expect(missingIngredientStock).to.equal(2);
+  });
+  
+  it('should be able to calculate how much extra of a recipe ingredient is in the the pantry', function () {
+    const ingredient = {
+      id: 1,
+      quantity: {
+        amount: 5,
+        unit: "cans"
+      }
+    };
+
+    const missingIngredientStock = pantry.checkIngredientStockInPantry(ingredient);
+
+    expect(missingIngredientStock).to.equal(-5);
+  });
+
+
+  it('should be able to check if it has the ingredients needed to make a recipe', function() {
+    const recipeIngredient1 = {
+      id: 1,
+      quantity: {
+        amount: 2,
+        unit: "cans"
+      }
+    };
+    const recipeIngredient2 = {
+      id: 2,
+      quantity: {
+        amount: 3,
+        unit: "cans"
+      }
+    };
+    const instruction1 = { instruction: 'Buy store-bought cookies.', number: 1 };
+    const instruction2 = { instruction: 'Say you made them from scratch.', number: 2 };
+    const recipe = new Recipe(1, 'https://pxhere.com/en/photo/1575227', [recipeIngredient1, recipeIngredient2], [instruction1, instruction2], 'cookies', ['dessert']);
+    const pantryStocked = pantry.checkForRecipeIngredients(recipe);
+
+    expect(pantryStocked).to.equal(true);
+  });
 })
