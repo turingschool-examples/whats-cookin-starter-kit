f1d7524634ffc0e7d618976f5648e6b46615818b
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-08 21:11:05 -0600

    Write test for Cookbook class to be able to filter recipes by tag #6.

--- src/Cookbook.js
+++ src/Cookbook.js
@@ -2,6 +2,15 @@ class Cookbook {
   constructor(allRecipes) {
     this.allRecipes = allRecipes;
   };
+
+  filterAllRecipesByTag(tag) {
+    return this.allRecipes.reduce((foundRecipes, recipe) => {
+			if (recipe.tags.includes(tag)) {
+				foundRecipes.push(recipe);
+			};
+			return foundRecipes;
+		}, []);
+  }
 };
 
 module.exports = Cookbook;
--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -6,13 +6,22 @@ const recipeInfo = require('../data/recipes').recipeData;
 
 describe('Cookbook', () => {
   it('Should hold recipes', () => {
-    const allRecipes = recipeInfo.map(object => {
-      return new Recipe(object);
+    const allRecipes = recipeInfo.map(item => {
+      return new Recipe(item);
     });
-
     const cookbook = new Cookbook(allRecipes);
 
     expect(cookbook.allRecipes).to.equal(allRecipes);
+  });
+
+  it('Should be able to filter all recipes by tag', () => {
+    const allRecipes = recipeInfo.map(item => {
+      return new Recipe(item);
+    });
+    const cookbook = new Cookbook(allRecipes);
+
+    expect(cookbook.filterAllRecipesByTag('sauce')[0]).to.equal(allRecipes[2]);
+    
   });
     
 });
