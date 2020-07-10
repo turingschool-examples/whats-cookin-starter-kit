5474f239587674695b9a80fb87597b7cd4913810
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-09 18:49:47 -0600

    Create functions to display recipe view and determine which recipe was clicked

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,7 @@
 const recipeCardsSection = document.querySelector('.recipe-cards')
 const pageBody = document.querySelector('body');
+const homeSection = document.querySelector('.home-view');
+const singleRecipeSection = document.querySelector('.single-recipe-view');
 let recipes; 
 
 window.onload = setUpHomePage; 
@@ -15,6 +17,7 @@ function clickAnalyzer(event) {
     console.log('Add me to meal plan!')
     //add the recipe to the user's meals to cook/meal plan recipes (target.parentElement.parentElement.parentElement.id to get index of recipe in recipes?)
   } else if (event.target.closest('.recipe-card')) {
+    displaySingleRecipe(event);
     //display that recipe (hide home view, un-hide recipe view, interpolating info from recipe clicked; can possibly use unique html id associated with each recipe card to identify it (target.parentElement.id grabs unique html id, which is the same as the index of that recipe in recipes))
   } else if (event.target.closest('header')) {
     event.preventDefault();
@@ -23,7 +26,29 @@ function clickAnalyzer(event) {
   };
 }
 
+//New//
+function displaySingleRecipe(event) {
+  changeToSingleRecipeView();
+  let recipe = determineRecipeToDisplay(event);
+  displayRecipeDetails(recipe); 
+}
 
+function changeToSingleRecipeView() {
+  homeSection.classList.add('hidden');
+  singleRecipeSection.classList.remove('hidden');
+}
+
+function determineRecipeToDisplay(event) {
+  const recipeCardId = event.target.closest('.recipe-card').id;
+  const recipeCardIndex = recipeCardId[recipeCardId.length - 1];
+  let recipeToDisplay = recipes[recipeCardIndex];
+  return recipeToDisplay); 
+}
+
+function displayRecipeDetails(recipe) {
+  
+
+//End new//
 function setUpHomePage() {
   recipes = instantiateRecipes(recipeData);
   displayRecipes(recipes);
