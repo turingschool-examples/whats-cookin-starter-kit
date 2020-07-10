fbcb50899db56706b445067af67ecad3f1e664c6
Nick Hart <nick.hart.des@gmail.com>
2020-07-10 15:00:08 -0600

    Refactor card display functions

--- src/main.js
+++ src/main.js
@@ -67,17 +67,21 @@ const populateRecipeCard = (event) => {
   const ingredientList = currentRecipe.createIngredientList();
   const instructionList = currentRecipe.giveInstructions();
 
+  insertCardHTML(currentRecipe);
+  populateElements('ingredients', ingredientList, 'ingredient', 'name');
+  populateElements('instructions', instructionList, 'instruction');
+}
+
+const insertCardHTML = (recipe) => {
   mainRecipeCard.innerHTML =
   `<button class="exit-button">Back to all recipes</button>
   <img class="star-icon"" src="https://www.clipartmax.com/png/middle/175-1753277_free-image-on-pixabay-star-icon-png.png" />
-  <img class="recipe-img" src="${currentRecipe.image}"></img>
+  <img class="recipe-img" src="${recipe.image}"></img>
   <h2>Ingredients</h2>
   <section class="ingredients"></section>
   <h2>Instructions</h2>
   <section class="instructions"></section>
   `
-  populateElements('ingredients', ingredientList, 'ingredient', 'name');
-  populateElements('instructions', instructionList, 'instruction');
 }
 
 const populateElements = (sectionName, elementList, className, argument) => {
