4373ed55a6e0efe2664cd5e7ded5a0bae86eb516
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-07-09 20:48:16 -0600

    Write test to check if pantry has enough ingredients for a recipe and pass test #10

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,4 +1,5 @@
 const userInfo = require('../data/users').usersData;
+const recipeInfo = require('../data/recipes').recipeData;
 
 class Pantry {
 	constructor({ name, id, pantry }) {
@@ -6,6 +7,35 @@ class Pantry {
 		this.id = id;
 		this.pantry = pantry;
 	};
+
+	checkIngredients(recipe) {
+		let checks = recipe.ingredients.reduce((acc, ingredient) => {
+			let recipeAmount = ingredient.quantity.amount;
+			let pantryItem = this.pantry.find(item => item.ingredient === ingredient.id);
+			let pantryAmount = null;
+			if (pantryItem) {
+				pantryAmount = pantryItem.amount
+			};
+			if (recipeAmount < pantryAmount) {
+				acc.push(true);
+			} else {
+				acc.push(false);
+			};
+			return acc;
+		}, []);
+		if (checks.includes(false)) {
+			return "You do not have enough ingredients for this recipe"
+		} else {
+			return "You have enough ingredients for this recipe"
+		};
+	};
 };
 
+//get the id of the ingredient that is in the recipe and in the pantry
+//declare variable for recipe ingredient amount
+//declare variable for pantry ingredient amount
+//compare the two values, if recipe ingredient > pantry ingredient then return false, else return true
+
 module.exports = Pantry;
+
+// compare 
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,7 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 const userInfo = require('../data/users').usersData;
+const recipeInfo = require('../data/recipes').recipeData;
 const Pantry = require('../src/Pantry');
+const Recipe = require('../src/Recipe');
 
 describe('Pantry', () => {
 	it('Should hold users ingredients', () => {
@@ -9,4 +11,17 @@ describe('Pantry', () => {
 
 		expect(userPantry.pantry).to.deep.equal(userInfo[0].pantry);
 	});
+
+	it('Should determine if it has ingredients for a recipe', () => {
+		const userPantry = new Pantry(userInfo[0]);
+		const userRecipe = new Recipe(recipeInfo[0]);
+
+		const result = userPantry.checkIngredients(userRecipe);
+
+		expect(result).to.equal("You do not have enough ingredients for this recipe");
+	});
 });
+
+// Instantiate pantry and recipe and compare the two
+// return true / false on each item in recipe
+// If any are false, return GO TO STORE
