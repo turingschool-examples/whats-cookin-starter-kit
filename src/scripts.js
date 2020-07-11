392a3bc88865804f2d79c7c0214786106a092571
Joshua Sevy <joshuasevy@outlook.com>
2020-07-11 13:36:29 -0600

    Add click listener for pantry and recipe page

--- src/scripts.js
+++ src/scripts.js
@@ -12,8 +12,33 @@ const userRecipes;
 const shoppingList;
 
 //eventListeners
+window.addEventListener('click', /*function*/clickHandler);
 
 //eventHandlers
+function clickHandler(event) {
+  if (event.target.classList.contains('recipes-button')) {
+    displayRecipesPage();
+  } else if (event.target.classList.contains('recipes-button')) {
+    displayUsersPage();
+  } 
+}
 
+
+const displayRecipesPage = () => {
+  hideElement('pantry-body');
+  displayElement('cards-body');
+}
+
+const displayUsersPage = () => {
+  hideElement('cards-body');
+  displayElement('pantry-body');
+}
+
+function displayElement(className) {
+  document.querySelector('.${ className } ').classList.remove(‘hidden’);
+}
+  function hideElement(className) {
+  document.querySelector('.${ className } ').classList.add(‘hidden’);
+}
 
 //helper functions
