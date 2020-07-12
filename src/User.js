253105c50d87b5186feff7ec717afe946310b6a6
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-12 16:51:52 -0600

    Refactor User test file and User search method

--- src/User.js
+++ src/User.js
@@ -1,11 +1,16 @@
+//Note: the below line breaks scripts.js
+//If you comment it out, scripts.js works, but test no longer works
+//Look into this more (other groups have same problem)
+const Pantry = require('../src/Pantry');
+
 class User {
-  constructor(usersData) {
-    this.name = usersData.name
-    this.id = usersData.id
-    this.pantry = new Pantry(usersData.pantry)
-    this.favoriteRecipes = []
-    this.recipesToCook = []
-    this.groceryList = []
+  constructor(name, id, pantry) {
+    this.name = name;
+    this.id = id;
+    this.pantry = new Pantry(pantry);
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+    this.groceryList = [];
   }
 
   toggleFavoriteRecipe(recipe) {
@@ -13,36 +18,44 @@ class User {
       let index = this.favoriteRecipes.indexOf(recipe)
       this.favoriteRecipes.splice(index, 1);
     } else {
-      this.favoriteRecipes.push(recipe)
+      this.favoriteRecipes.push(recipe);
     }
   }
 
   toggleRecipeToCook(recipe) {
     if (this.recipesToCook.includes(recipe)) {
-      let index = this.recipesToCook.indexOf(recipe)
+      let index = this.recipesToCook.indexOf(recipe);
       this.recipesToCook.splice(index, 1);
     } else {
-      this.recipesToCook.push(recipe)
+      this.recipesToCook.push(recipe);
     }
   }
 
   getSavedRecipes() {
-    return this.favoriteRecipes.concat(this.recipesToCook)
+    return this.favoriteRecipes.concat(this.recipesToCook);
   }
 
-  searchByRecipeOrIngr(userQuery) {
-    userQuery = userQuery.toLowerCase()
-    let savedRecipes = this.getSavedRecipes()
-    let filteredIngredients = savedRecipes.filter(recipe => {
-      if (recipe.name.toLowerCase().includes(userQuery) || recipe.ingredients.find(ingredient => {
-        return ingredient.name.includes(userQuery)
-      })) {
-        return recipe
-      }
-    })
-    return filteredIngredients
+  searchByRecipeOrIngr(userQuery, ingredientsList) {
+    userQuery = userQuery.toLowerCase();
+    let savedRecipes = this.getSavedRecipes();
+    let filteredRecipes = savedRecipes.filter(recipe => {
+      if (recipe.name.toLowerCase().includes(userQuery) || recipe.ingredients.find(ingredient => this.getIngredientName(ingredientsList, ingredient.id).includes(userQuery))) {
+        return recipe;
+      };
+    });
+    return filteredRecipes;
+  }
+
+  getIngredientName(ingredientsList, ingredientId) {
+    const matchingIngredient = ingredientsList.find(ingredient => ingredient.id === ingredientId);
+    if (matchingIngredient === undefined) {
+      return '';
+    }
+    return matchingIngredient.name;
   }
 }
+
+
 
 if (typeof module !== 'undefined') {
   module.exports = User;
