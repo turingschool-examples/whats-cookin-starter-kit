a07802bb8e20b7364b95ed087c03ac3b1c62fcd6
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-13 14:47:57 -0600

    Refactor pantry-test to use beforeEach.

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -2,47 +2,122 @@ const chai = require('chai');
 const expect = chai.expect;
 const Pantry = require('../src/Pantry');
 const Recipe = require('../src/Recipe');
-const testData = require('../data/test-data');
-const recipeTestData = testData.recipeTestData;
-const usersTestData = testData.usersTestData;
 
 describe('Pantry', () => {
+	let userPantry, userRecipe;
+	beforeEach(() => {
+		userPantry = new Pantry(
+			{
+			"name": "Nicole",
+			"id": 1,
+			"pantry": [
+				{
+					"ingredient": 11,
+					"amount": 4
+				},
+				{
+					"ingredient": 00,
+					"amount": 4
+				},
+				{
+					"ingredient": 1123,
+					"amount": 10
+				},
+				{
+					"ingredient": 44,
+					"amount": 2
+				},
+				{
+					"ingredient": 55,
+					"amount": 2
+				}]
+			});
+		userRecipe = new Recipe(
+			{
+				"id": 1111,
+				"image": "image",
+				"ingredients": [
+					{
+						"id": 11,
+						"quantity": {
+							"amount": 1.5,
+							"unit": "c"
+						}
+					},
+					{
+						"id": 22,
+						"quantity": {
+							"amount": 0.5,
+							"unit": "tsp"
+						}
+					},
+					{
+						"id": 1123,
+						"quantity": {
+							"amount": 1,
+							"unit": "large"
+						}
+					},
+					{
+						"id": 44,
+						"quantity": {
+							"amount": 1,
+							"unit": "large"
+						}
+					},
+					{
+						"id": 55,
+						"quantity": {
+							"amount": 0.5,
+							"unit": "c"
+						}
+					}
+				],
+				"instructions": [
+					{
+						"instruction": "Do the thing",
+						"number": 1
+					},
+					{
+						"instruction": "Then another thing",
+						"number": 2
+					},
+					{
+						"instruction": "Lastly, the thing",
+						"number": 3
+					},
+				],
+				"name": "Loaded Chocolate Chip Pudding Cookie Cups",
+				"tags": [
+					"lunch",
+					"sauce",
+					"main dish"
+				]
+			}
+		);
+	});
+
 	it('Should hold users ingredients', () => {
-		const userPantry = new Pantry(usersTestData[0]);
-
-		expect(userPantry.pantry).to.deep.equal(usersTestData[0].pantry);
+		expect(userPantry.pantry).to.deep.equal([
+			{"ingredient": 11, "amount": 4},
+			{"ingredient": 00, "amount": 4},
+			{"ingredient": 1123, "amount": 10},
+			{"ingredient": 44, "amount": 2},
+			{"ingredient": 55, "amount": 2}]);
 	});
 
 	it('Should determine if it has ingredients for a recipe', () => {
-		const userPantry = new Pantry(usersTestData[0]);
-		const userRecipe = new Recipe(recipeTestData[0]);
-
 		const result = userPantry.checkIngredients(userRecipe);
 
-		expect(result).to.deep.equal("You do not have enough ingredients for this recipe");
-	});
-
-	it('Should determine if it has ingredients for a recipe', () => {
-		const userPantry = new Pantry(usersTestData[1]);
-		const userRecipe = new Recipe(recipeTestData[1]);
-
-		const result = userPantry.checkIngredients(userRecipe);
-
-		expect(result).to.equal("You have enough ingredients for this recipe");
+		expect(result).to.equal("You do not have enough ingredients for this recipe");
 	});
 
 	it('Should determine amount of ingredients needed for a recipe', () => {
-		const userPantry = new Pantry(usersTestData[0]);
-		const userRecipe = new Recipe(recipeTestData[0]);
-
 		userPantry.checkIngredients(userRecipe);
 		const result = userPantry.calculateIngredientsNeeded();
 
 		expect(result).to.deep.equal([
-			{ ingredient: 9079, neededAmount: 1.5 },
-			{ ingredient: 11935, neededAmount: 0.5 },
-			{ ingredient: 2042, neededAmount: 1 },
-			{ ingredient: 2069, neededAmount: 0.5 }
+			{ ingredient: 22, neededAmount: 0.5 }
 		]);
 	});
 });
