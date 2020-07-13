d1954681559b4d1f28cd181fe47f4ac2841a6b55
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-13 15:38:53 -0600

    Add window.onload function generateRandomUserr that creates the user and displays the username #37

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,37 @@
-console.log('Hello world');
+//query selectors
+const username = document.querySelector(".username");
+let user;
+
+//event listeners
+window.onload = loadPage();
+
+//event handlers
+function loadPage() {
+  generateRandomUser();
+  // generateCookbook();
+  // displayRandomRecipes();
+};
+
+//other functions
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function generateRandomUser() {
+  let index = getRandomIndex(usersData);
+  user = new User(usersData[index], ingredientsData);
+  displayUsername();
+};
+
+function generateCookbook() {
+
+};
+
+function displayRandomRecipes() {
+
+};
+
+//DOM manipulation
+function displayUsername() {
+  username.innerText = `Username: ${user.name}`;
+};
