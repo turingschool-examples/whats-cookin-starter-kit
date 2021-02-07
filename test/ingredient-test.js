ccc3b494abf0e6f9c07d8a1a7ef1b95b7b9665ab
Alex Thompson <ahthomps1@gmail.com>
2021-02-07 14:53:11 -0700

    Create ingredienttest file and class file, test ingredient properties

---
+++ test/ingredient-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Ingredient = require('../src/ingredient');
+
+describe('Ingredient', () => {
+  let ingredient;
+
+  beforeEach(() => {
+    ingredient = new Ingredient(1, 'cheese', 350);
+  })
+
+  it('should be a function', () => {
+    expect(Ingredient).to.be.a('function');
+  })
+
+  it('should be an instance of Ingredient class', () => {
+    expect(ingredient).to.be.an.instanceof(Ingredient);
+  })
+
+  it('should have an id, name, estimated cost in cents', () => {
+    expect(ingredient.id).to.equal(1);
+    expect(ingredient.name).to.equal('cheese');
+    expect(ingredient.estimatedCostInCents).to.equal(350);
+  })
+})
