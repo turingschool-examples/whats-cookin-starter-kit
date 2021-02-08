0b7db712fcafab8674f60d07af9109ad10be216a
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-08 15:58:13 -0700

    Build recipe class attributes and pass test

---
+++ src/recipe.js
@@ -0,0 +1,15 @@
+class Recipe {
+  constructor(id, image, ingredients, instructions, name, tags) {
+    this.id = id;
+    this.image = image;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.name = name;
+    this.tags = tags;
+  }
+  returnIngredientNames() {}
+}
+
+if (typeof module !== "undefined") {
+  module.exports = Recipe;
+}
