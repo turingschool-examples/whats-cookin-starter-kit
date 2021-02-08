7c4fad9d54bf4b8b2eee4f7d411e6f858f0ca4b0
bhohnco <bryanhohn13@gmail.com>
2021-02-08 14:35:55 -0700

    Add ingredient function

--- src/recipe.js
+++ src/recipe.js
@@ -1,14 +1,15 @@
 /* eslint-disable indent */
-class Recipe {
-  constructor(recipe) {
-        this.id = recipe.id;
-        this.image = recipe.image;
-        this.ingredients = recipe.ingredients
-        this.instructions = recipe.instructions;
-        this.name = recipe.name;
-        this.tags = recipe.tags;
+let ingredientsData = require('../data/ingredients.js')
 
-    }
+class Recipe {
+  constructor(id, image, ingredients, instructions, name, tags) {
+        this.id = id;
+        this.image = image;
+        this.ingredients = ingredients
+        this.instructions = instructions;
+        this.name = name;
+        this.tags = tags;
+  }
 
     generateIngredientNames(recipe) {
         recipe.ingredients.forEach(ingredient => {
@@ -17,16 +18,14 @@ class Recipe {
              if (ingredientData.id === id) {
                ingredient.name = ingredientData.name;
                ingredient.cost = ingredientData.estimatedCostInCents / 100;
+                 ((ingredient.cost / 100) * 100);
+                 Math.round(ingredient.cost)
              }
             })
         })
-
     }
 }
 
-
-module.exports = Recipe;
-
-// if (typeof module !== 'undefined') {
-//     module.exports = Recipe;
-// }
+if (typeof module !== 'undefined') {
+    module.exports = Recipe;
+}
