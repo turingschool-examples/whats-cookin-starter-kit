24bfbbf8c65dc9bb5dc96cf909420f4ca4049117
bhohnco <bryanhohn13@gmail.com>
2021-02-08 14:33:58 -0700

    Add function testing

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -3,50 +3,176 @@ const expect = chai.expect;
 
 const Recipe = require('../src/recipe.js');
 
-const recipeData = require('../data/recipes.js');
-
-let recipe = recipeData[0];
 
 describe('Recipe', function() {
 
-  it.skip('should be a function', function() {
-    const recipe = new Recipe();
-    expect(recipe).to.be.a('function');
+  it('should be a function', function() {
+    expect(Recipe).to.be.a('function');
   });
 
-  it.skip('should be an instance of Recipe', function() {
+  it('should be an instance of Recipe', function() {
     const recipe = new Recipe();
     expect(recipe).to.be.an.instanceOf(Recipe);
   });
 
-  it.skip('should initialize with an ID', function () {
-    const recipe = new Recipe();
-    expect(recipe.id).to.eq(123456);
+  it('should initialize with an ID', function () {
+    const recipe = new Recipe(336688);
+    expect(recipe.id).to.eq(336688);
   });
 
-  it.skip('should initialize with an Image', function () {
-    const recipe = new Recipe();
-    expect(recipe.image).to.eq("image");
+  it('should initialize with an Image', function () {
+    const recipe = new Recipe(336688, 'https://whatscookin.jpg');
+    expect(recipe.image).to.eq('https://whatscookin.jpg');
   });
 
-  it.skip('should initialize with an array of ingredients', function () {
-    const recipe = new Recipe();
-    expect(recipe.ingredients).to.deep.eq([]);
+  it('should initialize with an array of ingredients', function () {
+    const ingredients = [
+      {
+        "id": 33333,
+        "quantity": {
+          "amount": 15,
+          "unit": "tsp"
+        }
+      },
+      {
+        "id": 4567,
+        "quantity": {
+          "amount": 1,
+          "unit": "large"
+        }
+      }];
+    const recipe = new Recipe(336688, 'https://whatscookin.jpg', ingredients);
+    expect(recipe.ingredients).to.deep.eq(ingredients);
   });
 
-  it.skip('should initialize with an array of instructions', function () {
-    const recipe = new Recipe();
-    expect(recipe.instructions).to.eq([]);
+  it('should initialize with an array of instructions', function () {
+    const ingredients = [
+      {
+        "id": 33333,
+        "quantity": {
+          "amount": 15,
+          "unit": "tsp"
+        }
+      },
+      {
+        "id": 4567,
+        "quantity": {
+          "amount": 1,
+          "unit": "large"
+        }
+      }];
+    const instructions = [
+      {
+        "instruction": "slice the bread",
+        "number": 1
+      },
+      {
+        "instruction": "butter the bread",
+        "number": 2
+      }];
+    const recipe = new Recipe(336688, 'https://whatscookin.jpg', ingredients, instructions)
+    expect(recipe.instructions).to.eq(instructions);
   });
 
-  it.skip('should have a name', function () {
-    const recipe = new Recipe();
-    expect(recipe.name).to.eq("")
+  it('should have a name', function () {
+    const ingredients = [
+      {
+        "id": 33333,
+        "quantity": {
+          "amount": 15,
+          "unit": "tsp"
+        }
+      },
+      {
+        "id": 4567,
+        "quantity": {
+          "amount": 1,
+          "unit": "large"
+        }
+      }];
+    const instructions = [
+      {
+        "instruction": "slice the bread",
+        "number": 1
+      },
+      {
+        "instruction": "butter the bread",
+        "number": 2
+      }];
+    const recipe = new Recipe(336688, 'https://whatscookin.jpg', ingredients, instructions, 'sandwich');
+    expect(recipe.name).to.eq('sandwich')
   });
 
-  it.skip('should initialize with an array of tags', function () {
-    const recipe = new Recipe();
-    expect(recipe.tags).to.eq([])
+  it('should initialize with an array of tags', function () {
+    const ingredients = [
+      {
+        "id": 33333,
+        "quantity": {
+          "amount": 15,
+          "unit": "tsp"
+        }
+      },
+      {
+        "id": 4567,
+        "quantity": {
+          "amount": 1,
+          "unit": "large"
+        }
+      }];
+    const instructions = [
+      {
+        "instruction": "slice the bread",
+        "number": 1
+      },
+      {
+        "instruction": "butter the bread",
+        "number": 2
+      }];
+    const recipe = new Recipe(336688, 'https://whatscookin.jpg', ingredients, instructions, 'sandwich', ['dinner', 'lunch']);
+    expect(recipe.tags).to.deep.equal(['dinner', 'lunch']);
   });
 
+    it('should be able to add name to ingredients ID', function () {
+    const ingredients = [
+      {
+        "id": 20081,
+        "quantity": {
+          "amount": 1.5,
+          "unit": "c"
+        }
+      },
+      {
+        "id": 18372,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "tsp"
+        }
+      }];
+    const ingredientsData = [
+      {
+        "id": 20081,
+        "name": "wheat flour",
+        "estimatedCostInCents": 142
+      },
+      {
+        "id": 18372,
+        "name": "bicarbonate of soda",
+        "estimatedCostInCents": 582
+      }];
+    const instructions = [
+      {
+        "instruction": "slice the bread",
+        "number": 1
+      },
+      {
+        "instruction": "butter the bread",
+        "number": 2
+      }];
+
+    const recipe = new Recipe(336688, 'https://whatscookin.jpg', ingredients, instructions, 'sandwich', ['dinner', 'lunch'])
+
+    recipe.generateIngredientNames(recipe);
+    expect(recipe.ingredients[0].name).to.equal('wheat flour');
+    expect(recipe.ingredients[1].name).to.equal('bicarbonate of soda');
+    });
 });
