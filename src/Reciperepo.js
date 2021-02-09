4562c01b5290a6d48ead4a2bc9eb8872dfd54524
Matt Umland <mattumland@gmail.com>
2021-02-09 08:37:08 -0500

    Add recipeRepo-test, test data file and psuedocoded Reciperepo class

---
+++ src/Reciperepo.js
@@ -0,0 +1,22 @@
+class Reciperepo() {
+    constructor(recipeList) {
+      this.recipeList = recipeList;
+    }
+
+    returnTagList(tags) {
+      //take in tags
+      //do any recipes have any of the search terms in recipe.tags
+    }
+
+    returnNameList(searchTerms) {
+      //take in search terms
+      //let returnArray
+      //do any recipes have any of the search terms in recipe.name
+        // if so put the recipe into the return array
+      //do any recipes return any search terms when recipe.generateIngredientNames() is invoked
+        // if so put the recipe into the return array
+      //return the return array
+    }
+}
+
+module.exports = Reciperepo;
