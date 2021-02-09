75d612a83caed1ffb7f94694b5e5a7100a24c62c
bhohnco <bryanhohn13@gmail.com>
2021-02-09 15:50:39 -0700

    Add cost and instruction method

--- src/recipe.js
+++ src/recipe.js
@@ -2,29 +2,50 @@
 let ingredientsData = require('../data/ingredients.js')
 
 class Recipe {
-  constructor(id, image, ingredients, instructions, name, tags) {
+    constructor(id, image, ingredients, instructions, name, tags) {
         this.id = id;
         this.image = image;
-        this.ingredients = ingredients
+        this.ingredients = ingredients;
         this.instructions = instructions;
         this.name = name;
         this.tags = tags;
-  }
+    }
 
     generateIngredientNames(recipe) {
+        let ingredientNameList = [];
         recipe.ingredients.forEach(ingredient => {
             const id = ingredient.id;
-            ingredientsData.forEach(ingredientData => {
-             if (ingredientData.id === id) {
-               ingredient.name = ingredientData.name;
-               ingredient.cost = ingredientData.estimatedCostInCents / 100;
-                 ((ingredient.cost / 100) * 100);
-                 Math.round(ingredient.cost)
-             }
+            ingredientsData.find(ingredientData => {
+                if (ingredientData.id === id) {
+                    ingredientNameList.push(ingredientData.name);
+                }
             })
         })
+        return ingredientNameList;
+    }
+
+    calculateRecipeCost() {
+        let totalCost = 0;
+        this.ingredients.forEach(listIngredient => {
+            ingredientsData.forEach(ingredient => {
+                if (listIngredient.id === ingredient.id) {
+                    totalCost += (ingredient.estimatedCostInCents * listIngredient.quantity.amount) / 100
+                }
+            });
+        });
+        return totalCost;
+    }
+
+    getInstructions() {
+         let detailList = [];
+         console.log(typeof(this.instructions))
+        this.instructions.forEach(instruction => {
+            detailList.push(instruction.instruction)
+         });
+         return detailList;
     }
 }
+
 
 if (typeof module !== 'undefined') {
     module.exports = Recipe;
