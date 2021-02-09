3f4afde88dc312a982fa867f61d5cf43ab21c159
Alex Thompson <ahthomps1@gmail.com>
2021-02-08 20:19:55 -0700

    Set up reciperepo test file

---
+++ test/reciperepo-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Ingredient = require('../src/reciperepo');
+
+describe('RecipeRepo', () => {
+  let ingredient1;
+  let ingredient2;
+
+  beforeEach(() => {
+    ingredient1 = new Ingredient(1, 'cheese', 350);
+    ingredient2 = new Ingredient(2, 'wine', 1250);
+  })
+})
