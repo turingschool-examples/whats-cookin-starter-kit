5a0245a85e4f89459c2db07f3f7a05d68c29bfaf
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-08 19:19:09 -0700

    Build recipe class methods, pass tests

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,5 @@
+// const Ingredient = require("./ingredient.js");
+
 class Recipe {
   constructor(id, image, ingredients, instructions, name, tags) {
     this.id = id;
@@ -7,7 +9,38 @@ class Recipe {
     this.name = name;
     this.tags = tags;
   }
-  returnIngredientNames() {}
+  returnIngredientNames(ingredientData) {
+    const recipeIngredients = [];
+    this.ingredients.forEach((recipeIngredient) => {
+      return recipeIngredients.push(
+        ingredientData.find((ingredient) => {
+          return recipeIngredient.id === ingredient.id;
+        })
+      );
+    });
+    const ingredientNames = recipeIngredients.map((ingredient) => {
+      return ingredient.name;
+    });
+
+    return ingredientNames;
+  }
+  calculateRecipeCost(ingredientData) {
+    const recipeIngredients = [];
+    this.ingredients.forEach((recipeIngredient) => {
+      return recipeIngredients.push(
+        ingredientData.find((ingredient) => {
+          return recipeIngredient.id === ingredient.id;
+        })
+      );
+    });
+    const cost = recipeIngredients.reduce((acc, curr) => {
+      return acc + curr.estimatedCostInCents;
+    }, 0);
+    return Number((cost / 100).toFixed(2));
+  }
+  returnRecipeInstructions() {
+    return this.instructions;
+  }
 }
 
 if (typeof module !== "undefined") {
