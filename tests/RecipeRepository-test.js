b56c9d11c88029f2347e94264623620ac7e7e8ca
Josh Aragon <josharagon99@gmail.com>
2021-02-09 19:38:12 -0700

    Finish test file

--- tests/RecipeRepository-test.js
+++ tests/RecipeRepository-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.export;
 
-const testRecipes = require('./test-data')
+const testRecipes = require('./test-recipes')
 const RecipeRepository = require('../src/reciperepository')
 
 describe('RecipeRepository', function() {
@@ -26,6 +26,17 @@ describe('RecipeRepository', function() {
       const recipeRepo = new RecipeRepository(testRecipes)
       expect(recipeRepo.filterRecipesViaTags('testMultiple1', 'testMultiple2')).to.deep.equal(
         ["Loaded Chocolate Chip Pudding Cookie Cups", "Maple Dijon Apple Cider Grilled Pork Chops"]
+    })
+
+    it('should be able to filter via the recipe name', function(){
+      const recipeRepo = new RecipeRepository(testRecipes)
+      expect(recipeRepo.filterRecipesViaName('Name').to.deep.equal(["Name Search Test"]))
+      expect(recipeRepo.filterRecipesViaName('Pork').to.deep.equal(["Maple Dijon Apple Cider Grilled Pork Chops"]))
+    })
+
+    it('should be able to filter via the ingredient name', function(){
+      const recipeRepo = new RecipeRepository(testRecipes)
+      expect(recipeRepo.filterRecipesViaName('wheat flour').to.deep.equal(["Loaded Chocolate Chip Pudding Cookie Cups", "Elvis Pancakes", "3 Tag Search Test"]))
     })
 
   })
