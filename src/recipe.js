fcf2f9de7b7683a74f9434b161ec0b942be42173
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-10 09:25:48 -0600

    refactor methods

--- src/recipe.js
+++ src/recipe.js
@@ -1,44 +1,67 @@
 const testIngredients = require('../tests/test-ingredients')
 
 class Recipe {
-    constructor(recipe) {
-        this.id = recipe.id,
-        this.name = recipe.name,
-        this.ingredients = recipe.ingredients,
-        this.instructions = recipe.instructions,
-        this.tags = recipe.tags,
-        this.image = recipe.image
-    }
-    
-    returnIngredientNames(ourIngredients)  {
-      const names = ourIngredients.reduce((ingredientNames, item) => {
-          this.ingredients.forEach(function(ingredient) {
-            if(ingredient.id === item.id) {
-                ingredientNames.push(item.name);
-              }
-            })
-            console.log(ingredientNames)
-          return ingredientNames;
-        }, [])
-      return names
-    }
+  constructor(recipe) {
+    this.id = recipe.id,
+    this.name = recipe.name,
+    this.ingredients = recipe.ingredients,
+    this.instructions = recipe.instructions,
+    this.tags = recipe.tags,
+    this.image = recipe.image,
+    this.ingredientCodes = this.ingredients.map((item) => {
+      return item.id
+    });
+  }
 
-    returnTotalCost() {
-        let totalCost = testIngredients.reduce((ingredientNames, item) => {
-          this.ingredients.forEach(function(ingredient) {
-              if(ingredient.id === item.id) {
-                  ingredientNames += (item.estimatedCostInCents * ingredient.quantity.amount);
-                  }
-              })
-            return ingredientNames;
-        }, 0)
-        totalCost /= 100;
-        return totalCost;
-    }
+  returnIngredientNames(ourIngredients)  {
+    // const names = ourIngredients.reduce((ingredientNames, item) => {
+    //     this.ingredients.forEach(function(ingredient) {
+    //       if(ingredient.id === item.id) {
+    //           ingredientNames.push(item.name);
+    //         }
+    //       })
+    //       console.log(ingredientNames)
+    //     return ingredientNames;
+    //   }, [])
+    // return names
+    //const ingredientCodes = this.ingredients.map((item) => {return item.id});
+    const names = ourIngredients.reduce((ingredientNames, item) => {
+      if (this.ingredientCodes.includes(item.id)) {
+        ingredientNames.push(item.name);
+      }
+      return ingredientNames;
+    }, [])
+    return names
+  }
 
-    returnInstructions() {
-        return this.instructions;
+  returnTotalCost() {
+    // let totalCost = testIngredients.reduce((ingredientNames, item) => {
+    //   this.ingredients.forEach(function(ingredient) {
+    //       if(ingredient.id === item.id) {
+    //           ingredientNames += (item.estimatedCostInCents * ingredient.quantity.amount) / 100;
+    //           }
+    //       })
+    //     return ingredientNames;
+    // }, 0)
+    // return totalCost;
+    let totalCost = 0;
+    const ingredientQuantity = this.ingredients.map(item => {
+      return item.quantity.amount
+    });
+    const cost = testIngredients.map(item => {
+      if (this.ingredientCodes.includes(item.id)) {
+        return item.estimatedCostInCents;
+      }
+    })
+    for (let i = 0; i < ingredientQuantity.length; i++) {
+      totalCost += (ingredientQuantity[i] * cost[i]) / 100
     }
+    return totalCost;
+  }
+
+  returnInstructions() {
+    return this.instructions;
+  }
 }
 
 
