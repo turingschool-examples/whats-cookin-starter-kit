ca872bb7d29d7d89b1cffe416bb468dd6698ecb2
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-10 14:13:24 -0700

    Create ingredientRepo class and test files

---
+++ src/ingredientRepo.js
@@ -0,0 +1,9 @@
+const Ingredient = require('../src/Ingredient');
+
+class IngredientRepo {
+  constructor(ingredientData = []) {}
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = IngredientRepo;
+}
