fe5c8df675215ec9cc822e13c3fa6a29751d0e5c
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-02-09 21:01:00 -0700

    Add ifunctionality to view specific recipe details on recipe card

--- src/scripts.js
+++ src/scripts.js
@@ -8,12 +8,15 @@ const searchPage = document.querySelector('.search-page')
 const homePage = document.querySelector('.home-page')
 const pageTitle = document.querySelector('.page-title')
 
+const instructionCardDirections = document.querySelector('.instruction-card-directions')
+
 
 window.addEventListener('load', compileRecipeRepository);
 window.addEventListener('load', populateRecipeCarousel);
 
+recipeCarousel.addEventListener('click', () => loadRecipeCard(event))
 document.addEventListener('keydown', searchAllRecipes);
-allRecipesButton.addEventListener('click', loadAllRecipesPage)
+allRecipesButton.addEventListener('click', () => loadSearchPage(recipeRepository.recipes))
 pageTitle.addEventListener('click', loadHomePage)
 
 function loadHomePage() {
@@ -21,11 +24,29 @@ function loadHomePage() {
   searchPage.classList.add('hidden')
 }
 
-function loadAllRecipesPage() {
+function loadRecipeCard(event) {
+  let selectedRecipe = recipeRepository.recipes.find(recipe => recipe.id === parseInt(event.target.closest('.recipe-card').id));
+  let instructions = selectedRecipe.returnInstructions().reduce((acc, instruction) => acc += `<p class="instruction-card-steps">${instruction}</p>`)
+  let ingredients = selectedRecipe.ingredients.reduce((acc, ingredient) => acc += `<tr><td class="instruction-card-ingredient">${ingredient.name}</td><td class="instruction-card-unit">${ingredient.quantity.amount} ${ingredient.quantity.unit}</td></tr>`, '')
+  console.log(ingredients)
+  instructionCardDirections.innerHTML = `
+    <h1 class="instruction-card-recipe-name">${selectedRecipe.name}</h1>
+              <h2 class="instruction-card-header">Directions</h2>
+              ${instructions}
+              <h2 class="instruction-card-header">Ingredients</h2>
+              <table class="instruction-card-ingredient-list">
+                ${ingredients}
+              </table>
+  `
+  document.querySelector('.instruction-card-img').src = selectedRecipe.image;
+}
+
+function loadSearchPage(array) {
+  searchPage.innerHTML = "";
   searchPage.classList.remove('hidden')
   homePage.classList.add('hidden')
-  recipeRepository.recipes.map(recipe => searchPage.innerHTML += `
-    <article class="recipe-card">
+  array.map(recipe => searchPage.innerHTML += `
+    <article class="recipe-card" id="${recipe.id}">
       <img class="recipe-card-img" src="${recipe.image}">
       <p class="recipe-card-name">${recipe.name}</p>
       <p class="recipe-card-cost">${recipe.getIngredientsCost()}</p>
@@ -42,7 +63,7 @@ function populateRecipeCarousel() {
   for (i = 0; i < 5; i++) {
     let randomRecipe = recipeRepository.recipes[Math.floor(Math.random() * recipeRepository.recipes.length)]
     recipeCarousel.innerHTML += `
-      <article class="recipe-card">
+      <article class="recipe-card" id="${randomRecipe.id}">
         <img class="recipe-card-img" src="${randomRecipe.image}">
         <p class="recipe-card-name">${randomRecipe.name}</p>
         <p class="recipe-card-cost">${randomRecipe.getIngredientsCost()}</p>
@@ -53,8 +74,8 @@ function populateRecipeCarousel() {
 }
 
 function searchAllRecipes(event) {
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && searchBox.value) {
     event.preventDefault()
-    console.log(recipeRepository.masterSearch(searchBox.value))
+    loadSearchPage(recipeRepository.masterSearch(searchBox.value));
   }
 }
