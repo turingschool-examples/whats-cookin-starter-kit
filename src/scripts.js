e4e441b30e61a452a4279fea36b7164f5fde7009
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-10 16:38:35 -0600

    Add dropdown feature, start basic script.js framework

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,24 @@
 console.log('Hello world');
+const nav = document.querySelector('#nav');
+const userKitchen = document.querySelector('#userDropdown');
+
+nav.addEventListener('click', navPress)
+
+function navPress() {
+  if (event.target.id === 'whatsCookin') {
+    showKitchen()
+  }
+}
+
+
+function showKitchen() {
+  document.querySelector('#whatsCookin').classList.toggle("active");
+  userKitchen.classList.toggle("collapsed");
+  if (userKitchen.style.maxHeight) {
+    userKitchen.style.maxHeight = null;
+    userKitchen.style.height = null;
+  } else {
+    userKitchen.style.maxHeight = '275px';
+    userKitchen.style.height = '275px';
+  }
+}
