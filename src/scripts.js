31f31142962b127ab79ed8be3952f5b3671ac724
Jackson McGuire <jaxmcguire@gmail.com>
2021-02-11 19:54:25 -0500

    Build page change functions

--- src/scripts.js
+++ src/scripts.js
@@ -3,14 +3,18 @@ const currentRecipeContainer = document.querySelector('.current-recipe-container
 const currentRecipeIngredients = currentRecipeContainer.querySelector('.current-recipe-ingredients');
 const currentRecipeInstructions = currentRecipeContainer.querySelector('.current-recipe-instructions')
 const recipeList = document.querySelector('.recipe-list');
+const tagContainer = document.querySelector('.tag-container');
 const buttonContainer = document.querySelector('.button-container');
-const nextPageArrow = recipeList.querySelector('.arrow');
+const nextPageArrow = recipeList.querySelector('.right');
+const prevPageArrow = recipeList.querySelector('.left');
 const recipeRepo = new RecipeRepo(recipeData);
 
 
 recipeList.addEventListener('click', clickRecipeCard);
 window.addEventListener('load', generateRecipeCards);
 nextPageArrow.addEventListener('click', showNextPage);
+prevPageArrow.addEventListener('click', showPrevPage);
+
 
 function clickRecipeCard(e) {
   const currentRecipeCard = e.target.closest('.recipe-card');
@@ -30,11 +34,9 @@ function showFeaturedRecipe (recipeTitle) {
     currentRecipeInstructions.innerHTML += `<p class="recipe-instruction">
     ${instruction.number}: ${instruction.instruction}</p>`;
   })
-
   featuredRecipe.ingredients.forEach(ingredient => {
     const currentIngredient = (ingredientsData.find(ingredientData => ingredientData.id === ingredient.id)).name;
     currentRecipeIngredients.innerText += ` ${currentIngredient},`;
-
   })
   buttonContainer.style.backgroundImage = `url(${featuredRecipe.image})`;
 }
@@ -50,22 +52,42 @@ function generateRecipeCards(newRecipes) {
   const recipeCards = recipeList.querySelectorAll('.recipe-card');
   let iterationCount = 0;
   recipeCards.forEach(recipeCard => {
-    // console.log(re)
     const currentRecipe = newRecipes[iterationCount] || recipeRepo.recipes[iterationCount];
     iterationCount ++;
-    console.log(currentRecipe)
     recipeCard.querySelector('.recipe-title').innerText = currentRecipe.name;
     recipeCard.querySelector('.recipe-img').src = currentRecipe.image;
+    randomizeCardColor(recipeCard);
   }
   )
 }
 
 function showNextPage() {
-  let currentRecipes = recipeList.querySelectorAll('.recipe-card');
-  lastRecipeCard = currentRecipes[4];
+  tagContainer.classList.add('hidden');
+  prevPageArrow.classList.remove('vis-hidden');
+  const currentRecipes = recipeList.querySelectorAll('.recipe-card');
+  const lastRecipeCard = currentRecipes[4];
   const lastRecipeTitle = lastRecipeCard.querySelector('.recipe-title').innerText;
   const lastRecipe = recipeRepo.recipes.find(recipe => recipe.name === lastRecipeTitle);
   const futureRecipes = recipeRepo.recipes.slice(recipeRepo.recipes.indexOf(lastRecipe));
   generateRecipeCards(futureRecipes);
-  console.log(futureRecipes);
+}
+
+function showPrevPage() {
+  const currentRecipes = recipeList.querySelectorAll('.recipe-card');
+  const firstRecipeCard = currentRecipes[0];
+  const firstRecipeTitle = firstRecipeCard.querySelector('.recipe-title').innerText;
+  const firstRecipe = recipeRepo.recipes.find(recipe => recipe.name === firstRecipeTitle);
+  const pastRecipes = recipeRepo.recipes.slice(recipeRepo.recipes.indexOf(firstRecipe) - 4);
+  generateRecipeCards(pastRecipes);
+  if (recipeList.querySelector('.recipe-card').innerText === recipeRepo.recipes[0].name) {
+    tagContainer.classList.remove('hidden');
+    prevPageArrow.classList.add('vis-hidden');
+  }
+}
+
+function randomizeCardColor(recipeCard) {
+  const colorArr = ['green-card', 'blue-card', 'orange-card', 'pink-card'];
+  var color = colorArr[Math.floor(Math.random() * colorArr.length)];
+  console.log(colorArr);
+  recipeCard.querySelector('.recipe').classList.toggle(color);
 }
