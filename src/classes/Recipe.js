7d36eea3912f03935b8d2e8243e2d98f75891b36
Dean Cook <deancook371@gmail.com>
2021-05-16 15:36:09 -0600

    Outline class structure for all classes in src files

---
+++ src/classes/Recipe.js
@@ -0,0 +1,15 @@
+class Recipe {
+  constructor() {
+    //should take in an id, and it can set name, image, ingredients, instructions, and tags based on that id
+    //idea -- function that finds recipe data based on id inputted and assigns name, image, ingredients, etc. of instance
+  }
+
+  //method getIngredientName() => determine the names of ingredients needed. Goes inside this.ingredients and iterates through
+    // to return each one
+
+  //method getIngredientCost() => iterate through this.ingredients and returns cost of each one
+
+  //method returnInstructions() => iterate through this.instructions and return the text of each one
+}
+
+export default Recipe;
--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,8 +1,16 @@
 class RecipeRepository {
   constructor() {
-
-    // One class to get you started!
+    //should hold on to all Recipe objects
+    //should take in recipe data and create Recipe objects based on data (?)
   }
+
+  //filterRecipesByTag() => will iterate through array of Recipes (this.recipes) and return new array of Recipes that match tag
+
+  //filterRecipesByKeyword() => will iterate through this.recipes and return new array of Recipes that contain keyword in this.recipes.name OR this.recipes.ingredients.name
+
+  //removeRecipe() => remove recipe from array of Recipe objects
+
+  //addRecipe() => add recipe to array of Recipe objects
 }
 
 export default RecipeRepository;
