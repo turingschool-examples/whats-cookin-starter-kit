4bf94ebdaaf750a5cdecd3b69a8911f75a9f4111
StevenTheBearded <>
2021-05-18 19:40:35 -0400

    Add styling to modal box

--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,20 @@ import apiCalls from './apiCalls';
 // Query Selectors
 const viewRecipe = document.getElementById('viewRecipe')
 const closeButton = document.getElementById('close')
+const modalBox = document.getElementById('modalBox')
 
 //Event Listeners
 viewRecipe.addEventListener('click', () => {
-  console.log('test')
+  modalBox.classList.add('show')
 })
 
-const modalBox = (element) => toggle(element)
+closeButton.addEventListener('click', () => {
+  modalBox.classList.remove('show')
+})
 
-const toggle = (element) => element.classList.toggle('hidden') 
+
+// closeButton
+
+// const modalBox = (element) => toggle(element)
+
+// const toggle = (element) => element.classList.toggle('hidden') 
