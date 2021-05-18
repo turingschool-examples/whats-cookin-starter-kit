fb92deaf4ecaa228b7c43904e203c885365ce5cd
StevenTheBearded <>
2021-05-18 17:46:13 -0400

    Add modal box

--- src/scripts.js
+++ src/scripts.js
@@ -2,12 +2,13 @@ import './styles.css';
 import apiCalls from './apiCalls';
 // Query Selectors
 const viewRecipe = document.getElementById('viewRecipe')
+const closeButton = document.getElementById('close')
 
 //Event Listeners
 viewRecipe.addEventListener('click', () => {
   console.log('test')
 })
 
+const modalBox = (element) => toggle(element)
 
-
-console.log('Hello world');
+const toggle = (element) => element.classList.toggle('hidden') 
