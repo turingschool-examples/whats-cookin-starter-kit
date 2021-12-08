daeaa89fb8c5433b6d17ebc6ba61885b2e1a3aaf
Gabby Recny <gjtrecny@gmail.com>
2021-12-07 20:44:17 -0600

    Attempt to get names of ingredients

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,6 +1,4 @@
 import Ingredient from './Ingredient';
-
-
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
@@ -10,7 +8,44 @@ class Recipe {
     this.name = recipe.name;
     this.tags = recipe.tags;
   }
+  accessIngredientName() {
+    //Find a way to reassign forcefully for the id
+    //to be the names
+    let bellies = this.ingredients;
+    let ingredient = new Ingredient ({id: 9003, name: "apple", estimatedCostInCents: 207});
+    let belliesIds = bellies.forEach((belly) => {
+      // console.log(belly.id)
+      bellies.find((id) => {
+        if(id === ingredient.id) {
+          return ingredient.name
+        }
+      })
+    })
+  }
 }
 
 
+// belliesIds.find(id => {
+//   if (id === ingredient.id) {
+//     console.log("YASS BITCH")
+//   }
+// })
+
+
 export default Recipe;
+
+
+
+// accessIngredientName() {
+//   return "HIIIIII"
+//   let randomIn = this.ingredients;
+//   console.log(ingredient)
+//   console.log(randomIn)
+//   randomIn.filter(() => {
+//     if (randomIn.includes(ingredient.id)) {
+//       console.log(ingredient.name)
+//       return ingredient.name
+//     }
+//   })
+//
+// }
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -2,44 +2,69 @@ import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe';
 import Ingredient from '../src/classes/Ingredient';
+import sampleRecipeData from '../src/data/sampleRecipeData.js';
+import ingredientsData from '../src/data/ingredients.js';
 
-describe('Recipe', () => {
-  let recipe;
+describe.only('Recipe', () => {
+  let recipeSample1, recipeSample2, ingredient;
   beforeEach(() => {
-    recipe = new Recipe({id: 22, image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg', ingredients: ['salt', 'chicken'], instructions: ['step1', 'step2'], name: 'Strawberry and Tomato Soup', tags: ['main course']});
+    recipeSample1 = new Recipe(sampleRecipeData[0]);
+    recipeSample2 = new Recipe(sampleRecipeData[1]);
+    ingredient = new Ingredient(ingredientsData[0])
   });
   it('Should be a function', () => {
     expect(Recipe).to.be.a('function');
   });
   it('should be an instance of recipe', () => {
-    expect(recipe).to.be.an.instanceof(Recipe)
+    expect(recipeSample1).to.be.an.instanceof(Recipe)
   })
   it('should have an id', () => {
-    expect(recipe.id).to.equal(22)
+    expect(recipeSample1.id).to.equal(sampleRecipeData[0].id)
+    expect(recipeSample2.id).to.equal(sampleRecipeData[1].id)
+
   })
   it('should have an image', () => {
-    expect(recipe.image).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg')
+    expect(recipeSample1.image).to.equal(sampleRecipeData[0].image)
+    expect(recipeSample2.image).to.equal(sampleRecipeData[1].image)
   })
   it('should have an ingredients list', () => {
-    expect(recipe.ingredients).to.deep.equal(['salt', 'chicken'])
+    expect(recipeSample1.ingredients.length).to.deep.equal(11)
+    expect(recipeSample2.ingredients).to.deep.equal(sampleRecipeData[1].ingredients)
   })
   it('should have instructions', () => {
-    expect(recipe.instructions).to.deep.equal(['step1', 'step2'])
+    expect(recipeSample1.instructions.length).to.deep.equal(6)
+    expect(recipeSample2.instructions).to.deep.equal(sampleRecipeData[1].instructions)
   })
   it('should be able to have a recipe title', () => {
-    expect(recipe.name).to.equal('Strawberry and Tomato Soup')
+    expect(recipeSample1.name).to.equal(sampleRecipeData[0].name)
+    expect(recipeSample2.name).to.equal(sampleRecipeData[1].name)
   })
-  it('should have tags', () => {
-    expect(recipe.tags).to.deep.equal(['main course'])
+  it('should be able to have multiple tags', () => {
+    expect(recipeSample1.tags).to.equal(sampleRecipeData[0].tags);
+    expect(recipeSample2.tags).to.equal(sampleRecipeData[1].tags);
   })
-  // it('should be able to have multiple tags', () => {
-  //   expect(recipe.tags).to.equal(['side dish', 'main course', 'snack'])
-  // })
   it('should get the names of each ingredient', () => {
-    expect(recipe.ingredients[0]).to.equal('salt')
+
+    //Ingredients are linked through ids in different data sets (Recipe and Ingredient)
+    //Method to check the match
+    //Method to access name
+    // console.log(ingredient)
+    // console.log(recipeSample1.ingredients[0])
+    //ingredient.id === recipe.ingredients[0].id
+        //=>ingredient.name
+    //expect(recipe.indreerwe).to.equal.(ingredients.name)
+
+    // expect(recipeSample1.ingredients[0]).to.equal('salt')
+    //
+    //if id = id, print name;
+    recipeSample2.accessIngredientName()
+    expect(recipeSample2.ingredients[1].id).to.equal(ingredient.name)
+    // expect(recipeSample2.ingredients.id).to.equal(ingredient.name)
+    //Write different test
+
   })
   it('should get different names of each ingredient', () => {
-    expect(recipe.ingredients[1]).to.equal('chicken')
+    expect(recipeSample2.ingredients.name).to.equal(sampleRecipeData[1].ingredients.name)
   })
 //   it.skip('should calculate the total cost of its ingredients', () => {
 //   })
