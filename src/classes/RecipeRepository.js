5fe77b0bada2008d0d0e3e59436780c4c14f04e1
Angele Williams <angele.williams5@gmail.com>
2022-03-27 16:59:58 -0600

    Write tests and implementation for RecipeRepository

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,7 +1,18 @@
 class RecipeRepository {
-  constructor() {
-
+  constructor(recipeData) {
+    this.recipeObjects = recipeData;
     // One class to get you started!
+    //this is going to have methods to sort our data
+  }
+  filterByTag(tag){
+   return this.recipeObjects.filter((recipeObject) => {
+     return recipeObject.tags.includes(tag);
+   });
+  }
+  filterByName(recipeName){
+    return this.recipeObjects.filter((recipeObject) => {
+      return recipeObject.name.includes(recipeName);
+    });
   }
 }
 
