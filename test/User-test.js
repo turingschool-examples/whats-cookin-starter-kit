f57252860cb9296a0d0a2173edb6a5ff59a1029d
Dylan Duke <laytonmaes@gmail.com>
2022-03-29 16:54:30 -0600

    Add User class file and User test.

---
+++ test/User-test.js
@@ -0,0 +1,5 @@
+import { expect } from 'chai';
+import Recipe from '../src/classes/Recipe';
+const {recipeData} = require('../src/data/recipes');
+import Ingredient from '../src/classes/Ingredient';
+import Recipe from '../src/classes/Recipe';
