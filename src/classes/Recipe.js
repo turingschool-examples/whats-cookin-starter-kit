1e5eb69019e509019457715fecfef88597c7384c
Michael Putnam <mikeputnam713@gmail.com>
2022-03-28 21:38:06 -0700

    Next Update of recipe functionality

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,11 +1,54 @@
+const {ingredientsData} = require('../data/ingredients');
+const {recipeData} = require('../data/recipes');
+import Ingredient from './Ingredient';
+
 class Recipe {
 	constructor(recipe) {
 		this.id = recipe.id
 		this.img = recipe.image
 		this.ingredientsInfo = recipe.ingredients // or ingredients listed (what the recipe object gives us)
 		this.instructions = recipe.instructions
-		// this.ingredients = []
+		this.name = recipe.name
+		this.tags = recipe.tags
+		this.ingredients;
+		this.ingredientNames;
 	}
+
+	collectIngredients() {
+		this.ingredients = this.ingredientsInfo.map((ingredient) => {
+			ingredientsData.forEach((dataPoint) => {
+				if(dataPoint.id === ingredient.id) {
+					ingredient = new Ingredient(dataPoint)
+				} 
+			})
+			return ingredient
+		})
+	}
+
+	nameIngredients() {
+		this.ingredientNames = this.ingredients.map((ingredient) => {
+			return ingredient.name
+		})
+	}
+
+	calculateCost() {
+		return this.ingredients.reduce((cost, ingredient) => {
+			let numOfUnits;
+			this.ingredientsInfo.forEach((data) => {
+				if(data.id === ingredient.id) {
+					numOfUnits = data.quantity.amount
+				}
+			})
+			cost += (ingredient.costInCents * numOfUnits)
+			return cost
+		}, 0)
+		// get the cost of its ingredients
+	}
+
+	giveInstructions () {
+		// return its instructions 
+	}
+
 };
 
 
