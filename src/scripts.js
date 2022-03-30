f409c16ec123f743be2cce0dbaba782873522c02
Dylan Duke <laytonmaes@gmail.com>
2022-03-30 14:15:13 -0600

    Work on more html

--- src/scripts.js
+++ src/scripts.js
@@ -2,6 +2,6 @@ import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import './assets/icon_fire_symbol_unlit.png'
 
 console.log('Hello world');
