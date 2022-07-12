b90f185066c77966f7aff680e396a4ba55b86b3d
Thomas Fettig <thomasmfettig@gmail.com>
2022-07-11 21:27:25 -0600

    Feat: Add data and test files to previous PR

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,38 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+const data = require('../src/data/recipes.js')
+const testData = data.allRecipeData.testData
 
 describe('Recipe', () => {
+
+  let repository;
+
+  beforeEach(() => {
+    repository = new RecipeRepository(testData);
+  });
+  
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
-})
+
+  it('should be able to take in recipe data as an argument', () => {
+
+    expect(repository.recipeData[0].id).to.equal(595736);
+    expect(repository.recipeData[1].id).to.equal(678353);
+  });
+
+  it('should be able to filter recipes based on a tag', () => {
+
+    expect(repository.filterByTag('sauce')).to.be.an('array');
+    expect(repository.filterByTag('sauce')[0].name).to.equal('Dirty Steve\'s Original Wing Sauce');
+    expect(repository.filterByTag('snack').length).to.equal(1)
+    expect(repository.filterByTag('snack')[0].name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups')
+  });
+
+  it('should be able to filter recipes based on name', () => {
+
+    expect(repository.filterByName('Dirty Steve\'s Original Wing Sauce')[0].id).to.equal(412309);
+    expect(repository.filterByName('Loaded Chocolate Chip Pudding Cookie Cups')[0].id).to.equal(595736);
+  });
+
+});
