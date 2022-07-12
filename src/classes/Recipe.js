d51c348ffb7ac6a6047c648e1c47020221888fbf
emmacintosh <emmammrussell@gmail.com>
2022-07-11 21:46:57 -0600

    Add additional key value pairs to recipe constructor and add tests to Recipe-test.js

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -3,6 +3,9 @@ class Recipe {
     this.id = recipeData.id;
     this.image = recipeData.image;
     this.ingredients = recipeData.ingredients;
+    this.instructions = recipeData.instructions;
+    this.name = recipeData.name;
+    this.tags = recipeData.tags;
   }
 };
 
