7385d3a9ac48b4fbc8a2ff4c81febc8e8b16ff5b
Ivy Nguyen <animeweirdo24@gmail.com>
2022-07-11 20:52:31 -0600

    Make the carousel work

--- src/scripts.js
+++ src/scripts.js
@@ -6,10 +6,11 @@ import './images/turing-logo.png'
 import Glide from '@glidejs/glide'
 
 import "/node_modules/@glidejs/glide/dist/css/glide.core.min.css"
+import "/node_modules/@glidejs/glide/dist/css/glide.theme.min.css"
 
 const config = {
     type:'carousel',
-    perView: 1
+    perView: 3
 }
 
 new Glide('.glide', config).mount()
