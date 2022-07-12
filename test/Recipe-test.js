4af025f687d8e5319d7c57cbb952f7dd9018a442
Dinne Kopelevich <dinne22@hotmail.com>
2022-07-12 16:55:52 -0600

    Add tests for Recipe methods

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -29,62 +29,6 @@ describe("Recipe", () => {
             unit: "large",
           },
         },
-        {
-          id: 19335,
-          quantity: {
-            amount: 0.5,
-            unit: "c",
-          },
-        },
-        {
-          id: 19206,
-          quantity: {
-            amount: 3,
-            unit: "Tbsp",
-          },
-        },
-        {
-          id: 19334,
-          quantity: {
-            amount: 0.5,
-            unit: "c",
-          },
-        },
-        {
-          id: 2047,
-          quantity: {
-            amount: 0.5,
-            unit: "tsp",
-          },
-        },
-        {
-          id: 1012047,
-          quantity: {
-            amount: 24,
-            unit: "servings",
-          },
-        },
-        {
-          id: 10019903,
-          quantity: {
-            amount: 2,
-            unit: "c",
-          },
-        },
-        {
-          id: 1145,
-          quantity: {
-            amount: 0.5,
-            unit: "c",
-          },
-        },
-        {
-          id: 2050,
-          quantity: {
-            amount: 0.5,
-            unit: "tsp",
-          },
-        },
       ],
       instructions: [
         {
@@ -101,31 +45,9 @@ describe("Recipe", () => {
             "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
           number: 3,
         },
-        {
-          instruction:
-            "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
-          number: 4,
-        },
-        {
-          instruction:
-            "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
-          number: 5,
-        },
-        {
-          instruction:
-            "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
-          number: 6,
-        },
       ],
       name: "Loaded Chocolate Chip Pudding Cookie Cups",
-      tags: [
-        "antipasti",
-        "starter",
-        "snack",
-        "appetizer",
-        "antipasto",
-        "hor d'oeuvre",
-      ],
+      tags: ["antipasti", "starter", "snack"],
     };
   });
   it.skip("Should be a function", () => {
@@ -160,5 +82,36 @@ describe("Recipe", () => {
 
   it.skip("Should have tags", () => {
     expect(recipe1.tags).to.deep.equal(newRecipe.tags);
+  });
+
+  it.skip("Should determine the name of ingredients", () => {
+    expect(recipe1.getIngedients()).to.deep.equal([
+      "wheat flour",
+      "bicarbonate of soda",
+      "eggs",
+    ]);
+  });
+
+  it.skip("Should determine the instructions", () => {
+    expect(recipe1.getInstructions()).to.deep.equal([
+      {
+        instruction:
+          "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+        number: 1,
+      },
+      {
+        instruction: "Add egg and vanilla and mix until combined.",
+        number: 2,
+      },
+      {
+        instruction:
+          "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+        number: 3,
+      },
+    ]);
+  });
+
+  it.skip("Should determine the cost of ingredients", () => {
+    expect(recipe1.getCost()).to.equal(23.03);
   });
 });
