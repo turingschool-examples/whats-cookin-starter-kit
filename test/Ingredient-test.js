ab62031636a32da664529155f3affa18e97ce647
Anthony Shellman <atshellman@gmail.com>
2022-07-11 22:02:14 -0600

    Add tests, verified passing

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -1,13 +1,38 @@
-const chai = require('chai');
-const expect = chai.expect;
-const Ingredient = require('../src/Ingredient');
+import { expect } from 'chai';
+import Ingredient from '../src/classes/Ingredient';
 
+let ingredient;
+let ingredientsData;
+  
+  beforeEach(function() {
+    ingredientsData = 
+      {
+      "id": 20081,
+      "name": "wheat flour",
+      "estimatedCostInCents": 142
+      }
+    
+  ingredient = new Ingredient(ingredientsData);
+  });
 
 describe('Ingredient', () => {
+    it('should be a function', function() {
+      expect(Ingredient).to.be.a('function');
+    })
+  
+    it('should be an instance of ingredient', function() {
+      expect(ingredient).to.be.an.instanceOf(Ingredient);
+    })
+  
+    it('should have an id of an ingredient', function() {
+      expect(ingredient.id).to.equal(20081)
+    });
 
+    it('should have an ingredient name', function() {
+      expect(ingredient.name).to.equal("wheat flour")
+    });
 
-
-
-
-
+    it('should have an ingredient estimatedCostInCents', function() {
+      expect(ingredient.estimatedCostInCents).to.equal(142)
+  });
 });
