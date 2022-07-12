08c6132bb55ed9ce98b623f3b10bc04f63221b09
Thomas Fettig <thomasmfettig@gmail.com>
2022-07-12 16:09:50 -0600

    Feat: Add Ingredient class and add syntax changes to RecipeRepository

---
+++ src/classes/Ingredient.js
@@ -0,0 +1,9 @@
+class Ingredient {
+  constructor(ingredientData) {
+    this.id = ingredientData.id
+    this.name = ingredientData.name
+    this.estimatedCostInCents = ingredientData.estimatedCostInCents
+  }
+}
+
+export default Ingredient;
---
+++ test/Ingredient-test.js
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import Ingredient from '../src/classes/Ingredient';
+const data = require('../src/data/ingredients.js');
+const testData = data.testIngredients;
+
+describe('Ingredient', () => {
+
+  let ingredient;
+  let ingredient2;
+  let ingredient3;
+  let ingredient4;
+
+  beforeEach(() => {
+    ingredient = new Ingredient(testData[0]);
+    ingredient2 = new Ingredient(testData[1]);
+    ingredient3 = new Ingredient(testData[2]);
+    ingredient4 = new Ingredient(testData[3]);
+  });
+
+  it('should be a function', () => {
+    expect(Ingredient).to.be.a('function');
+  });
+
+  it('should be a new instance of Ingredient', () => {
+    expect(ingredient).to.be.an.instanceOf(Ingredient);
+  });
+
+  it('should have an id', () => {
+    expect(ingredient.id).to.equal(20081);
+    expect(ingredient2.id).to.equal(18372);
+  });
+
+  it('should have a name', () => {
+    expect(ingredient.name).to.equal('wheat flour');
+    expect(ingredient3.name).to.equal('eggs');
+  });
+
+  it('should have an estimated cost', () => {
+    expect(ingredient.estimatedCostInCents).to.equal(142);
+    expect(ingredient4.estimatedCostInCents).to.equal(902);
+  });
+
+
+});
+
