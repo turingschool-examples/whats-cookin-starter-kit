ae1743bab9cb8d000cded6cdd37834c12fbe37ed
Ana Bennett <anabennett1111@gmail.com>
2022-07-11 19:45:28 -0500

    Add the Ingredient class

---
+++ src/classes/Ingredient.js
@@ -0,0 +1,9 @@
+class Ingredient {
+    constructor(ingredientDetails) {
+        this.id = ingredientDetails.id;
+        this.name = ingredientDetails.name;
+        this.estimatedCostInCents = ingredientDetails.estimatedCostInCents;
+    }
+}
+
+export default Ingredient;
