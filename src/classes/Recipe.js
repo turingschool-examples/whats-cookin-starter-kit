77d8ef2f0903a875085230ae4d06444099f0be5d
Danielle Sweeny <dsweeny1@gmail.com>
2022-07-12 17:08:43 -0600

    Add Classes, start test file for Recipe Repository

---
+++ src/classes/Recipe.js
@@ -0,0 +1,15 @@
+import Ingredient from './Ingredient';
+
+class Recipe {
+    constructor(recipe) {
+        this.id = recipe.id;
+        this.image = recipe.image;
+        this.recipeIngredients = recipe.ingredients;
+        this.instructions = recipe.instructions;
+        this.name = recipe.name;
+        this.tags = recipe.tags;
+        this.ingredients = this.collectIngredients(ingredientData);
+    }
+}
+
+export default Recipe;
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,22 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
-describe('Recipe', () => {
+// const RecipeRepository = require('../src/RecipeRepository');
+
+describe('Recipe Repository', () => {
+
+// let recipeRepository
+
+//   beforeEach(() => {
+//     recipeRepository = new RecipeRepository(tag)
+//   })
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
+  it('should filter recipes using a tag', function(tag){
+    let recipeRepository = new RecipeRepository(tag)
+    expect(recipeRepository.filterByTag(tag)).to.equal(tag)
+  })
 })
