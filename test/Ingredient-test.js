0233f4b0869f64297d44d72a2b3fc8e298dd5ca3
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-11 20:54:40 -0600

    Created testing file for Ingredient

---
+++ test/Ingredient-test.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai'; 
+import Ingredient from '../src/classes/Ingredient';
+import Recipe from '../src/classes/Ingredient'; 
+
+
+describe('Ingredient', () => {
+    let ingredient;
+    let dataIngredient;
+
+    beforeEach(() => {
+        dataIngredient = {
+            "id": 20081,
+            "name": "wheat flour",
+            "estimatedCostInCents": 142
+          }
+        
+        ingredient = new Ingredient( dataIngredient.id, dataIngredient.name, dataIngredient.estimatedCostInCents )
+    });
+
+    it( 'should be a function', () => {
+        expect(Ingredient).to.be.a('function');
+    });
+
+    it( 'should be an instance of Ingredient', () => {
+        expect(ingredient).to.be.an.instanceOf(Ingredient)
+    });
+
+    it( 'should take in an ingredient id', () => {
+        expect(ingredient.id).to.equal(20081)
+    });
+
+    it( 'should take in an ingredient name', () => {
+        expect(ingredient.name).to.equal('wheat flour')
+    });
+
+    it( 'should take in an ingredients estimatedCostInCents', () => {
+        expect(ingredient.estimatedCostInCents).to.equal(142)
+    })
+});
