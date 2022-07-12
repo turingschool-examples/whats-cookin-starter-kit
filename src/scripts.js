7c044efce3956e66dbfee74974ef23cee2b9b933
Nick Liggett <nick.liggett@gmail.com>
2022-07-11 20:26:27 -0600

    Start HTML structure

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,15 @@ import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import Glide from '@glidejs/glide'
 
+import "/node_modules/@glidejs/glide/dist/css/glide.core.min.css"
+
+const config = {
+    type:'carousel',
+    perView: 1
+}
+
+new Glide('.glide', config).mount()
 
 console.log('Hello world');
