f871b32d37bc264d85183e01587c3a747c7a977d
Maya Kappen <mayakappen@gmail.com>
2022-07-12 17:12:55 -0600

    Add testing for properties

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -2,7 +2,17 @@ import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('Recipe', () => {
+  let recipeRepository;
+  beforeEach(function() {
+    recipeRepository = new RecipeRepository();
+  })
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+  it('Should instantiate an instance of RecipeRepository', () => {
+    
+    expect(recipeRepository).to.be.an.instanceof(RecipeRepository);
+  })
+
+
 })
