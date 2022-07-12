83c6bce74fb1706f3a878674e4f7c5f80a96cc6a
Thomas Fettig <thomasmfettig@gmail.com>
2022-07-11 21:12:13 -0600

    Feat: Add test file and implementation code for recipe repository class

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,8 +1,24 @@
+const data = require('../data/recipes')
+
 class RecipeRepository {
-  constructor() {
+  constructor(recipeData) {
+    this.recipeData = recipeData
 
     // One class to get you started!
   }
+
+  filterByTag(tag) {
+    let filteredRecipes = this.recipeData.filter(recipe => recipe.tags.includes(tag));
+    return filteredRecipes;
+  }
+
+  filterByName(name) {
+    let filteredRecipes = this.recipeData.filter(recipe => recipe.name === name);
+    return filteredRecipes;
+  }
+
+  // try reduce later for the extention using multiple parameters? Maybe use ternary operator
+
 }
 
 export default RecipeRepository;
