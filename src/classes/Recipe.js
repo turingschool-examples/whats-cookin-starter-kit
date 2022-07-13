639032467ff5bdb5342cd7faea387815caa153d2
Shane Way <shane.c.way2@gmail.com>
2022-07-13 15:25:10 -0600

    Add complete Recipe.js class implementation code

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,9 +1,35 @@
 class Recipe {
-    constructor() {
-  
-      // Properties here!
+    constructor(recipeDetails) {
+        this.id = recipeDetails.id;
+        this.name = recipeDetails.name;
+        this.image = recipeDetails.image;
+        this.portions = recipeDetails.portions;
+        this.instructions = recipeDetails.instructions;
+        this.tags = recipeDetails.tags;
+
+    }
+    showIngredientsNeeded() {
+        return this.portions.map((portion) => {
+            return portion.name;  
+        });
+    }
+    calcTotalIngredientCost() {
+        const ingredientsCost = this.portions.map((portion) => {
+            return (portion.amount * portion.cost).toFixed(2) / 100;
+        });
+        return ingredientsCost;
+    }
+    calcRecipeCost() {
+        const storedIngredients = this.calcTotalIngredientCost();
+        const reducedIngredientCost = storedIngredients.reduce((acc, cur) => {
+            acc += cur;
+            return acc;
+        }, 0);
+        return reducedIngredientCost;
+    }
+    returnRecipeSteps() {
+        return this.instructions
     }
   }
-
 
 export default Recipe;
