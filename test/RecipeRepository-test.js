df9a29b622d0f9090185466a4bbc8e1dc1dabc5f
Anthony Shellman <atshellman@gmail.com>
2022-07-13 15:17:11 -0600

    Add test file and add import statement

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe';
+import RecipeTest from '../src/data/recipes-test';
 
 describe('RecipeRepository', () => {
   let recipeData;
