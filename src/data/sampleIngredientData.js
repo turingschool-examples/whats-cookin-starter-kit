30b8f8a22d84505265e4ae9179a7a199ba704473
Emily Miles <ms.emily.j.miles@gmail.com>
2022-07-13 19:26:38 -0400

    Add more recipes and ingredients to datasets/fix syntax errors

--- src/data/sampleIngredientData.js
+++ src/data/sampleIngredientData.js
@@ -53,4 +53,90 @@ const ingredientsData = [
       "id": 2050,
       "name": "vanilla",
       "estimatedCostInCents": 926
+    },
+    {
+      "id": 18371,
+      "name": "baking powder",
+      "estimatedCostInCents": 346
+    },
+    {
+      "id": 9040,
+      "name": "ripe banana",
+      "estimatedCostInCents": 331
+    },
+    {
+      "id": 20011,
+      "name": "buck wheat flour",
+      "estimatedCostInCents": 865
+    },
+    {
+      "id": 1001,
+      "name": "butter",
+      "estimatedCostInCents": 618
+    },
+    {
+      "id": 1230,
+      "name": "buttermilk",
+      "estimatedCostInCents": 773
+    },
+    {
+      "id": 19296,
+      "name": "runny honey",
+      "estimatedCostInCents": 742
+    },
+    {
+      "id": 16098,
+      "name": "peanut butter",
+      "estimatedCostInCents": 490
+    },
+    {
+      "id": 11135,
+      "name": "cauliflower",
+      "estimatedCostInCents": 486
+    },
+    {
+      "id": 6172,
+      "name": "chicken stock",
+      "estimatedCostInCents": 454
+    },
+    {
+      "id": 1002046,
+      "name": "dijon style mustard",
+      "estimatedCostInCents": 619
+    },
+    {
+      "id": 11215,
+      "name": "whole garlic clove",
+      "estimatedCostInCents": 220
+    },
+    {
+      "id": 93632,
+      "name": "ghee",
+      "estimatedCostInCents": 370
+    },
+    {
+      "id": 12120,
+      "name": "hazelnut",
+      "estimatedCostInCents": 812
+    },
+    {
+      "id": 93690,
+      "name": "nutritional yeast flakes",
+      "estimatedCostInCents": 969
+    },
+    {
+      "id": 11282,
+      "name": "onions",
+      "estimatedCostInCents": 439
+    },
+    {
+      "id": 10010123,
+      "name": "proscuitto",
+      "estimatedCostInCents": 217
+    },
+    {
+      "id": 11096,
+      "name": "rapini",
+      "estimatedCostInCents": 846
     }
+]
