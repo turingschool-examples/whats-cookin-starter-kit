3792149f8753cff04e158195e4c3a733551af8d5
Nicole Forseth <forseth.nico@gmail.com>
2022-07-13 00:22:35 -0600

    Create recipe class

--- src/classes/Ingredient.js
+++ src/classes/Ingredient.js
@@ -1,8 +1,8 @@
 class Ingredient {
   constructor(ingredientData) {
-    this.id = ingredientData.id
-    this.name = ingredientData.name
-    this.estimatedCostInCents = ingredientData.estimatedCostInCents
+    this.id = ingredientData.id;
+    this.name = ingredientData.name;
+    this.estimatedCostInCents = ingredientData.estimatedCostInCents;
   }
 }
 
--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,1 +1,37 @@
-const data = require('../data/recipes')
+import Ingredient from './Ingredient';
+
+class Recipe {
+  constructor(recipeData, ingredientData) {
+    this.id = recipeData.id
+    this.image = recipeData.image
+    this.ingredients = recipeData.ingredients
+    this.instructions = recipeData.instructions
+    this.name = recipeData.name
+    this.tags = recipeData.tags
+    this.ingredientData = ingredientData
+  }
+
+    getIngredientNames() {
+    const matchedIngredientNames = this.ingredients.map(ingredient => {
+      const foundIngredient = this.ingredientData.find(
+        data => data.id === ingredient.id
+      );
+      //console.log('foundIngredient:', foundIngredient)
+      //console.log('ingredient:', ingredient)
+      const newObject = Object.assign(foundIngredient, ingredient);
+      //console.log('new obj:', newObject)
+      const newIngredient = new Ingredient(newObject);
+      return newIngredient.name
+    });
+    return matchedIngredientNames
+   }
+
+  getCostofIngredients() {
+    //get id from recipe, go to ingredients find ID, get costs in cents * recipeAmt
+  }
+  returnRecipeInstructions() {
+
+  }
+}
+
+export default Recipe;
