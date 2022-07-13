6dac73316cc81087ebd1394c1dbd95478c672488
Shane Way <shane.c.way2@gmail.com>
2022-07-13 17:03:11 -0600

    Refactor Recipe-test suite. Codebase was correctly formatted for indentation issues

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -3,136 +3,140 @@ import Recipe from '../src/classes/Recipe';
 
 describe('Recipe', () => {
     it('Should be a function', () => {
-      expect(Recipe).to.be.a('function');
+        expect(Recipe).to.be.a('function');
     });
     let recipeNoTags;
     let recipeNoInstructions;
-    let recipeUndefined; 
-    let recipeNoPortions;   
+    let recipeUndefined;
+    let recipeNoPortions;
     let recipe1;
     let recipe2;
-    let recipe3; 
-    let recipe4;   
+    let recipe3;
+    let recipe4;
 
     beforeEach(() => {
 
-        recipeNoTags = new Recipe({id: 6, name: 'Succotash', image:'https://recipe-image-7.jpg', portions: [{ ingredientId: 1, name: 'Lima Beans',  amount: 1, cost: 605, unit: 'Bag'}], instructions: ['Open Bag'], tags: []});
+        recipeNoTags = new Recipe({ id: 6, name: 'Succotash', image: 'https://recipe-image-7.jpg', portions: [{ ingredientId: 1, name: 'Lima Beans', amount: 1, cost: 605, unit: 'Bag' }], instructions: ['Open Bag'], tags: [] });
 
-        recipeNoInstructions = new Recipe({id: 6, name: 'Pancakes', image:'https://recipe-image-6.jpg', portions: [{ ingredientId: 1, name: 'Batter',  amount: 1, cost: 201, unit: 'Bag'}], instructions: [], tags: ['Breakfast', 'Brunch']});
+        recipeNoInstructions = new Recipe({ id: 6, name: 'Pancakes', image: 'https://recipe-image-6.jpg', portions: [{ ingredientId: 1, name: 'Batter', amount: 1, cost: 201, unit: 'Bag' }], instructions: [], tags: ['Breakfast', 'Brunch'] });
 
-        recipeNoPortions = new Recipe({id: 5, name: 'Frittata', image:'https://recipe-image-1.jpg', portions: [], instructions: ['Bake In Oven'], tags: ['Breakfast', 'Brunch']});
+        recipeNoPortions = new Recipe({ id: 5, name: 'Frittata', image: 'https://recipe-image-1.jpg', portions: [], instructions: ['Bake In Oven'], tags: ['Breakfast', 'Brunch'] });
 
-        recipeUndefined = new Recipe({id: undefined, name: 'Chips', image:'https://recipe-image-Und.jpg', portions: [{ ingredientId: 1, name: 'Potato',  amount: 1, cost: 101, unit: 'Bag'}], instructions: ['Fry it'], tags: ['snack', 'Dinner']});
+        recipeUndefined = new Recipe({ id: undefined, name: 'Chips', image: 'https://recipe-image-Und.jpg', portions: [{ ingredientId: 1, name: 'Potato', amount: 1, cost: 101, unit: 'Bag' }], instructions: ['Fry it'], tags: ['snack', 'Dinner'] });
 
 
-        recipe1 = new Recipe({id: 1, name: 'Chocolate Chip Cookie', image:'https://recipe-image-1.jpg', portions: [{ ingredientId: 1, name: 'Flour',  amount: 2, cost: 101, unit: 'C'}], instructions: ['Bake it'], tags: ['snack', 'dessert']});
+        recipe1 = new Recipe({ id: 1, name: 'Chocolate Chip Cookie', image: 'https://recipe-image-1.jpg', portions: [{ ingredientId: 1, name: 'Flour', amount: 2, cost: 101, unit: 'C' }], instructions: ['Bake it'], tags: ['snack', 'dessert'] });
 
-        recipe2 = new Recipe({id: 2, name: 'Ham Sandwich', image:'https://recipe-image-2.jpg', portions: [{ingredientId: 2, name: 'Bread', amount: 5, cost: 200,  unit: 'loaf'}], instructions: ['Make Sandwich'],  tags: ['snack', 'lunch']});
+        recipe2 = new Recipe({ id: 2, name: 'Ham Sandwich', image: 'https://recipe-image-2.jpg', portions: [{ ingredientId: 2, name: 'Bread', amount: 5, cost: 200, unit: 'loaf' }], instructions: ['Make Sandwich'], tags: ['snack', 'lunch'] });
 
-        recipe3 = new Recipe({id: 3, name: 'Glazed Chops', image:'https://recipe-image-3.jpg', portions: [{ingredientId: 3, name: 'Pork Chop', amount: 1, cost: 300, unit: 'serving'}], instructions: ['Grill it up'],  tags: ['pork', 'dinner']});
+        recipe3 = new Recipe({ id: 3, name: 'Glazed Chops', image: 'https://recipe-image-3.jpg', portions: [{ ingredientId: 3, name: 'Pork Chop', amount: 1, cost: 300, unit: 'serving' }], instructions: ['Grill it up'], tags: ['pork', 'dinner'] });
 
-        recipe4 = new Recipe({id: 4, name: 'Thai Chicken Noodles', image:'https://recipe-image-4.jpg', portions: [{ingredientId: 4, name: 'Chicken', amount: 1, cost: 423, unit: 'each'}, {ingredientId: 5, name: 'Curry', amount: 2, cost: 500, unit: 'Cup'} ], instructions: ['Saute it', 'Simmer it, Eat it'],  tags: ['Chicken', 'Spicy']});
+        recipe4 = new Recipe({ id: 4, name: 'Thai Chicken Noodles', image: 'https://recipe-image-4.jpg', portions: [{ ingredientId: 4, name: 'Chicken', amount: 1, cost: 423, unit: 'each' }, { ingredientId: 5, name: 'Curry', amount: 2, cost: 500, unit: 'Cup' }], instructions: ['Saute it', 'Simmer it, Eat it'], tags: ['Chicken', 'Spicy'] });
 
-});
+    });
 
     it('the constructor should have an id number', () => {
-    expect(recipe1.id).to.equal(1);
-    expect(recipe2.id).to.equal(2);
-    
-}); 
+
+        expect(recipe1.id).to.equal(1);
+
+        expect(recipe2.id).to.equal(2);
+
+    });
 
     it('the constructor should generate a random number by default', () => {
-    expect(recipe1.id).to.equal(1);
+        expect(recipe1.id).to.equal(1);
 
-    expect(recipeUndefined.generateRandomId()).to.be.a('number');
-    
-});
+        expect(recipeUndefined.generateRandomId()).to.be.a('number');
+
+    });
 
     it('the constructor should contain a recipe name', () => {
-    
-    expect(recipe1.name).to.equal('Chocolate Chip Cookie');
-    expect(recipe2.name).to.equal('Ham Sandwich');
 
-});
+        expect(recipe1.name).to.equal('Chocolate Chip Cookie');
+
+        expect(recipe2.name).to.equal('Ham Sandwich');
+
+    });
 
     it('the constructor should contain a recipe image', () => {
 
-    expect(recipe1.image).to.equal('https://recipe-image-1.jpg');
-    expect(recipe2.image).to.equal('https://recipe-image-2.jpg');
+        expect(recipe1.image).to.equal('https://recipe-image-1.jpg');
 
-});
+        expect(recipe2.image).to.equal('https://recipe-image-2.jpg');
 
-it('the constructor should default to an empty portions array', () => {
+    });
 
-    expect(recipeNoPortions.portions).to.deep.equal([]);
+    it('the constructor should default to an empty portions array', () => {
 
-});
+        expect(recipeNoPortions.portions).to.deep.equal([]);
 
-it('the constructor should have list of ingredients needed', () => {
+    });
 
-    expect(recipe3.portions).to.deep.equal([{ingredientId: 3, name: 'Pork Chop', amount: 1, cost: 300, unit: 'serving'}])
+    it('the constructor should have list of ingredients needed', () => {
 
-    expect(recipe3.portions.length).to.equal(1);
-    expect(recipe3.portions[0].name).to.equal('Pork Chop');
+        expect(recipe3.portions).to.deep.equal([{ ingredientId: 3, name: 'Pork Chop', amount: 1, cost: 300, unit: 'serving' }])
 
-    expect(recipe2.portions.length).to.equal(1);
-    expect(recipe1.portions[0].name).to.deep.equal('Flour');
+        expect(recipe3.portions.length).to.equal(1);
+        expect(recipe3.portions[0].name).to.equal('Pork Chop');
 
-    
-});
+        expect(recipe2.portions.length).to.equal(1);
+        expect(recipe1.portions[0].name).to.deep.equal('Flour');
 
-it('the constructor should default to an empty portions array', () => {
-    expect(recipeNoInstructions.instructions).to.deep.equal([]);
 
-});
+    });
 
-it('the constructor should contain instructions to make a recipe', () => {
+    it('the constructor should default to an empty portions array', () => {
+        expect(recipeNoInstructions.instructions).to.deep.equal([]);
 
-    expect(recipe1.instructions[0]).to.equal('Bake it');
-    expect(recipe2.instructions[0]).to.equal('Make Sandwich');
+    });
 
-});
+    it('the constructor should contain instructions to make a recipe', () => {
 
-it('the constructor should default to an empty tags array', () => {
+        expect(recipe1.instructions[0]).to.equal('Bake it');
+        expect(recipe2.instructions[0]).to.equal('Make Sandwich');
 
-    expect(recipeNoTags.tags).to.deep.equal([]);
+    });
 
-});
+    it('the constructor should default to an empty tags array', () => {
 
-it('should have a list of tags', () => {
+        expect(recipeNoTags.tags).to.deep.equal([]);
 
-    expect(recipe2.tags).to.deep.equal(['snack', 'lunch']);
-    expect(recipe2.tags).to.have.lengthOf(2);
-});
+    });
 
-it('should be able to show the names of ingredients needed', () => {
+    it('should have a list of tags', () => {
 
-    expect(recipe1.getPortionNames().length).to.equal(1)
+        expect(recipe2.tags).to.deep.equal(['snack', 'lunch']);
+        expect(recipe2.tags).to.have.lengthOf(2);
+    });
 
-    expect(recipe1.getPortionNames()[0]).to.equal('Flour');
+    it('should be able to show the names of ingredients needed', () => {
 
-    expect(recipe2.getPortionNames()[0]).to.equal('Bread');
+        expect(recipe1.getPortionNames().length).to.equal(1)
 
-});
+        expect(recipe1.getPortionNames()[0]).to.equal('Flour');
 
-it('should be able to calculate the total cost of ingredients', () => {
+        expect(recipe2.getPortionNames()[0]).to.equal('Bread');
 
-    expect(recipe1.getPortionCosts(2, 100)[0]).to.equal(2.02);
+    });
 
-});
+    it('should be able to calculate the total cost of ingredients', () => {
 
-it('should be able to calculate the total cost of a recipe', () => {
+        expect(recipe1.getPortionCosts(2, 100)[0]).to.equal(2.02);
 
-    expect(recipe1. calcTotalRecipeCost()).to.equal(2.02);
+    });
 
-    expect(recipe4. calcTotalRecipeCost()).to.equal(14.23);
+    it('should be able to calculate the total cost of a recipe', () => {
 
-});
+        expect(recipe1.calcTotalRecipeCost()).to.equal(2.02);
 
-it('should be return the recipe instructions', () => {
+        expect(recipe4.calcTotalRecipeCost()).to.equal(14.23);
 
-    expect(recipe4.getInstructions()).to.deep.equal(['Saute it', 'Simmer it, Eat it'])
-});
+    });
+
+    it('should be return the recipe instructions', () => {
+
+        expect(recipe4.getInstructions()).to.deep.equal(['Saute it', 'Simmer it, Eat it'])
+    });
 
 });
 
