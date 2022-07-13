f616dc89bc030d8d18fcf6cd999d678e559f6245
Anna Spitz <aspitzemail@gmail.com>
2022-07-13 18:58:22 -0500

    Implement RecipeRepository class function

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,7 +1,31 @@
-class RecipeRepository {
-  constructor() {
+import Recipe from "./Recipe";
 
-    // One class to get you started!
+class RecipeRepository {
+  constructor(recipeData) {
+    this.recipeData = recipeData;
+    this.recipeList = [];
+  }
+
+  listRecipes() {
+    this.recipeData.forEach(recipe => {
+      this.recipeList.push(new Recipe(recipe));
+    });
+  }
+
+  findRecipeByTag(tag) {
+    if (this.recipeList.filter(recipe => recipe.tags.includes(tag)).length) {
+      return this.recipeList.filter(recipe => recipe.tags.includes(tag));
+    } 
+    
+    return `Sorry, no recipe with ${tag}.`;
+  }
+
+  findRecipeByName(name) {
+    if (this.recipeList.filter(recipe => recipe.name.includes(name)).length) {
+      return this.recipeList.filter(recipe => recipe.name.includes(name));
+    } 
+    
+    return `Sorry, no recipe named ${name}.`;
   }
 }
 
