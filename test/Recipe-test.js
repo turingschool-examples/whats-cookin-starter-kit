22eebfb9971944d49d02ba94f1d502c37c7adf00
Shane Way <shane.c.way2@gmail.com>
2022-07-13 16:16:17 -0600

    Add test for generateRandomId()

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -6,12 +6,17 @@ describe('Recipe', () => {
       expect(Recipe).to.be.a('function');
     });
 
+let recipeUndefined;    
 let recipe1;
 let recipe2;
 let recipe3; 
 let recipe4;   
 
  beforeEach(() => {
+
+recipeUndefined = new Recipe({id: undefined, name: 'Chocolate Chip Cookie', image:'https://recipe-image-1.jpg', portions: [{ ingredientId: 1, name: 'Flour',  amount: 2, cost: 101, unit: 'C'}], instructions: ['Bake it'], tags: ['snack', 'dessert']});
+
+
 recipe1 = new Recipe({id: 1, name: 'Chocolate Chip Cookie', image:'https://recipe-image-1.jpg', portions: [{ ingredientId: 1, name: 'Flour',  amount: 2, cost: 101, unit: 'C'}], instructions: ['Bake it'], tags: ['snack', 'dessert']});
 
 recipe2 = new Recipe({id: 2, name: 'Ham Sandwich', image:'https://recipe-image-2.jpg', portions: [{ingredientId: 2, name: 'Bread', amount: 5, cost: 200,  unit: 'loaf'}], instructions: ['Make Sandwich'],  tags: ['snack', 'lunch']});
@@ -23,11 +28,17 @@ recipe4 = new Recipe({id: 4, name: 'Thai Chicken Noodles', image:'https://recipe
  });
 
     it('the constructor should have an id number', () => {
-    // expect the id to be a 'number'
     expect(recipe1.id).to.equal(1);
     expect(recipe2.id).to.equal(2);
     
   }); 
+
+  it.only('the constructor should generate a random number by default', () => {
+    expect(recipe1.id).to.equal(1);
+
+    expect(recipeUndefined.generateRandomId()).to.be.a('number');
+    
+  });
 
     it('the constructor should contain a recipe name', () => {
     
@@ -37,7 +48,7 @@ recipe4 = new Recipe({id: 4, name: 'Thai Chicken Noodles', image:'https://recipe
 });
 
     it('the constructor should contain a recipe image', () => {
-    // expect the recipeData.image to be a 'string'
+
     expect(recipe1.image).to.equal('https://recipe-image-1.jpg');
     expect(recipe2.image).to.equal('https://recipe-image-2.jpg');
 
@@ -45,7 +56,6 @@ recipe4 = new Recipe({id: 4, name: 'Thai Chicken Noodles', image:'https://recipe
 
 it('the constructor should have list of ingredients needed', () => {
 
-    // expect recipe.portions to be an 'array'
     expect(recipe3.portions).to.deep.equal([{ingredientId: 3, name: 'Pork Chop', amount: 1, cost: 300, unit: 'serving'}])
 
     expect(recipe3.portions.length).to.equal(1);
@@ -59,23 +69,21 @@ it('the constructor should have list of ingredients needed', () => {
 
 it('the constructor should contain instructions to make a recipe', () => {
 
-    // expect instructions[0] to equal ('SPECIFIC STRING HERE')
     expect(recipe1.instructions[0]).to.equal('Bake it');
     expect(recipe2.instructions[0]).to.equal('Make Sandwich');
 
 });
 
   it('should have a list of tags', () => {
-    // expect the reciep1.tags[0] to deep equal('snack')
 
     expect(recipe2.tags).to.deep.equal(['snack', 'lunch']);
     expect(recipe2.tags).to.have.lengthOf(2);
   });
 
   it('should be able to show the names of ingredients needed', () => {
-    // is a method test
-    //showIngredientsNeeded();
+
     expect(recipe1.showIngredientsNeeded().length).to.equal(1)
+
     expect(recipe1.showIngredientsNeeded()[0]).to.equal('Flour');
 
     expect(recipe2.showIngredientsNeeded()[0]).to.equal('Bread');
@@ -83,26 +91,17 @@ it('the constructor should contain instructions to make a recipe', () => {
   });
 
   it('should be able to calculate the total cost of ingredients', () => {
-    // is a method test
-    //calcTotalIngredientCost()
-
-    // recipe1.calcTotalIngredientCost(2, 101);
 
     expect(recipe1.calcTotalIngredientCost(2, 100)[0]).to.equal(2.02);
   });
 
   it('should be able to calculate the total cost of a recipe', () => {
-    // is a method test
-    // calcRecipeCost()
-
 
     expect(recipe1.calcRecipeCost()).to.equal(2.02);
 
 
     expect(recipe4.calcRecipeCost()).to.equal(14.23);
 
-
-    
   });
 
   it('should be return the recipe instructions', () => {
