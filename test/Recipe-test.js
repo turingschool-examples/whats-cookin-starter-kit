85945caf15acf9578233745e5a476890f2f61364
Shane Way <shane.c.way2@gmail.com>
2022-07-12 19:30:43 -0600

    Add first 3 tests

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,1 +1,30 @@
 import { expect } from 'chai';
+import Recipe from '../src/classes/Recipe';
+
+describe('RecipeRepository', () => {
+    it('Should be a function', () => {
+      expect(Recipe).to.be.a('function');
+    });
+  });
+
+    it.skip('should have an id number', () => {
+
+  }); 
+
+    it.skip('should contain a recipe image', () => {
+    // expect the image to be a 'string'
+    // expect image to not be an 'object'
+    // expect image to not be an 'array'
+});
+
+it.skip('should have list of ingredients needed', () => {
+    // expect ingredients to be an 'array'
+    // expect ingredients["id"] to be 'number'
+    // expect ingredients["quantity"]to be an 'object'
+    // expect ingredients["quantity"].amount to be a 'number'
+    // expect ingredients['quantity"].unit to be a 'string'  
+});
+
+it.skip('should contain a recipe image', () => {
+
+});
