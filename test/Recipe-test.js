be89bdb082cb37550c4fe7cfd45a57ea3f1f8561
Shane Way <shane.c.way2@gmail.com>
2022-07-12 19:40:25 -0600

    Add Recipe-test - "should contain instructions"

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -8,23 +8,34 @@ describe('RecipeRepository', () => {
   });
 
     it.skip('should have an id number', () => {
-
+    // expect the id to be a 'number'
+    // expect the id to not be a 'string'
+    // expect the id to not be an 'object'
   }); 
 
     it.skip('should contain a recipe image', () => {
     // expect the image to be a 'string'
     // expect image to not be an 'object'
     // expect image to not be an 'array'
+
 });
 
 it.skip('should have list of ingredients needed', () => {
     // expect ingredients to be an 'array'
+
     // expect ingredients["id"] to be 'number'
     // expect ingredients["quantity"]to be an 'object'
     // expect ingredients["quantity"].amount to be a 'number'
     // expect ingredients['quantity"].unit to be a 'string'  
 });
 
-it.skip('should contain a recipe image', () => {
+it.skip('should contain instructions to make a recipe', () => {
+    // expect instructions to be an 'array'
 
+    //expect instructions to deep equal([]) **NOT SURE IF NEEDED
+
+    // expect instructions[0] to equal ('SPECIFIC STRING HERE')
+    // expect instruction["instruction"] to be a 'string'
+    // expect instruction["number"] to be a 'number'
+    //expect instruction["number"] to equal(specific) 
 });
