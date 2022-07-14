b66010a186ebcc889c75d9564e25ccb43f84868f
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-07-13 20:38:30 -0600

    Add query selectors and event listeners

--- src/scripts.js
+++ src/scripts.js
@@ -17,10 +17,20 @@ const allRecipesButton = document.querySelector('#recipe-button')
 const favoriteButton = document.querySelector('#favorite-button')
 const cookbookButton = document.querySelector('#cookbook-button')
 const searchBar = document.querySelector('.search-container')
+const radioSearchButton = document.querySelector('.radio-search-container')
+const viewRecipeButton = document.querySelector('.view-recipe-button')
+const addToCookbookButton = document.querySelector('.add-to-cookbook-button')
+const cardFavoriteButton = document.querySelector('.favorite-button')
+const unFavoriteButton = document.querySelector('.un-favorite-button')
 
 // EventListeners
-
-
+favoriteButton.addEventListener('click',)
+cookbookButton.addEventListener('click',)
+radioSearchButton.addEventListener('click',)
+viewRecipeButton.addEventListener('click',)
+addToCookbookButton.addEventListener('click',)
+cardFavoriteButton.addEventListener('click',)
+unFavoriteButton.addEventListener('click',)
 
 // Global Variables
 let recipeRepo = new RecipeRepository(recipeData)
@@ -33,6 +43,7 @@ function showAllRecipes() {
     hide(searchBar)
     console.log('IT WOERKS')
 }
+
 allRecipesButton.addEventListener('click', showAllRecipes, console.log(recipeRepo.createAllRecipes()))
 
 function show(element) {
