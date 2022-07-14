c3612d36657646ff24c669c6bbb020984494ec24
emmacintosh <emmammrussell@gmail.com>
2022-07-14 16:55:34 -0600

    Add pseudocode to the user class and 1 test + basic ForEach skeleton to user test file

--- test/User-test.js
+++ test/User-test.js
@@ -4,9 +4,25 @@ import User from '../src/classes/User'
 
 describe('User', () => {
 
+    beforeEach( () => {
 
+    })
+
+    it('should be a function', () => {
+        expect(User).to.be.a('function');
+  
+      })
+
+    it('should allow a user to add a recipe to their recipesToCook list', () => {
+
+    expect().to.equal()
+    })
 
 
 
 
 });
+
+// Allow a user to add/remove a recipe to their recipesToCook list (add to my recipesToCook)
+// Filter my recipesToCook by a tag. (Extension option: filter by multiple tags)
+// Filter my recipesToCook by its name. (Extension option: filter by name or ingredients)
