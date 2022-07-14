88c4bb28d1eb93e0b78b6dde9d61f900a3a7bd1b
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-14 13:15:02 -0600

    Updated repo data to display correctly

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,13 +1,17 @@
+import Recipe from "./Recipe";
+
 class RecipeRepository {
   constructor( recipeList ) {
     this.recipes = recipeList;
-    
+    // this.ingredientList = ingredientList;
+    // this.repository = this.recipes.map(recipe => new Recipe( recipeList, ingredientList))
+    // console.log(this.recipes)
   };
 
   filterRecipeByTag(userInput) {
      return this.recipes.filter((recipe) => {
       if( recipe.tags.includes(userInput) ) {
-        console.log({recipe})
+        // console.log({recipe})
         return recipe
       }
     });
@@ -16,7 +20,7 @@ class RecipeRepository {
   filterRecipeByName(userInput){
     return this.recipes.filter((recipe) => {
       if( recipe.name.includes(userInput) ) {
-        console.log({recipe})
+        // console.log({recipe})
         return recipe
       }
     });
