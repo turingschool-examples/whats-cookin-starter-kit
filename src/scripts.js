80724399e70dfc7832966d0388dff3ae0691bbcc
Nick Liggett <nick.liggett@gmail.com>
2022-07-14 12:22:00 -0600

    Build out start of user-functionality

--- src/scripts.js
+++ src/scripts.js
@@ -24,6 +24,69 @@ new Glide('.glide', config).mount()
 
 let recipeRepository = new RecipeRepository(recipeData)
 
+const viewAllButton = document.querySelector('#viewAllButton')
+const homePageView = document.querySelector('.home-page-view')
+const viewAllPage = document.querySelector('.view-all-page')
+const homeButton = document.querySelector('#homeButton')
+const favoritesButton = document.querySelector('#favoritesButton')
+const glideRecipes = document.querySelector('.glide__slides')
+const recipeDetailsPage = document.querySelector('.recipe-details-page')
+const allRecipesContainer = document.querySelector('.all-recipes')
+
+viewAllButton.addEventListener('click', showViewAllPage)
+homeButton.addEventListener('click', showHomePage)
+favoritesButton.addEventListener('click', showFavoritesPage)
+allRecipesContainer.addEventListener('click', function(event) {
+    console.log(event.target)
+    showRecipeDetailsPage(event)
+  })
+// viewAllRecipeIconImage.addEventListener('event', () => {
+//     console.log('event')
+// })
+
+function showViewAllPage() {
+    homePageView.classList.add('hidden')
+    viewAllPage.classList.remove('hidden')
+    recipeDetailsPage.classList.add('hidden')
+}
+
+function showHomePage() {
+    homePageView.classList.remove('hidden')
+    viewAllPage.classList.add('hidden')
+    recipeDetailsPage.classList.add('hidden')
+}
+
+function showFavoritesPage() {
+    homePageView.classList.add('hidden')
+    viewAllPage.classList.add('hidden')
+    recipeDetailsPage.classList.add('hidden')
+}
+
+function populateAllRecipes() {
+    recipeData.forEach(recipe => {
+    viewAllPage.childNodes[3].innerHTML += `<section class="recipe-icon">
+    <img class="view-all-recipe-image" src="${recipe.image}" alt="random-recipe-image">
+    <p>
+      ${recipe.name}
+    </p>
+  </section>`
+    // glideRecipes.innerHTML += `<li class="glide__slide"><img class="recipe-icon-image" src="${recipe.image}"></li>`
+})
+// console.log(glideRecipes.innerHTML)
+}
+
+populateAllRecipes()
+
+function showRecipeDetailsPage(event) {
+    if (event.target.classList.contains('view-all-recipe-image')){
+        homePageView.classList.add('hidden')
+        viewAllPage.classList.add('hidden')
+        recipeDetailsPage.classList.remove('hidden')
+    }
+    
+}
+
+// 
 // const recipe = new Recipe(recipeData, ingredientsData)
 //instatiate a new recipe using data sets as arguments?
 //for each recipe that we are passing through from the data, create new recipe instance
