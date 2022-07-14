4eff67b91cbc2fdbc56c1ceda626a1d6dcb3f12d
Thomas Fettig <thomasmfettig@gmail.com>
2022-07-14 16:57:29 -0600

    Feat: Add function to randomize recipe images and insert into main page

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,42 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+import RecipeRepository from './classes/RecipeRepository';
+import Recipe from './classes/Recipe';
+import Ingredient from './classes/Ingredient';
+import User from './classes/User';
+const recipeData = require('./data/recipes');
+const ingredientData = require('./data/ingredients');
+const userData = require('./data/users');
 
+// ***** Query Selectors ***** //
 
+const recipeIconContainer = document.querySelector('.recipe-icon-container');
+const icon1Img = document.querySelector('.icon-1-img');
+const icon2Img = document.querySelector('.icon-2-img');
+const icon3Img = document.querySelector('.icon-3-img');
+const icon4Img = document.querySelector('.icon-4-img');
 
-console.log('Hello world');
+// ***** Event Listeners ***** //
+
+window.addEventListener('load', updateMainPageRecipeIcons);
+
+// ***** Global Variables ***** //
+
+const recipeTest = recipeData.recipeData.map(recipe => {
+  return new Recipe(recipe);
+})
+
+// ***** Functions ***** //
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function updateMainPageRecipeIcons() {
+  icon1Img.src = recipeTest[getRandomIndex(recipeTest)].image;
+  icon2Img.src = recipeTest[getRandomIndex(recipeTest)].image;
+  icon3Img.src = recipeTest[getRandomIndex(recipeTest)].image;
+  icon4Img.src = recipeTest[getRandomIndex(recipeTest)].image;
+}
