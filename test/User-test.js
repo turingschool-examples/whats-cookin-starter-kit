6d8954340bfc55290fd9e02ceffbdc06ae7a5420
jmedina <ndgns2@gmail.com>
2022-07-14 16:55:25 -0600

    Feature/ create 3 new instances of User for testing

--- test/User-test.js
+++ test/User-test.js
@@ -5,7 +5,62 @@ import User from "../src/classes/User";
 
 
 describe("User", () => {
+
   it("Should be a function", () => {
     expect(User).to.be.a("function");
   })
-}
+
+    let user1;
+    let user2;
+    let user3;
+
+    beforeEach(() => {
+      user1 = new User({
+        name: 'Josh', id: 28, pantry: [{
+            ingredient: 11297,
+            amount: 4
+        },
+           {
+          ingredient: 1082047,
+          amount: 10
+        },]
+    });
+
+       user2 = new User({
+         name: "Shane",
+         id: 111,
+         pantry: [
+           {
+             ingredient: 6150,
+             amount: 3,
+           },
+           {
+             ingredient: 1032009,
+             amount: 7,
+           },
+         ],
+       });
+
+          user3 = new User({
+            name: "David",
+            id: 4444,
+            pantry: [
+              {
+                ingredient: 18372,
+                amount: 8,
+              },
+              {
+                ingredient: 1102047,
+                amount: 1,
+              },
+            ],
+          });
+    });
+
+
+
+
+
+
+
+})
