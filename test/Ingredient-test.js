cf7f3d66d58ea066071d19355effd56402a735a3
Stephanie Guzman <sguzman247@gmail.com>
2022-07-13 20:02:22 -0500

    Update test data to test multiple ingredients

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -24,40 +24,37 @@ describe('Ingredient', () => {
     });
 
     it('should have a name', () => {
-        // expect(ingredient.name).to.equal(ingredientData[0].name);
         expect(ingredient.name).to.equal('wheat flour');
+        expect(ingredient2.name).to.equal('sucrose');
 
     });
 
     it('should have an id', () => {
         expect(ingredient.id).to.equal(20081);
-        expect(ingredient2.id).to.equal(18372);
+        expect(ingredient2.id).to.equal(19335);
     });
 
     it('should have a quantity amount', () => {
-        // expect(ingredient.quantityAmount).to.equal(recipeData[0].ingredients[0].quantity.amount);
         expect(ingredient.quantityAmount).to.equal(1.5);
-        expect(ingredient2.quantityAmount).to.equal(1.5);
+        expect(ingredient2.quantityAmount).to.equal(0.5);
     });
 
     it('should have a quantity unit', () => {
-        // expect(ingredient.quantityUnit).to.equal(recipeData[0].ingredients[0].quantity.unit);
         expect(ingredient.quantityUnit).to.equal('c');
         expect(ingredient2.quantityUnit).to.equal('c');
     });
 
     it('should have an estimated cost in cents per unit', () => {
-        // expect(ingredient.costPerUnitInCents).to.equal(ingredient[0].estimatedCostInCents);
         expect(ingredient.costPerUnitInCents).to.equal(142);
-        expect(ingredient2.costPerUnitInCents).to.equal(142);
+        expect(ingredient2.costPerUnitInCents).to.equal(902);
     });
 
     it('should be able to calculate the cost per ingredient', () => {
         var costPerRecipeIngredient;
         costPerRecipeIngredient = ingredient.calculateCost();
-        expect(costPerRecipeIngredient).to.equal(2.13)
+        expect(costPerRecipeIngredient).to.equal(2.13);
 
         costPerRecipeIngredient = ingredient2.calculateCost();
-        expect(costPerRecipeIngredient).to.equal(2.13)
+        expect(costPerRecipeIngredient).to.equal(4.51);
     });
 });
