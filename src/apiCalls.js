88402b3d5667bc421d579becc9a8c65d20683ab2
Emily Miles <ms.emily.j.miles@gmail.com>
2022-07-16 16:27:13 -0400

    Add first ideas for feth api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,18 @@
 // Your fetch requests will live here!
+fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error));
+
+fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error));
+
+fetch('Get all recipes	https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error))
 
 
 console.log('I will be a fetch request!')
