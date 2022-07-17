209e656d5e1cc7effd14b6b53aea9d157ff346ad
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-17 13:15:23 -0600

    Cleaned up spacing and text output so it isn't bulky

--- src/classes/User.js
+++ src/classes/User.js
@@ -4,7 +4,6 @@ class User {
         this.id = user.id;
         this.pantry = user.pantry;
         this.recipesToCook = [ ];
-        // this.favoriteRecipes = [ ];
     };
 
     addRecipeToRecipesToCook( recipe ) {
@@ -12,31 +11,24 @@ class User {
     }
 
     removeRecipeFromRecipesToCook( recipe ) {
-            return  this.recipesToCook.splice(this.recipesToCook.indexOf(recipe.id), 1) 
-        }
+        return this.recipesToCook.splice( this.recipesToCook.indexOf( recipe.id ), 1 ) 
+    }
 
     filterRecipesToCookByTag( userInput ){
-    let tagMatches = this.recipesToCook.filter((recipe) => {
-        return recipe.tags.includes(userInput.toLowerCase())
-    });
-        return tagMatches[0].tags;
+        let tagMatches = this.recipesToCook.filter( recipe => {
+            return recipe.tags.includes(userInput.toLowerCase())
+        });
+            return tagMatches[ 0 ].tags;
     };
       
-    filterRecipesToCookByName(userSearch){
-
-        let nameMatches = this.recipesToCook.filter((recipe) => {
-            return recipe.name.includes(userSearch)
+    filterRecipesToCookByName( userSearch ){
+        let nameMatches = this.recipesToCook.filter( recipe => {
+            return recipe.name.includes( userSearch )
         })
-          return nameMatches[0].name
+            return nameMatches[ 0 ].name
       };
   };
 
   
   
   export default User;
-
-//   Allow a user to add/remove a recipe to their recipesToCook 
-// list (add to my recipesToCook)
-
-// Filter my recipesToCook by a tag. (Extension option: filter by multiple tags)
-// Filter my recipesToCook by its name. (Extension option: filter by name or ingredients)
