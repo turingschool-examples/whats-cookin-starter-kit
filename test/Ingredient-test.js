e408e780b6236ee08219933317b6618e1e39fefe
Michael Martinelli <turtelboy123@gmail.com>
2022-07-17 00:38:49 -0600

    Refactors a few methods, as well as creates logic for iteration 2 dom manipulation

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -1,5 +1,3 @@
-import { expect } from 'chai';
-import Ingredient from '../src/classes/Ingredient';
 import { ingredientsData } from '../src/data/ingredients';
 
 describe('Ingredient Test', () => {
@@ -8,13 +6,8 @@ describe('Ingredient Test', () => {
         expect(Ingredient).to.be.a('function');
     })
 
-    describe('Ingredient Test', () => {
-        it('Should be a function', () => {
-            expect(Ingredient).to.be.a('function');
-        })
-        it('Should be an instance of Ingredient', () => {
-            const ingredient = new Ingredient(ingredientsData[0])
-            expect(ingredient).to.be.an.instanceOf(Ingredient)
-        })
+    it('Should be an instance of Ingredient', () => {
+        const ingredient = new Ingredient(ingredientsData[0])
+        expect(ingredient).to.be.an.instanceOf(Ingredient)
     })
 })
