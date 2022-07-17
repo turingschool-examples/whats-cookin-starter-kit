8752b135dc36a1587a995890860f18fd1bcf868a
Cole Stephenson <colestephenson94@gmail.com>
2022-07-17 11:43:02 -0600

    Style previously deep fried image in recipe view

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -49,8 +49,8 @@ class Recipe {
     const sumTotalCost = multCostByAmt.reduce((sum, cost) => {
       return sum + cost;
     }, 0)
-
-    return (sumTotalCost / 100)
+     const parsedSum = parseFloat((sumTotalCost/100).toFixed(2));
+    return parsedSum;
   }
 
   returnRecipeInstructions() {
