0312c0af29daa6fef85f83490c11ae147bb65af6
Nicole Forseth <forseth.nico@gmail.com>
2022-07-17 22:45:51 -0600

    Reconfigure API calls to  separate functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,24 +1,30 @@
 // Your fetch requests will live here!
-export const apiCalls = {
+const getIngredientData = () => {
+    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
+    .then(response => response.json())
+    .catch(error => console.log('API error: ${error.message}'));
+}
 
-  const recipePromise = fetchApiData("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
-  const ingredientPromise = fetchApiData("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
-  const userPromise = fetchApiData("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+  const getRecipeData = () => {
+    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+    .then(response => response.json())
+    .catch(error => console.log('API error: ${error.message}'));
+  }
 
-  fetchApiData(url) {
-    return fetch(url)
-      .then(response => response.json())
-      .catch(error => console.log('API error: ${error.message}'));
-  },
+  const getUserData = () => {
+    return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+    .then(response => response.json())
+    .catch(error => console.log('API error: ${error.message}'));
+  }
 
-  getAllData() {
-    const result =  Promise.all([recipePromise, ingredientPromise, userPromise])
+  const getAllData = () => {
+    const result =  Promise.all([getRecipeData(), getIngredientData(), getUserData()])
       .then(responses => {
         return responses;
       })
-    return result;  
+    return result;
   }
-}
 
 
+export default { getAllData }
 console.log('I will be a fetch request!')
--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,5 @@
 import './styles.css';
 import apiCalls from './apiCalls';
-import { fetchApiData } from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 import RecipeRepository from './classes/RecipeRepository';
