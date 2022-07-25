1cee6047e49dd485e782f4472a02f6ddd5b2822d
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-25 16:24:43 -0600

    Refactored sample data to 2 recipes

--- src/sample-data/recipe-sample-data.js
+++ src/sample-data/recipe-sample-data.js
@@ -60,27 +60,6 @@ const recipeData =
             "amount": 24,
             "unit": "servings"
             }
-        },
-        {
-        "id": 10019903,
-        "quantity": {
-            "amount": 2,
-            "unit": "c"
-            }
-        },
-        {
-        "id": 1145,
-        "quantity": {
-            "amount": 0.5,
-            "unit": "c"
-            }
-        },
-        {
-        "id": 2050,
-        "quantity": {
-            "amount": 0.5,
-            "unit": "tsp"
-            }
         }
     ],
     "instructions": 
@@ -188,27 +167,6 @@ const recipeData =
             "amount": 24,
             "unit": "ounce"
             }
-        },
-        {
-        "id": 1102047,
-        "quantity": {
-            "amount": 4,
-            "unit": "servings"
-            }
-        },
-        {
-        "id": 16124,
-        "quantity": {
-            "amount": 1,
-            "unit": "tablespoon"
-            }
-        },
-        {
-        "id": 1016168,
-        "quantity": {
-            "amount": 1,
-            "unit": "tablespoon"
-            }
         }
     ],
     "instructions": 
@@ -225,115 +183,7 @@ const recipeData =
     "main dish",
     "dinner"
     ]
-    },
-    {
-    "id": 412309,
-    "image": "https://spoonacular.com/recipeImages/412309-556x370.jpeg",
-    "ingredients": 
-    [
-        {
-        "id": 1002030,
-        "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-            }
-        },
-        {
-        "id": 19334,
-        "quantity": {
-            "amount": 8,
-            "unit": "tablespoons"
-            }
-        },
-        {
-        "id": 1001,
-        "quantity": {
-            "amount": 2,
-            "unit": "cups"
-            }
-        },
-        {
-        "id": 4582,
-        "quantity": {
-            "amount": 4,
-            "unit": "servings"
-            }
-        },
-        {
-        "id": 2031,
-        "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-            }
-        },
-        {
-        "id": 5100,
-        "quantity": {
-            "amount": 1,
-            "unit": "pound"
-            }
-        },
-        {
-        "id": 2009,
-        "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-            }
-        },
-        {
-        "id": 1022020,
-        "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-            }
-        },
-        {
-        "id": 6168,
-        "quantity": {
-            "amount": 8,
-            "unit": "cups"
-            }
-        },
-        {
-        "id": 9176,
-        "quantity": {
-            "amount": 0.5,
-            "unit": "cup"
-            }
-        },
-        {
-        "id": 2026,
-        "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-            }
-        },
-        {
-        "id": 1042047,
-        "quantity": {
-            "amount": 1.5,
-            "unit": "tablespoons"
-            }
-        },
-        {
-        "id": 1042047,
-        "quantity": {
-            "amount": 4,
-            "unit": "teaspoons"
-            }
-        }
-    ],
-    "instructions": [
-    {
-    "instruction": "Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.",
-    "number": 1
-    }
-    ],
-    "name": "Dirty Steve's Original Wing Sauce",
-    "tags": [
-    "sauce"
-    ]
-    }
+  }
 ]
 
 
