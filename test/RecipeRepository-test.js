ad548917ddb6ec5ea42844c828933bee30883df7
Cole Stephenson <colestephenson94@gmail.com>
2022-07-26 13:09:51 -0600

    Refactor User-test to account for changes to filter functions (filters regardless of caps)

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -36,18 +36,18 @@ describe('RecipeRepository', () => {
     expect(tagResults[0].id).to.deep.equal(412309);
   })
 
-  it('should be able to filter recipes by name regardless of caps usage', () => {
-    let nameResultsWithCapsAsArg = recipeRepository.listRecipeNames("LOADED CHOCOLATE CHIP PUDDING COOKIE CUPS");
-    expect(nameResultsWithCapsAsArg[0].name).to.equal("Loaded Chocolate Chip Pudding Cookie Cups");
+  it('should be able to filter recipes by tag regardless of caps usage', () => {
+    let tagResultsWithCapsAsArg = recipeRepository.listRecipeTags("STARTER");
+    expect(tagResultsWithCapsAsArg[0].tags).to.include("starter");
   })
-
 
   it('should have a filtered list of recipes based on a name', () => {
     expect(nameResults[0].name).to.deep.equal("Loaded Chocolate Chip Pudding Cookie Cups");
   })
 
-  it('should be able to filter recipes by tag regardless of caps usage', () => {
-    let tagResultsWithCapsAsArg = recipeRepository.listRecipeTags("STARTER");
-    expect(tagResultsWithCapsAsArg[0].tags).to.include("starter");
+  it('should be able to filter recipes by name regardless of caps usage', () => {
+    let nameResultsWithCapsAsArg = recipeRepository.listRecipeNames("LOADED CHOCOLATE CHIP PUDDING COOKIE CUPS");
+    expect(nameResultsWithCapsAsArg[0].name).to.equal("Loaded Chocolate Chip Pudding Cookie Cups");
   })
+
 })
