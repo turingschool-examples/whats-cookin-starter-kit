fe5c705823900fb6c1947db8ac480cd29c3873a6
Jeffrey Cook <cookturingedu@gmail.com>
2022-07-27 18:44:18 -0400

    Add catch method to fetch promise

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,15 @@
 export const apiCalls = {
-    fetchData() {
-        const result = Promise.all([apiCalls.getAllData('recipes'), apiCalls.getAllData('ingredients'), apiCalls.getAllData('users')])
-            .then(responses => {
-                return responses
-            });
-        // console.log(result);
-        return result;
-    },
-
-    getAllData(endpoint) {
-        const root = 'http://localhost:3001/api/v1/'
-        const url = `${root}${endpoint}`
-        return fetch(url)
-        .then(response => response.json());
-            }
-        }
-
-    
-
+  fetchData() {
+    const result = Promise.all([apiCalls.getAllData('recipes'), apiCalls.getAllData('ingredients'), apiCalls.getAllData('users')])
+    .then(responses => {
+      return responses
+    });
+    return result;
+  },
+  getAllData(endpoint) {
+    const root = 'http://localhost:3001/api/v1/'
+    const url = `${root}${endpoint}`
+    return fetch(url)
+    .then(response => response.json());
+  }
+}
