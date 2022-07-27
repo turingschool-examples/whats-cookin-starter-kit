3781fe92c6e8fd6eea622adb504dfc40caa19024
Anthony Shellman <atshellman@gmail.com>
2022-07-27 15:02:48 -0600

    Add method and testing for returnIngredientNamesAndAmounts ; test on line 168 needs adjustment

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,12 +1,41 @@
+// import usersData from '../data/users'
+import ingredientsData from '../data/ingredients'
+
 class Pantry {
-  constructor() {
-    this.pantry = pantry;
+  constructor(user) {
+    this.name = user.name;
+    this.id = user.id;
+    this.pantry = user.pantry;
+    this.recipesToCook = user.recipesToCook;
   }
+
+  returnIngredientNamesAndAmounts() {
+  // A method that takes in pantry data and ingredient data, 
+// and returns an array of strings, where the strings are the 
+// ingredient names and amounts on hand.
+const test = this.pantry.reduce((acc, item) => {
+  let currentIngredient = item.ingredient;
+  ingredientsData.forEach(ingredient => {
+    if (ingredient.id === currentIngredient) {
+      acc.push(`
+  ${ingredient.name}: ${item.amount}`) 
+    }
+  })
+  return acc;
+  }, [])
+  return test.join();
+  }
+
+  // checkIngredients() {
+  //   // Do stuff
+  // }
 }
 
 //check pantry/how many items user has in their pantry
 //determine name of item/missing ingredient
 
 
-//module.exports = Pantry;
-//export default Recipe;
+
+
+// module.exports = Pantry;
+export default Pantry;
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,56 +1,201 @@
 import { expect } from 'chai';
 import Pantry from '../src/classes/Pantry';
+import User from '../src/classes/User'
 
-beforeEach(function() {
 
-});
+
 
 describe('Pantry', () => {
+
+      let usersData;
+      let user;
+      let usersData2;
+      let user2;
+      let recipeData;
+      let pantry;
+      let pantry2;
+      let ingredientsData;
+
+    beforeEach( () => {
+    usersData =
+    {
+      "name": "Saige O'Kon",
+      "id": 1,
+      "pantry": [
+        {
+          "ingredient": 11297,
+          "amount": 4
+        },
+        {
+          "ingredient": 1082047,
+          "amount": 10
+        },
+        {
+          "ingredient": 20081,
+          "amount": 5
+        }
+      ]
+    }
+
+    usersData2 =
+    {
+      "name": "Ephraim Goyette",
+      "id": 2,
+      "pantry": [
+    {
+      "ingredient": 6150,
+      "amount": 3
+    },
+    {
+      "ingredient": 1032009,
+      "amount": 7
+    },
+    {
+      "ingredient": 1082047,
+      "amount": 8
+    }
+    ]
+    }
+
+    recipeData = [
+    {
+    "id": 595736,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [
+      {
+        "id": 20081,
+        "quantity": {
+          "amount": 1.5,
+          "unit": "c"
+        }
+      },
+    ],
+    "instructions": [
+      {
+        "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+        "number": 1
+      },
+    ],
+    "name": "Dirty Steve's Original Wing Sauce",
+    "tags": [
+      "sauce"
+    ]
+    },
+
+    {
+    "id": 593432,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [
+      {
+        "id": 20081,
+        "quantity": {
+          "amount": 1.5,
+          "unit": "c"
+        }
+      },
+    ],
+    "instructions": [
+      {
+        "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+        "number": 1
+      },
+    ],
+    "name": "Dirty Pete's Original Wing Powder",
+    "tags": [
+      "sauce"
+    ]
+    }
+    ]
+
+    ingredientsData = [
+    {
+      "id": 11297,
+      "name": "flat leaf parsley leaves",
+      "estimatedCostInCents": 1030
+    },
+    {
+      "id": 1082047,
+      "name": "kosher salt",
+      "estimatedCostInCents": 972
+    },
+    {
+      "id": 20081,
+      "name": "wheat flour",
+      "estimatedCostInCents": 142
+    },
+    {
+      "id": 6150,
+      "name": "bar b que sauce",
+      "estimatedCostInCents": 966
+    },
+    {
+      "id": 1032009,
+      "name": "dried red chili",
+      "estimatedCostInCents": 1015
+    },
+    ]
+
+    user = new User(usersData);
+    user2 = new User(usersData2);
+    pantry = new Pantry(user);
+    pantry2 = new Pantry(user2);
+    });
+
     it('should be a function', function() {
         expect(Pantry).to.be.a('function');
       })
 
-      it('should be able to tell the user they have enough ingredients for a given recipe', function() {
+      it.skip('should be able to tell the user they have enough ingredients for a given recipe', function() {
+      // console.log(user.pantry)
+      // user.addRecipeToCook(recipeData[0]);
+      // console.log(user.recipesToCook[0].ingredients)
+      // expect(user.checkIngredients()).to.equal('You can cook it!')
+      // Check recipe to see which ingeredients are needed
+      // We want to check the users pantry for current ingeredients
+      // If the ingredients that are in the pantry are greater than equal to the 
+      // ingredients needed in the recipe, then they can cook the meal - let user know
         expect;
       })
 
-      it('should be able to tell the user they do not have enough ingredients for a given recipe', function() {
+      it.skip('should be able to tell the user they do not have enough ingredients for a given recipe', function() {
         expect;
       })
 
-      it('should be able to determine the amount of missing ingredients still needed to cook a given recipe', function() {
+      it.skip('should be able to determine the amount of missing ingredients still needed to cook a given recipe', function() {
         expect;
       })
 
-      it('should be able to show a user what ingredients exist inside the pantry', function() {
+      it('should be able to show a user what ingredients and amounts exist inside their pantry', function() {
+        // console.log(pantry.returnIngredientNamesAndAmounts())
+        const values = "  \nflat leaf parsley leaves: 4,\n  kosher salt: 10,\n  wheat flour: 5"
+        expect(pantry.returnIngredientNamesAndAmounts()).to.equal(values);
+      })
+
+      it.skip('should allow user to check the list of recipes to cook to see if there are enough ingredients', function() {
         expect;
       })
 
-      it('should allow user to check the list of recipes to cook to see if there are enough ingredients', function() {
+      it.skip('should be able to tell the user what ingredients are still needed if there are not enough in the pantry', function() {
         expect;
       })
 
-      it('should be able to tell the user what ingredients are still needed if there are not enough in the pantry', function() {
+      it.skip('should', function() {
         expect;
       })
 
-      it('should', function() {
+      it.skip('should not allow a user to cook a recipe if there are not sufficient ingredients', function() {
         expect;
       })
 
-      it('should not allow a user to cook a recipe if there are not sufficient ingredients', function() {
+      it.skip('should not allow a user to cook a recipe if there are not sufficient ingredients', function() {
         expect;
       })
 
-      it('should not allow a user to cook a recipe if there are not sufficient ingredients', function() {
+      it.skip('should remove used ingredients from the pantry after cooking a recipe', function() {
         expect;
       })
 
-      it('should remove used ingredients from the pantry after cooking a recipe', function() {
-        expect;
-      })
-
-      it('should allow a user to add more ingredients', function() {
+      it.skip('should allow a user to add more ingredients', function() {
         expect;
       })
 })
