269aa244440713ae2e1d6fb19b94b786b895aee6
Cole Stephenson <colestephenson94@gmail.com>
2022-07-29 08:57:25 -0600

    Refactor pantry method for returning if recipe can be cooked

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -24,20 +24,60 @@ class Pantry {
   }
 
   returnIfRecipeIsCookable(recipe) {
+    const neededIngredients = [];
+    const neededIngredientNames = [];
+
     const isCookable = recipe.ingredients.reduce((arr, ingredient) => {
       this.pantry.forEach(item => {
         if (ingredient.id === item.ingredient && ingredient.quantity.amount <= item.amount) {
-          arr.push(ingredient)
+          arr.push(ingredient);
+        }
+
+      })
+      return arr
+    }, [])
+
+    recipe.ingredients.forEach(ingredient => {
+      if (!isCookable.includes(ingredient)) {
+        neededIngredients.push(ingredient)
+      }
+    })
+
+    const sumNeededIngredients = neededIngredients.reduce((arr, ingredient) => {
+      let sumNeeded = 0
+      if (!this.pantry.includes(ingredient)) {
+        arr.push(ingredient.quantity.amount)
+      }
+      this.pantry.forEach((item, index) => {
+        if (ingredient.id === item.ingredient) {
+          sumNeeded = ingredient.quantity.amount - item.amount;
+          arr.push(sumNeeded)
         }
       })
       return arr
     }, [])
+
+    neededIngredients.forEach(ingredient => {
+      ingredientsData.forEach(item => {
+        if (item.id === ingredient.id) {
+          neededIngredientNames.push(item.name)
+        }
+      })
+    })
+
+    let formattedNameAndSum = neededIngredientNames.map((name, index) => {
+      return `${name}: ${sumNeededIngredients[index]}`
+    })
+
+
     if (isCookable.length === recipe.ingredients.length) {
       return "Yes! You can cook this recipe";
     } else {
-      return "Sorry! You don't have enough to cook this recipe";
+      return `Sorry! You don't have enough ingredients to cook ${recipe.name}. you need: ${formattedNameAndSum.join("\n")}.`;
     }
   }
+
+
 
 }
 
