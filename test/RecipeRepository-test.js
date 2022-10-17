a6a4d3ed23a8edaf3b5bbef2060b425c9972ab55
Courtney Lippman <courtney.lippman@colorado.edu>
2022-10-17 14:47:16 -0600

    Add basic test structure for recipeRepository class.

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,22 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import  from '../src/data/recipes';
 
 describe('Recipe', () => {
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
-})
+
+  it('Should have a parameter to take in recipe data', () => {
+    expect(RecipeRepository.allRecipes).to.equal(recipies);
+  });
+
+  it('Should have a method that creates a filtered list of recipies based on a tag', () => {
+    expect().to.equal();
+  });
+
+  it('Should have a method that creates a filtered list of recipies based on its name', () => {
+    expect().to.equal();
+  });
+
+});
