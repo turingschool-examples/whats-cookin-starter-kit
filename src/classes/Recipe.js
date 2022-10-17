fd3bd1007e9c4e4f422cb3a4ed8ed7b314eca574
Lauren Black <laurenblack5280@gmail.com>
2022-10-17 15:24:17 -0600

    add sample data to test

---
+++ src/classes/Recipe.js
@@ -0,0 +1,16 @@
+class Recipe {
+  constructor() {
+    
+  }
+}
+// A Recipe represents one recipe object.
+//
+// It should hold on to all its information
+//  (provided in the data file).
+// It should have methods to:
+// Determine the names of ingredients needed
+// Get the cost of its ingredients
+// Return its directions / instructions
+// export default Recipe
+
+export default Recipe
