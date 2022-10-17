4b1632b626c7b013fc78d5879af1a9c3eb16e618
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-17 12:49:24 -0600

    Create sample data

---
+++ src/sampleData/recipeSample.js
@@ -0,0 +1,185 @@
+const recipeSampleData = [
+  {
+    id: 12345,
+    image: "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    ingredients: [
+      {
+        id: 20081,
+        quantity: {
+          amount: 1.5,
+          unit: "c",
+        },
+      },
+      {
+        id: 18372,
+        quantity: {
+          amount: 0.5,
+          unit: "tsp",
+        },
+      },
+      {
+        id: 1123,
+        quantity: {
+          amount: 1,
+          unit: "large",
+        },
+      },
+      {
+        id: 2047,
+        quantity: {
+          amount: 0.5,
+          unit: "tsp",
+        },
+      },
+      {
+        id: 1012047,
+        quantity: {
+          amount: 24,
+          unit: "servings",
+        },
+      },
+    ],
+    instructions: [
+      {
+        instruction: "Step 1",
+        number: 1,
+      },
+      {
+        instruction: "Step 2",
+        number: 2,
+      },
+      {
+        instruction: "Step 3",
+        number: 3,
+      },
+      {
+        instruction: "Step 4",
+        number: 4,
+      },
+    ],
+    name: "Chocolate Chip Cookies",
+    tags: ["antipasti", "starter", "snack", "appetizer"],
+  },
+  {
+    id: 23456,
+    image: "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    ingredients: [
+      {
+        id: 20083,
+        quantity: {
+          amount: 1.5,
+          unit: "c",
+        },
+      },
+      {
+        id: 18876,
+        quantity: {
+          amount: 0.5,
+          unit: "tsp",
+        },
+      },
+      {
+        id: 93847,
+        quantity: {
+          amount: 1,
+          unit: "large",
+        },
+      },
+      {
+        id: 23897,
+        quantity: {
+          amount: 0.5,
+          unit: "tsp",
+        },
+      },
+      {
+        id: 10120,
+        quantity: {
+          amount: 24,
+          unit: "servings",
+        },
+      },
+    ],
+    instructions: [
+      {
+        instruction: "Step 1",
+        number: 1,
+      },
+      {
+        instruction: "Step 2",
+        number: 2,
+      },
+      {
+        instruction: "Step 3",
+        number: 3,
+      },
+      {
+        instruction: "Step 4",
+        number: 4,
+      },
+    ],
+    name: "Chocolate Chip Pancakes",
+    tags: ["antipasti", "starter", "appetizer", "sweet"],
+  },
+  {
+    id: 34567,
+    image: "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    ingredients: [
+      {
+        id: 98749,
+        quantity: {
+          amount: 1.5,
+          unit: "c",
+        },
+      },
+      {
+        id: 34980,
+        quantity: {
+          amount: 0.5,
+          unit: "tsp",
+        },
+      },
+      {
+        id: 23948,
+        quantity: {
+          amount: 1,
+          unit: "large",
+        },
+      },
+      {
+        id: 203942,
+        quantity: {
+          amount: 0.5,
+          unit: "tsp",
+        },
+      },
+      {
+        id: 243982,
+        quantity: {
+          amount: 24,
+          unit: "servings",
+        },
+      },
+    ],
+    instructions: [
+      {
+        instruction: "Step 1",
+        number: 1,
+      },
+      {
+        instruction: "Step 2",
+        number: 2,
+      },
+      {
+        instruction: "Step 3",
+        number: 3,
+      },
+      {
+        instruction: "Step 4",
+        number: 4,
+      },
+    ],
+    name: "Chocolate Chip Cookies",
+    tags: ["antipasti", "starter", "snack", "appetizer"],
+  },
+];
