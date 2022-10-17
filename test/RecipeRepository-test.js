460f2cc581e8fa2051eff01a4f9ae64629f20131
Courtney Lippman <courtney.lippman@colorado.edu>
2022-10-17 17:04:44 -0600

    Add webpack connecting info to test and class files.

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,22 +1,31 @@
 import { expect } from 'chai';
+import recipeRepositorySampleData from '../sampleSets/recipeRepositorySample';
 import RecipeRepository from '../src/classes/RecipeRepository';
-import  from '../src/data/recipes';
 
 describe('Recipe', () => {
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
 
+  beforeEach(function() {
+    const allRecipes = new RecipeRepository(recipeRepositorySampleData);
+  });
+
   it('Should have a parameter to take in recipe data', () => {
-    expect(RecipeRepository.allRecipes).to.equal(recipies);
+    const allRecipes = new RecipeRepository(recipeRepositorySampleData);
+
+    expect(allRecipes.listOfRecipes).to.equal(recipeRepositorySampleData);
   });
 
   it('Should have a method that creates a filtered list of recipies based on a tag', () => {
-    expect().to.equal();
+
+    expect(filterByTag('snack')).to.equal(recipeRepositorySampleData[0]);
   });
 
   it('Should have a method that creates a filtered list of recipies based on its name', () => {
-    expect().to.equal();
+
+    expect(filterByName('Maple Dijon Apple Cider Grilled Pork Chops')).to.equal(recipeRepositorySampleData[1]);
   });
 
 });
