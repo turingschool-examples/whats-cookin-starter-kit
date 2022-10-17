08bf29646cf8e0c15bdf8138ee1ae577e0ee95f0
Brett Kuhn <brettkuhn87@gmail.com>
2022-10-17 15:36:09 -0600

    Added before each block to tests to include test variables (variables not written yet)

--- test/User-test.js
+++ test/User-test.js
@@ -3,6 +3,13 @@ import User from '../src/classes/User';
 
 describe('User', () => {
 
+  //let: 👉 test variables
+
+  beforeEach('define variables for test suite', function() {
+    //define variables in here
+
+  });
+
   it('Should be a function', () => {
     expect(User).to.be.a('function');
   });
