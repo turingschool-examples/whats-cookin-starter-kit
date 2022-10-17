fef34695b350af271e33846ce7ec2730f47b9f7a
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-10-17 16:08:39 -0400

    Add ingrediants class file

---
+++ test/Ingrediants.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const expect = chai.expect;
+const Ingredients = require('..src/classes/ingredients.js'
+
+describe("Ingriendents",function(){
+    beforeEach(function (){
+
+    })
+})
+
