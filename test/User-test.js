67183425b49349c30ba7fa1d1d37b4eadc7b2868
Brett Kuhn <brettkuhn87@gmail.com>
2022-10-17 15:24:21 -0600

    Added describe block and first test to user-test.js

--- test/User-test.js
+++ test/User-test.js
@@ -1,3 +1,10 @@
 import { expect } from 'chai';
-import User from '../src/classes/RecipeRepository';
+import User from '../src/classes/User';
 
+describe('User', () => {
+
+  it('Should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
+})
