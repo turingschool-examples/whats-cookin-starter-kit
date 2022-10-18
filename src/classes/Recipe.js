1280aec82dba88aa51fac21179e807d270589dc5
Eleanor Ruth <ellie.ruth@me.com>
2022-10-17 18:16:36 -0600

    Create properties of Recipe class

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -3,7 +3,11 @@ import recipeData from '../data/recipes';
 class Recipe {
     constructor(data) {
         this.id = data.id;
-        this.name = "eleanor"
+        this.image = data.image;
+        this.ingredients = data.ingredients;
+        this.instructions = data.instructions;
+        this.name = data.name;
+        this.tags = data.tags
     }
 }
 
