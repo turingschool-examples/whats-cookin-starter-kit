f04486db775ada5e70c231c11d3e224c481b1a7f
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-10-17 19:39:52 -0600

    Create determine ingredients method

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,4 +1,5 @@
 import recipeData from '../data/recipes';
+import {sampleIngredientsData} from '../data/sample-data';
 
 class Recipe {
     constructor(data) {
@@ -7,12 +8,18 @@ class Recipe {
         this.ingredients = data.ingredients;
         this.instructions = data.instructions;
         this.name = data.name;
-        this.tags = data.tags
-        this.ingredientsList 
+        this.tags = data.tags;
+        this.ingredientsList = [];
     }
     determineIngredients() {
-        
-    }
-}
-
+        this.ingredientsList = sampleIngredientsData.reduce((acc, ingredient) => {
+            this.ingredients.forEach((id) => {
+                if (id.id === ingredient.id)
+                  {acc.push(ingredient.name)};
+              });
+              return this.ingredientsList = acc;
+          }, []);
+            return this.ingredientsList;
+          }
+        }
 export default Recipe;
