c63819cc14c61bb95d34710216e27a3e3f991fd6
Brett Kuhn <brettkuhn87@gmail.com>
2022-10-17 20:19:35 -0600

    Added constructor, comment for future methods

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,6 +1,14 @@
 class User {
-  
+  constructor(name, id, pantry) {
+    this.name = name;
+    this.id = id;
+    this.pantry = pantry;
+    this.recipesToCook = [];
+  }
+
+
 }
 
+//fxn to generate random user, add name, add/create/update pantry ?, add recipes to cook/remove
 
-export default User;
+module.exports = User;
