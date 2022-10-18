2bbb87f1e60458812077c8a86e67d1b14f1b98a5
Courtney Lippman <courtney.lippman@colorado.edu>
2022-10-18 07:21:33 -0600

    Edit syntax and add beforeEach.

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -3,19 +3,22 @@ import recipeRepositorySampleData from '../sampleSets/recipeRepositorySample';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('Recipe', () => {
+  let allRecipes;
+
+  beforeEach(function() {
+    allRecipes = new RecipeRepository(recipeRepositorySampleData);
+  })
 
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
 
   it('Should have a parameter to take in recipe data', () => {
-    const allRecipes = new RecipeRepository(recipeRepositorySampleData);
 
     expect(allRecipes.listOfRecipes).to.equal(recipeRepositorySampleData);
   });
 
-  it('Should have a method that creates a filtered list of recipies based on a tag', () => {
-    const allRecipes = new RecipeRepository(recipeRepositorySampleData);
+  it('Should have a method that creates a filtered list of recipes based on a tag', () => {
 
     expect(allRecipes.filterByTag('hello')).to.deep.equal([]);
     expect(allRecipes.filterByTag('snack')).to.deep.equal([recipeRepositorySampleData[0]]);
@@ -23,8 +26,7 @@ describe('Recipe', () => {
 
   });
 
-  it('Should have a method that creates a filtered list of recipies based on its name', () => {
-    const allRecipes = new RecipeRepository(recipeRepositorySampleData);
+  it('Should have a method that creates a filtered list of recipes based on its name', () => {
     
     expect(allRecipes.filterByName('Hello')).to.deep.equal([]);
     expect(allRecipes.filterByName('Maple Dijon Apple Cider Grilled Pork Chops')).to.deep.equal([recipeRepositorySampleData[1]]);
