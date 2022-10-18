ab7b55f3abfe89743e2dc8206c5f15cc1cb33f00
Angela Wirth <angiewirth92@gmail.com>
2022-10-18 10:07:52 -0400

    Add beforeEach and first few tests

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,55 @@
 import { expect } from 'chai';
+import { beforeEach } from 'mocha';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
+
 describe('Recipe', () => {
+  beforeEach(() => {
+    var recipe1 = new RecipeRepository ([
+      {
+        "id": 595736,
+        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+        "ingredients": [
+          {
+            "id": 20081,
+            "quantity": {
+              "amount": 1.5,
+              "unit": "c"
+            }
+          },
+          {
+            "id": 18372,
+            "quantity": {
+              "amount": 0.5,
+              "unit": "tsp"
+            }
+          },
+          {
+            "id": 1123,
+            "quantity": {
+              "amount": 1,
+              "unit": "large"
+            }
+          }]
+      )
+  })
+
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
+  it('should instatiate a new Recipe', () => {
+    expect(recipe1).to.equal(new RecipeRepository)
+  })
+
+  it('should have an id', () => {
+    expect(recipe1).to.equal(RecipeRepository.id,'[{"id": 595736}]')
+  })
+
+  it('should have an image', () => {
+    expect(recipe1).to.equak(RecipeRepository.image, '[{"image": "https://spoonacular.com/recipeImages/595736-556x370.jpg"}]')
+  })
+
+
 })
