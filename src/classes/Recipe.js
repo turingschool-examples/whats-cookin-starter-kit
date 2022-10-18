ec227796b2b279f09104597232affc6fc1ce134e
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-10-17 20:05:56 -0600

    creates recipie class and test file

---
+++ src/classes/Recipe.js
@@ -0,0 +1,12 @@
+class Recipe{
+    constructor(id,image,ingerdients,instructions,name,tags) {
+        this.id = id
+        this.image = image
+        this.ingerdients = ingerdients
+        this.instructions = instructions
+        this.name = name
+        this.tags = tags 
+    }
+}
+
+export default Recipe;
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
-describe('Recipe', () => {
+describe('RecipeRepository', () => {
   let recipeData, recipeRepo;
 
   beforeEach(() => {
