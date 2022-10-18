572aa51ecb2d5c080ae2a52dc6ebad8ac54a4580
Eleanor Ruth <ellie.ruth@me.com>
2022-10-18 17:55:06 -0600

    Add recipe property test

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,27 +1,36 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
-import sampleIngredientsData from '../src/data/sample-data';
-import sampleUsersData from '../src/data/sample-data';
-import samplesRecipeData from '../src/data/sample-data';
-
-
+import Recipe from '../src/classes/Recipe';
+import { sampleIngredientsData, sampleUsersData, sampleRecipeData } from '../src/data/sample-data';
 
 describe('RecipeRepo', () => {
+  sampleIngredientsData
+  sampleUsersData
+  sampleRecipeData
+  let recipe
+  let recipeRepository
+
+  beforeEach(() => {
+    recipe = new Recipe(sampleRecipeData[0]);
+    recipeRepository = new RecipeRepository(recipe)
+    sampleIngredientsData;
+    sampleUsersData;
+    sampleRecipeData;
+  });
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+  it('Should have a property of recipe', () => {
+    expect(recipeRepository.recipe).to.be.an.instanceOf(Recipe);
+    expect(recipeRepository.recipe).to.deep.equal(recipe);
+  });
 })
 
-//properties - parameter for recipe data
-//methods - filtered list
-//properties = id, image, incredients, recipe objects(parameter), cost, directions/instructions, ingredients
 
 // A RecipeRepository should hold onto all Recipe objects.
-
 
 // It should have methods to determine:
 // A filtered list of recipes based on a tag. (Extension option: filtering by multiple tags)
 // A filtered list of recipes based on its name. (Extension option: filtering by name or ingredients)
-// A Recipe represents one recipe object. - instances of recipe
-
 
