b67a38b27e6b12455244deb92a8e73180edbdc1a
Eleanor Ruth <ellie.ruth@me.com>
2022-10-18 18:49:42 -0600

    Write filterName method

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,10 +1,24 @@
 import Recipe from "./Recipe";
+import { recipeData } from "../data/recipes";
 
 class RecipeRepository {
-  constructor(recipe) {
-    this.recipe = recipe
-    // One class to get you started!
+  constructor(recipes) {
+    this.recipes = new Recipe(recipes)
+    //console.log(this.recipes)
+  }
+  filterTag(tag) {
+    return this.recipes.filter((recipe) => {
+      return recipe.tags.includes(tag)
+    })
+  }
+  filterName(name) {
+    return this.recipes.filter((recipe) => {
+      return recipe.name === name
+    })
   }
 }
+
+// let recRep = new RecipeRepository(recipeData)
+// console.log(recRep)
 
 export default RecipeRepository;
