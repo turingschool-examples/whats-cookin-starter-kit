a51a1c1754752083c1607becec6570909430bcb2
Courtney Lippman <courtney.lippman@colorado.edu>
2022-10-19 10:42:58 -0600

    Refactor semicolons.

--- src/classes/User.js
+++ src/classes/User.js
@@ -5,41 +5,43 @@ class User {
     this.id = id;
     this.pantry = pantry;
     this.recipesToCook = [];
-  }
+  };
+
   generateRandomUser(usersList) {
     // Takes argument of the array of users data from either users.js or the fetch() request
-    const totalUsers = usersList.length
+    const totalUsers = usersList.length;
 
     function createRandomIndex(totalUsers) {
       return Math.floor(Math.random() * totalUsers);
-    }
+    };
     
     let randomIndex = createRandomIndex(totalUsers);
 
     this.name = usersList[randomIndex]["name"];
     this.id = usersList[randomIndex]["id"];
     this.pantry = usersList[randomIndex]["pantry"];
-  }
+  };
+
   addRecipe(recipeID, allRecipes) { 
     // 🚨 Important to set up dom/HTML/CSS to have IDs so we can use that data to match here below
     this.recipesToCook.push(allRecipes.listOfRecipes.find(recipe => recipe["id"] === recipeID));
-  }
+  };
+
   removeRecipe(recipeID) {
-    const index = this.recipesToCook.findIndex(recipe => recipe["id"] === recipeID)
-    this.recipesToCook.splice(index, 1)
-  }
+    const index = this.recipesToCook.findIndex(recipe => recipe["id"] === recipeID);
+    this.recipesToCook.splice(index, 1);
+  };
+
   filterByTag(tag) {
     return this.recipesToCook.filter(recipe => recipe["tags"].includes(tag));
-  }
+  };
+
   filterByName(name) {
     return this.recipesToCook.filter(recipe => recipe["name"] === name);
-  }
-}
-
+  };
+};
 
 export default User;
-
-
 
 // ❓Do we need something that can create or manipulate the pantry
 //   property? Should this be based on the recipesToCook in some way?
