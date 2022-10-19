5a4885560caf97b5cc0872e300cbe23ddcc89c0c
Angie Staffieri <angie.staffieri@gmail.com>
2022-10-18 18:14:04 -0600

    Add the basic layout for the scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+//  IMPORTS LIVE HERE
 import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
@@ -5,4 +6,41 @@ import './images/turing-logo.png'
 
 
 
+
+
+//  QUERYSELECTORS LIVE HERE
+
+
+
+
+//  EVENT LISTENERS LIVE HERE
+
+
+
+
+// GLOBAL VARIABLES LIVE HERE
+
+
+
+
+
+
+// HELPER FUNCTIONS LIVE HERE
+
+
+
+
+
+
+//  PROMISES LIVE HERE
+
+
+
+
+
+
+
+//  ON LOAD FUNCTIONS LIVE HERE
+
 console.log('Hello world');
+ 
