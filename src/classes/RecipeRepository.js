8cef0a07e806123252db18e6aff0fdd036230c45
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-19 16:22:55 -0600

    Fixed module exports and wepback bugs

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,21 +3,23 @@ import Recipe from "../classes/Recipe";
 class RecipeRepository {
   constructor() {
     this.listOfAllRecipes = [];
-  };
+  }
 
   filterByTag(tag) {
-return this.listOfAllRecipes.filter(recipe => recipe['tags'].includes(tag));
-  };
+    return this.listOfAllRecipes.filter((recipe) =>
+      recipe["tags"].includes(tag)
+    );
+  }
 
   filterByName(name) {
-return this.listOfAllRecipes.filter(recipe => recipe.name === name);
-  };
+    return this.listOfAllRecipes.filter((recipe) => recipe.name === name);
+  }
 
   addRecipes(recipeData) {
-    this.listOfAllRecipes = recipeData.map(recipe => {
+    this.listOfAllRecipes = recipeData.map((recipe) => {
       return new Recipe(recipe);
     });
-  };
-};
+  }
+}
 
-module.exports = RecipeRepository;
+export default RecipeRepository;
