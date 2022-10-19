df7fbe29a9bf8c29cb9a62c10c00552066e759a0
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-18 19:24:09 -0600

    Add recipeRepo to merge

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,8 +1,36 @@
-class RecipeRepository {
-  constructor() {
+import { expect } from "chai";
+import recipeRepositorySampleData from "../sampleSets/recipeRepositorySample";
+import RecipeRepository from "../src/classes/RecipeRepository";
 
-    // One class to get you started!
-  }
-}
+describe("Recipe", () => {
+  let allRecipes;
 
-export default RecipeRepository;
+  beforeEach(function () {
+    allRecipes = new RecipeRepository(recipeRepositorySampleData);
+  });
+
+  it("Should be a function", () => {
+    expect(RecipeRepository).to.be.a("function");
+  });
+
+  it("Should have a parameter to take in recipe data", () => {
+    expect(allRecipes.listOfAllRecipes).to.equal(recipeRepositorySampleData);
+  });
+
+  it("Should have a method that creates a filtered list of recipes based on a tag", () => {
+    expect(allRecipes.filterByTag("hello")).to.deep.equal([]);
+    expect(allRecipes.filterByTag("snack")).to.deep.equal([
+      recipeRepositorySampleData[0],
+    ]);
+    expect(allRecipes.filterByTag("lunch")).to.deep.equal(
+      recipeRepositorySampleData
+    );
+  });
+
+  it("Should have a method that creates a filtered list of recipes based on its name", () => {
+    expect(allRecipes.filterByName("Hello")).to.deep.equal([]);
+    expect(
+      allRecipes.filterByName("Maple Dijon Apple Cider Grilled Pork Chops")
+    ).to.deep.equal([recipeRepositorySampleData[1]]);
+  });
+});
