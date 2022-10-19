c54ada00d1e1a8cbd1ec939822825a721917180c
Adam Hughes <adamjordanhughes@gmail.com>
2022-10-19 11:32:36 -0400

    Add complete Recipe class functionality with static data and without the ingredients class

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -8,18 +8,34 @@ class Recipe {
         this.instructions = recipe.instructions;
         this.name = recipe.name;
         this.tags = recipe.tags;
+
     }
+    
     ingredientsNeeded() {
         let ingredientsNeeded = [];
         this.ingredients.forEach((ingredient) => {
-            let ingredientsID = ingredient.id
-            let ingredientFound = ingredientsData.find( ing => ingredientsID === ing.id)
+            let ingredientFound = ingredientsData.find( ing => ingredient.id === ing.id)
             ingredientsNeeded.push(ingredientFound.name)
         })
-        console.log(ingredientsNeeded)
-        console.log(ingredientsNeeded.length)
         return ingredientsNeeded
     }
+
+    getIngredientsCost() {
+        let ingredientsNeededInfo = [];
+        this.ingredients.forEach((ingredient) => {
+            var info = ingredientsData.find( ing => ingredient.id === ing.id)
+            ingredientsNeededInfo.push({...info,...ingredient})
+        })
+        var totalIngredientCost = ingredientsNeededInfo.reduce(function(acc,item){
+            let ingredientCost = item.estimatedCostInCents * item.quantity.amount
+            return acc + ingredientCost
+        }, 0)
+       return totalIngredientCost
+    }
+
+    getInstructions() {
+        return this.instructions
+    }
 };
-  
+
 export default Recipe;
--- test/Recipe-tesing.js
+++ test/Recipe-tesing.js
@@ -267,6 +267,39 @@ describe('Recipe', function() {
     });
 
     it('should have a method that gives a list of the names of ingredients needed', function() {
-        expect(recipe.ingredientsNeeded()).to.equal(["wheat flour","bicarbonate of soda","eggs","sucrose","instant vanilla pudding","brown sugar","salt","fine sea salt","semi sweet chips","unsalted butter","vanilla"]);
+        expect(recipe.ingredientsNeeded()).to.deep.equal(["wheat flour","bicarbonate of soda","eggs","sucrose","instant vanilla pudding","brown sugar","salt","fine sea salt","semi sweet chips","unsalted butter","vanilla"]);
+    });
+
+    it('should have a method that returns the cost of the recipe', function() {
+        expect(recipe.getIngredientsCost()).to.deep.equal(17776);
+    });
+
+    it('should have a method that returns the recipe instructions', function() {
+        expect(recipe.getInstructions()).to.deep.equal([
+            {
+              "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+              "number": 1
+            },
+            {
+              "instruction": "Add egg and vanilla and mix until combined.",
+              "number": 2
+            },
+            {
+              "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+              "number": 3
+            },
+            {
+              "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
+              "number": 4
+            },
+            {
+              "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
+              "number": 5
+            },
+            {
+              "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
+              "number": 6
+            }
+          ]);
     });
   });
