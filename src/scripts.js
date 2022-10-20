301e6fc5d0c8115a5bdb8f487ca54acae72a6e16
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-20 17:08:56 -0600

    Add function random user generation

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@ import loadData from "./apiCalls";
 import RecipeRepository from "./classes/RecipeRepository";
 import Recipe from "./classes/Recipe";
 import Ingredient from "./classes/Ingredient-class";
+import User from "./classes/User";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/turing-logo.png";
 import "./images/AndrewProfile.png";
@@ -32,20 +33,22 @@ Promise.all([
   ingredientsData = data[1];
   recipeData = data[2];
 
-  createInstances(recipeData, ingredientsData);
+  createInstances(recipeData, ingredientsData, userData);
 
   let allRecipes = new RecipeRepository(recipeData);
 
   console.log("USER DAAAATA", userData);
   console.log("INGREEEEEDIENT", ingredientsData);
   console.log("RECIPEEEE", recipeData);
-  console.log(currentUser);
+  console.log("USSSSEEER", currentUser);
   console.log(allRecipes);
 });
 
-function createInstances(dataSet1, dataSet2) {
+function createInstances(dataSet1, dataSet2, dataSet3) {
   makeRecipesList(dataSet1);
   makeIngredientsList(dataSet2);
+  currentUser = new User();
+  currentUser.generateRandomUser(dataSet3.usersData);
 }
 
 function makeRecipesList(dataSet) {
