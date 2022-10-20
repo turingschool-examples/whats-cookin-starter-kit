adb2e8feab39de2acc2669a02a87011680c50b3b
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-10-19 22:05:48 -0600

    Refactor recipe class

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,39 +1,38 @@
+const Ingredient = require("./Ingredient");
+
 class Recipe {
-  constructor(recipeList) {
-    this.recipeList = recipeList
-
+  constructor(recipe) {
+    this.id = recipe.id;
+    this.name = recipe.name;
+    this.image = recipe.image;
+    this.ingredients = recipe.ingredients;
+    this.instructions = recipe.instructions;
+    this.tags = recipe.tags;
+  }
+  retrieveIngredients(ingredientsData) {
+    this.ingredients = this.ingredients.map((item) => {
+      const element = ingredientsData.find((ingr) => ingr.id === item.id);
+      return new Ingredient(element, item.quantity);
+    });
   }
 
-  getRecipeById(currentId){
-    let singleRecipe = this.recipeList.find((recipe) => {
-      return recipe.id === currentId
-    })
-    return singleRecipe
+  determineNames() {
+    const ingredientNames = this.ingredients.map((item) => {
+      return item.name;
+    });
+    return ingredientNames;
   }
 
-  getIngredientIds(currentId) {
-    let singleRecipe = this.recipeList.find((recipe) => {
-      return recipe.id === currentId
-    })
-    console.log('singleRecipe', singleRecipe.ingredients);
-    return singleRecipe.ingredients.map(ingredient => {
-      return ingredient.id
-    })
-    }
+  getCost() {
+    return this.ingredients.reduce((acc, currentItem) => {
+      const totalCost = acc + currentItem.countEstCost();
+      return totalCost;
+    }, 0);
+  }
 
-// estimatedCostInCents(id) {
-//     The price (estimatedCostInCents) of an ingredient is per unit.
-// For example, if flour is marked as 100 cents, and the recipe
-// calls for 1.5 cups, then it would cost 150 cents.
-// Similarly, if a tomato is $2, and the recipe requires 2 tomatoes, then the cost would be $4.
-//  }
-
-  getInstructions(currentId) {
-    let singleRecipe = this.recipeList.find((recipe) => {
-      return recipe.id === currentId
-    })
-    return singleRecipe.instructions
+  getInstructions() {
+    return this.instructions;
   }
 }
 
-export default Recipe
+module.exports = Recipe;
