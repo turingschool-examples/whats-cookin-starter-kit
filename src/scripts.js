1a06f2783ad9a7ec8c03376e385da08465991011
Gus Deribeaux <Gusderibeaux@gmail.com>
2022-10-20 18:21:51 -0400

    add querySlecectors and add html

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,38 @@
+
+//Imports
 import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+
+import Recipe from '../src/classes/Recipe';
+import Ingredients from '../src/classes/Ingredients';
+import RecipeRepository from '../src/classes/RecipeRepository';
+
+
+//QuerySelector
+const currentRecipeName = document.querySelector(".current-recipe-name")
+const currentRecipeImage = document.querySelector(".current-recipe-image")
+const currentRecipeIngredients = document.querySelector(".current-recipe-ingredients")
+const leftRandomImageCard = document.querySelector(".left-random-card")
+const middleRandomImageCard = document.querySelector(".middle-random-card")
+const rightRandomImageCard = document.querySelector(".right-random-card")
+
+
+
+let currentRecipe = 
+
+
+function loadHandler(){
+
+}
+
+function clickHandler(){
+    
+}
+
+
+
 
 
 
