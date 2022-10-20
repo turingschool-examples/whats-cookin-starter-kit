24887c1ea83bff5a37952c46f61375f0d69ccce7
Angela Wirth <angiewirth92@gmail.com>
2022-10-20 17:22:15 -0400

    Update file name for consistency

--- test/user-class-testing.js
+++ test/user-class-testing.js
@@ -1,4 +1,14 @@
 import { expect } from 'chai';
 
 
-describe ('User', ()
+describe ('User', () => {
+  let user1
+  beforeEach(() => {
+    let user1 = new user()
+  })
+
+  it('Should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
+})
