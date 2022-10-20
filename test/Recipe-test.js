b26f8198c7116af8925ab5603bc4aafbe822f2f0
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-10-20 15:38:11 -0600

    Refactor recipe test with mock data

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,40 +1,62 @@
 import { expect } from "chai";
 import Ingredient from "../src/classes/Ingredient";
 import Recipe from "../src/classes/Recipe";
-import recipeData from "../src/data/recipes";
-import ingredientsData from "../src/data/ingredients";
+import {
+  rawCookieRecipe,
+  rawPorkChopsRecipe,
+  mockIngredientsData,
+} from "./mock-data";
 
 describe("Recipe", function () {
-  let recipe, rawRecipe;
+  let cookieRecipe, porkChopsRecipe;
   beforeEach(() => {
-    rawRecipe = recipeData[0];
-    recipe = new Recipe(rawRecipe);
+    cookieRecipe = new Recipe(rawCookieRecipe);
+    porkChopsRecipe = new Recipe(rawPorkChopsRecipe);
   });
 
   it("should have an id", () => {
-    expect(recipe.id).to.equal(595736);
+    expect(cookieRecipe.id).to.equal(595736);
+    expect(rawPorkChopsRecipe.id).to.equal(678353);
   });
 
   it("should have a name", () => {
-    expect(recipe.name).to.equal("Loaded Chocolate Chip Pudding Cookie Cups");
+    expect(rawCookieRecipe.name).to.equal(
+      "Loaded Chocolate Chip Pudding Cookie Cups"
+    );
+    expect(porkChopsRecipe.name).to.equal(
+      "Maple Dijon Apple Cider Grilled Pork Chops"
+    );
   });
 
   it("should have an image", () => {
-    expect(recipe.image).to.equal(
+    expect(cookieRecipe.image).to.equal(
       "https://spoonacular.com/recipeImages/595736-556x370.jpg"
+    );
+    expect(rawPorkChopsRecipe.image).to.equal(
+      "https://spoonacular.com/recipeImages/678353-556x370.jpg"
     );
   });
 
   it("should have ingredients", () => {
-    expect(recipe.ingredients).to.deep.equal(rawRecipe.ingredients);
+    expect(cookieRecipe.ingredients).to.deep.equal([
+      { id: 20081, quantity: { amount: 1.5, unit: "c" } },
+      { id: 18372, quantity: { amount: 0.5, unit: "tsp" } },
+    ]);
+    expect(porkChopsRecipe.ingredients).to.deep.equal([
+      { id: 1009016, quantity: { amount: 1.5, unit: "cups" } },
+      { id: 9003, quantity: { amount: 2, unit: "" } },
+    ]);
   });
 
   it("should have instructions", () => {
-    expect(recipe.instructions).to.deep.equal(rawRecipe.instructions);
+    expect(cookieRecipe.instructions).to.deep.equal([
+      { instruction: "Instruction for step 1 for Cookie Cups", number: 1 },
+      { instruction: "Instruction for step 2 for Cookie Cups", number: 2 },
+    ]);
   });
 
   it("should have tags", () => {
-    expect(recipe.tags).to.deep.equal([
+    expect(cookieRecipe.tags).to.deep.equal([
       "antipasti",
       "starter",
       "snack",
@@ -42,39 +64,54 @@ describe("Recipe", function () {
       "antipasto",
       "hor d'oeuvre",
     ]);
-  });
-
-  it("should retrieve ingredient", () => {
-    recipe.retrieveIngredients(ingredientsData);
-    expect(recipe.ingredients[0]).instanceOf(Ingredient);
-    expect(recipe.ingredients[0].name).to.equal("wheat flour");
-  });
-
-  it("should determine ingredient names", () => {
-    recipe.retrieveIngredients(ingredientsData);
-    const ingredientNames = recipe.determineNames();
-    expect(ingredientNames).to.deep.equal([
-      "wheat flour",
-      "bicarbonate of soda",
-      "eggs",
-      "sucrose",
-      "instant vanilla pudding",
-      "brown sugar",
-      "salt",
-      "fine sea salt",
-      "semi sweet chips",
-      "unsalted butter",
-      "vanilla",
+    expect(porkChopsRecipe.tags).to.deep.equal([
+      "lunch",
+      "main course",
+      "main dish",
+      "dinner",
     ]);
   });
 
+  it("should retrieve ingredient", () => {
+    cookieRecipe.retrieveIngredients(mockIngredientsData);
+    expect(cookieRecipe.ingredients[0]).instanceOf(Ingredient);
+    expect(cookieRecipe.ingredients[0].name).to.equal("wheat flour");
+
+    porkChopsRecipe.retrieveIngredients(mockIngredientsData);
+    expect(porkChopsRecipe.ingredients[0]).instanceOf(Ingredient);
+    expect(porkChopsRecipe.ingredients[0].name).to.equal("apple cider");
+  });
+
+  it("should determine ingredient names", () => {
+    cookieRecipe.retrieveIngredients(mockIngredientsData);
+    const cookieRecipeIngredients = cookieRecipe.determineNames();
+    expect(cookieRecipeIngredients).to.deep.equal([
+      "wheat flour",
+      "bicarbonate of soda",
+    ]);
+
+    porkChopsRecipe.retrieveIngredients(mockIngredientsData);
+    const porkChopsRecipeIngredients = porkChopsRecipe.determineNames();
+    expect(porkChopsRecipeIngredients).to.deep.equal(["apple cider", "apple"]);
+  });
+
   it("should get cost of its ingredients", () => {
-    recipe.retrieveIngredients(ingredientsData);
-    const cost = recipe.getCost();
-    expect(cost).to.equal(17776);
+    cookieRecipe.retrieveIngredients(mockIngredientsData);
+    const cookieRecipeCost = cookieRecipe.getCost();
+    expect(cookieRecipeCost).to.equal(504);
+
+    porkChopsRecipe.retrieveIngredients(mockIngredientsData);
+    const porkChopsRecipeCost = porkChopsRecipe.getCost();
+    expect(porkChopsRecipeCost).to.equal(1116);
   });
 
   it("should return its instructions", () => {
-    expect(recipe.getInstructions()).to.deep.equal(recipe.instructions);
+    expect(cookieRecipe.getInstructions()).to.deep.equal(
+      cookieRecipe.instructions
+    );
+
+    expect(porkChopsRecipe.getInstructions()).to.deep.equal(
+      porkChopsRecipe.instructions
+    );
   });
 });
