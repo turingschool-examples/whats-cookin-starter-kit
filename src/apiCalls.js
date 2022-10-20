afdb1279a8cd718b0002242af6e3f33aba6dad10
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-20 17:29:44 -0600

    add api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,3 @@
-// Your fetch requests will live here!
-console.log("I will be a fetch request!");
-
 var loadData = (url) => {
   return fetch(url)
     .then((response) => response.json())
@@ -8,23 +5,5 @@ var loadData = (url) => {
       console.log("There was an error loading the data!", error)
     );
 };
-
-// fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-// fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-// fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
 
 export default loadData;
