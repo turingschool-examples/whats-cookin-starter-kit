c86be81e7a3bb1edf6f39c03608d366e2df02692
Ryan Nagel <RNagel29@gmail.com>
2022-10-21 17:41:35 -0600

    Add recipe names in when clicking All Recipes

--- src/scripts.js
+++ src/scripts.js
@@ -3,14 +3,15 @@ import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
-
-
+import RecipeRepository from './classes/RecipeRepository';
+import recipeData from './data/recipes';
 
 //  QUERYSELECTORS LIVE HERE
 const userWelcome = document.getElementById('section-user-section');
 const homeButton = document.getElementById('button--home');
 const myRecipesButton = document.getElementById('button--my-recipes');
 const saveRecipeButton = document.getElementById('button--save-recipe')
+const allRecipesButton = document.getElementById('button--all-recipes')
 const myRecipeList = document.getElementById('button--recipe-list');
 const searchField = document.getElementById('input--search');
 const filterField = document.getElementById('input--filter');
@@ -21,8 +22,8 @@ const allRecipesContainer = document.getElementById('section--all-recipes');
 const ingredientContainer = document.getElementById('ul--ingredient-list');
 const instructionsContainer = document.getElementById('ul--instructions');
 const allRecipes0to9 = document.getElementById('list--recipes-0-9');
-const allRecipes10to29 = document.getElementById('list--recipes-10-19');
-const allRecipes20to99 = document.getElementById('list--recipes-20-29');
+const allRecipes10to19 = document.getElementById('list--recipes-10-19');
+const allRecipes20to29 = document.getElementById('list--recipes-20-29');
 const allRecipes30to39 = document.getElementById('list--recipes-30-39');
 const allRecipes40to49 = document.getElementById('list--recipes-40-49');
 
@@ -45,12 +46,14 @@ homeButton.addEventListener('click', () => {
     show(cardsContainer);
     hide(recipeContainer);
     hide(savedRecipesContainer);
+    hide(allRecipesContainer);
 });
 
 myRecipesButton.addEventListener('click', () => {
     hide(cardsContainer);
     hide(recipeContainer);
     show(savedRecipesContainer);
+    hide(allRecipesContainer);
 });
 
 cardsContainer.addEventListener('click', (event) => {
@@ -58,7 +61,16 @@ cardsContainer.addEventListener('click', (event) => {
         hide(cardsContainer);
         show(recipeContainer);
         hide(savedRecipesContainer);
+        hide(allRecipesContainer);
     }
+});
+
+allRecipesButton.addEventListener('click', () => {
+    hide(cardsContainer);
+    hide(recipeContainer);
+    hide(savedRecipesContainer);
+    show(allRecipesContainer);
+    displayAllRecipes();
 });
 
 // saveRecipeButton.addEventListener('click', );
@@ -66,8 +78,7 @@ cardsContainer.addEventListener('click', (event) => {
 
 
 // GLOBAL VARIABLES LIVE HERE
-
-
+const allRecipes = new RecipeRepository(recipeData);
 
 
 
@@ -75,6 +86,28 @@ cardsContainer.addEventListener('click', (event) => {
 // HELPER FUNCTIONS LIVE HERE
 const show = element => element.classList.remove('hidden');
 const hide = element => element.classList.add('hidden');
+const displayAllRecipes = () => {
+    allRecipes.recipeData.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        };
+    });
+    allRecipes.recipeData.filter((recipe, index) => {
+        if (index < 10) {
+            allRecipes0to9.innerHTML += `<li data-id="${recipe.id}">${recipe.name}</li>`;
+        } else if (index < 20) {
+            allRecipes10to19.innerHTML += `<li data-id="${recipe.id}">${recipe.name}</li>`;
+        } else if (index < 30) {
+            allRecipes20to29.innerHTML += `<li data-id="${recipe.id}">${recipe.name}</li>`;
+        } else if (index < 40) {
+            allRecipes30to39.innerHTML += `<li data-id="${recipe.id}">${recipe.name}</li>`;
+        } else {
+            allRecipes40to49.innerHTML += `<li data-id="${recipe.id}">${recipe.name}</li>`;
+        };
+    });
+};
 
 
 
@@ -92,4 +125,3 @@ const hide = element => element.classList.add('hidden');
 //  ON LOAD FUNCTIONS LIVE HERE
 
 console.log('Hello world');
- 
