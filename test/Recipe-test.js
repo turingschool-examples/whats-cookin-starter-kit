7ea906bb8d92cf06908b8286004572e4a18aa3d0
thomas <105405396+thomedpete@users.noreply.github.com>
2022-10-21 15:49:42 -0600

    Update Recipe-test.js

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,11 +1,7 @@
 import { expect } from 'chai';
 import Recipe from '../src/classes/Recipe';
+import { Ingredient } from '../src/classes/Ingredient';
 
-
-describe('Recipie', () => {
-let recipeInfo, singleRecipe;
-
-import { Ingredients } from '../src/classes/IngredientsClass';
 
 describe('Recipie', () => {
 let recipeInfo, singleRecipe, ingredient;
@@ -235,14 +231,8 @@ it('should have a method that returns an array of ingredient ids', () => {
 it('should have a method that returns instructions', () => {
     let expectedResults = `1) In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.2) Add egg and vanilla and mix until combined.3) Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.4) Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.5) Bake for 9 to 10 minutes, or until you see the edges start to brown.6) Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.`;
     let methodUsed =  recipeInfo.returnRecipeInstructions()
-    
-
-    expect(methodUsed).to.equal(expectedResults)    
-    
-})
-
-    expect(methodUsed).to.equal(expectedResults)      
-})
+    expect(methodUsed).to.equal(expectedResults)     
+});
 
 it('should have a method that returns an array of ingrident class objects', () => {
     expect(recipeInfo.returnAllIngredientsArray()).to.be.an('array')
