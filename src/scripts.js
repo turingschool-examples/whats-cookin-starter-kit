adef5fd5b3a90ebcf8a008afea23c364111fe0a4
Evan Swanson <evan.sundelius@gmail.com>
2022-10-20 20:19:26 -0500

    Add fetch calls

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,11 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+//import functionName from './apiCalls';
+const varName = require('../src/apiCalls');
+const functionName2 = varName.testFetch;
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
-
 console.log('Hello world');
+
