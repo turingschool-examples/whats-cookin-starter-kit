0cffa1198b8e2a53d6c44158e24b7150be2d03b2
Anna Peterson <reveragb@gmail.com>
2022-10-20 20:16:44 -0600

    Refactor seeRecipe function and add renderRecipe

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,11 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 import Recipe from './classes/Recipe';
-import RecipeRepository from './classes/RecipeRepository'
+import RecipeRepository from './classes/RecipeRepository';
 import Ingredient from './classes/Ingredient';
+import ingredientsData from './data/ingredients';
 
 
 let recipe1 = {
@@ -124,132 +125,132 @@ let recipe1 = {
       "antipasto",
       "hor d'oeuvre"
     ]
-  }
-  let recipe2 = {
-    "id": 678353,
-    "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
-    "ingredients": [
-      {
-        "id": 1009016,
-        "quantity": {
-          "amount": 1.5,
-          "unit": "cups"
-        }
-      },
-      {
-        "id": 9003,
-        "quantity": {
-          "amount": 2,
-          "unit": ""
-        }
-      },
-      {
-        "id": 20027,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 1002046,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 11215,
-        "quantity": {
-          "amount": 1,
-          "unit": "clove"
-        }
-      },
-      {
-        "id": 1012046,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 19911,
-        "quantity": {
-          "amount": 0.25,
-          "unit": "cup"
-        }
-      },
-      {
-        "id": 16112,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 10010062,
-        "quantity": {
-          "amount": 24,
-          "unit": "ounce"
-        }
-      },
-      {
-        "id": 1102047,
-        "quantity": {
-          "amount": 4,
-          "unit": "servings"
-        }
-      },
-      {
-        "id": 16124,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 1016168,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
+}
+let recipe2 = {
+  "id": 678353,
+  "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
+  "ingredients": [
+    {
+      "id": 1009016,
+      "quantity": {
+        "amount": 1.5,
+        "unit": "cups"
       }
-    ],
-    "instructions": [
-      {
-        "instruction": "Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!",
-        "number": 1
+    },
+    {
+      "id": 9003,
+      "quantity": {
+        "amount": 2,
+        "unit": ""
       }
-    ],
-    "name": "Maple Dijon Apple Cider Grilled Pork Chops",
-    "tags": [
-      "lunch",
-      "main course",
-      "main dish",
-      "dinner"
-    ]
-  }
-  let ingredient1 = {
-    "id": 20081,
-    "name": "wheat flour",
-    "estimatedCostInCents": 142
-  }
-  let ingredient2 = {
-    "id": 18372,
-    "name": "bicarbonate of soda",
-    "estimatedCostInCents": 582
-  }
-  let ingredient3 = {
-    "id": 1123,
-    "name": "eggs",
-    "estimatedCostInCents": 472
-  }
-  let ingredient4 = {
-    "id": 19335,
-    "name": "sucrose",
-    "estimatedCostInCents": 902
-  }
+    },
+    {
+      "id": 20027,
+      "quantity": {
+        "amount": 1,
+        "unit": "tablespoon"
+      }
+    },
+    {
+      "id": 1002046,
+      "quantity": {
+        "amount": 1,
+        "unit": "tablespoon"
+      }
+    },
+    {
+      "id": 11215,
+      "quantity": {
+        "amount": 1,
+        "unit": "clove"
+      }
+    },
+    {
+      "id": 1012046,
+      "quantity": {
+        "amount": 1,
+        "unit": "tablespoon"
+      }
+    },
+    {
+      "id": 19911,
+      "quantity": {
+        "amount": 0.25,
+        "unit": "cup"
+      }
+    },
+    {
+      "id": 16112,
+      "quantity": {
+        "amount": 1,
+        "unit": "tablespoon"
+      }
+    },
+    {
+      "id": 10010062,
+      "quantity": {
+        "amount": 24,
+        "unit": "ounce"
+      }
+    },
+    {
+      "id": 1102047,
+      "quantity": {
+        "amount": 4,
+        "unit": "servings"
+      }
+    },
+    {
+      "id": 16124,
+      "quantity": {
+        "amount": 1,
+        "unit": "tablespoon"
+      }
+    },
+    {
+      "id": 1016168,
+      "quantity": {
+        "amount": 1,
+        "unit": "tablespoon"
+      }
+    }
+  ],
+  "instructions": [
+    {
+      "instruction": "Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!",
+      "number": 1
+    }
+  ],
+  "name": "Maple Dijon Apple Cider Grilled Pork Chops",
+  "tags": [
+    "lunch",
+    "main course",
+    "main dish",
+    "dinner"
+  ]
+}
+let ingredient1 = {
+  "id": 20081,
+  "name": "wheat flour",
+  "estimatedCostInCents": 142
+}
+let ingredient2 = {
+  "id": 18372,
+  "name": "bicarbonate of soda",
+  "estimatedCostInCents": 582
+}
+let ingredient3 = {
+  "id": 1123,
+  "name": "eggs",
+  "estimatedCostInCents": 472
+}
+let ingredient4 = {
+  "id": 19335,
+  "name": "sucrose",
+  "estimatedCostInCents": 902
+}
 
-let allIngredients = [ingredient1, ingredient2, ingredient3, ingredient4]
+let allIngredients = ingredientsData
 let allRecipes = [recipe1, recipe2]
 let recipeRepository = new RecipeRepository(allRecipes)
 let newRecipe = new Recipe(recipe1)
@@ -257,26 +258,58 @@ let newIngredient = new Ingredient(ingredient1)
 
 let allRecipesButton = document.querySelector(".all-recipes-button");
 let allRecipesPage = document.querySelector(".all-recipes-page");
-let homePage = document.querySelector(".home-page")
-
+let homePage = document.querySelector(".home-page");
+let currentRecipePage = document.querySelector(".current-recipe")
 
 allRecipesButton.addEventListener("click", viewAllRecipes)
-
-
 
 function viewAllRecipes() {
     addHidden(homePage)
     removeHidden(allRecipesPage)
     recipeRepository.newRecipes.forEach((recipe) => {
-         allRecipesPage.innerHTML += `<section class="recipe-card-container">
-        <img class="image" src="${recipe.image}">
-        <p class="recipe-name"> ${recipe.name} </p>
-        </section>`
-        return recipe
-   })
+      const newImage = document.createElement('section')
+      newImage.className = 'recipe-card-container'
+      newImage.innerHTML = `
+        <img class="image" id="${recipe.id}" src="${recipe.image}">
+        <p class="recipe-name"> ${recipe.name} </p>`
 
+      allRecipesPage.appendChild(newImage)
+      newImage.addEventListener('click', seeRecipe)
+   });
+};
+
+//click on image, grab the recipe and display the whole recipe 
+//section in html called current recipe
+//render recipe will allow a recipe to be displayed in innerHTML of current-recipe
+function seeRecipe(event) {
+  let visibleRecipe = recipeRepository.newRecipes.find(recipe => {
+    return parseInt(event.target.id) === recipe.id
+  }); 
+  let recipeInstructions = new Recipe(visibleRecipe);
+  // let totalCost = recipeInstructions.getCost()
+  console.log(recipeInstructions.instructions);
+  console.log(allIngredients)
+  renderRecipe(recipeInstructions);
+};
+
+function renderRecipe(recipe) {
+  addHidden(allRecipesPage)
+  removeHidden(currentRecipePage)
+  recipe.retrieveIngredients(allIngredients)
+ 
+  const newImage = document.createElement('section')
+    newImage.className = 'recipe-details'
+    newImage.innerHTML = `
+      <img class="image" src="${recipe.image}">
+      <ul>
+        <li> ${recipe.instructions} </li>
+        <li> ${recipe.ingredients} </li>
+        <li> ${recipe.getCost()} </li>
+      </ul>`
+
+    allRecipesPage.appendChild(newImage)
+    newImage.addEventListener('click', seeRecipe)
 }
-
 
 function addHidden(element) {
     element.classList.add("hidden")
