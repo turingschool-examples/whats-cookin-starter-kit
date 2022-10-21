6e01d6ab26569e1ffcca570de42301f4498fbacd
Tim T <tim.email.personal@gmail.com>
2022-10-21 12:19:16 -0700

    Add onload listener for instantiating RecipeRepository and User

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,10 @@ import apiCalls from './apiCalls';
 import MicroModal from 'micromodal';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import RecipeRepository from './classes/RecipeRepository'
+import recipeData from "./data/recipes"
+import ingredientsData from "./data/ingredients"
+// import Recipe from "./Recipe"
 
 MicroModal.init({
   onShow: modal => console.info(`${modal.id} is shown. Callback ftn to poppulate modal called here?`),
@@ -16,5 +20,12 @@ MicroModal.init({
   awaitCloseAnimation: false,
   debugMode: true 
 });
+
+window.onload = function(){
+  var newRecipeRepo = new RecipeRepository(recipeData, ingredientsData)
+  // var newUser = new User()
+  console.log("newRecipeRepo", newRecipeRepo)
+  // console.log("newUser", newUser)
+}
 
 console.log('Hello world');
