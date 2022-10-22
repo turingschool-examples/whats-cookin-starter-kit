15126fb7e8404b5c5b6251d0c76b37b9befe1acf
Sam Rice <samsrice@gmail.com>
2022-10-21 19:17:47 -0700

    add modal styling and add tile bookmark icons

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,9 @@
-import './styles.css';
-import apiCalls from './apiCalls';
-import MicroModal from 'micromodal';
+import './styles.css'
+import apiCalls from './apiCalls'
+import MicroModal from 'micromodal'
 // import '../src/images'
+import "./images/bookmark-tiles-unsaved.png"
+import "./images/bookmark-tiles-saved.png"
 import "./images/bookmark-unsaved.png"
 import "./images/bookmark-saved.png"
 import RecipeRepository from '../src/classes/RecipeRepository'
@@ -13,45 +15,55 @@ import User from '../src/classes/User'
 // ---------------------------DATA MODEL---------------------------
 
 const recipeRepository = new RecipeRepository(recipeData, ingredientsData)
+const user = new User(usersData[0])
+
+let currentlyViewedRecipe
 
 // ---------------------------QUERY SELECTORS---------------------------
 
 const allRecipesContainer = document.querySelector('.all-recipes-container')
 const closeModalButton = document.getElementById("close-modal-button")
+const modalSaveRecipeButton = document.getElementById("modal-save-recipe")
 const modalTagParent = document.getElementById("modal-tag-button-parent")
+const modalRecipeTitle = document.getElementById("modal-title")
+const modalImage = document.getElementById("modal-image")
+const ingredientsParent = document.getElementById("ingr-parent")
+const instructionsParent = document.getElementById("instructions-parent")
 
 // ---------------------------EVENT LISTENERS---------------------------
 
-window.onload = function(){
-    displayAllRecipeTiles()
-    var newUser = new User(usersData[0])
-    MicroModal.init({
-      onShow: modal => console.info(`${modal.id} is shown. Callback ftn to poppulate modal called here?`),
-      onClose: modal => console.info(`${modal.id} is hidden`),
-      openTrigger: 'data-micromodal-trigger',
-      closeTrigger: 'data-custom-close',
-      openClass: 'is-open',
-      disableScroll: true,
-      disableFocus: true,
-      awaitOpenAnimation: false,
-      awaitCloseAnimation: false,
-      debugMode: true 
-    })
+window.onload = function () {
+  displayAllRecipeTiles()
+  MicroModal.init({
+    // onShow: modal => console.info(`${modal.id} is shown. Callback ftn to poppulate modal called here?`),
+    // onClose: modal => console.info(`${modal.id} is hidden`),
+    // openTrigger: 'data-micromodal-trigger',
+    // closeTrigger: 'data-custom-close',
+    openClass: 'is-open',
+    disableScroll: true,
+    disableFocus: true,
+    awaitOpenAnimation: false,
+    awaitCloseAnimation: false,
+    debugMode: true
+  })
 }
-
-closeModalButton.addEventListener("click", () => MicroModal.close("modal-1"))
 
 allRecipesContainer.addEventListener("click", (event) => {
   if (event.target.nodeName === "SECTION") { return }
-  updateModal(event);
-  MicroModal.show("modal-1")
+  let targetObject = recipeRepository.recipeList.find(recipe => recipe.id == event.target.parentNode.id)
+  currentlyViewedRecipe = targetObject
+  updateModal(targetObject)
 })
+
+closeModalButton.addEventListener("click", () => MicroModal.close("modal-1"))
+
+modalSaveRecipeButton.addEventListener("click", () => user.storedFavoriteRecipes.push(currentlyViewedRecipe))
 
 // ---------------------------DOM UPDATING---------------------------
 
 function createRecipeTile(recipe) {
-    allRecipesContainer.innerHTML += 
-        `<div class="recipe-tile" id=${recipe.id}>
+  allRecipesContainer.innerHTML +=
+    `<div class="recipe-tile" id=${recipe.id}>
             <div class= "tile-image" style="background-image: url(${recipe.image})">
             <img class="modal-bookmark-icon" src="./images/bookmark-unsaved.png" alt="save recipe">
             </div>
@@ -61,18 +73,26 @@ function createRecipeTile(recipe) {
 }
 
 function displayAllRecipeTiles() {
-    for(var i = 0; i < recipeRepository.recipeList.length; i++) {
-        createRecipeTile(recipeRepository.recipeList[i])
-    }
+  for (var i = 0; i < recipeRepository.recipeList.length; i++) {
+    createRecipeTile(recipeRepository.recipeList[i])
+  }
 }
 
-let updateModal = event => {
-  let targetObject = recipeRepository.recipeList.find(recipe => recipe.id == event.target.parentNode.id)
-  
+let updateModal = targetObject => {
   modalTagParent.innerHTML = ``
   targetObject.tags.forEach(tag => {
     modalTagParent.innerHTML += `<button>${tag}</button>`
   })
-  
-  console.log(targetObject)
+  modalRecipeTitle.innerHTML = targetObject.name
+  modalImage.src = targetObject.image
+  modalImage.alt = targetObject.name
+  ingredientsParent.innerHTML = ``
+  targetObject.ingredients.forEach(ingredient => {
+    ingredientsParent.innerHTML += `<ul>${ingredient.amount} ${ingredient.unit} ${ingredient.name}</ul>`
+  })
+  instructionsParent.innerHTML = ``
+  targetObject.instructions.forEach(item => {
+    instructionsParent.innerHTML += `<p>${item.number}. ${item.instruction}`
+  })
+  MicroModal.show("modal-1")
 }
