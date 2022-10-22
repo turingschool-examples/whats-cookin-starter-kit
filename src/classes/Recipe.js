1769be08c11155613dec7ef4458b50928c5ad4d3
Justen Anderson <justenanderson@gmail.com>
2022-10-22 14:45:55 -0600

    Partially refactor and format Recipe.js.

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,5 +1,6 @@
-import  ingredientsData  from '../data/ingredients';
-import Ingredient from '../classes/Ingredient'
+import ingredientsData from '../data/ingredients';
+import Ingredient from '../classes/Ingredient';
+
 class Recipe {
     constructor(recipeData) {
         this.id = recipeData.id
@@ -10,89 +11,84 @@ class Recipe {
         this.tags = recipeData.tags
     }
 
-returnRecipeIngredientsIds() {
-   let ingredientIDs = this.ingredients.map(ingredient => ingredient.id);
-     return ingredientIDs  
-}
-
-returnAllIngredientsIds() {
-    let ingredientIDs = ingredientsData.map(ingredient => ingredient.id);
-        return ingredientIDs
-}
-
-returnIngredientById(id) {
-    let allIngred = this.returnAllIngredientsArray()
-  let singleIngred = allIngred.find((element) => {
-            return element.id === id
-        })
-        console.log('singleIngred: ', singleIngred)
-        return singleIngred
+    returnRecipeIngredientsIds() {
+        let ingredientIDs = this.ingredients.map(ingredient => ingredient.id);
+        return ingredientIDs;
     }
 
-returnRecipeInstructions() {
-    let instructionsParagraph = this.instructions.reduce((string, instruction) => {
-        return string += `${instruction.number}) ${instruction.instruction}`
-    }, '')
-    return instructionsParagraph;
-  }
+    returnAllIngredientsIds() {
+        let ingredientIDs = ingredientsData.map(ingredient => ingredient.id);
+        return ingredientIDs;
+    }
 
-returnRecipieIngredientsArray() {
-    let recipeIngredients = this.ingredients
-}
+    returnIngredientById(id) {
+        let allIngred = this.returnAllIngredientsArray()
+        let singleIngred = allIngred.find((element) => {
+            return element.id === id;
+        })
 
-returnAllIngredientsArray() {     
-  let ingredientsArray = ingredientsData.map((ingredient) => {
-        return  new Ingredient(ingredient)
-   });
-  return ingredientsArray;
-}
+        return singleIngred;
+    }
 
-returnRecipeIngredientsInfo() {   
-    let allIngredients = this.returnAllIngredientsArray()
-    let ingredientInfoForDOM = this.ingredients.map(ingredient => {
-        let ingredientDetails = allIngredients.find(element => element.id === ingredient.id)
-        if (ingredient.quantity.amount < 1) {
-            return ingredient.quantity.amount.toFixed(2) + ' ' + ingredient.quantity.unit + ' ' + ingredientDetails.name;
+    returnRecipeInstructions() {
+        let instructionsParagraph = this.instructions.reduce((string, instruction) => {
+            return string += `${instruction.number}) ${instruction.instruction}`
+        }, '')
+        return instructionsParagraph;
+    }
+
+    returnRecipieIngredientsArray() {
+        let recipeIngredients = this.ingredients;
+    }
+
+    returnAllIngredientsArray() {
+        let ingredientsArray = ingredientsData.map((ingredient) => {
+            return new Ingredient(ingredient);
+        });
+        return ingredientsArray;
+    }
+
+    returnRecipeIngredientsInfo() {
+        let allIngredients = this.returnAllIngredientsArray()
+        let ingredientInfoForDOM = this.ingredients.map(ingredient => {
+            let ingredientDetails = allIngredients.find(element => element.id === ingredient.id)
+            if (ingredient.quantity.amount < 1) {
+                return ingredient.quantity.amount.toFixed(2) + ' ' + ingredient.quantity.unit + ' ' + ingredientDetails.name;
+            } else {
+                return ingredient.quantity.amount + ' ' + ingredient.quantity.unit + ' ' + ingredientDetails.name;
+            }
+        })
+     
+        return ingredientInfoForDOM;
+    }
+
+    returnAllIngredientsNames() {
+        let ingredientsNamesArray = ingredientsData.map((ingredient) => {
+            return ingredient.name;
+        });
+        return ingredientsNamesArray;
+    }
+
+    returnCostOfIngredients() {
+        let allIngredients = this.returnAllIngredientsArray()
+        let recipeIngredientIDs = this.returnRecipeIngredientsIds();
+        let newRecipeIngredients = allIngredients.filter(ingredient => (recipeIngredientIDs.includes(ingredient.id)));
+        let ingredCosts = newRecipeIngredients.map(ingredient => ingredient.estimatedCostInCents);
+        let ingredQuantitiesNeeded = this.ingredients.map(ingredient => ingredient.quantity.amount);
+        let multiplyCostByAmmount = ingredCosts.map((cost, index) => {
+            let quantity = ingredQuantitiesNeeded[index];
+            return cost * quantity;
+        })
+        let total = multiplyCostByAmmount.reduce((sum, cost) => {
+            return sum + cost;
+        }, 0)
+        let finalTotal = parseFloat((total / 100).toFixed(2));
+        if (finalTotal >= 1) {
+            return `$ ${finalTotal}`;
         } else {
-        return ingredient.quantity.amount + ' ' + ingredient.quantity.unit + ' ' + ingredientDetails.name;
+            return finalTotal;
         }
-    })
-    // console.log('ingredientinfoforDom: ', ingredientInfoForDOM)
-    return ingredientInfoForDOM;
-}   
-
-returnAllIngredientsNames() {
-  let ingredientsNamesArray = ingredientsData.map((ingredient) => {
-      return ingredient.name
-    });
-  return ingredientsNamesArray;
-}
-
-returnCostOfIngredients() {
-  let allIngredients = this.returnAllIngredientsArray()
-  let recipeIngredientIDs = this.returnRecipeIngredientsIds();
-  let newRecipeIngredients = allIngredients.filter(ingredient => (recipeIngredientIDs.includes(ingredient.id)));  
-  let ingredCosts = newRecipeIngredients.map(ingredient => ingredient.estimatedCostInCents);
-  let ingredQuantitiesNeeded = this.ingredients.map(ingredient => ingredient.quantity.amount);
-  let multiplyCostByAmmount = ingredCosts.map((cost, index) => {
-        let quantity = ingredQuantitiesNeeded[index];
-        return cost * quantity
-    })
-    let total = multiplyCostByAmmount.reduce((sum, cost) => {
-          return sum + cost;
-    }, 0)
-    let finalTotal = parseFloat(( total / 100).toFixed(2));
-        if(finalTotal >= 1) {
-            return `$ ${finalTotal}`
-        } else {
-            return finalTotal
-        }
-}
-
-
-
-
-
+    }
 };
 
 
