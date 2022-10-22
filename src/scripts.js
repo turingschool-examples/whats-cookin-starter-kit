abfa83d86f0cfd4662f539b6835d317ebbf4b2d4
Justen Anderson <justenanderson@gmail.com>
2022-10-22 17:52:00 -0600

    Refactor and clean up code.

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 //  IMPORTS LIVE HERE
 import './styles.css';
 import apiCalls from './apiCalls';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 import RecipeRepository from './classes/RecipeRepository';
 import recipeData from './data/recipes';
@@ -31,7 +30,6 @@ const allRecipes40to49 = document.getElementById('list--recipes-40-49');
 const savedRecipesList1 = document.getElementById('list--saved-recipes-0-9');
 const savedRecipesList2 = document.getElementById('list--saved-recipes-10-19');
 const savedRecipesList3 = document.getElementById('list--saved-recipes-20-29');
-
 const recipeCard1 = document.getElementById('card--recipe1');
 const recipeCard2 = document.getElementById('card--recipe2');
 const recipeCard3 = document.getElementById('card--recipe3');
@@ -47,8 +45,6 @@ const totalCost = document.getElementById('text--total-cost');
 
 
 //  EVENT LISTENERS LIVE HERE
-
-
 homeButton.addEventListener('click', () => {
     show(cardsContainer);
     hide(recipeContainer);
@@ -64,7 +60,6 @@ myRecipesButton.addEventListener('click', () => {
     hide(allRecipesContainer);
     show(allRecipesButton);
     displaySavedRecipes();
-
 });
 
 cardsContainer.addEventListener('click', (event) => {
@@ -91,11 +86,6 @@ recipeListsContainer.addEventListener('click', (event) => {
     show(allRecipesButton);
     displayRecipeDetails(event);
 });
-
-
-
-// saveRecipeButton.addEventListener('click', );
-// myRecipesButton.addEventListener('click', );
 
 
 // GLOBAL VARIABLES LIVE HERE
@@ -137,152 +127,152 @@ const displayAllRecipes = () => {
 };
 
 
-const userData =  {
+const userData = {
     "name": "Saige O'Kon",
     "id": 1,
     "pantry": [
-      {
-        "ingredient": 11297,
-        "amount": 4
-      },
-      {
-        "ingredient": 1082047,
-        "amount": 10
-      },
-      {
-        "ingredient": 20081,
-        "amount": 5
-      },
-      {
-        "ingredient": 11215,
-        "amount": 5
-      },
-      {
-        "ingredient": 2047,
-        "amount": 6
-      },
-      {
-        "ingredient": 1123,
-        "amount": 8
-      },
-      {
-        "ingredient": 11282,
-        "amount": 4
-      },
-      {
-        "ingredient": 6172,
-        "amount": 2
-      },
-      {
-        "ingredient": 2044,
-        "amount": 2
-      },
-      {
-        "ingredient": 2050,
-        "amount": 4
-      },
-      {
-        "ingredient": 1032009,
-        "amount": 3
-      },
-      {
-        "ingredient": 5114,
-        "amount": 3
-      },
-      {
-        "ingredient": 1017,
-        "amount": 2
-      },
-      {
-        "ingredient": 18371,
-        "amount": 7
-      },
-      {
-        "ingredient": 1001,
-        "amount": 6
-      },
-      {
-        "ingredient": 99223,
-        "amount": 2
-      },
-      {
-        "ingredient": 1230,
-        "amount": 2
-      },
-      {
-        "ingredient": 9152,
-        "amount": 4
-      },
-      {
-        "ingredient": 10611282,
-        "amount": 2
-      },
-      {
-        "ingredient": 93607,
-        "amount": 2
-      },
-      {
-        "ingredient": 14106,
-        "amount": 4
-      },
-      {
-        "ingredient": 1077,
-        "amount": 4
-      },
-      {
-        "ingredient": 6150,
-        "amount": 2
-      },
-      {
-        "ingredient": 1124,
-        "amount": 2
-      },
-      {
-        "ingredient": 10011693,
-        "amount": 4
-      },
-      {
-        "ingredient": 1102047,
-        "amount": 2
-      },
-      {
-        "ingredient": 19206,
-        "amount": 2
-      },
-      {
-        "ingredient": 1145,
-        "amount": 4
-      },
-      {
-        "ingredient": 1002030,
-        "amount": 4
-      },
-      {
-        "ingredient": 12061,
-        "amount": 2
-      },
-      {
-        "ingredient": 19335,
-        "amount": 4
-      },
-      {
-        "ingredient": 15152,
-        "amount": 3
-      },
-      {
-        "ingredient": 9003,
-        "amount": 2
-      },
-      {
-        "ingredient": 18372,
-        "amount": 3
-      },
-      {
-        "ingredient": 2027,
-        "amount": 2
-      }
+        {
+            "ingredient": 11297,
+            "amount": 4
+        },
+        {
+            "ingredient": 1082047,
+            "amount": 10
+        },
+        {
+            "ingredient": 20081,
+            "amount": 5
+        },
+        {
+            "ingredient": 11215,
+            "amount": 5
+        },
+        {
+            "ingredient": 2047,
+            "amount": 6
+        },
+        {
+            "ingredient": 1123,
+            "amount": 8
+        },
+        {
+            "ingredient": 11282,
+            "amount": 4
+        },
+        {
+            "ingredient": 6172,
+            "amount": 2
+        },
+        {
+            "ingredient": 2044,
+            "amount": 2
+        },
+        {
+            "ingredient": 2050,
+            "amount": 4
+        },
+        {
+            "ingredient": 1032009,
+            "amount": 3
+        },
+        {
+            "ingredient": 5114,
+            "amount": 3
+        },
+        {
+            "ingredient": 1017,
+            "amount": 2
+        },
+        {
+            "ingredient": 18371,
+            "amount": 7
+        },
+        {
+            "ingredient": 1001,
+            "amount": 6
+        },
+        {
+            "ingredient": 99223,
+            "amount": 2
+        },
+        {
+            "ingredient": 1230,
+            "amount": 2
+        },
+        {
+            "ingredient": 9152,
+            "amount": 4
+        },
+        {
+            "ingredient": 10611282,
+            "amount": 2
+        },
+        {
+            "ingredient": 93607,
+            "amount": 2
+        },
+        {
+            "ingredient": 14106,
+            "amount": 4
+        },
+        {
+            "ingredient": 1077,
+            "amount": 4
+        },
+        {
+            "ingredient": 6150,
+            "amount": 2
+        },
+        {
+            "ingredient": 1124,
+            "amount": 2
+        },
+        {
+            "ingredient": 10011693,
+            "amount": 4
+        },
+        {
+            "ingredient": 1102047,
+            "amount": 2
+        },
+        {
+            "ingredient": 19206,
+            "amount": 2
+        },
+        {
+            "ingredient": 1145,
+            "amount": 4
+        },
+        {
+            "ingredient": 1002030,
+            "amount": 4
+        },
+        {
+            "ingredient": 12061,
+            "amount": 2
+        },
+        {
+            "ingredient": 19335,
+            "amount": 4
+        },
+        {
+            "ingredient": 15152,
+            "amount": 3
+        },
+        {
+            "ingredient": 9003,
+            "amount": 2
+        },
+        {
+            "ingredient": 18372,
+            "amount": 3
+        },
+        {
+            "ingredient": 2027,
+            "amount": 2
+        }
     ]
-  }
+}
 const user = new User(userData);
 user.recipesToCook = allRecipes.recipeData;
 
@@ -290,7 +280,7 @@ const displaySavedRecipes = () => {
     savedRecipesList1.innerHTML = '';
     savedRecipesList2.innerHTML = '';
     savedRecipesList3.innerHTML = '';
-// ---------------- Can this be refactored????
+    // ---------------- Can this be refactored????
     user.recipesToCook.sort((a, b) => {
         if (a.name > b.name) {
             return 1;
