89f6053fa85e2aa712850f118c57a9016b9bfd82
Sam Rice <samsrice@gmail.com>
2022-10-21 18:00:54 -0700

    add listener/DOM updating for modal tags

--- src/scripts.js
+++ src/scripts.js
@@ -1,44 +1,59 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 import MicroModal from 'micromodal';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import '../src/images'
+// import '../src/images'
+import "./images/bookmark-unsaved.png"
+import "./images/bookmark-saved.png"
 import RecipeRepository from '../src/classes/RecipeRepository'
 import recipeData from './data/recipes'
 import ingredientsData from "./data/ingredients"
 import usersData from "./data/users"
 import User from '../src/classes/User'
 
-const allRecipesContainer = document.querySelector('.all-recipes-container')
+// ---------------------------DATA MODEL---------------------------
 
 const recipeRepository = new RecipeRepository(recipeData, ingredientsData)
+
+// ---------------------------QUERY SELECTORS---------------------------
+
+const allRecipesContainer = document.querySelector('.all-recipes-container')
+const closeModalButton = document.getElementById("close-modal-button")
+const modalTagParent = document.getElementById("modal-tag-button-parent")
+
+// ---------------------------EVENT LISTENERS---------------------------
 
 window.onload = function(){
     displayAllRecipeTiles()
     var newUser = new User(usersData[0])
+    MicroModal.init({
+      onShow: modal => console.info(`${modal.id} is shown. Callback ftn to poppulate modal called here?`),
+      onClose: modal => console.info(`${modal.id} is hidden`),
+      openTrigger: 'data-micromodal-trigger',
+      closeTrigger: 'data-custom-close',
+      openClass: 'is-open',
+      disableScroll: true,
+      disableFocus: true,
+      awaitOpenAnimation: false,
+      awaitCloseAnimation: false,
+      debugMode: true 
+    })
 }
-import "./images/bookmark-unsaved.png"
-import "./images/bookmark-saved.png"
 
-MicroModal.init({
-  onShow: modal => console.info(`${modal.id} is shown. Callback ftn to poppulate modal called here?`),
-  onClose: modal => console.info(`${modal.id} is hidden`),
-  openTrigger: 'data-micromodal-trigger',
-  closeTrigger: 'data-custom-close',
-  openClass: 'is-open',
-  disableScroll: true,
-  disableFocus: false,
-  awaitOpenAnimation: false,
-  awaitCloseAnimation: false,
-  debugMode: true 
-});
+closeModalButton.addEventListener("click", () => MicroModal.close("modal-1"))
+
+allRecipesContainer.addEventListener("click", (event) => {
+  if (event.target.nodeName === "SECTION") { return }
+  updateModal(event);
+  MicroModal.show("modal-1")
+})
+
+// ---------------------------DOM UPDATING---------------------------
 
 function createRecipeTile(recipe) {
     allRecipesContainer.innerHTML += 
-        `<div class="recipe-tile">
+        `<div class="recipe-tile" id=${recipe.id}>
             <div class= "tile-image" style="background-image: url(${recipe.image})">
-                <button class="bookmark-button">Test</button>
+            <img class="modal-bookmark-icon" src="./images/bookmark-unsaved.png" alt="save recipe">
             </div>
             <h1>${recipe.name}</h1>
             <h2>${recipe.tags.join(', ')}</h2>
@@ -49,4 +64,15 @@ function displayAllRecipeTiles() {
     for(var i = 0; i < recipeRepository.recipeList.length; i++) {
         createRecipeTile(recipeRepository.recipeList[i])
     }
+}
+
+let updateModal = event => {
+  let targetObject = recipeRepository.recipeList.find(recipe => recipe.id == event.target.parentNode.id)
+  
+  modalTagParent.innerHTML = ``
+  targetObject.tags.forEach(tag => {
+    modalTagParent.innerHTML += `<button>${tag}</button>`
+  })
+  
+  console.log(targetObject)
 }
