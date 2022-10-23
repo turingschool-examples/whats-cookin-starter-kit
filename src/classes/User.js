cf8af05cfa4cb38838fe7d1de1f27339eca16c32
Spencer Haka <spencer.haka@gmail.com>
2022-10-22 20:54:41 -0600

    Refactor search by name or ingredient method on user class to filter recipes more effectively

--- src/classes/User.js
+++ src/classes/User.js
@@ -25,18 +25,22 @@ class User {
 
   filterByNameOrIngredient(input) {
     let filteredRecipes = [];
+    input = input.toLowerCase();
     
     this.favoriteRecipes.forEach(recipe => {
-      if (recipe.name.includes(input)) {
+      if (recipe.name.toLowerCase().includes(input)) {
         filteredRecipes.push(recipe)
       } else {
         recipe.ingredients.forEach(ingredient => {
-          if (ingredient.includes(input)) {
-            filteredRecipes.push(recipe)
+          if (ingredient.name.toLowerCase().includes(input)) {
+            if (!filteredRecipes.includes(recipe)) {
+              filteredRecipes.push(recipe)
+            }
           }
         })
       }
     })
+
     return filteredRecipes;
   }
 
