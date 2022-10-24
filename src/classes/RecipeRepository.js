24cd81264b4f4b445230db5ef6ceedd7c615542b
Zac Walters <zacwalters4@gmail.com>
2022-10-24 11:41:11 -0600

    Alphabetize methods in RecipeRepository.js

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,17 +3,7 @@ import Recipe from "./Recipe"
 class RecipeRepository {
   constructor(recipeData, ingredientsData) {
     this.recipeList = this.instantiateRecipes(recipeData, ingredientsData)
-    this.featuredRecipe = this.getFeaturedRecipe()
-  }
-
-  instantiateRecipes(recipeData, ingredientsData) {
-    //Note: we used dot notation below to access the correct array in recipeData.recipeData and ingredientsData.ingredientsData. Refactor this in the second part of the project to use locally hosted data.
-
-    return recipeData.map(recipe => new Recipe(recipe, ingredientsData))
-  }
-
-  filterByTag(tag) {
-    return this.recipeList.filter(recipe => recipe.tags.includes(tag))
+    this.featuredRecipe = this.getFeaturedRecipe()  
   }
 
   filterByNameOrIngredient(input) {
@@ -37,9 +27,19 @@ class RecipeRepository {
     return filteredRecipes
   }
 
+  filterByTag(tag) {
+    return this.recipeList.filter(recipe => recipe.tags.includes(tag))
+  }
+
   getFeaturedRecipe = () => {
     let randomNum = Math.floor(Math.random() * this.recipeList.length)
     return this.recipeList[randomNum]
+  }
+
+  instantiateRecipes(recipeData, ingredientsData) {
+    //Note: we used dot notation below to access the correct array in recipeData.recipeData and ingredientsData.ingredientsData. Refactor this in the second part of the project to use locally hosted data.
+
+    return recipeData.map(recipe => new Recipe(recipe, ingredientsData))
   }
 }
 
