2982d29658f1af3b0d0822e0d6461f18a610149c
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-10-24 16:37:29 -0600

    Adds fetches

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,8 @@
-// Your fetch requests will live here!
+let fetchData = (endPoint) => {
+    return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${endPoint}`)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
 
+export default fetchData;
 
-console.log('I will be a fetch request!')
