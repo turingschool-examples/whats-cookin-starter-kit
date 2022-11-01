8f70d0c960ddbe1d25c4d045bb1755679147cb20
Evan Swanson <evan.sundelius@gmail.com>
2022-11-01 16:12:35 -0500

    Commit for npm start

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,3 +1,5 @@
+import { prototype } from "mocha";
+
 class User {
     constructor(user) {
         this.name = user.name;
@@ -17,6 +19,25 @@ class User {
     filterByName(name) {
         return this.recipesToCook.filter(recipe => recipe.name.includes(name));
     };
+
+    addToPantry(ingredient) {
+        //NOTICE! this.pantry stores the ingredients in this format:
+        //id, quantity, amount, unit. <<NO NAME GIVEN!!
+        this.pantry.push(ingredient);
+    };
+
+    // checkPantry(recipe) {
+    //     //iterate thru... we need to check if each ingredient+amount for a given recipe
+    //     //is in the user's pantry
+    //     //for each RecIngredient, does the User have it? Does the User have enough of it?
+    //     recipe.ingredients.forEach(ingredient => {
+    //         if(this.pantry[i].id.includes(ingredient.id)) {
+    //             if(this.pantry[i].id === ingredient.id) {
+    //                 //check if it has enough of it. If not, report back how much is missing.
+    //             }
+    //         })
+    //     })
+    // }
 
 };
 
