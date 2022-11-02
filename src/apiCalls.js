2427f2c89788c04400f56286b7287d972aba4b62
Lauren Black <laurenblack5280@gmail.com>
2022-11-01 22:59:49 -0600

    clean up code by adding helper function, add sample post properties

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,38 +3,48 @@
 const usersUrl = 'http://localhost:3001/api/v1/users'
 const ingredientsUrl = 'http://localhost:3001/api/v1/ingredients'
 const recipesUrl = 'http://localhost:3001/api/v1/recipes'
-// const urlList = [usersUrl, ingredientsUrl, recipesUrl]
+const requestProperties = {
+  userID: /**/,
+  ingredientID: /**/,
+  ingredientModification: /**/
+}
+const samplePostProperties = {
+  userID: 1,
+  ingredientID: 11297,
+  ingredientModification: -1
+}
+
+function andThen() {
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.log('To err is human', err))
+}
 
 function getUsersData() {
   let fetchedUsers = fetch(usersUrl)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log('To err is human', err));
+    andThen()
     return fetchedUsers
 }
 
 function getIngredientsData() {
   let fetchedIngredients = fetch(ingredientsUrl)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log('To err is human', err));
+    andThen()
     return fetchedIngredients
 }
 
 function getRecipeData() {
   let fetchedRecipes = fetch(recipesUrl)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log('To err is human', err));
+    andThen()
     return fetchedRecipes
 }
 
 function postData() {
   let postedData = fetch(usersUrl, {
     method: 'POST',
-    body: JSON.stringify(),
+    body: JSON.stringify(samplePostProperties),
     headers: { 'content-type': 'application/json'}
   })
+  andThen()
   return postedData
 }
 
@@ -44,5 +54,6 @@ function deleteData() {
     body: JSON.stringify(),
     headers: { 'content-type': 'application/json'}
   })
+  andThen()
   return deletedData
 }
