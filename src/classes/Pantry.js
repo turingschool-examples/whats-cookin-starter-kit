3c7eacd8134272292c2ac745e4e140f3a2de6f92
Ryan Nagel <RNagel29@gmail.com>
2022-11-02 11:55:59 -0600

    Update pseudocode for checkIngredients method of Pantry

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -6,12 +6,16 @@ class Pantry {
     checkIngredients(recipe) {
         // let missingIngredients = [{ingredientName: 'sugar', quantity: 4, unit: 'c'}, {etc}]
         // then iterate through the recipe's ingredients (recipe.ingredients)
-        // check to see if the ingredient exists in the pantry (pantry.includes())
-            // if not included then push ingredient and full amount needed to missingIngredients
+        // forEach ingredient check to see if this.pantry.includes(ingredient.id)
+            // if not included then push ingredient object to missingIngredients
         // then check to see if the pantry ingredient quantity >= recipe ingredient quantity
-            // if not enough quantity, then push ingredient amount missing to missingIngredients
-        // maybe can use returnAllIngredientsArray to access name
+            // let missingAmount = ingredient.quantity.amount - pantry.ingredient.amount
+            // if not enough quantity, then push ingredient object with updated amount to missingIngredients
+        // maybe can use returnAllIngredientsArray to access name and update the missingIngredients array to include the ingredient names
         // return missingIngredients
+        let missingIngredients = [];
+
+
     }
 }
 export default Pantry
