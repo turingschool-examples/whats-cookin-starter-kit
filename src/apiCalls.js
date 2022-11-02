e05ddfc5d7820ecdd66e8a4a33c8d6e9ddd3eb03
Lauren Black <laurenblack5280@gmail.com>
2022-11-01 22:16:16 -0600

    contain floating code in functions, update fetch calls to get data from local server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,47 @@
 // Your fetch requests will live here!
-export const getUsersAPIData = fetch(
-  "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users"
-)
-  .then((response) => response.json())
-  .catch((err) => console.log(err));
 
-export const getRecipesAPIData = fetch(
-  "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes"
-)
-  .then((response) => response.json())
-  .catch((err) => console.log(err));
+const usersUrl = 'http://localhost:3001/api/v1/users'
+const ingredientsUrl = 'http://localhost:3001/api/v1/ingredients'
+const recipesUrl = 'http://localhost:3001/api/v1/recipes'
+// const urlList = [usersUrl, ingredientsUrl, recipesUrl]
 
-export const getIngredientsAPIData = fetch(
-  "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
-)
-  .then((response) => response.json())
-  .catch((err) => console.log(err));
+function getUsersData() {
+  let fetchedUsers = fetch(usersUrl)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log('To err is human', err));
+    return fetchedUsers
+}
+
+function getIngredientsData() {
+  let fetchedIngredients = fetch(ingredientsUrl)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log('To err is human', err));
+    return fetchedIngredients
+}
+
+function getRecipeData() {
+  let fetchedRecipes = fetch(recipesUrl)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log('To err is human', err));
+    return fetchedRecipes
+}
+// export const getUsersAPIData = fetch(
+//   "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users"
+// )
+//   .then((response) => response.json())
+//   .catch((err) => console.log(err));
+//
+// export const getRecipesAPIData = fetch(
+//   "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes"
+// )
+//   .then((response) => response.json())
+//   .catch((err) => console.log(err));
+//
+// export const getIngredientsAPIData = fetch(
+//   "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
+// )
+//   .then((response) => response.json())
+//   .catch((err) => console.log(err));
