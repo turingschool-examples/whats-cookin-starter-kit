1098b4b626e050490f4c30cd24c462144a9e2e44
Zac Walters <zacwalters4@gmail.com>
2022-11-06 13:38:37 -0700

    Use response.status and return Promise.reject

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,21 +1,19 @@
 function getData(url) {
   return fetch(url)
     .then(response => {
-      console.log(response)
+      console.log(response.status)
       if(response.ok) {
         return response.json()
       } else if(response.status === 404) {
-        return Promise.reject(' error 404')
-      }
-      // else if(response.status ) {
-      //   return Promise.reject(' some other error: ' + response.status)
-      // } 
-      else {
-        console.log(response.status)
-      }
+        return Promise.reject('error 404')
+      } else {
+        return Promise.reject('some other error: ' + response.status)
+      } 
     })
-    .catch(error => alert("There was an error, please refresh the page and try again."))
+    .then(data => console.log('data is', data))
+    .catch(error => console.log('error is', error))
 }
+// "There was an error, please refresh the page and try again."
 
 function postData(body, url) {
   return fetch(url, {
