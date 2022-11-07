898601e2e3b586c7b65bcd50729adb64d5e36639
Tim T <tim.email.personal@gmail.com>
2022-11-06 19:50:01 -0800

    Write function to merge duplicate ingredients

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,3 +1,4 @@
+const { testIngredients, recipeData, ingredientsData } = require("../data/testData")
 const Ingredient = require("./Ingredient")
 
 class Recipe {
@@ -8,7 +9,7 @@ class Recipe {
     this.instructions = recipe.instructions
     this.name = recipe.name
     this.tags = recipe.tags
- }
+  }
 
   getTotalCost() {
     let total = this.ingredients.reduce((acc, ingredient) => {
@@ -16,12 +17,38 @@ class Recipe {
     }, 0) / 100
     return `$${total.toFixed(2).toString()}`
   }
-  
+
   instantiateIngredients(recipe, ingredientsData) {
-    return recipe.ingredients.map(recipeObject => {
+    let a = recipe.ingredients.map(recipeObject => {
       let targetDatasetObject = ingredientsData.find(datasetObj => recipeObject.id === datasetObj.id)
       return new Ingredient(targetDatasetObject, recipeObject)
     })
+
+    return this.mergeIngredients(a)
+  }
+  mergeIngredients(ingredientsData) {
+    let counter = 0
+    let finalArray = []
+    const listUniqueIds = ingredientsData.reduce((acc, curr) => {
+      if (!acc.includes(curr.id)) {
+        acc.push(curr.id)
+      }
+      return acc
+    }, [])
+    for (var i = 0; i < listUniqueIds.length; i++) {
+      counter = 0
+      const matches = ingredientsData.filter((originalIngredient) => {
+        return listUniqueIds[i] === originalIngredient.id
+      })
+      const addedAmounts = matches.reduce((mergedIngredient, duplicate) => {
+        counter += duplicate.amount
+        mergedIngredient = duplicate
+        mergedIngredient.amount = counter
+        return mergedIngredient
+      }, {})
+      finalArray.push(addedAmounts)
+    }
+    return finalArray
   }
 }
 
