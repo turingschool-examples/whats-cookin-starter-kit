b2367e9ed4d92f3da94634c41960f5e7154b849e
El Brewster <brewsterel3@gmail.com>
2022-12-27 14:50:13 -0800

    Add basic api call functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,24 @@
-// Your fetch requests will live here!
+//--> fetchy fetch stuff:
+const usersURL = "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users";
+const ingredientsURL = "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients";
+const recipesURL = "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes";
+// const URLArray = [usersURL, ingredientsURL, recipesURL];
 
 
-console.log('I will be a fetch request!')
+
+
+const fetchUsersUrl = (usersURL) => {
+    fetch(usersURL);
+}
+
+const fetchIngredientsURL = (ingredientsURL) => {
+    fetch(ingredientsURL);
+}
+
+const fetchRecipesURL = (recipesURL) => {
+    fetch(recipesURL);
+}
+
+Promise.all([fetchUsersUrl, fetchIngredientsURL, fetchRecipesURL]).then((values) => {
+    console.log(values);
+});
