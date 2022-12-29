c6d2eb9b9d15eb878b3c730862e3adf03d8c8511
El Brewster <brewsterel3@gmail.com>
2022-12-28 16:03:38 -0800

    Add some testing and first method implementation

--- src/classes/RecipesContainer.js
+++ src/classes/RecipesContainer.js
@@ -1,8 +1,16 @@
 class RecipesContainer {
-  constructor() {
-
-    // One class to get you started!
+  constructor(testData) {
+    this.data = testData;
   }
+
+  tagRecipes(tag) {
+    const taggedRecipes = this.data.filter(element => {
+      return element.tags.includes(tag);
+    }).map(element1 => element1.id);
+    console.log(taggedRecipes);
+    return taggedRecipes;
+  }
+
 }
 
 export default RecipesContainer;
