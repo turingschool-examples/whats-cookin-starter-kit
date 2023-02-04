8b120be49b0c8830fa59b007436f63ac70c99621
Amber Shipley <ashipley2@me.com>
2023-02-04 14:20:04 -0800

    Added parameter to constructor, created properties for holding the data, and implemented filterByTag function

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,8 +1,16 @@
-class RecipeRepository {
-  constructor() {
+const recipeData = require('../data/recipes');
 
-    // One class to get you started!
+class RecipeRepository {
+  constructor(recipeData) {
+    this.recipeData = recipeData;
+    this.filteredRecipes;
+  }
+  filterByTag(tag) {
+    this.filteredRecipes = this.recipeData.filter(recipe => recipe.tags.includes(tag));
+  }
+  filterByName() {
+
   }
 }
 
-export default RecipeRepository;
+// export default RecipeRepository;
