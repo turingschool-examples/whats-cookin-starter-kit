ee96e99eb282435ac1ffd02c24ff7d776ad3cdd1
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-02-05 12:06:21 -0800

    Add testing properties for recipee for id, image, ingredients, instructions, name, and tag

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -8,14 +8,36 @@ describe("Recipe", () => {
       
     beforeEach(() => {
         recipe = new Recipe(sampleIngredientsData[0])
-        console.log(recipe)
     })
 
     it("should be a function", () => {
         expect(Recipe).to.be.a('function')
     })
 
-    it.only("should be an instance of")
+    it("should be an instance of Recipe", () => {
+        expect(recipe).to.be.an.instanceOf(Recipe)
+    })
+
+    it("should have an ID property", () => {
+        expect(recipe.id).to.deep.equal(sampleIngredientsData[0].id)
+    })
+
+    it("should have an image property", () => {
+        expect(recipe.image).to.deep.equal(sampleIngredientsData[0].image)
+    })
+
+    it("should have an ingredients property", () => {
+        expect(recipe.ingredients).to.deep.equal(sampleIngredientsData[0].ingredients)
+    })
+    it("should have an instructions property", () => {
+        expect(recipe.instructions).to.deep.equal(sampleIngredientsData[0].instructions)
+    })
+    it("should have a name property", () => {
+        expect(recipe.name).to.deep.equal(sampleIngredientsData[0].name);
+    });
+    it.only("should have a tags property", () => {
+        expect(recipe.tags).to.deep.equal(sampleIngredientsData[0].tags);
+    });
 })
 
 // Don't want to use recipee data but make a sample data file
