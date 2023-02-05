e21d8b7a02bdcf79e3a97c3bc73b6dcce06074c7
Kelli Watkins <watkinskellil@gmail.com>
2023-02-05 13:48:59 -0700

    test: Add function test

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -23,69 +23,6 @@ describe('Recipe', () => {
                   "unit": "tsp"
                 }
               },
-              {
-                "id": 1123,
-                "quantity": {
-                  "amount": 1,
-                  "unit": "large"
-                }
-              },
-              {
-                "id": 19335,
-                "quantity": {
-                  "amount": 0.5,
-                  "unit": "c"
-                }
-              },
-              {
-                "id": 19206,
-                "quantity": {
-                  "amount": 3,
-                  "unit": "Tbsp"
-                }
-              },
-              {
-                "id": 19334,
-                "quantity": {
-                  "amount": 0.5,
-                  "unit": "c"
-                }
-              },
-              {
-                "id": 2047,
-                "quantity": {
-                  "amount": 0.5,
-                  "unit": "tsp"
-                }
-              },
-              {
-                "id": 1012047,
-                "quantity": {
-                  "amount": 24,
-                  "unit": "servings"
-                }
-              },
-              {
-                "id": 10019903,
-                "quantity": {
-                  "amount": 2,
-                  "unit": "c"
-                }
-              },
-              {
-                "id": 1145,
-                "quantity": {
-                  "amount": 0.5,
-                  "unit": "c"
-                }
-              },
-              {
-                "id": 2050,
-                "quantity": {
-                  "amount": 0.5,
-                  "unit": "tsp"
-                }
-              }
             ],
             "instructions": [
               {
@@ -96,31 +33,12 @@ describe('Recipe', () => {
                 "instruction": "Add egg and vanilla and mix until combined.",
                 "number": 2
               },
-              {
-                "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
-                "number": 3
-              },
-              {
-                "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
-                "number": 4
-              },
-              {
-                "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
-                "number": 5
-              },
-              {
-                "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
-                "number": 6
-              }
             ],
             "name": "Loaded Chocolate Chip Pudding Cookie Cups",
             "tags": [
               "antipasti",
               "starter",
               "snack",
-              "appetizer",
-              "antipasto",
-              "hor d'oeuvre"
             ]
           })
         })
@@ -129,8 +47,17 @@ describe('Recipe', () => {
     expect(Recipe).to.be.a('function');
   });
 
-  it('test', () => {
-    console.log(recipe.ingredients)
-    console.log(recipe.calculateCost())
+  it('should be an instance of ingredient', () => {
+    expect(ingredient1).to.be.an.instanceOf(Ingredient);
+  });
+
+  it('', () => {
+    
   })
+
+  it('should be an instance of recipe', () => {
+    expect(recipe).to.be.an.instanceOf(Recipe);
+  });
+
+  
 })
