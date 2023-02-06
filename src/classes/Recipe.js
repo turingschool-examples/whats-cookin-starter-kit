7b9023b2a19fd02cd90ed3ba4b598f44351c89ee
Joe Fogiato <joe.fogiato@gmail.com>
2023-02-06 17:12:59 -0500

    Add: Recipe class file and Recipe Test file

---
+++ src/classes/Recipe.js
@@ -0,0 +1,12 @@
+class Recipe {
+  constructor(recipeData) {
+    this.id = recipeData.id;
+    this.image = recipeData.image;
+    this.ingredients = recipeData.ingredients;
+    this.instructions = recipeData.instructions;
+    this.name = recipeData.name;
+    this.tags = recipeData.tags;
+  }
+}
+
+export default Recipe;
