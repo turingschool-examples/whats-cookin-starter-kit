b24f4f5b8b0bdd59e58febc0e898b1b422895501
Winston Calhoun <superdevant@gmail.com>
2023-02-06 18:34:52 -0600

    Add lines 4-28 of our recipe repository testing suite

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,3 +1,6 @@
+// import recipe
+// import ingredient
+
 class RecipeRepository {
   constructor() {
 
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,28 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('Recipe', () => {
-  it('Should be a function', () => {
-    expect(RecipeRepository).to.be.a('function');
+  beforeEach(() => {
+    let recipes = new RecipeRepository(recipeData)
+  });
+
+  it.skip('Should be a function', () => {
+    assert.isFunction(RecipeRepository);
+  });
+
+  it.skip('Should contain recipe data', () => {
+    assert.equal(recipes.recipeList, recipeData);
+  });
+
+  it.skip('Should get recipes based on a tag', () => {
+    assert.equal(recipes.filterByTag(), 'eggs');
+  });
+
+  it.skip('Should get recipes based of ingredient', () => {
+    assert.equal(recipes.filterByName(), 'someName');
+  });
+
+  it.skip('Should contain a filtered list of returned results', () => {
+    assert.equal(recipes.filteredList, [{}]);
   });
 })
