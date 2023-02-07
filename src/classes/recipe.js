0599349a9170877f8c864c1247fcab6e2eeac99a
Quynh Luu <quynhtlluu@gmail.com>
2023-02-06 17:26:34 -0800

    Construct initial model of Recipe object

---
+++ src/classes/recipe.js
@@ -0,0 +1,11 @@
+class Recipe {
+    constructor(recipeData, ingredientsData) {
+        this.id = recipeData.id
+        this.image = recipeData.image
+        this.ingredients = recipeData.ingredients
+        this.instructions = recipeData.instructions
+        this.name = recipeData.name
+        this.tags = recipeData.tags
+        this.ingredientsData = ingredientsData
+    }
+}
