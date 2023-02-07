7f4635c6d81cb2403454861edbc878192cbed949
Tom Doder <tdoder89@gmail.com>
2023-02-07 17:38:52 -0600

    feat: creation of RecipeRepository classes and methods

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -2,15 +2,23 @@ import Recipe from "./Recipe";
 
 class RecipeRepository {
   constructor(recipeData) {
-    this.recipes = recipeData.map(recipe => new Recipe(recipe))
-
+    this.recipes = recipeData.map(recipe => new Recipe(recipe));
   }
+
   filterByTag(tag) {
     let filteredRecipes = this.recipes.filter(recipe => {
-      return recipe.tags.includes(tag)
-    })
-     return filteredRecipes
-  }
-}
+      return recipe.tags.includes(tag);
+    });
+     return filteredRecipes;
+  };
+
+  filterByName(name) {
+    let filteredNames = this.recipes.filter(recipe => {
+      return recipe.name.toLowerCase().includes(name.toLowerCase());
+    });
+    return filteredNames;
+  };
+   
+};
 
 export default RecipeRepository;
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -3,7 +3,7 @@ import Recipe from '../src/classes/Recipe';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('Recipe Repo', () => {
-  let recipes, recipeRepo
+  let recipes, recipeRepo;
   beforeEach(() => {
      recipes = [{
       "id": 595736,
@@ -333,22 +333,32 @@ describe('Recipe Repo', () => {
         "sauce"
       ]
     }];
-     recipeRepo = new RecipeRepository(recipes)
+     recipeRepo = new RecipeRepository(recipes);
+  });
 
-  })
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
   it('Should be an instance of RecipeRepository', () => {
-    expect(recipeRepo).to.be.an.instanceOf(RecipeRepository)
-  })
-  it('Should hold an array of recipes', () =>{
-    expect(recipeRepo.recipes).to.be.an.instanceOf(Array)
-    expect(recipeRepo.recipes[1].id).to.equal(678353)
-    expect(recipeRepo.recipes[2]).to.be.an.instanceOf(Recipe)
-  })
+    expect(recipeRepo).to.be.an.instanceOf(RecipeRepository);
+  });
+
+  it('Should hold an array of recipes', () => {
+    expect(recipeRepo.recipes).to.be.an.instanceOf(Array);
+    expect(recipeRepo.recipes[1].id).to.equal(678353);
+    expect(recipeRepo.recipes[2]).to.be.an.instanceOf(Recipe);
+  });
+
   it('Should filter recipe based on a tag', () => {
-    expect(recipeRepo.filterByTag("lunch")).to.deep.equal([new Recipe(recipes[1])])
-  })
+    expect(recipeRepo.filterByTag("lunch")).to.deep.equal([new Recipe(recipes[1])]);
+    expect(recipeRepo.filterByTag("starter")).to.deep.equal([new Recipe(recipes[0])]);
+    expect(recipeRepo.filterByTag("sauce")).to.deep.equal([new Recipe(recipes[2])]);
+  });
+
+  it('Should be able to filter recipe by name', () => {
+    expect(recipeRepo.filterByName("Loaded Chocolate Chip Pudding Cookie Cups")).to.deep.equal([new Recipe(recipes[0])]);
+    expect(recipeRepo.filterByName("PUDDING")).to.deep.equal([new Recipe(recipes[0])]);
+  });
   
-})
+});
