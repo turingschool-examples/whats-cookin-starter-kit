6cd3c8391224a10887dd3961f8e2368337791e6a
Quynh Luu <quynhtlluu@gmail.com>
2023-02-07 14:50:56 -0800

    Set up describe block for Recipe class

--- src/classes/recipe.js
+++ src/classes/recipe.js
@@ -14,7 +14,12 @@ class Recipe {
     calculateIngredientsCosts() {
 
     }
-    listIngredients() {
-        
+    returnIngredients() {
+
     }
 }
+
+/* It should have methods to:
+Determine the names of ingredients needed
+Get the cost of its ingredients
+Return its directions / instructions */
