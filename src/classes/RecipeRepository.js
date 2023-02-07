18f35a6efad9ec579a3f09045affbff028ec2f90
Joshua Martin <ursa8minority@gmail.com>
2023-02-07 16:26:30 -0700

    Feat:recipe repository class

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,6 +3,15 @@ class RecipeRepository {
     this.recipes = recipes
     // One class to get you started!
   }
+  getRecipeByTag(tag) {
+    const recipeTag = this.recipes.filter(recipe => recipe === tag);
+    console.log(recipeTag)
+    }
+
+    getRecipeByName(name){
+      const recipeName = this.recipes.filter(recipe => recipe === name);
+      console.log(recipeName)
+    }
 }
 
 export default RecipeRepository;
