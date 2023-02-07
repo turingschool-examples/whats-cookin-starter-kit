677a66e911106abec7161d305abc0e8d4e9efbd8
Michael Lobato <mike.lobato.wft@gmail.com>
2023-02-07 15:51:11 -0800

    Add import data and a describe block

---
+++ test/User-test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import User from '../src/classes/User';
+import sampleUserData from '../src/data/sample-users';
+import ingredientsData from '../src/data/ingredients';
+import sampleRecipeData from '../src/data/sample-recipe-data';
+
+describe('User', () => {
+    let firstUser
+
+})
