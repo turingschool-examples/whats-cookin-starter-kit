aef7bb46ee3b6f1c7531e177fca8bcf51f94fffb
Tom Doder <tdoder89@gmail.com>
2023-02-08 15:32:16 -0600

    feat: import recipe classes and start on first function

--- src/scripts.js
+++ src/scripts.js
@@ -2,14 +2,15 @@
 // import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
-import './classes/Recipe'
-import './classes/RecipeRepository'
+
+import Recipe from './classes/Recipe'
+import RecipeRepository from './classes/RecipeRepository'
+import ingredientsData from './data/ingredients'
+import recipeData from './data/recipes'
 
 // global variables
 const recipeSection = document.getElementById('allRecipes')
-
-
-
+const recipeRepo = new RecipeRepository(recipeData)
 
 
 //event listeners
@@ -19,13 +20,11 @@ window.addEventListener('load', createRecipeCards)
 
 //functions
 function createRecipeCards() {
-    let count = 0
-    recipeNames.forEach(recipe => {
-        count++
+    recipeRepo.recipes.forEach(recipe => {
         let size = (2 - (recipe.length / 65)).toFixed(2)
         recipeSection.innerHTML += `
-        <article class="recipe-card" id="recipe${count}">
-            <img class="recipe-img" src="../src/images/cookies-placeholder.jpeg" alt="Cookies placeholder">
+        <article class="recipe-card" id="${recipe.id}">
+            <img class="recipe-img" src="${recipe.image}" alt="picture of ${recipe.name}">
             <img class="star-icon hidden" id="star-icon" src="../src/images/star-icon.png" alt="This recipe is in my recipes!">
             <h3 style="font-size: ${size}rem">${recipe}</h3>
         </article>`
