4b021eedc7654b2a13ff3af63bd665befbc2c61e
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-02-08 15:11:04 -0700

    Update ingredient class and testing suite

--- src/classes/Ingredient.js
+++ src/classes/Ingredient.js
@@ -1,12 +1,25 @@
 import ingredientTestData from "../data/ingredientTestData";
+import ingredientsData from "../data/ingredients";
+import recipeData from "../data/recipes";
 
 
 class Ingredient {
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
-    this.costInCents = data['estimatedCostInCents'];
-  }
-}
+    this.costInCents = data.estimatedCostInCents;
+  };
+//return ingredient by name using id
+//return cost using id 
+
+  returnIngredientByName(num) {
+    const findName = ingredientsData.find(ingredient => ingredient.id === num);
+    if (findName === undefined) {
+      return "Error"
+    } else {
+      return findName.name;
+    }
+  };
+};
 
 export default Ingredient;
--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -3,51 +3,36 @@ import Ingredient from '../src/classes/Ingredient'
 import ingredientTestData from '../src/data/ingredientTestData';
 
 describe('Ingredient', () => {
-  let ingredient
-  beforeEach(()=> {
-    // console.log(ingredientTestData)
-    ingredient = new Ingredient(ingredientTestData[0])
-    // console.log('line 10:', ingredient)
-  })
+  let ingredient;
 
-  it('Should be a function', ()=> {
+  beforeEach(() => {
+    ingredient = new Ingredient(ingredientTestData[0]);
+  });
+
+  it('should be a function', () => {
     assert.isFunction(Ingredient);
-  })
+  });
 
-  it('Should be able to create an instance of Ingredient', ()=> {
-  assert.instanceOf(ingredient, Ingredient)
-  })
+  it('should be able to create an instance of Ingredient', () => {
+  assert.instanceOf(ingredient, Ingredient);
+  });
 
-  it('Should have an id number when created', ()=> {
+  it('should have an id number when created', () => {
     assert.equal(ingredient.id, 20081);
-  })
-  it('Should have a name when created', ()=> {
+  });
+
+  it('should have a name when created', () => {
     assert.equal(ingredient.name, 'wheat flour');
-  })
-  it('Should have an estimated cost in cents (per unit)', ()=> {
+  });
+
+  it('should have an estimated cost in cents (per unit)', () => {
     assert.equal(ingredient.costInCents, 142);
-  })
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+  it('should return ingredients by name', () => {
+    const findName = ingredient.returnIngredientByName(20081);
+    const wrongName = ingredient.returnIngredientByName(2);
+    assert.equal(findName, 'wheat flour');
+    assert.equal(wrongName, 'Error');
+  });
+});
