05bf00437eb5791f9db306c88643e8cf0659b5f1
Tom Doder <tdoder89@gmail.com>
2023-02-08 16:22:21 -0600

    feat: create recipe modal function

--- src/scripts.js
+++ src/scripts.js
@@ -12,11 +12,11 @@ import recipeData from './data/recipes'
 // global variables
 const recipeSection = document.getElementById('allRecipes')
 const recipeRepo = new RecipeRepository(recipeData)
-
+const modalSection = document.getElementById('recipeModalBackground')
 
 //event listeners
 window.addEventListener('load', createRecipeCards)
-
+recipeSection.addEventListener('click', createRecipeModal)
 
 
 //functions
@@ -30,4 +30,24 @@ function createRecipeCards() {
             <h3 style="font-size: ${size}rem">${recipe.name}</h3>
         </article>`
     })
+}
+
+function createRecipeModal(event) {
+let recipeID = event.target.c
+   modalSection.innerHTML = `
+    <div class="recipe-popup">
+    <h2>${}</h2>
+    <div class="image-ingredients">
+      <img class="recipe-img" src="../src/images/cookies-placeholder.jpeg" alt="Cookies placeholder">
+      <ul class="ingredient-list">
+        <h3>Ingredients:</h3>
+        <li>1.5 cups - wheat flour</li>
+    </ul>
+  </div>
+    <ol class="direction-list">
+      <h3>Directions:</h3>
+      <li>In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.</li>
+  </ol>
+  <h2>TOTAL COST $10</h1>
+  </div>`
 }
