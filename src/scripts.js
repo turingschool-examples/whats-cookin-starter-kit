b86e1873b78f798516b304697ae2d645f47038aa
Tom Doder <tdoder89@gmail.com>
2023-02-08 15:17:46 -0600

    fix: reorganize scripts file and import classes

--- src/scripts.js
+++ src/scripts.js
@@ -2,66 +2,22 @@
 // import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
+import './classes/Recipe'
+import './classes/RecipeRepository'
 
-
-
-
-const recipeNames = [
-    'Loaded Chocolate Chip Pudding Cookie Cups',
-    'Maple Dijon Apple Cider Grilled Pork Chops',
-    "Dirty Steve's Original Wing Sauce",
-    'Elvis Pancakes',
-    'Mock Udi’s Gluten Free Whole Grain Bread',
-    'Ambrosia Cupcakes',
-    'Creamy Coconut Yogurt Bowl with Chocolate Granola (Video)',
-    'Sesame Cookies',
-    'Thai Chicken Tenders with Broiled Pineapple Slaw',
-    'Egg and Rapini Casserole',
-    'Pulled Pork',
-    'Pumpkin Cheesecake Breakfast Smoothie',
-    'Cinnamon Raisin Overnight French Toast w/ Apple Filling',
-    'Brown Butter Garlic Shrimp',
-    'Baked Stuffed Artichokes',
-    'Puppy Chow Cookies',
-    'Easy Creamy Potato Salad with Yogurt',
-    'Slow-Cooker Italian-Stuffed Peppers',
-    'Whole Wheat Milk and Honey Sandwich Bread',
-    'The Ultimate Healthy Soft & Chewy Pumpkin Chocolate Chip Cookies',
-    'Butterscotch Pudding',
-    'quinoa tabbouleh',
-    'Birthday Cake Jello Shots',
-    'Artichoke Spinach Dip Stuffed Mushrooms',
-    'Baked Manicotti with Sausage and Peas',
-    'Spinach Artichoke Quinoa Casserole',
-    'Double Raspberry Soufflés',
-    'A Cake To Warm Any Heart – Banana Split Cake',
-    'Avocado Chickpea Salad',
-    'Slow Cooker Peach Cobbler',
-    '4 Cheese White Pizza',
-    'Cereal Marshmallow Bars',
-    'Hummus Deviled Eggs',
-    'Creamsicle Pie',
-    'Smothered Green Beans',
-    'Pear & Walnut Salad with a Pear Vinaigrette',
-    'Mexican Vegetables on Cornbread',
-    'Barbecue Shrimp Stir-Fry',
-    'Clayudas',
-    'Farro with Shiitake Mushrooms and Baby Spinach',
-    'Bang Bang Shrimp with Napa Cabbage Slaw',
-    'Homemade Italian Meatballs',
-    'Curried Strawberry Chicken Salad',
-    'Bacon Wrapped Stuffed Pork Tenderloin',
-    'Rolo Cookie Bars',
-    'Buffalo Chicken Sliders',
-    "Reese's Pieces Peanut Butter Cookies",
-    'Farmer’s Market Flatbread Pizza',
-    'Vegan Lentil Loaf',
-    'Pastry Cream'
-  ]
-
+// global variables
 const recipeSection = document.getElementById('allRecipes')
+
+
+
+
+
+//event listeners
 window.addEventListener('load', createRecipeCards)
 
+
+
+//functions
 function createRecipeCards() {
     let count = 0
     recipeNames.forEach(recipe => {
