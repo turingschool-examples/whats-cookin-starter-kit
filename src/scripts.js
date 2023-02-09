fa146e53f779a1ae6b2ead020e025d4be19240c9
Quynh Luu <quynhtlluu@gmail.com>
2023-02-08 18:57:38 -0800

    Add browser tab icon and change default font

--- src/scripts.js
+++ src/scripts.js
@@ -6,3 +6,10 @@ import './images/turing-logo.png'
 
 
 console.log('Hello world');
+
+/*
+As a user, I should be able to view a list of all recipes.
+As a user, I should be able to click on a recipe to view more information including directions, ingredients needed, and total cost.
+As a user, I should be able to filter recipes by a tag. (Extension option: by multiple tags)
+As a user, I should be able to search recipes by their name. (Extension option: by name or ingredients)
+*/
