121ca8246a94c142e9d7c369e32012e6051c4c9d
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-02-08 22:22:11 -0800

    Update the user class with methods to create a recipe list, add and remove recipes, and find recipes, and update the the user test file

--- src/classes/userClass.js
+++ src/classes/userClass.js
@@ -1,16 +1,41 @@
-import usersTestData from "../data/userTestData";
+import RecipeRepository from "./RecipeRepository";
+import Recipe from "./recipeClass";
 
 class User {
-  constructor(){
-    // 
-    // array of saved recipesToCook
-    // 
-  }
+  constructor(userInfo){
+    this.name = userInfo.name;
+    this.id = userInfo.id;
+    this.pantryItems = userInfo.pantry;
+    this.recipesToCook = new RecipeRepository();
+    this.totalRecipes;
+  };
 
-  // method to add/ from the recipesTo Cook
-  // method to remove from the recipesTo Cook
+  makeRecipeList(array){
+    this.totalRecipes = new RecipeRepository(array);
+  };
+  // need to make a test to test this method too
 
-  // method to use the filter methods from recipie repo
-}
+  addRecipeToCook(item) {
+    // need the item that is passing through the be an instance of a new recipe in some way? maybe this happens in the recipe class too
+    //  in the test file I could also have this check that they both have the new instance of recipe in there
+    this.recipesToCook.recipeList.push(item);
+    return this.recipesToCook.recipeList;  
+  };
+
+  removeRecipeFromCook(item) {
+    this.recipesToCook.recipeList.splice(item, 1);
+    return this.recipesToCook.recipeList;
+  };
+
+  findRecipeByTag(thing) {
+    const findTag = this.recipesToCook.filterByTag(thing);
+    return findTag;
+  };
+
+  findRecipeByTag(thing) {
+    const findName = this.recipesToCook.filterByName(thing);
+    return findName;
+  };
+};
 
 export default User;
--- test/user-test.js
+++ test/user-test.js
@@ -1,34 +1,52 @@
 import { assert, expect } from 'chai';
 import User from '../src/classes/userClass';
 import usersTestData from '../src/data/userTestData';
+import recipeTestData from '../src/data/recipeTestData';
+import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('User', () => {
   let user
   beforeEach(() => {
-    user = new User();
+    user = new User(usersTestData[0]);
+    user.makeRecipeList(recipeTestData);
   });
 
-  it.skip('should be an instance of User', () => {
+  it('should be an instance of User', () => {
     assert.instanceOf(user, User);
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     assert.isFunction(User);
   });
 
-  it.skip('should have a recipe to cook list', () => {
-
+  it('should have a name, Id, and pantry', () => {
+    assert.equal(user.name, usersTestData[0].name);
+    assert.equal(user.id, usersTestData[0].id);
+    assert.equal(user.pantryItems, usersTestData[0].pantry);
   });
 
-  it.skip('should be able to add or remove a recipe from their list', () => {
+  it('should have a recipe to cook list', () => {
+    assert.instanceOf(user.recipesToCook, RecipeRepository);
+  });
 
+  it('should be able to add or remove a recipe from their list', () => {
+    user.addRecipeToCook(recipeTestData[0]);
+    assert.equal(user.recipesToCook.recipeList[0], user.totalRecipes.recipeList[0]);
+    
+    user.addRecipeToCook(recipeTestData[1]);
+    assert.equal(user.recipesToCook.recipeList.length, 2);
+    
+    user.removeRecipeFromCook(user.recipesToCook.recipeList[1], 1);
+    assert.deepEqual(user.recipesToCook.recipeList.length, 1);
   });
 
   it.skip('should be able to filter my recipes by tag', () => {
-
+    user.addRecipeToCook(recipeTestData[1]);
+    user.findRecipeByTag('lunch');
+    assert.equal(user.totalRecipes.filteredList[0], recipeTestData[1]);
   });
 
   it.skip('should be able to filter my recipes by name', () => {
-
+    assert.equal();
   });
 });
