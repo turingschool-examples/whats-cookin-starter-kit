9ebdce2c6e8995f5e34f7cee4b2ddfe2d331d83f
Joshua Martin <ursa8minority@gmail.com>
2023-02-09 13:49:58 -0700

    Cleanup Test

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
-import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/recipe';
 import sampleData from '../src/sampleData';
+import RecipeRepository from '../src/classes/RecipeRepository';
 
-describe('Recipe', () => {
+
+describe('RecipeRepository', () => {
   let recipe
 
   beforeEach(() => {
@@ -46,3 +47,5 @@ describe('Recipe', () => {
     expect(result4).to.deep.equal([]);
   });
 })
+
+yes
