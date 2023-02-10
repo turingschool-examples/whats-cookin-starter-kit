2c89a844459f2239e44ffda00d69b7f452693154
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-02-10 12:54:01 -0800

    Add method to populate page with recipies and update the styling for them

--- src/scripts.js
+++ src/scripts.js
@@ -2,43 +2,66 @@ import './styles.css';
 import apiCalls from './apiCalls';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe'
+import recipeData from './data/recipes';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+
 
 //header that welcomes user based on user data
 
 //even handler /listener for form and form data
 //search bar will return recipe based on taking in
 //name or tag from recipeRepository
-const recipeCard = new Recipe();
-const globalArray = [];
+// const recipeInfo = new Recipe();
+// const globalArray = [];
 
-const searchBtn = document.querySelector("#searchButton");
-const savedBtn = document.querySelector("#savedButton");
-const userNamePrompt = document.querySelector("#userName");
-const savedView = document.querySelector("#savedCards");
+const searchBarBtn = document.querySelector('#searchButton');
+const savedViewBtn = document.querySelector('#savedViewButton');
+const userNamePrompt = document.querySelector('#userName');
+const cardTileDisplay = document.querySelector('#cardTileView');
 
-searchBtn.addEventListener( 'click', function() {
-    getRecipeByTag();
-    getRecipeByName();
+// Event Listeners
+// window.addEventListener('load', function(){
+//     console.log('hello')
+// });
+window.addEventListener('load', () => {insertRecipeCards(recipeData)})
+
+searchBarBtn.addEventListener( 'click', function() {
+  getRecipeByTag();
+  getRecipeByName();
 });
 
+// Event handlers 
 function getRecipeByTag(userInput) {
-    if ( userInput.includes(RecipeRepository.filteredList.filterByTag()))
+  if ( userInput.includes(RecipeRepository.filteredList.filterByTag()));
 };
 
 function getRecipeByName() {
-
+    
 };
 
+function insertRecipeCards(array) {
+  for(let i = 0; i < array.length; i++){
+    cardTileDisplay.innerHTML += 
+      `<section class="card" id="${array[i].id}">
+      <h2>${array[i].name}</h2>
+      <img src="${array[i].image}" alt="image of ${array[i].name}">
+      <div class="rating">*****</div>
+      </section>`;
+  };
+};
+// Functions
+// this will populate all the cards in an array to either using the saved recipes or the original load recipes
+
+// Helper Functions
 function show(element) {
-    element.classList.remove('hidden');
+  element.classList.remove('hidden');
 };
   
 function hide(element) {
-    element.classList.add('hidden');
+  element.classList.add('hidden');
 };
 
 function getRandomIndex(array) {
-    return Math.floor(Math.random() * array.length);
+  return Math.floor(Math.random() * array.length);
 };
