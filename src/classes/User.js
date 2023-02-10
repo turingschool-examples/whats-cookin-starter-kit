ea6334714a1566e0c1cd687df88e9cde07fe4454
Jesus Borjas <jesusbor006@gmail.com>
2023-02-10 15:45:07 -0600

    Add favorite functionality

--- src/classes/User.js
+++ src/classes/User.js
@@ -4,34 +4,31 @@ class User {
     this.id = user.id;
     this.pantry = user.pantry;
     this.favorites = [];
-  };
+  }
 
   recipesToCook(recipe) {
-    this.favorites.push(recipe);
-  };
+    if (!this.favorites.includes(recipe)) {
+      this.favorites.push(recipe);
+    }
+  }
 
   removeRecipes(recipe) {
     this.favorites.splice(recipe, 1);
-  };
+  }
 
   filterTag(tag) {
     const filteredRecipe = this.favorites.filter((recipe) => {
       return recipe.tags.includes(tag);
     });
     return filteredRecipe;
-  };
+  }
 
   filterName(name) {
     const filteredName = this.favorites.filter((recipe) => {
       return recipe.name.includes(name);
     });
     return filteredName;
-  };
-
-};
-
-
-
-
+  }
+}
 
 export default User;
