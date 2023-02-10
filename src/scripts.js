45436e8eaf631ba66d4aa85e55cf248e40146a43
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-02-09 20:11:16 -0700

    Add skeleton for search btn event listeners and add getRandomIndex function

--- src/scripts.js
+++ src/scripts.js
@@ -16,15 +16,37 @@ const globalArray = [];
 
 
 const searchBtn = document.querySelector(".search-button");
-const breakfastBtn = document.querySelector(".breakfast-bar");
-const lunchBtn = document.querySelector(".lunch-bar");
-const dinnerBtn = document.querySelector(".dinner-bar");
-const dessertBtn = document.querySelector(".dessert-bar");
-const condimentsBtn = document.querySelector(".condiments-bar");
-const sidesBtn = document.querySelector(".sides-bar");
-const appetizersBtn = document.querySelector(".appetizers-bar");
-const saladsBtn = document.querySelector(".salads-bar");
+// const breakfastBtn = document.querySelector(".breakfast-bar");
+// const lunchBtn = document.querySelector(".lunch-bar");
+// const dinnerBtn = document.querySelector(".dinner-bar");
+// const dessertBtn = document.querySelector(".dessert-bar");
+// const condimentsBtn = document.querySelector(".condiments-bar");
+// const sidesBtn = document.querySelector(".sides-bar");
+// const appetizersBtn = document.querySelector(".appetizers-bar");
+// const saladsBtn = document.querySelector(".salads-bar");
 
 
 
 
+const searchBtn = document.querySelector(".search-button");
+const userNamePrompt = document.querySelector("#userName")
+
+searchBtn.addEventListener( 'click', function() {
+    getRecipeByTag();
+    getRecipeByName();
+});
+
+function getRecipeByTag(userInput) {
+    if ( userInput.includes(RecipeRepository.filteredList.filterByTag()))
+
+
+};
+
+function getRecipeByName() {
+
+
+};
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+};
