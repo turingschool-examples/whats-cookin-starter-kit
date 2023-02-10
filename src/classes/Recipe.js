1b5ea7760d10596fc09cc46595ee03e204d85578
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-02-10 12:48:18 -0800

    Update file names to remove errors when running page

---
+++ src/classes/Recipe.js
@@ -0,0 +1,43 @@
+import Ingredient from './Ingredient';
+
+class Recipe {
+  constructor(recipe) {
+    this.id = recipe.id;
+    this.image = recipe.image;
+    this.ingredients = recipe.ingredients;
+    this.instructions = recipe.instructions;
+    this.name = recipe.name;
+    this.tags = recipe.tags;
+  };
+
+  retrieveIngredientInfo() {
+    const currentIngredients = this.ingredients.map(i => {
+      let newIng = new Ingredient(i);
+      newIng.name = newIng.returnIngredientName(i.id);
+      newIng.costInCents = newIng.returnIngredientCost(i.id);
+      newIng.quantity = i.quantity;
+      return newIng;
+    });
+    this.ingredients = currentIngredients;
+    return this.ingredients;
+  };
+
+  returnCostOfIngredients() {
+    const total = this.ingredients.reduce((total, cur) => {
+      total += (cur.costInCents * cur.quantity.amount);
+      return total;
+    }, 0);
+    const sum = (total / 100).toFixed(2);
+    return sum;
+  };
+
+  giveInstructionsForRecipe() {
+    const steps = this.instructions.map(instruction => {
+      return `Step ${instruction.number}: ${instruction.instruction}`
+    });
+    this.instructions = steps;
+    return this.instructions;
+  };
+};
+
+export default Recipe;
