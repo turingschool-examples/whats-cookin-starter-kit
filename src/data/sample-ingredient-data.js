95e0e85dc8bb2c068a39be9369b25cff77ed52a0
Amber Shipley <ashipley2@me.com>
2023-02-10 19:31:29 -0800

    Update sample data

--- src/data/sample-ingredient-data.js
+++ src/data/sample-ingredient-data.js
@@ -23,6 +23,51 @@ const sampleIngredientsData = [
       "id": 19206,
       "name": "instant vanilla pudding",
       "estimatedCostInCents": 660
+    },
+    {
+      "id": 19334,
+      "name": "brown sugar",
+      "estimatedCostInCents": 559
+    },
+    {
+      "id": 2047,
+      "name": "salt",
+      "estimatedCostInCents": 280
+    },
+    {
+      "id": 1012047,
+      "name": "fine sea salt",
+      "estimatedCostInCents": 528
+    },
+    {
+      "id": 10019903,
+      "name": "semi sweet chips",
+      "estimatedCostInCents": 253
+    },
+    {
+      "id": 1145,
+      "name": "unsalted butter",
+      "estimatedCostInCents": 617
+    },
+    {
+      "id": 2050,
+      "name": "vanilla",
+      "estimatedCostInCents": 926
+    },
+    {
+      "id": 1009016,
+      "name": "apple cider",
+      "estimatedCostInCents": 468
+    },
+    {
+      "id": 9003,
+      "name": "apple",
+      "estimatedCostInCents": 207
+    },
+    {
+      "id": 20027,
+      "name": "corn starch",
+      "estimatedCostInCents": 236
     }
 ]
 
