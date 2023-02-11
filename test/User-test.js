cd239ff35211897d10415cd82c3995bbdaf681c2
Amber Shipley <ashipley2@me.com>
2023-02-10 19:32:00 -0800

    Delete unused import data file

--- test/User-test.js
+++ test/User-test.js
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
 import User from '../src/classes/User';
 import sampleUserData from '../src/data/sample-users';
-import ingredientsData from '../src/data/ingredients';
 import sampleRecipeData from '../src/data/sample-recipe-data';
 
 describe('User', () => {
