ec5c29b0eb38bd73449086357bc2ffcac6b61e06
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-02-10 17:31:50 -0800

    Refactor exiting code and wrap existing code in a function to be called after the promises have been resolved

--- src/scripts.js
+++ src/scripts.js
@@ -10,144 +10,160 @@ import Recipe from './classes/Recipe'
 MicroModal.init()
 
 const usersDataFetch = fetch(
-  "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users"
+    "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users"
 ).then((response) => response.json());
 const ingredientsDataFetch = fetch(
-  "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
+    "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
 ).then((response) => response.json());
 const recipesDataFetch = fetch(
-  "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes"
+    "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes"
 ).then((response) => response.json());
 
-let allCookingData = { users: [], ingredients: [], recipes: [] };
+let allCookingData = {
+    users: [],
+    ingredients: [],
+    recipes: []
+};
 
 let recipeRepository;
 let user;
 
 Promise.all([usersDataFetch, ingredientsDataFetch, recipesDataFetch])
-  .then((data) => {
-    console.log("1111", data);
-    allCookingData.users = data[0];
-    allCookingData.ingredients = data[1];
-    allCookingData.recipes = data[2];
-    console.log("allcookingdata", allCookingData)
-    return allCookingData;
-  })
-  .then(
-    (allCookingData) =>
-      (recipeRepository = new RecipeRepository(allCookingData.recipes.recipeData))
-  )
-  .then(
-    (allCookingData) =>
-      (user = new User(allCookingData.users.usersData))
-  );
-
-const saved = []
-
-var morningMeal = ['breakfast', 'morning meal, ']
-var snack = ['dip',  'snack',  'appetizer']
-var other = [ 'condiment', 'spread']
-var mainDish = ['main dish', 'dinner', 'lunch']
-var complimentaryDish = ['antipasti', 'hor d\'oeuvre', 'starter', 'salad', 'side dish',  'appetizer']
-
-const navigationSection = document.querySelector(".navigation-section")
-const recipeSection  = document.querySelector(".recipe-section")
-const pantrySection = document.querySelector(".pantry-section")
-const savedRecipes = document.querySelector("#saved-recipes")
-const navigationSeciton = document.querySelector(".navigation-section")
-const recipes = document.querySelector(".recipe")
-
-const topButton = document.querySelector("#top-button")
-const allRecipes = document.querySelector("#recipe-button")
-const breakfastFilter = document.querySelector("#breakfast-filter")
-const snacksAppFilter = document.querySelector("#snack-appetizers-filter")
-const brunchFilter = document.querySelector("#brunch-filter")
-const mainDishFilter = document.querySelector("#main-dish-filter")
-const compDishFilter = document.querySelector("#complimentary-dish-filter")
-const searchBar = document.querySelector(".search-bar")
-const searchGo = document.querySelector("#search-button")
-const pantryButton = document.querySelector("#your-pantry")
-const buttons = document.querySelectorAll('button')
-const recipeModal = document.querySelector('#modal')
-
-
-for(var i of buttons) {
-    i.addEventListener('click', function() {
-        if(event.target.id !== "top-button") {hideAll()}
-      });
-}
-topButton.addEventListener('click', function() {document.documentElement.scrollTop = 0})
-allRecipes.addEventListener('click', showAllRecipes)
-savedRecipes.addEventListener('click', displaySavedRecipes)
-breakfastFilter.addEventListener('click', function() {showFilteredRecipes(morningMeal)})
-snacksAppFilter.addEventListener('click', function() {showFilteredRecipes(snack)})
-brunchFilter.addEventListener('click', function() {showFilteredRecipes(other)})
-mainDishFilter.addEventListener('click', function() {showFilteredRecipes(mainDish)})
-compDishFilter.addEventListener('click', function() {showFilteredRecipes(complimentaryDish)})
-searchGo.addEventListener('click', function() {searchRecipeByName()})
-pantryButton.addEventListener('click', displayPantry)
-recipeSection.addEventListener('click', (event) => {
-    assignCurrentRecipe(event)
-    renderCurrentRecipe()
-})
-
-let currentDisplayedRecipes = []
-let currentRecipeId
-let currentRecipe
-
-    // for(var i = 0; i < recipeRepository.recipes.length; i++) {
-    //     currentDisplayedRecipes.push(recipeData[i])
-    // }
-    // generateRandUser()
-    // popularRecipes()
-
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 350) {
-        topButton.style.display = "block";
-      } else {
-        topButton.style.display = "none";
-    }
-}
-
-function assignCurrentRecipe(event) {
-    if (event.target.dataset.allRecipes) {
-        currentRecipeId = event.target.dataset.allRecipes
-    }
-    else {
-        currentRecipeId = event.target.parentElement.dataset.allRecipes
-    }
-    if (!currentRecipeId) {
-        return
-    }
-
-    currentRecipeId = parseInt(currentRecipeId)
-    currentRecipe = recipeRepository.recipes.filter(recipe => recipe.id === currentRecipeId) 
-    currentRecipe = new Recipe(currentRecipe[0])
-}
-
-function renderCurrentRecipe() {
-    recipeModal.innerHTML = ''
-    fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
-    .then((response) => response.json())
     .then((data) => {
-    let ingredients = currentRecipe.determineRecipeIngredients(data.ingredientsData)
-    // const ingredientsQuantity = currentRecipe.map(recipe => recipe.ingredients.map(ingredient => ingredient.quantity))
+        console.log("1111", data);
+        allCookingData.users = data[0];
+        allCookingData.ingredients = data[1];
+        allCookingData.recipes = data[2];
+        console.log("allcookingdata", allCookingData)
+        return allCookingData;
+    })
+    .then(
+        (allCookingData) => {
+            recipeRepository = new RecipeRepository(allCookingData.recipes.recipeData)
+            let num = Math.floor(Math.random() * allCookingData.users.usersData.length);
+            user = new User(allCookingData.users.usersData[num])
+            loadPage(recipeRepository, user, allCookingData.ingredients.ingredientsData)
+        }
+    )
 
-    const ingredientsHTML = ingredients.map(ingredient => {
-        return '<li>' + ingredient.ingredient + '</li>'
-    }).join('')
+function loadPage(recipeRepository, user, ingredientsData) {
 
-    if(saved.filter(current => current.id === currentRecipe.id).length !== 0) {
-        var isSaved = "Saved"
-    } else {
-        var isSaved = "♥️"
+    const saved = []
+
+    var morningMeal = ['breakfast', 'morning meal, ']
+    var snack = ['dip', 'snack', 'appetizer']
+    var other = ['condiment', 'spread']
+    var mainDish = ['main dish', 'dinner', 'lunch']
+    var complimentaryDish = ['antipasti', 'hor d\'oeuvre', 'starter', 'salad', 'side dish', 'appetizer']
+
+    const navigationSection = document.querySelector(".navigation-section")
+    const recipeSection = document.querySelector(".recipe-section")
+    const pantrySection = document.querySelector(".pantry-section")
+    const savedRecipes = document.querySelector("#saved-recipes")
+    const navigationSeciton = document.querySelector(".navigation-section")
+    const recipes = document.querySelector(".recipe")
+
+    const topButton = document.querySelector("#top-button")
+    const allRecipes = document.querySelector("#recipe-button")
+    const breakfastFilter = document.querySelector("#breakfast-filter")
+    const snacksAppFilter = document.querySelector("#snack-appetizers-filter")
+    const brunchFilter = document.querySelector("#brunch-filter")
+    const mainDishFilter = document.querySelector("#main-dish-filter")
+    const compDishFilter = document.querySelector("#complimentary-dish-filter")
+    const searchBar = document.querySelector(".search-bar")
+    const searchGo = document.querySelector("#search-button")
+    const pantryButton = document.querySelector("#your-pantry")
+    const buttons = document.querySelectorAll('button')
+    const recipeModal = document.querySelector('#modal')
+
+
+    for (var i of buttons) {
+        i.addEventListener('click', function () {
+            if (event.target.id !== "top-button") {
+                hideAll()
+            }
+        });
+    }
+    topButton.addEventListener('click', function () {
+        document.documentElement.scrollTop = 0
+    })
+    allRecipes.addEventListener('click', showAllRecipes)
+    savedRecipes.addEventListener('click', displaySavedRecipes)
+    breakfastFilter.addEventListener('click', function () {
+        showFilteredRecipes(morningMeal)
+    })
+    snacksAppFilter.addEventListener('click', function () {
+        showFilteredRecipes(snack)
+    })
+    brunchFilter.addEventListener('click', function () {
+        showFilteredRecipes(other)
+    })
+    mainDishFilter.addEventListener('click', function () {
+        showFilteredRecipes(mainDish)
+    })
+    compDishFilter.addEventListener('click', function () {
+        showFilteredRecipes(complimentaryDish)
+    })
+    searchGo.addEventListener('click', function () {
+        searchRecipeByName()
+    })
+    // pantryButton.addEventListener('click', displayPantry)
+    recipeSection.addEventListener('click', (event) => {
+        assignCurrentRecipe(event)
+        renderCurrentRecipe()
+    })
+
+    let currentDisplayedRecipes = []
+    let currentRecipeId
+    let currentRecipe
+
+    for (var i = 0; i < recipeRepository.recipes.length; i++) {
+        currentDisplayedRecipes.push(recipeData[i])
+    }
+    popularRecipes()
+
+    window.onscroll = function () {
+        if (document.documentElement.scrollTop > 350) {
+            topButton.style.display = "block";
+        } else {
+            topButton.style.display = "none";
+        }
     }
 
-    const instructionsHTML = currentRecipe.returnInstructions().map(instruction => {
-        return '<li>' + instruction + '</li>' 
-    }).join('')
-    recipeModal.innerHTML = 
-    `
+    function assignCurrentRecipe(event) {
+        if (event.target.dataset.allRecipes) {
+            currentRecipeId = event.target.dataset.allRecipes
+        } else {
+            currentRecipeId = event.target.parentElement.dataset.allRecipes
+        }
+        if (!currentRecipeId) {
+            return
+        }
+
+        currentRecipeId = parseInt(currentRecipeId)
+        currentRecipe = recipeRepository.recipes.filter(recipe => recipe.id === currentRecipeId)
+        currentRecipe = new Recipe(currentRecipe[0])
+    }
+
+    function renderCurrentRecipe() {
+        recipeModal.innerHTML = ''
+                let ingredients = currentRecipe.determineRecipeIngredients(ingredientsData)
+
+                const ingredientsHTML = ingredients.map(ingredient => {
+                    return '<li>' + ingredient.ingredient + '</li>'
+                }).join('')
+
+                if (saved.filter(current => current.id === currentRecipe.id).length !== 0) {
+                    var isSaved = "Saved"
+                } else {
+                    var isSaved = "♥️"
+                }
+
+                const instructionsHTML = currentRecipe.returnInstructions().map(instruction => {
+                    return '<li>' + instruction + '</li>'
+                }).join('')
+                recipeModal.innerHTML =
+                    `
     <header class="modal__header">
           <h2 class="modal__title" id="modal-1-title">
             ${currentRecipe.name}
@@ -163,141 +179,121 @@ function renderCurrentRecipe() {
           <ol type="1">
             ${instructionsHTML}
           </ol>
-          <h4>Recipe Cost:$${currentRecipe.calculateRecipeCost(data.ingredientsData)}</h4>
+          <h4>Recipe Cost:$${currentRecipe.calculateRecipeCost(ingredientsData)}</h4>
           <button type="button" class="modal__btn">${isSaved}</button>
           <button class="modal__close" aria-label="Close modal" data-micromodal-close>CLOSE</button>
         </main>
         `
-        MicroModal.show('modal-1')
-        var saveButton = document.querySelector('.modal__btn')
-        if(saved.filter(current => current.id === currentRecipe.id).length !== 0) {
-            saveButton.style.backgroundColor = "red"
+                MicroModal.show('modal-1')
+                var saveButton = document.querySelector('.modal__btn')
+                if (saved.filter(current => current.id === currentRecipe.id).length !== 0) {
+                    saveButton.style.backgroundColor = "red"
+                }
+                saveButton.addEventListener('click', function () {
+                    saveRecipe(saveButton)
+                })
+    }
+
+    function saveRecipe(button) {
+        var sa = saved.filter(current => current.id === currentRecipe.id)
+        if (sa.length === 0) {
+            saved.push(currentRecipe)
+            button.innerText = 'Saved'
+            button.style.backgroundColor = "red"
+        } else {
+            saved.splice(saved.indexOf(currentRecipe))
+            button.innerText = '♥️'
+            button.style.backgroundColor = "#e6e6e6"
         }
-        saveButton.addEventListener('click', function(){saveRecipe(saveButton)})
-
-    })
-}
-
-function saveRecipe(button) {
-    var sa = saved.filter(current => current.id === currentRecipe.id)
-    if(sa.length === 0){
-        saved.push(currentRecipe)
-        button.innerText = 'Saved'
-        button.style.backgroundColor = "red"
+        //sort through the saved array
+        //locate the index of 
     }
-    else {
-        saved.splice(saved.indexOf(currentRecipe))
-        button.innerText = '♥️'
-        button.style.backgroundColor = "#e6e6e6"
-    }
-    //sort through the saved array
-    //locate the index of 
-}
 
-function displayRecipes() {
-    recipeSection.innerHTML = ''
-    currentDisplayedRecipes.filter(recipe => {
-        recipeSection.innerHTML += 
-        `
+    function displayRecipes() {
+        recipeSection.innerHTML = ''
+        currentDisplayedRecipes.filter(recipe => {
+            recipeSection.innerHTML +=
+                `
         <img src="${recipe.image}" class="recipe"></img>
-        `  
-    })
-    // for(var i = 0; i < currentDisplayedRecipes.length; i++) {
-    //     recipeSection.innerHTML += 
-    //     `
-    //     <img src="${currentDisplayedRecipes[i].image}" class="recipe"></img>
-    //     `
-    // }
-}
-
-function displaySavedRecipes() {
-    recipeSection.innerHTML = ''
-    saved.filter(savedRecipe => {
-        recipeSection.innerHTML += 
         `
+        })
+
+    }
+
+    function displaySavedRecipes() {
+        recipeSection.innerHTML = ''
+        saved.filter(savedRecipe => {
+            recipeSection.innerHTML +=
+                `
         <img src="${savedRecipe.image}" class="recipe"></img>
         `
-    })
-    // for(var i = 0; i < saved.length; i++) {
-    //     recipeSection.innerHTML += 
-    //     `
-    //     <img src="${saved[i].image}" class="recipe"></img>
-    //     `
-    // }
-}
-
-
-function displayPantry() {
-    pantrySection.innerHTML += `Hello, ${user.name}. You have these items in your pantry. `
-    for(var i = 0; i < user.pantry.length; i++) {
-        var currentIngredient = ingredientsData.findIndex(x => x.id === user.pantry[i].ingredient)
-        pantrySection.innerHTML += 
-        `
-        <h1 class="pantry">• ${ingredientsData[currentIngredient].name}</h1>
-        `
+        })
     }
-}
 
-function popularRecipes() {
-    for(var i = 0; i < 10; i++) {
-        recipeSection.innerHTML += 
-        `
+
+    // function displayPantry() {
+    //     pantrySection.innerHTML += `Hello, ${user.name}. You have these items in your pantry. `
+    //     for (var i = 0; i < user.pantry.length; i++) {
+    //         var currentIngredient = ingredientsData.findIndex(x => x.id === user.pantry[i].ingredient)
+    //         pantrySection.innerHTML +=
+    //             `
+    //     <h1 class="pantry">• ${ingredientsData[currentIngredient].name}</h1>
+    //     `
+    //     }
+    // }
+
+    function popularRecipes() {
+        for (var i = 0; i < 10; i++) {
+            recipeSection.innerHTML +=
+                `
         <img src="${recipeRepository.recipes[i].image}" class="recipe"></img>
         `
+        }
     }
-}
 
-function showAllRecipes() {
-    recipeSection.innerHTML = ''
-    recipeData.forEach(recipe => {
-        recipeSection.innerHTML += 
-        `
+    function showAllRecipes() {
+        recipeSection.innerHTML = ''
+        recipeRepository.recipes.forEach(recipe => {
+            recipeSection.innerHTML +=
+                `
         <section class='recipe' data-all-recipes='${recipe.id}'>
         <h3 id='${recipe.id}' class='small-recipe-text'>${recipe.name}</h3>
         <img src="${recipe.image}" class="recipe-img">
         </section>
         `
-    })
-}
+        })
+    }
 
-function showFilteredRecipes(tag) {
-    currentDisplayedRecipes = []
-    var filteredRecipes = recipeRepository.filterByTag(tag)
-        for(var i = 0; i < filteredRecipes.length; i++) {
+    function showFilteredRecipes(tag) {
+        currentDisplayedRecipes = []
+        var filteredRecipes = recipeRepository.filterByTag(tag)
+        for (var i = 0; i < filteredRecipes.length; i++) {
             currentDisplayedRecipes.push(filteredRecipes[i])
         }
-    displayRecipes()
-}
+        displayRecipes()
+    }
 
-function searchRecipeByName() {
-    currentDisplayedRecipes = []
-    let filterByName = recipeRepository.filterByName(searchBar.value)
-    let filteredRecipes = recipeRepository.filterByTag(searchBar.value)
-        if(filterByName !== undefined) {
+    function searchRecipeByName() {
+        currentDisplayedRecipes = []
+        let filterByName = recipeRepository.filterByName(searchBar.value)
+        let filteredRecipes = recipeRepository.filterByTag(searchBar.value)
+        if (filterByName !== undefined) {
             for (var i = 0; i < filterByName.length; i++) {
                 currentDisplayedRecipes.push(filterByName[i]);
             }
-        } else if(filteredRecipes !== undefined) {
-            for(var i = 0; i < filteredRecipes.length; i++) {
+        } else if (filteredRecipes !== undefined) {
+            for (var i = 0; i < filteredRecipes.length; i++) {
                 currentDisplayedRecipes.push(filteredRecipes[i])
             }
         } else {
             recipeSection.innerHTML = `<p>NO RESULTS</p>`
         }
 
-    displayRecipes();
-}
+        displayRecipes();
+    }
 
-function generateRandUser() {
-    fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
-    .then((response) => response.json())
-    .then((data) => {
-    var num = Math.floor(Math.random() * data.usersData.length)
-    user = new User(data.usersData[num])
-    })
-}
-
-function hideAll() {
-    recipeSection.innerHTML = ''
-    pantrySection.innerHTML = ''
+    function hideAll() {
+        recipeSection.innerHTML = ''
+        pantrySection.innerHTML = ''
+    }
 }
