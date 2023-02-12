b97039b8aa1feeeb205df24e04e07011a6513fdf
Amber Shipley <ashipley2@me.com>
2023-02-12 13:52:58 -0800

    Update User tests for implementing recipesToCook to be an instance of RecipeRepository

--- test/User-test.js
+++ test/User-test.js
@@ -31,13 +31,13 @@ describe('User', () => {
 
     it('should start with no recipes to cook', () => {
 
-        expect(user.recipesToCook).to.have.lengthOf(0);
+        expect(user.recipesToCook.recipes).to.have.lengthOf(0);
     });
 
     it('should be able to add recipe to recipes to cook', () => {
 
         user.addToRecipesToCook(sampleRecipeData[0]);
-        expect(user.recipesToCook).to.have.lengthOf(1);
+        expect(user.recipesToCook.recipes).to.have.lengthOf(1);
     });
 
     it('should be able to remove recipe from recipes to cook', () => {
@@ -45,52 +45,11 @@ describe('User', () => {
         user.addToRecipesToCook(sampleRecipeData[0]);
         user.addToRecipesToCook(sampleRecipeData[1]);
 
-        expect(user.recipesToCook).to.have.lengthOf(2);
+        expect(user.recipesToCook.recipes).to.have.lengthOf(2);
 
-        user.removeFromRecipesToCook(user.recipesToCook[1]);
+        user.removeFromRecipesToCook(sampleRecipeData[1]);
 
-        expect(user.recipesToCook).to.have.lengthOf(1);
+        expect(user.recipesToCook.recipes).to.have.lengthOf(1);
     });
-
-    it('should be able to filter recipes to cook by tag', () => {
-
-        user.addToRecipesToCook(sampleRecipeData[0]);
-        user.addToRecipesToCook(sampleRecipeData[1]);
-        user.filterRecipesToCook('starter', null);
-
-        expect(user.filteredRecipes).to.have.lengthOf(1);
-        expect(user.filteredRecipes[0].name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
-    });
-
-    it('should NOT be able to filter recipes to cook by tag if no recipes match that tag', () => {
-
-        user.addToRecipesToCook(sampleRecipeData[0]);
-        user.addToRecipesToCook(sampleRecipeData[1]);
-        user.filterRecipesToCook('heaven', null);
-
-        expect(user.filteredRecipes).to.have.lengthOf(0);
-    });
-
-    it('should be able to filter recipes to cook by name', () => {
-
-        user.addToRecipesToCook(sampleRecipeData[0]);
-        user.addToRecipesToCook(sampleRecipeData[1]);
-        user.addToRecipesToCook(sampleRecipeData[2]);
-        user.filterRecipesToCook(null, 'Pork Chops');
-
-        expect(user.filteredRecipes).to.have.lengthOf(1);
-        expect(user.filteredRecipes[0].name).to.equal('Maple Dijon Apple Cider Grilled Pork Chops');
-    });
-
-    it('should NOT be able to filter recipes to cook if no recipes match that name', () => {
-
-        user.addToRecipesToCook(sampleRecipeData[0]);
-        user.addToRecipesToCook(sampleRecipeData[1]);
-        user.addToRecipesToCook(sampleRecipeData[2]);
-        user.filterRecipesToCook(null, 'shortbread');
-
-        expect(user.filteredRecipes).to.have.lengthOf(0);
-    });
-
 
 });
