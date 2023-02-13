2344f36e621915dc77c3168c45441d53204aa5e6
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-02-12 17:48:51 -0800

    Refactor filterByTag method functionality to include method to filter tag if it is an array

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -4,35 +4,49 @@ class RecipeRepository {
   constructor(recipeData) {
     this.recipes = recipeData.map(recipe => new Recipe(recipe));
   }
-  
+
   addRecipe(recipeToAdd) {
     if (!this.recipes.find(recipe => recipe.id === recipeToAdd.id)) {
       this.recipes.push(new Recipe(recipeToAdd));
-    }  
+    }
   }
   removeRecipe(recipeId) {
     this.recipes = this.recipes.filter(recipe => recipeId !== recipe.id);
 
   }
   filterByTag(tag) {
-    // for(var i = 0; i < tag.length; i++) {
-    //   if (!tag[i]) {
-    //     return;
-    //   }
-      this.filteredRecipes = this.recipes.filter(recipe => recipe.tags.includes(tag));
 
-      if (this.filteredRecipes.length === 0) {
-        this.filteredRecipes = null;
-        return
-      } else {
-        return this.filteredRecipes;
-      }
-    // }
+    //Goal: filter by tag.
+    //Input: Object
+    //Output: Array of objects
+    //Steps: First we need to loop through the tags array. For each tag we want to look through the recipes and see if the tags match. If so push it into the filtered recipes.
+    if (Array.isArray(tag)) {
+      this.filteredRecipes = [];
+      this.recipes.forEach((recipe) => {
+        tag.forEach((tag) => {
+          if (recipe.tags.includes(tag) && !this.filteredRecipes.includes(recipe)) {
+            this.filteredRecipes.push(recipe)
+          }
+        })
+      })
+    } else {
+      this.filteredRecipes = this.recipes.filter((recipe) =>
+        recipe.tags.includes(tag)
+      );
+    }
+    if (this.filteredRecipes.length === 0) {
+      this.filteredRecipes = null;
+      return
+    } else {
+      return this.filteredRecipes;
+    }
+
   }
+
   filterByName(name) {
     if (!name) {
       return;
-    } 
+    }
     this.filteredRecipes = this.recipes.filter(recipe => (recipe.name.toUpperCase().includes(name.toUpperCase())));
     if (this.filteredRecipes.length === 0) {
       this.filteredRecipes = null;
