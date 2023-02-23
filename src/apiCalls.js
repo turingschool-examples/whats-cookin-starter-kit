45bbb6ac66f5ae770f1d789bca825a3465ee10df
Daniel Curtin <dcurtin.work@gmail.com>
2023-02-22 17:14:29 -0700

    Change the api endpoints from heroku to local server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,23 +1,27 @@
 let apiCalls;
 
-const userData = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+const userData = fetch('http://localhost:3001/api/v1/users')
 .then(response => response.json())
 .then(data => data)
 .catch(err => console.log(err))
 
-const ingData = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
+const ingData = fetch('	http://localhost:3001/api/v1/ingredients')
 .then(response => response.json())
 .then(data => data)
 .catch(err => console.log(err))
 
-const recData = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+const recData = fetch('http://localhost:3001/api/v1/recipes')
 .then(response => response.json())
 .then(data => data)
 .catch(err => console.log(err))
 
 apiCalls = [userData, ingData, recData]
 
-export default apiCalls
+function saveFavorites() {
+
+};
+
+export { apiCalls, saveFavorites }
 
 
 
