d1819217942b3ca74c8addb00fe16ba333b3095c
Michael Lobato <mike.lobato.wft@gmail.com>
2023-02-22 17:56:26 -0800

    Add: functionality to change ids to recipes

--- src/classes/User.js
+++ src/classes/User.js
@@ -6,11 +6,25 @@ class User {
         this.id = user.id
         this.pantry = user.pantry
         this.savedRecipes = new RecipeRepository([])
+        this.recipesToCook = user.recipesToCook
     }
 
     addToSavedRecipes(recipeToAdd) {
         this.savedRecipes.addRecipe(recipeToAdd)
     }
+    changeIdToRecipe(recipes) {
+        if(this.recipesToCook.length) {
+            let matchingRecipe = []
+            this.recipesToCook.forEach(id => {
+                recipes.recipes.forEach(element => {
+                    if(element.id === id){
+                        matchingRecipe.push(element)
+                    }        
+                })
+            })
+            return new RecipeRepository(matchingRecipe)
+        }
+    } 
 
     removeFromSavedRecipes(recipeToRemove) {
       this.savedRecipes.removeRecipe(recipeToRemove.id) 
