4df920b8d753cbd3fa241fc48a0dfea2c95e327f
Heather Faerber <heather@turing.com>
2023-03-23 14:10:36 -0600

    Modify wc starter kit for fp

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,12 @@
-import './styles.css';
-import apiCalls from './apiCalls';
+//NOTE: Data model and non-dom manipulating logic will live in this file.
+
+import './styles.css'
+import apiCalls from './apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//Example of one way to import functions from the domUpdates file. You will delete these examples.
+import {exampleFunction1, exampleFunction2} from './domUpdates.js'
 
-
-console.log('Hello world');
+exampleFunction1('heather')
+exampleFunction2('heather')
