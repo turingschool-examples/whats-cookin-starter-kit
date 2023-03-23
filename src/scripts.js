74bb0a5366429876d0383014023517e0ea1d9a2c
Heather Faerber <heather@turing.com>
2023-03-23 14:35:56 -0600

    Import and export ingredients data to scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -4,9 +4,12 @@ import './styles.css'
 import apiCalls from './apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import ingredientsData from './data/ingredients.js'
 
 //Example of one way to import functions from the domUpdates file. You will delete these examples.
 import {exampleFunction1, exampleFunction2} from './domUpdates.js'
 
 exampleFunction1('heather')
 exampleFunction2('heather')
+
+console.log(ingredientsData)
