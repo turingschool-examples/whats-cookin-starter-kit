1d6bbbc4267bac76d494d93448ff02dfc98f5735
“Saki <sakisandra.c@gmail.com>
2023-05-13 16:07:02 -0600

    Fix recipe-repository sample test and import/export example

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,4 @@
 //NOTE: Your DOM manipulation will occur in this file
 
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-function exampleFunction1(person) {
-  console.log(`oh hi there ${person}`)
-}
-
-function exampleFunction2(person) {
-  console.log(`bye now ${person}`)
-}
-
-
-export {
-  exampleFunction1,
-  exampleFunction2,
-}
+import './styles.css'
