ff673c3500be03f72d865465f36c8c0a4920a290
Jade Shi <ziyushi1997@gmail.com>
2023-05-13 22:08:19 -0700

    Add basic test structure

---
+++ test/get-ingredient-name-test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { getIngredientName } from '../src/get-ingredient-name'
+// import '../src/test/'
+describe('getIngredientName', () => {
+  it('should be a fuction', () => {
+    expect(getIngredientName).to.be.a('function');
+  });
+
+  it
+})
