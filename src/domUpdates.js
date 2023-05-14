53e1242f141b5a2ac23d6697030c2e435acef750
“Saki <sakisandra.c@gmail.com>
2023-05-14 15:03:25 -0600

    Render cards from data

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -11,4 +11,24 @@ const toDashboardView = () => {
   myRecipesView.classList.add('hidden')
 }
 
+// DOM FUNCTIONS
+const renderRecipeCards = (view, recipes) => {
+  view.innerHTML = ''
+  recipes.forEach((recipe) => {
+    view.innerHTML += `
+    <article class="recipe-card">
+      <img class="recipe-img"
+      src="${recipe.img}">
+      <p class="recipe-tag">tag</p>
+      <div class="recipe-title-flex">
+        <h2 class="recipe-name">${recipe.name}</h2>
+        <div class="bookmark-flex">
+          <img src="./images/bookmark.png" class="bookmark-icon">
+        </div>
+      </div>
+    </article>`
+  })
+
+}
+
 export { toMyRecipeView , toDashboardView }
