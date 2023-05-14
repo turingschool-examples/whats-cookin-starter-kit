5187f4c13a9a7a86f43a439e534bc42658da7afd
Caitlin Cradick <caitlincradick@gmail.com>
2023-05-14 16:28:58 -0700

    Fix: Add correct/imports and exports

---
+++ src/recipe.js
@@ -0,0 +1,3 @@
+const returnInstructions = recipe => recipe 
+
+export {returnInstructions}
--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,13 @@ import ingredientsData from './data/ingredients.js'
 //Example of one way to import functions from the domUpdates file. You will delete these examples.
 import {exampleFunction1, exampleFunction2} from './domUpdates.js'
 
+import {ingredientTestData, recipeTestData, userTestData} from './data/testData.js'
+
+
+
+
 exampleFunction1('heather')
 exampleFunction2('heather')
 
 console.log(ingredientsData)
+
