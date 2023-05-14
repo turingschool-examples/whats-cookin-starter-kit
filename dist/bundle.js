b67a1d2d8c9b6007c87ecbe5980cad6c582cfda2
Benjamin holden <benjamin.f.holden@outlook.com>
2023-05-14 17:16:47 -0600

    Add HTML and CSS 1st draft

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,8 @@
 
    
-/******/ (() => { // webpackBootstrap
-  var __webpack_exports__ = {};
-  alert('hello')
-  /******/
-})()
-  ;
+// /******/ (() => { // webpackBootstrap
+//   var __webpack_exports__ = {};
+//   alert('hello')
+//   /******/
+// })()
+//   ;
