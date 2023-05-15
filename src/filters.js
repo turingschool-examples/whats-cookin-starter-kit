5fcd38a2f334f9ed066362c78fcb088099195c48
Seth Burns <seth@sethburns.com>
2023-05-15 14:30:23 -0700

    Adjust tests to be successful with new sad paths

--- src/filters.js
+++ src/filters.js
@@ -1,24 +1,29 @@
-
 const filterByTag = (tag, recipes) => {
   const searchedTag = tag.toLowerCase();
-  const searchedRecipes = recipes.filter(recipe => {
-    return recipe.tags.includes(searchedTag)
-  })
-  if (!searchedRecipes.length) {
-    return 'Sorry, there are no recipes with this tag!'
+  const searchedRecipes = recipes.filter((recipe) => {
+    return recipe.tags.includes(searchedTag);
+  });
+  if (!searchedTag) {
+    return 'You need to enter a tag before you search!';
   }
-  return searchedRecipes
-}
+  if (!searchedRecipes.length) {
+    return 'Sorry, there are no recipes with this tag!';
+  }
+  return searchedRecipes;
+};
 
 const filterByName = (name, recipes) => {
   const searchedName = name.toLowerCase();
-  const searchedRecipes = recipes.filter(recipe => {
-    return recipe.name.toLowerCase().includes(searchedName)
-  })
-  if (!searchedRecipes.length) {
-    return 'Sorry, there are no recipes with this name!'
+  const searchedRecipes = recipes.filter((recipe) => {
+    return recipe.name.toLowerCase().includes(searchedName);
+  });
+  if (!searchedName) {
+    return 'You need to enter a name before you search!';
   }
-  return searchedRecipes
-}
+  if (!searchedRecipes.length) {
+    return 'Sorry, there are no recipes with this name!';
+  }
+  return searchedRecipes;
+};
 
-export { filterByTag, filterByName }
+export { filterByTag, filterByName };
