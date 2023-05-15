710b5c07d0208f9ed05057fc8aa922b1b93e7b6e
Daniel Cochico <dcochico.20@gmail.com>
2023-05-14 21:28:07 -0700

    refactor: Add more ingredients to test data

--- src/data/testData.js
+++ src/data/testData.js
@@ -23,6 +23,36 @@ const ingredientTestData = [
     "id": 19206,
     "name": "instant vanilla pudding",
     "estimatedCostInCents": 660
+  },
+  {
+    "id": 19334,
+    "name": "brown sugar",
+    "estimatedCostInCents": 559
+  },
+  {
+    "id": 2047,
+    "name": "salt",
+    "estimatedCostInCents": 280
+  },
+  {
+    "id": 1012047,
+    "name": "fine sea salt",
+    "estimatedCostInCents": 528
+  },
+  {
+    "id": 10019903,
+    "name": "semi sweet chips",
+    "estimatedCostInCents": 253
+  },
+  {
+    "id": 1145,
+    "name": "unsalted butter",
+    "estimatedCostInCents": 617
+  },
+  {
+    "id": 2050,
+    "name": "vanilla",
+    "estimatedCostInCents": 926
   }
 ];
 
