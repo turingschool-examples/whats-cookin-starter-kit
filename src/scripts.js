d13b7429c67cd6005e3113b03d9e9f5a9cec4349
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-15 19:21:36 -0400

    Add recipe test and fix config

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,16 @@
 //NOTE: Data model and non-dom manipulating logic will live in this file.
+// query selectors and event listeners in here 
 
 import './styles.css'
-import apiCalls from './apiCalls'
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import ingredientsData from './data/ingredients.js'
+// import apiCalls from './apiCalls'
+// // An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// import './images/turing-logo.png'
+// import ingredientsData from './data/ingredients.js'
 
-//Example of one way to import functions from the domUpdates file. You will delete these examples.
-import {exampleFunction1, exampleFunction2} from './domUpdates.js'
+// //Example of one way to import functions from the domUpdates file. You will delete these examples.
+// import {exampleFunction1, exampleFunction2} from './domUpdates.js'
 
-exampleFunction1('heather')
-exampleFunction2('heather')
+// exampleFunction1('heather')
+// exampleFunction2('heather')
 
-console.log(ingredientsData)
+// console.log(ingredientsData)
