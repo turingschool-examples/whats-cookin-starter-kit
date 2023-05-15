c69285c37cb8ac743f559851bafc5759b3b81e35
Elise Jones <02elise.jones02@gmail.com>
2023-05-15 14:57:25 -0600

    Add calculate recipe price function to pass tests

---
+++ src/calculate-recipe-price.js
@@ -0,0 +1,33 @@
+const calculateRecipePrice = (recipe, ingredientsData) => {
+  if(!recipe) {
+    return 'Sorry this is not a recipe!'
+  } else {
+  const ingredientQuantityInfo = recipe.ingredients.map((quantity) => {
+    quantity = {
+    id: quantity.id,
+    quantity: quantity.quantity.amount
+  }
+    return quantity});
+
+  const sumTotalQuantity = ingredientQuantityInfo.reduce((acc, curr) => {
+    acc += curr.quantity
+    return acc
+  }, 0)
+  
+  const costInCents = ingredientQuantityInfo.map(id=> {
+    const ingredient = ingredientsData.find(ingredient => { 
+      return ingredient.id === id.id;});
+    return ingredient.estimatedCostInCents;
+  });
+
+  const hello = costInCents.reduce((acc, curr) => {
+    acc += curr
+    return acc
+  },0)
+
+  const finalTotalCost = (sumTotalQuantity * hello)/100
+    return `$${finalTotalCost.toFixed(2)}`
+  }
+};
+
+export {calculateRecipePrice }
