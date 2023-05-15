4c2f1878b7c49776f76f14681760128b796cb35b
“Saki <sakisandra.c@gmail.com>
2023-05-14 20:23:01 -0600

    Add bookmark button to each recipe card

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -18,17 +18,37 @@ const renderRecipeCards = (view, recipes) => {
     view.innerHTML += `
     <article class="recipe-card">
       <img class="recipe-img"
-      src="${recipe.img}">
-      <p class="recipe-tag">tag</p>
+      src="${recipe.image}">
+      <p class="recipe-tag">${recipe.tags[0]}</p>
       <div class="recipe-title-flex">
         <h2 class="recipe-name">${recipe.name}</h2>
         <div class="bookmark-flex">
-          <img src="./images/bookmark.png" class="bookmark-icon">
+          <img src="./images/bookmark.png" id="${recipe.id}" class="bookmark-icon unchecked" alt="bookmark icon">
+          <img src="./images/bookmark-filled.png" id="${recipe.id}" class="bookmark-icon checked hidden" alt="bookmark icon filled in">
         </div>
       </div>
     </article>`
   })
-
 }
 
-export { toMyRecipeView , toDashboardView }
+const isUnchecked = (e) => {
+  if (e.target.classList.contains('unchecked')){
+      return true;
+  }
+}
+
+const toggleBookmark = (e) => {
+  if (e.target.classList[0]=== 'bookmark-icon') {
+    if(isUnchecked(e)) {
+      //and push into my saved recipes array
+      e.target.classList.add('hidden')
+      e.target.nextElementSibling.classList.remove('hidden');
+    } else {
+      //and remove from my recipe array
+      e.target.classList.add('hidden')
+      e.target.previousElementSibling.classList.remove('hidden');
+    }
+  }
+}
+
+export { toMyRecipeView , toDashboardView, renderRecipeCards, toggleBookmark }
--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,10 @@ import './images/home.png';
 import './images/search.png';
 import './images/restaurant.png';
 import './images/bookmark.png';
-import { toMyRecipeView , toDashboardView } from './domUpdates'
+import './images/bookmark-filled.png'
+
+import { toMyRecipeView , toDashboardView, renderRecipeCards, toggleBookmark } from './domUpdates'
+import {recipeData} from './data/recipes'
 
 
 // QUERY SELECTORS
@@ -13,9 +16,15 @@ const myRecipesBtn = document.querySelector('#myRecipes');
 const mainView = document.querySelector('#mainView');
 const myRecipesView = document.querySelector('#myRecipeView');
 const dashboardBtn = document.querySelector('#dashboardNav');
+const mainViewCardContainer = document.querySelector('#mainViewCardContainer');
 
 // EVENT LISTENERS
 myRecipesBtn.addEventListener('click', toMyRecipeView)
 dashboardBtn.addEventListener('click', toDashboardView)
+mainViewCardContainer.addEventListener('click', (e) => {
+  toggleBookmark(e)})
 
-export { mainView , myRecipesView }
+// FUNCTIONS
+renderRecipeCards(mainViewCardContainer, recipeData)
+
+export { mainView , myRecipesView, mainViewCardContainer }
