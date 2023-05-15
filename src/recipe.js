02a35ee1d1f2db1106520c3f8417e379d25751e6
Natalie Baker <nathelene76@gmail.com>
2023-05-14 18:47:03 -0600

    add filterByTag test

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,18 @@
+import { ingredientTestData } from "./data/testData"
+import { recipieTestData } from "./data/testData"
+
 const returnInstructions = recipe => recipe 
 
-export {returnInstructions}
+const filterByTag = (recipes,tag) => {
+    const findRecipe = recipes.filter(recipe => {
+
+       return recipe.tags.includes(tag)
+   })
+   
+   return findRecipe
+}
+
+
+export {returnInstructions,
+filterByTag}
+
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -1,9 +1,16 @@
 import { expect } from 'chai';
 import {ingredientTestData, recipeTestData, userTestData} from '../src/data/testData.js';
-import {returnInstructions} from '../src/recipe.js';
+import {returnInstructions, filterByTag} from '../src/recipe.js';
 
 describe('returnInstructions', () => {
   it('Should be a function', () => {
     expect(returnInstructions).to.be.a('function');
   });
+})
+
+describe('filterByTag', () => {
+  it('Should return recipie filtered by given tag', () => {
+    const result = filterByTag(recipeTestData,"antipasti")
+    expect(result).to.deep.equal([recipeTestData[0]])
+  })
 })
