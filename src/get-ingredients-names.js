2267cbea5420b1231c4c46e7afe599c3106bb599
Jade Shi <ziyushi1997@gmail.com>
2023-05-15 16:59:56 -0700

    Remove comments

--- src/get-ingredients-names.js
+++ src/get-ingredients-names.js
@@ -1,17 +1,3 @@
-// take an object recipe as argument
-// return an array of strings (ingredients names)
-
-/**
- * new method learned:
- * Array.sort().join() to compare array values
- * 
- * testing:
- * to use chai to compare array with same values: expect().to.have.same.members()
- * 
- * git:
- * mv/rename files in git: git mv old-file-name new-file-name
- */
-
 const getIngredientsNames = (recipe, ingredientsData) => {
   const ingredientsIDs = recipe.ingredients.map((ele) => {
     return ele.id
