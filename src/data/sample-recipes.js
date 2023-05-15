64f3dd57fe50810b70c6e2e52b34e1964f3a6085
Jason Alberto <alberto.jason@gmail.com>
2023-05-15 17:12:13 -0400

    Add sample ingredient list file for testing and reduce recipe dataset

--- src/data/sample-recipes.js
+++ src/data/sample-recipes.js
@@ -23,62 +23,6 @@ const sampleRecipeData = [
           "amount": 1,
           "unit": "large"
         }
-      },
-      {
-        "id": 19335,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 19206,
-        "quantity": {
-          "amount": 3,
-          "unit": "Tbsp"
-        }
-      },
-      {
-        "id": 19334,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 2047,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "tsp"
-        }
-      },
-      {
-        "id": 1012047,
-        "quantity": {
-          "amount": 24,
-          "unit": "servings"
-        }
-      },
-      {
-        "id": 10019903,
-        "quantity": {
-          "amount": 2,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 1145,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "c"
-        }
-      },
-      {
-        "id": 2050,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "tsp"
-        }
       }
     ],
     "instructions": [
@@ -141,69 +85,6 @@ const sampleRecipeData = [
           "amount": 1,
           "unit": "tablespoon"
         }
-      },
-      {
-        "id": 1002046,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 11215,
-        "quantity": {
-          "amount": 1,
-          "unit": "clove"
-        }
-      },
-      {
-        "id": 1012046,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 19911,
-        "quantity": {
-          "amount": 0.25,
-          "unit": "cup"
-        }
-      },
-      {
-        "id": 16112,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 10010062,
-        "quantity": {
-          "amount": 24,
-          "unit": "ounce"
-        }
-      },
-      {
-        "id": 1102047,
-        "quantity": {
-          "amount": 4,
-          "unit": "servings"
-        }
-      },
-      {
-        "id": 16124,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
-      },
-      {
-        "id": 1016168,
-        "quantity": {
-          "amount": 1,
-          "unit": "tablespoon"
-        }
       }
     ],
     "instructions": [
@@ -243,76 +124,6 @@ const sampleRecipeData = [
         "quantity": {
           "amount": 2,
           "unit": "cups"
-        }
-      },
-      {
-        "id": 4582,
-        "quantity": {
-          "amount": 4,
-          "unit": "servings"
-        }
-      },
-      {
-        "id": 2031,
-        "quantity": {
-          "amount": 4,
-          "unit": "teaspoons"
-        }
-      },
-      {
-        "id": 5100,
-        "quantity": {
-          "amount": 1,
-          "unit": "pound"
-        }
-      },
-      {
-        "id": 2009,
-        "quantity": {
-          "amount": 4,
-          "unit": "teaspoons"
-        }
-      },
-      {
-        "id": 1022020,
-        "quantity": {
-          "amount": 4,
-          "unit": "teaspoons"
-        }
-      },
-      {
-        "id": 6168,
-        "quantity": {
-          "amount": 8,
-          "unit": "cups"
-        }
-      },
-      {
-        "id": 9176,
-        "quantity": {
-          "amount": 0.5,
-          "unit": "cup"
-        }
-      },
-      {
-        "id": 2026,
-        "quantity": {
-          "amount": 4,
-          "unit": "teaspoons"
-        }
-      },
-      {
-        "id": 1042047,
-        "quantity": {
-          "amount": 1.5,
-          "unit": "tablespoons"
-        }
-      },
-      {
-        "id": 1042047,
-        "quantity": {
-          "amount": 4,
-          "unit": "teaspoons"
         }
       }
     ],
