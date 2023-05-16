9d056494c96d97508a532b1702b5f7f931bf596e
Jamie Caudill <j.caudill7177@gmail.com>
2023-05-15 17:16:37 -0700

    feat: Write sad test for recipe instructions and write function to pass tests

--- test/recipe-instructions-test.js
+++ test/recipe-instructions-test.js
@@ -1,9 +1,6 @@
+import { expect } from 'chai';
 import { recipeInstructions } from '../src/functions/recipe-instructions'
 import { sampleRecipeData } from '../src/data/sample-recipes'
-
-// function that returns recipe instructions
-// input: recipe
-// output: instructions
 
 describe('recipeInstructions', () => {
   it('should return recipe instructions', () => {
@@ -12,4 +9,9 @@ describe('recipeInstructions', () => {
     expect(instructions.length).to.equal(6);
     expect(instructions[0].instruction).to.equal("In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.");
   });
+
+  it('should return a message if no recipe', () => {
+    const instructions = recipeInstructions();
+    expect(instructions).to.equal('No recipe located');
+  })
 });
