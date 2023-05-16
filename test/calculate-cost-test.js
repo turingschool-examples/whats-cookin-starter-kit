8127916b3dc96f4d3a51be063a1447d266de5c83
Marisa Wyatt <risawyatt22@gmail.com>
2023-05-16 16:47:58 -0600

    feat: Write the calculateCost function

--- test/calculate-cost-test.js
+++ test/calculate-cost-test.js
@@ -2,10 +2,9 @@ import { expect } from 'chai';
 import { calculateCost } from '../src/functions/calculate-cost'
 
 describe('calculate cost', () => {
-  it('should be a function', () => {
-    expect(calculateCost).to.be.a('function')
-  })
   it('should calculate the cost of a recipe', () => {
-    expect(calculateCost("Maple Dijon Apple Cider Grilled Pork Chops")).to.equal('$28.65')
-  })
+    expect(calculateCost("Maple Dijon Apple Cider Grilled Pork Chops")).to.equal('$27.30')
+  });
+
+  it('should ')
 })
