3d5a26af846582e33e406458c0f529827078b93a
Denzel Turambi <dturambi26@gmail.com>
2023-05-15 22:18:40 -0600

    refactor: Change variable name

--- src/data/recipes-sample.js
+++ src/data/recipes-sample.js
@@ -1,4 +1,4 @@
-const recipeData = [
+const recipeData1 = [
   {
     "id": 595736,
     "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
@@ -474,4 +474,4 @@ const recipeData = [
   },
 ]
 
-export default recipeData
+export default recipeData1
