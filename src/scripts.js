4712c04b94f01b7410ee2a378ceccd473cbea1af
Daniel Cochico <dcochico.20@gmail.com>
2023-05-15 20:07:50 -0700

    feat: View all recipes

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,12 @@
 //NOTE: Data model and non-dom manipulating logic will live in this file.
 
 import './styles.css'
-import apiCalls from './apiCalls'
+// import apiCalls from './apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import ingredientsData from './data/ingredients.js'
 
 //Example of one way to import functions from the domUpdates file. You will delete these examples.
-import {exampleFunction1, exampleFunction2} from './domUpdates.js'
+import {viewAllRecipes} from './domUpdates.js'
 
-import {ingredientTestData, recipeTestData, userTestData} from './data/testData.js'
-
-
-
-
-exampleFunction1('heather')
-exampleFunction2('heather')
-
-console.log(ingredientsData)
-
+// Event Listeners
+window.addEventListener('load', viewAllRecipes);
