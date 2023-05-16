20d45e4cc38be9d09850711c16311ca6c1b72c08
Marisa Wyatt <risawyatt22@gmail.com>
2023-05-16 13:35:18 -0600

    feat: Test that calculateCost is a function

--- test/calculate-cost-test.js
+++ test/calculate-cost-test.js
@@ -1,2 +1,8 @@
+import { expect } from 'chai';
 import { calculateCost } from '../src/functions/calculate-cost'
 
+describe('calculate cost', () => {
+  it('should be a function', () => {
+    expect(calculateCost).to.be.a('function')
+  })
+})
