f7030c84e8e079c0bae2340a77dcb4876bdd6701
Denzel Turambi <dturambi26@gmail.com>
2023-05-15 22:16:40 -0600

    feat: Add showRecipes(), remove/addHiddenClass(), querySelectors, and event listener

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,25 @@
 //NOTE: Your DOM manipulation will occur in this file
+// Query Selectors:
+const allRecipesButton = document.querySelector('.all-recipes');
+const frontRecipeDisplay = document.querySelector('.front-recipe-display');
+const allRecipeDisplay = document.querySelector('.all-recipes-display');
+
+//Event Listeners
+allRecipesButton.addEventListener('click', showRecipes);
+
+//Event Handlers/Functions
+function showRecipes() {
+  addHiddenClass([frontRecipeDisplay]);
+  allRecipeDisplay.innerHTML = 'recipes';
+};
+
+function removeHiddenClass(elements) {
+  return elements.forEach(element => element.classList.remove('hidden'));
+};
+
+function addHiddenClass(elements) {
+  return elements.forEach(element => element.classList.add('hidden'));
+};
 
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
 function exampleFunction1(person) {
@@ -13,4 +34,7 @@ function exampleFunction2(person) {
 export {
   exampleFunction1,
   exampleFunction2,
+  showRecipes,
+  removeHiddenClass,
+  addHiddenClass
 }
