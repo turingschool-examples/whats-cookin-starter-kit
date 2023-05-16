bb0bca819ca4e0f68f7a734aa7abcb81d6c10e89
Marisa Wyatt <risawyatt22@gmail.com>
2023-05-16 14:06:27 -0600

    feat: Test that calculateCost returns the total ingredient cost

--- test/calculate-cost-test.js
+++ test/calculate-cost-test.js
@@ -5,4 +5,7 @@ describe('calculate cost', () => {
   it('should be a function', () => {
     expect(calculateCost).to.be.a('function')
   })
+  it('should calculate the cost of a recipe', () => {
+    expect(calculateCost("Maple Dijon Apple Cider Grilled Pork Chops")).to.equal('$28.65')
+  })
 })
