825dd137cab1407bfc8b70812992d846b7182e46
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-16 15:36:06 -0400

    Dry up data creation with beforeEach hook

--- test/user-test.js
+++ test/user-test.js
@@ -7,31 +7,31 @@ describe('users', () => {
   const ephriam = sampleUsersData[1];
   const cookies = sampleRecipeData[0];
   const porkChops = sampleRecipeData[1];
+  let newSaige, newerSaige, newEphriam, newerEphriam; 
+  beforeEach(() => {
+    newSaige = addRecipeToCook(saige, cookies); 
+    newerSaige = addRecipeToCook(newSaige, porkChops);
+    newEphriam = addRecipeToCook(ephriam, porkChops);
+    newerEphriam = addRecipeToCook(newEphriam, cookies);
+  })
   it('should be a function', () => {
     assert.isFunction(addRecipeToCook)
   })
-  it('should be able to add recipes to cook', () => {
-    let newSaige = addRecipeToCook(saige, cookies); 
-    let newEphriam = addRecipeToCook(ephriam, porkChops);
 
+  it('should be able to add recipes to cook', () => {
     assert.deepEqual(newSaige.recipesToCook, [cookies])
     assert.deepEqual(newEphriam.recipesToCook, [porkChops])
   })
+
   it('should only add unique recipes to cook', () => {
-    let newSaige = addRecipeToCook(saige, cookies); 
-    let newerSaige = addRecipeToCook(newSaige, cookies);
-    let newEphriam = addRecipeToCook(ephriam, porkChops);
-    let newerEphriam = addRecipeToCook(newEphriam, porkChops);
+    newerSaige = addRecipeToCook(newSaige, cookies);
+    newerEphriam = addRecipeToCook(newEphriam, porkChops);
 
     assert.deepEqual(newerSaige.recipesToCook, [cookies])
     assert.deepEqual(newerEphriam.recipesToCook, [porkChops])
   })
-  it('should add more unique recipes to cook', () => {
-    let newSaige = addRecipeToCook(saige, cookies); 
-    let newerSaige = addRecipeToCook(newSaige, porkChops);
-    let newEphriam = addRecipeToCook(ephriam, porkChops);
-    let newerEphriam = addRecipeToCook(newEphriam, cookies);
 
+  it('should add more unique recipes to cook', () => {
     assert.deepEqual(newerSaige.recipesToCook, [cookies, porkChops])
     assert.deepEqual(newerEphriam.recipesToCook, [porkChops, cookies])
   })
