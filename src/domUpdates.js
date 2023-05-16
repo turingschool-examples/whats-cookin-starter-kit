a4fd22f8e47d793703a20be8f36ec8242f224387
Jamie Caudill <j.caudill7177@gmail.com>
2023-05-16 14:34:01 -0700

    feat: Write functions for showing and hiding DOM elements

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,20 @@
 //NOTE: Your DOM manipulation will occur in this file
 
-//Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-function exampleFunction1(person) {
-  console.log(`oh hi there ${person}`)
-}
+const viewAll = document.querySelector('.categories__all');
 
-function exampleFunction2(person) {
-  console.log(`bye now ${person}`)
-}
+viewAll.addEventListener('click', () => {
+
+})
 
 
-export {
-  exampleFunction1,
-  exampleFunction2,
-}
+const show = (names, section) => {
+  names.forEach((name) => {
+    name.classList.remove(section);
+  })
+};
+
+const hide = (names, section) => {
+  names.forEach((name) => {
+    name.classList.add(section);
+  })
+};
