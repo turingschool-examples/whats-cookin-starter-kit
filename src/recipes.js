b921962636a2fd633b5943aacca6873eec3ea2d2
Jason Alberto <alberto.jason@gmail.com>
2023-05-16 10:43:49 -0400

    Add test to get another recipe's instructions and adjusted the functions to return a list

--- src/recipes.js
+++ src/recipes.js
@@ -1,12 +1,11 @@
 const getRecipeById = (recipes, id) => {
   const recipe = recipes.find(recipe => recipe.id === id);
-  console.log('recipe file:', recipe)
   return recipe;
 };
 
 const getRecipeInstructions = (recipe) => {
   return recipe.instructions.reduce((instructions, instruction) => {
-    instructions += `${instruction.number}. ${instruction.instruction}`;
+    instructions.push(`${instruction.number}. ${instruction.instruction}`);
     return instructions;
   }, []);
 };
