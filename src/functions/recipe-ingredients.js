bfc068349971b68b2008c6dd5c506968690e0519
Marisa Wyatt <risawyatt22@gmail.com>
2023-05-15 18:25:44 -0600

    feat: Write function for recipeIngredients

--- src/functions/recipe-ingredients.js
+++ src/functions/recipe-ingredients.js
@@ -1,7 +1,27 @@
 // recipe ingredients function here
 
-const recipeIngredients = () => {
+import ingredientsData from "../data/ingredients";
+import { sampleRecipeData } from "../data/sample-recipes";
 
+const recipeIngredients = (recipe) => {
+  const filteredRecipe = sampleRecipeData.find((item) => {
+   return item.name === recipe
+  });
+  if (!filteredRecipe){
+    return `Sorry, cannot find a recipe for ${recipe}.`
+  }
+  const ingredientIds = filteredRecipe.ingredients.map((ingredient) => {
+    return ingredient.id
+  })
+  let ingredientObjects = ingredientIds.map((ingedientId) => {
+    return ingredientsData.find((ingredient) => {
+      if(ingredient.id === ingedientId) {
+        return ingredient.name
+      }
+    })
+  })
+  let ingredientStrings = ingredientObjects.map((object) => object.name)
+  return ingredientStrings
 }
 
 export { recipeIngredients };
--- test/recipe-ingredients-test.js
+++ test/recipe-ingredients-test.js
@@ -1,1 +1,15 @@
-import { recipeIngredients } from '../src/functions/recipe-ingredients'
+import { expect } from 'chai';
+import { recipeIngredients } from '../src/functions/recipe-ingredients';
+
+describe('recipe ingredients', () => {
+  it('should be a function', function() {
+    expect(recipeIngredients).to.be.a('function')
+  })
+  it('should return the ingredients for a recipe', () => {
+    const ingredients = recipeIngredients("Maple Dijon Apple Cider Grilled Pork Chops")
+    expect(ingredients).to.deep.equal(["apple cider", "apple", "corn starch", "dijon style mustard", "whole garlic clove", "whole grain dijon mustard", "maple", "miso", "pork chop", "s&p", "soy sauce", "sriracha sauce"])
+  })
+  it('should return a error message when given an invalid recipe', () => {
+    expect(recipeIngredients('hotdog')).to.equal('Sorry, cannot find a recipe for hotdog.')
+  })
+})
