bc0fed0f2f029eb57d939f091828792cee648d79
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-16 15:55:52 -0400

    Pull logic from addUniqueRecipes into separate function

--- src/users.js
+++ src/users.js
@@ -1,10 +1,14 @@
 import { copyItem } from "./helper-functions";
 
+const addUniqueRecipes = (userRecipes, newRecipe) => {
+  if(!userRecipes.find(item => item.id === newRecipe.id)) userRecipes.push(newRecipe)
+};
+
 const addRecipeToCook = (user, recipe) => {
   const updatedUser = copyItem(user);
   let recipes = updatedUser.recipesToCook;
   if (!recipes) recipes = [recipe];
-  if (!recipes.find(item => item.id === recipe.id)) recipes.push(recipe);
+  addUniqueRecipes(recipes, recipe)
   updatedUser.recipesToCook = recipes;
   return updatedUser;
 };
