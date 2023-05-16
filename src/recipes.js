c52d1a213df6cdbefb685842fcd87e0464cb315e
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-16 19:49:16 -0400

    Pull logic from getIngredients into separate function for SRP and readability

--- src/recipes.js
+++ src/recipes.js
@@ -2,8 +2,13 @@ const getInstructions = recipe => {
   return recipe.instructions.map(item => item.instruction)
 }
 
+const getIngredientProperty = (currentIngredient, allIngredients, property) => {
+    let matchingIngredient = allIngredients.find(ingredient => ingredient.id === currentIngredient.id)
+    return matchingIngredient[property]
+}
+
 const getIngredients = (recipe, ingredients) => {
-  return recipe.ingredients.map(ingredient => ingredients.find(item => item.id === ingredient.id).name)
+  return recipe.ingredients.map(ingredient => getIngredientProperty(ingredient, ingredients, 'name'))
 }
 
 const calculateRecipeCost = recipe => {
