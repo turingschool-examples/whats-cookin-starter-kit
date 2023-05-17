be42d2098a2685aa0aa93666920ef5b701ea654d
Jamie Caudill <j.caudill7177@gmail.com>
2023-05-16 17:48:10 -0700

    feat: Add styling to view all recipes page

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,13 +1,22 @@
 //NOTE: Your DOM manipulation will occur in this file
 
 import sampleRecipeData from "./data/sample-recipes.js";
+import recipeData from "./data/recipes.js"
 
 const viewAll = document.querySelector('.categories__all');
 const allSection = document.querySelector('.all');
 const homePage = document.querySelector('.home');
+const allContainer = document.querySelector('.all__container');
 
-function viewAllRecipes () {
+const viewAllRecipes = () => {
   hide([homePage], 'home--hidden');
+  show([allSection], 'all--hidden');
+  recipeData.forEach(recipe => {
+    allContainer.innerHTML += 
+    `<div class='all__recipes'>
+      <p class='all__text'>${recipe.name}</p>
+    </div>`
+  })
 };
 
 const show = (names, section) => {
