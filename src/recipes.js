1cad5228216eb67dc743ce65f311bb24d99a37f5
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-16 20:59:09 -0400

    Move getIngredientProperty to helper functions

--- src/recipes.js
+++ src/recipes.js
@@ -1,10 +1,7 @@
+import { getIngredientProperty } from "./helper-functions"
+
 const getInstructions = recipe => {
   return recipe.instructions.map(item => item.instruction)
-}
-
-const getIngredientProperty = (currentIngredient, allIngredients, property) => {
-    let matchingIngredient = allIngredients.find(ingredient => ingredient.id === currentIngredient.id)
-    return matchingIngredient[property]
 }
 
 const getIngredients = (recipe, ingredients) => {
@@ -12,14 +9,6 @@ const getIngredients = (recipe, ingredients) => {
 }
 
 const calculateRecipeCost = (recipe, ingredients) => {
-  /*map over the array of ingredients in the recipe 
-      get the cost in cents property based on the matching id from the full ingredients array 
-      return an object with a key of how many of the item is needed and a key of the cost 
-    reduce over that new array, 
-      for the current value (each new recipe object )
-        multiply the properties and += the sum to the acc 
-  */
-  console.log(recipe.ingredients[0])
   const recipeIngredients = recipe.ingredients.map(ingredient => ({amount: ingredient.quantity.amount, costPerUnit: getIngredientProperty(ingredient, ingredients, 'estimatedCostInCents')}))
   const costInCents = recipeIngredients.reduce((totalCost, curr) => totalCost += (curr.amount * curr.costPerUnit), 0);
   return `$${(costInCents/100).toFixed(2)}`
