496e9b8aeac9ce3cc726100f665740f53bff7e00
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-16 20:43:51 -0400

    Pass calculate cost tests with implementation code

--- src/recipes.js
+++ src/recipes.js
@@ -11,8 +11,18 @@ const getIngredients = (recipe, ingredients) => {
   return recipe.ingredients.map(ingredient => getIngredientProperty(ingredient, ingredients, 'name'))
 }
 
-const calculateRecipeCost = recipe => {
-
+const calculateRecipeCost = (recipe, ingredients) => {
+  /*map over the array of ingredients in the recipe 
+      get the cost in cents property based on the matching id from the full ingredients array 
+      return an object with a key of how many of the item is needed and a key of the cost 
+    reduce over that new array, 
+      for the current value (each new recipe object )
+        multiply the properties and += the sum to the acc 
+  */
+  console.log(recipe.ingredients[0])
+  const recipeIngredients = recipe.ingredients.map(ingredient => ({amount: ingredient.quantity.amount, costPerUnit: getIngredientProperty(ingredient, ingredients, 'estimatedCostInCents')}))
+  const costInCents = recipeIngredients.reduce((totalCost, curr) => totalCost += (curr.amount * curr.costPerUnit), 0);
+  return `$${(costInCents/100).toFixed(2)}`
 }
 
 export {
