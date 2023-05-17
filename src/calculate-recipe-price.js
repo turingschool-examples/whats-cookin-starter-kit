9186f75b307d4840606c6d24848ff9246c9b5807
Elise Jones <02elise.jones02@gmail.com>
2023-05-16 20:01:22 -0600

    Finalize the function to past tests

--- src/calculate-recipe-price.js
+++ src/calculate-recipe-price.js
@@ -1,64 +1,52 @@
 import { sampleRecipeData } from "../test/sampleIngredients";
 
 const compileIngredientQuantity = (recipe) => {
-  console.log("RECIPE,", recipe)
   return recipe.ingredients.map((quantity) => {
-    quantity = {
-      id: quantity.id,
-      quantity: quantity.quantity.amount,
-    };
-    return quantity;
-  });
-};
-
-const sumTotalIngredientQuantity = (
-  sampleRecipeData,
-) => {
-  const ingQuantityData = compileIngredientQuantity(
-    sampleRecipeData
-  );
-  return ingQuantityData.reduce((acc, curr) => {
-    acc += curr.quantity;
-    console.log(acc)
-    return acc;
-  }, 0);
-};
-
-const costInCents = (sampleRecipeData, sampleIngredientsData) => {
-  const ingInfo = compileIngredientQuantity(
-    sampleRecipeData,
-    sampleIngredientsData
-  );
-  return ingInfo.map((id) => {
-    const ingredient = sampleIngredientsData.find((ingredient) => {
-      return ingredient.id === id.id;
+      quantity = {
+        id: quantity.id,
+        quantity: quantity.quantity.amount,
+      };
+      return quantity;
     });
-    return ingredient.estimatedCostInCents;
-  });
-};
+}
 
-const totalCostInCents = (sampleRecipeData, sampleIngredientsData) => {
-  const centsPerRecipe = costInCents(sampleRecipeData, sampleIngredientsData);
-  return centsPerRecipe.reduce((acc, curr) => {
-    acc += curr;
-    return acc;
-  }, 0);
-};
+const sumTotalIngredientQuantity = (ingredientQuantityInfo) => {
+return ingredientQuantityInfo.reduce((acc, curr) => {
+      acc += curr.quantity;
+      return acc;
+    }, 0);
+}
 
-const calculateRecipePrice = (sampleRecipeData, sampleIngredientsData) => {
-  if (!sampleRecipeData) {
-    return "Sorry no price could be calculated.";
+const costInCents = (ingredientQuantityInfo, ingredientsData) => { 
+  return ingredientQuantityInfo.map((id) => {
+      const ingredient = ingredientsData.find((ingredient) => {
+        return ingredient.id === id.id;
+      });
+      return ingredient.estimatedCostInCents;
+    });
+  }
+
+const totalCostInCents = (costInCents) => { 
+  return costInCents.reduce((acc, curr) => {
+      acc += curr;
+      return acc;
+    }, 0);
+}
+
+const calculateRecipePrice = (recipe, ingredientsData) => {
+  if (!recipe) {
+    return "Sorry this is not a recipe!";
   } else {
-    const totalQuantityAmt = sumTotalIngredientQuantity(
-      sampleRecipeData,
-      sampleIngredientsData
-    );
-    const totalCents = totalCostInCents(
-      sampleRecipeData[0],
-      sampleIngredientsData
-    );
-    const finalTotal = (totalQuantityAmt * totalCents) / 100;
-    return `$${finalTotal.toFixed(2)}`;
+    const ingredientQuantityInfo = compileIngredientQuantity(recipe)
+
+    const sumTotalQuantity = sumTotalIngredientQuantity(ingredientQuantityInfo)
+
+    const costInCentss = costInCents(ingredientQuantityInfo, ingredientsData)
+
+    const totalCost = totalCostInCents(costInCentss)
+
+    const finalTotalCost = (sumTotalQuantity * totalCost) / 100;
+    return `$${finalTotalCost.toFixed(2)}`;
   }
 };
 
