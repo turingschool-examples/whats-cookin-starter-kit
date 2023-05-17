d3bbf11e5b22642aab27003f2b285c0d5a96ccc2
Elise Jones <02elise.jones02@gmail.com>
2023-05-17 14:24:48 -0600

    Add functions for collecting bookmarked recipes and removing them

--- src/recipes-to-cook.js
+++ src/recipes-to-cook.js
@@ -1,10 +1,18 @@
-const recipesToCook = (recipe) => {
-
+const recipesToCook = (eventId, currentUser) => {
+  currentUser.recipesToCook.push(eventId)
 }
 
-export { recipesToCook}
+const removeRecipes = (eventId, currentUser) => {
+  const index = currentUser.recipesToCook.indexOf(eventId)
+    currentUser.recipesToCook.splice(index, 1)
+}
+
+export { recipesToCook, removeRecipes }
 // on page load we need to select a random user then push whatever
 //recipes they choose into their array
+//add event listener to the bookmark and then grab id
+//then push it into the userrecipes array 
+//event.target.classList.contains('bookmark-icon')
 //grab event.target of the recipe match that recipe id
 //just push id's into users recipestocook array
 //create a separate function that will match recipe id to the recipe data
