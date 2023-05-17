915167c3d292312685d406efdd24a0c36db3e81e
Elise Jones <02elise.jones02@gmail.com>
2023-05-17 15:55:35 -0600

    Add tests for edited funcitons for recipe objects

--- test/recipes-to-cook-test.js
+++ test/recipes-to-cook-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
-import { recipesToCook } from '../src/recipes-to-cook'
+import { recipesToCook, removeRecipes } from '../src/recipes-to-cook'
 import { sampleUserData} from './sampleUsers'
+import { sampleRecipeData } from './sampleIngredients';
 
 
 describe('recipesToCook', () => {
@@ -13,12 +14,37 @@ describe('recipesToCook', () => {
     name: "Sadye Welch",
     recipesToCook: []
   }
-  console.log(currentUser)
-  it('should be place bookmarked recipes into the recipesToCook array for current user', () => {
-    expect(recipesToCook).to.be.a('function');
+
+  it('user should be able to bookmark multiple recipes', () => {
+    recipesToCook(101, currentUser);
+    expect(currentUser.recipesToCook).to.deep.equal(sampleRecipeData[0]);
+    recipesToCook(106, currentUser);
+    expect(currentUser.recipesToCook).to.have.same.member(sampleRecipeData[0], sampleRecipeData[5]);
+    recipesToCook(102, currentUser);
+    expect(currentUser.recipesToCook).to.have.same.member(sampleRecipeData[0], sampleRecipeData[5], sampleRecipeData[1]);
   });
 });
 
+describe('removeRecipes', () => {
+  it('should be a function', () => {
+    expect(removeRecipes).to.be.a("function");
+  });
+  it('removes recipe when bookmark is unchecked', () => {
+
+    let currentUser = {
+      id: 1,
+      name: "Sadye Welch",
+      recipesToCook: []
+    }
+
+    recipesToCook(2, currentUser);
+    recipesToCook(3, currentUser);
+    recipesToCook(4, currentUser);
+    removeRecipes(2, currentUser)
+
+    expect(currentUser.recipesToCook).to.deep.equal([3, 4])
+  })
+});
 //we need to have current suer and assign it to a user
 //we can expect the recipes to cook to deep
 //equal an array of recipe numbers
