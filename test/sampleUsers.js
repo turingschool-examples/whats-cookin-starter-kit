2ab5d25a8b0ee9d5e3415c29e67d424ddcaf4332
“Saki <sakisandra.c@gmail.com>
2023-05-16 20:13:19 -0600

    Add sample users data

---
+++ test/sampleUsers.js
@@ -0,0 +1,31 @@
+const sampleUserData = {
+  users: [
+    {
+      id: 1,
+      name: "Sadye Welch",
+      recipesToCook: []
+    },
+    {
+      id: 2,
+      name: "Jordyn West",
+      recipesToCook: []
+    },
+    {
+      id: 3,
+      name: 'Willie Eichmann',
+      recipesToCook: []
+    },
+    {
+      id: 4,
+      name: 'Blanca Bergnaum',
+      recipesToCook: []
+    },
+    {
+      id: 5,
+      name: 'Wilhelm Lynch',
+      recipesToCook: []
+    }
+  ]
+}
+
+export { sampleUserData }
