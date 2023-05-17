fbc87fecdc90ed6440e8203dae2996c775ed918d
Elise Jones <02elise.jones02@gmail.com>
2023-05-17 14:24:07 -0600

    Add tests for function:

--- test/recipes-to-cook-test.js
+++ test/recipes-to-cook-test.js
@@ -4,10 +4,24 @@ import { sampleUserData} from './sampleUsers'
 
 
 describe('recipesToCook', () => {
-  
   it('should be a fuction', () => {
-    console.log(sampleUserData)
     expect(recipesToCook).to.be.a('function');
   });
 
+  let currentUser = {
+    id: 1,
+    name: "Sadye Welch",
+    recipesToCook: []
+  }
+  console.log(currentUser)
+  it('should be place bookmarked recipes into the recipesToCook array for current user', () => {
+    expect(recipesToCook).to.be.a('function');
+  });
 });
+
+//we need to have current suer and assign it to a user
+//we can expect the recipes to cook to deep
+//equal an array of recipe numbers
+//sad path: ? no event.target so undefined
+//removeREcips = test array length or a deep equal
+//sadpath
