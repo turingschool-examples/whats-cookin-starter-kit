cfae2cad48c164864d3b04e22fbd49a739ba3e50
Denzel Turambi <dturambi26@gmail.com>
2023-05-18 11:37:35 -0600

    refactor: Refactor renderFilteredRecipes function

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,29 +6,35 @@ import { recipesFromTag } from "./recipeUtils";
 const allRecipesButton = document.querySelector('.all-recipes');
 const frontRecipeDisplay = document.querySelector('.front-recipe-display');
 const allRecipeDisplay = document.querySelector('.all-recipes-display');
-const allFilterDisplay = document.querySelector('.all-filters')
+const allFilterDisplay = document.querySelector('.all-filters');
+const checkCategories = document.getElementsByName('checkbox');
 
 //Event Listeners
 allRecipesButton.addEventListener('click', showRecipes);
-allFilterDisplay.addEventListener('change', function(event) {
-  if (event.target.classList.contains('checkbox')){
-    renderFilteredRecipes(event);
+allFilterDisplay.addEventListener('click', function (event) {
+  if (event.target.classList.contains('checkbox')) {
+    renderFilteredRecipes(event)
   }
 });
 
-function renderFilteredRecipes(event) {
-  console.log(event.target.id)
-  var filtered = recipesFromTag(recipeData, [event.target.id]);
-  console.log(filtered);
+//Event Handlers/Functions
+function renderFilteredRecipes() {
+  const tags = Array.from(checkCategories).filter((category) => category.checked).map(c => c.id)
+  console.log(tags)
+  if (tags.length === 0) {
+    showRecipes()
+    return
+  }
+    let filtered = recipesFromTag(recipeData, tags);
   allRecipeDisplay.innerHTML = '';
   filtered.forEach(recipe => allRecipeDisplay.innerHTML += `<div class = "recipe-wrapper">
-  <img id="${recipe.name}" src="${recipe.image}" class="recipe">
-  <div class = "recipe-info">
-    <p>${recipe.name}</p>
-  </div>`)
-  }
+      <img id="${recipe.name}" src="${recipe.image}" class="recipe">
+      <div class = "recipe-info">
+        <p>${recipe.name}</p>
+        <p>Total Cost: $..</p
+      </div>`)
+}
 
-//Event Handlers/Functions
 function showRecipes() {
   removeHiddenClass([allRecipeDisplay, allFilterDisplay])
   addHiddenClass([frontRecipeDisplay]);
@@ -55,6 +61,33 @@ function exampleFunction1(person) {
 
 function exampleFunction2(person) {
   console.log(`bye now ${person}`)
+}
+
+// Andrea:
+const singleRecipeDisplay = document.querySelector('.single-recipe-display')
+allRecipeDisplay.addEventListener('click', e => {
+  addHiddenClass([allRecipeDisplay]);
+  console.log(singleRecipeDisplay)
+  removeHiddenClass([singleRecipeDisplay])
+  console.log(e.target)
+  viewSelectedRecipe(e)
+})
+import { findRecipe, findIngredientNames, calculateRecipeCost, recipeInstructions } from "../src/recipeUtils";
+const viewSelectedRecipe = e => {
+  singleRecipeDisplay.innerHTML = ''
+  const recipeName = e.target.id
+  console.log(e.target.id)
+  const selectedRecipe = findRecipe(recipeData, recipeName)
+  console.log(selectedRecipe)
+  const recipeCost = calculateRecipeCost(selectedRecipe)
+  console.log(recipeCost)
+  singleRecipeDisplay.innerHTML = `
+  <h2>${selectedRecipe.name}</h2>
+  <img id="${selectedRecipe.id}" src="${selectedRecipe.image}" class="recipe" alt='${selectedRecipe.name}'>
+  <p class="total-cost-box">This recipe costs a total of: $${recipeCost} to make!</p>
+  <p class="ingredient-box">Ingredients Box</p>
+  <p class="instruction-box"></p>
+  `
 }
 
 
