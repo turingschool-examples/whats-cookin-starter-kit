9c0732505c339f39d41715cc08de0388a859850c
“Saki <sakisandra.c@gmail.com>
2023-05-19 14:43:05 -0600

    Add combined fetch api function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,36 +1,12 @@
-let curUser 
-
-const getUsers = () => {
-  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
-  .then((res) => {
-    return res.json()
-  })
-  .then((data)=> {
-    // console.log(data)
-    return data
-  })
+const fetchAPI = (dataType) => {
+ return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${dataType}`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => { 
+      return data 
+    })
+    .catch(err => console.log(err));
 }
 
-const getRecipes = () => {
-  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
-  .then((res) => {
-    return res.json()
-  })
-  .then((data)=> {
-    // console.log(data)
-    return data
-  })
-}
-
-const getIngredients = () => {
-  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients')
-  .then((res) => {
-    return res.json()
-  })
-  .then((data)=> {
-    // console.log(data)
-    return data
-  })
-}
-
-export {getUsers, getRecipes, getIngredients}
+export { fetchAPI }
