66d838344c0d554505b560a03bc9452c72833415
“Saki <sakisandra.c@gmail.com>
2023-05-18 20:15:57 -0600

    Fix syntax and delete unused data

--- src/users.js
+++ src/users.js
@@ -1,8 +1,8 @@
 const getRandomIndex = (usersLength) => {
- return Math.floor(Math.random() * usersLength)
+ return Math.floor(Math.random() * usersLength);
 }
 const getRandomUser = (userData) => {
-  return userData.users[getRandomIndex(userData.users.length)]
+  return userData.users[getRandomIndex(userData.users.length)];
 }
 
-export { getRandomUser, getRandomIndex}
+export { getRandomUser, getRandomIndex }
