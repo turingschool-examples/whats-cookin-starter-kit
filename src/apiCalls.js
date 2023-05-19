461feae24aeb22d5d08382824402766535244383
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-19 17:27:20 -0400

    Fetch user data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,17 @@
 // Your fetch requests will live here!
 
+import { getRandomUser } from "./users"
+let currentUser = {};
 
-console.log('I will be a fetch request!')
+const assignCurrentUser = () => {
+  fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+    .then(response => response.json())
+    .then(users => {
+      currentUser = getRandomUser(users.users);
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+  
+export {assignCurrentUser, currentUser}
