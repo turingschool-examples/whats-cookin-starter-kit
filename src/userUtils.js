092fd2fd202bf1b776b344c60fa7d3fdbf4d7f5c
Fernando Robles <fernandorobles97@yahoo.com>
2023-05-20 13:17:40 -0500

    refactor: Delete unnecessary global variable and console.log

--- src/userUtils.js
+++ src/userUtils.js
@@ -1,11 +1,9 @@
 var recipesToCook = [];
-var currentUser;
 
 const saveRecipe = (recipes, recipeName) => {
   let fullRecipe = recipes.find(recipe => recipe.name === recipeName);
   if(!recipesToCook.includes(fullRecipe)) {
     recipesToCook.push(fullRecipe);
-    console.log(recipesToCook)
     return recipesToCook;
   }
   return recipesToCook;
