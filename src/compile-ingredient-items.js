f9ed4bc5d2807c69ee03d2d04cc59fa51a586fc7
Jade Shi <ziyushi1997@gmail.com>
2023-05-21 00:23:37 -0700

    Add sad path to compileIngredientItems

--- src/compile-ingredient-items.js
+++ src/compile-ingredient-items.js
@@ -1,6 +1,10 @@
 import { getIngredientName } from './get-ingredient-name';
 
 const compileIngredientItems = (recipe, ingredientsData) => {
+  if (typeof recipe !== 'object' || Array.isArray(recipe)|| (typeof ingredientsData !== 'object' && !Array.isArray(ingredientsData))) {
+    return 'Error: wrong intput type';
+  }
+
   const ingredientItems = recipe.ingredients.map(ele => {
     return `${getIngredientName(ele.id, ingredientsData)} × ${ele.quantity.amount} ${ele.quantity.unit}`;
   });
