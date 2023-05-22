bf9b3d95b53dcd9e8b00ff464dce79ba9d458717
Hollis Vohr <hollis.vohr@gmail.com>
2023-05-21 19:10:35 -0600

    feat: add fetch function" "

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,17 @@
 // Your fetch requests will live here!
+const getData = (type) => {
+  if (!['ingredients', 'users', 'recipes'].includes(type)) {
+    throw new Error("check spelling")
+  }
+    return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${type}`)
+      .then((response => response.json()))
+      .then((data) => { return data })
+}
+
+export {
+  getData
+}
+
 
 
 console.log('I will be a fetch request!')
