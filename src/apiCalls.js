282432aff91bdeeaa226fca48b64195c71e00a3a
Jade Shi <ziyushi1997@gmail.com>
2023-05-31 20:38:46 -0700

    Replace deplpoyed APIs and create errorHandling function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,13 @@
 const fetchAPI = (dataType) => {
- return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${dataType}`)
+ return fetch(`http://localhost:3001/api/v1/${dataType}`)
     .then((response) => {
       return response.json()
     })
-    .catch(() => alert('ERROR: FAILED TO FETCH!'));
+    .catch(err => errorHandling(err));
+}
+
+const errorHandling = err => {
+  alert(`${err.name}: ${err.message}!\nWhat's Cookin failed to obtain data from the server.`)
 }
 
 export { fetchAPI }
