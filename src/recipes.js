72dd73087403df8c2d3ae5ed78e92c6434e060cb
Logan Matheny <loganpaulmatheny@gmail.com>
2023-08-26 18:04:43 -0400

    Create basic filter functions

---
+++ src/recipes.js
@@ -0,0 +1,16 @@
+const filterByTag = (tag, recipes) => {
+  return recipes.filter((recipe) => {
+    return recipe.tags.includes(tag);
+  });
+};
+
+const searchRecipes = (searchTerm, recipes) => {
+  return recipes.filter((recipe) => {
+    return recipe.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+};
+
+module.exports = {
+  filterByTag,
+  searchRecipes,
+};
