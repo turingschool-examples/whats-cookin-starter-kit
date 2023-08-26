45a315e0ebf221d68577ba4ffc7ff030f51de886
Logan Matheny <loganpaulmatheny@gmail.com>
2023-08-26 19:32:55 -0400

    Begin recipe card functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,15 +2,22 @@
 
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
 function exampleFunction1(person) {
-  console.log(`oh hi there ${person}`)
+  console.log(`oh hi there ${person}`);
 }
 
 function exampleFunction2(person) {
-  console.log(`bye now ${person}`)
+  console.log(`bye now ${person}`);
 }
 
+const createRecipeCards = (recipes) => {
+  // INSERT SECTION WHERE YOU'RE BUILDING THE CARDS HERE .innerHTHML = ""
+  recipes.forEach((recipe) => {
+    const recipeCard = document.createElement("div");
+    recipeCard.classList.add("recipe-card");
+    recipeCard.id.add(recipe.id);
+    recipe
 
-export {
-  exampleFunction1,
-  exampleFunction2,
-}
+  });
+};
+
+export { exampleFunction1, exampleFunction2 };
