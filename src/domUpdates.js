86541a2ae410ebcafd8db175e8d080bd063d0940
Jen Nguyen <jenniferannnguyen@live.com>
2023-08-27 17:53:04 -0600

    Remove console.logs

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,30 +1,18 @@
-const recipeArea = document.querySelector(".recipe-area");
+const recipeArea = document.querySelector('.recipe-area');
 
-//NOTE: Your DOM manipulation will occur in this file
-
-//Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-// function exampleFunction1(person) {
-//   console.log(`oh hi there ${person}`);
-// }
-
-// function exampleFunction2(person) {
-//   console.log(`bye now ${person}`);
-// }
-
-const createRecipeCards = (recipes) => {
-  recipeArea.innerHTML = "";
-  console.log(recipes);
-  recipes.forEach((recipe) => {
-    let recipeCard = document.createElement("div");
-    recipeCard.classList.add("recipe-card");
-    recipeCard.setAttribute("id", recipe.id);
-    let recipeTitle = document.createElement("h2");
-    recipeTitle.classList.add("recipe-title");
+const createRecipeCards = recipes => {
+  recipeArea.innerHTML = '';
+  recipes.forEach(recipe => {
+    let recipeCard = document.createElement('div');
+    recipeCard.classList.add('recipe-card');
+    recipeCard.setAttribute('id', recipe.id);
+    let recipeTitle = document.createElement('h2');
+    recipeTitle.classList.add('recipe-title');
     recipeCard.appendChild(recipeTitle);
     recipeTitle.innerText = recipe.name;
-    let recipeImage = document.createElement("img");
-    recipeImage.classList.add("recipe-image");
-    recipeImage.setAttribute("src", recipe.image);
+    let recipeImage = document.createElement('img');
+    recipeImage.classList.add('recipe-image');
+    recipeImage.setAttribute('src', recipe.image);
     recipeCard.appendChild(recipeImage);
     recipeArea.appendChild(recipeCard);
   });
