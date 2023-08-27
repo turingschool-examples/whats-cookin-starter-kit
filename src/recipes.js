7c11c19ee966bde3648d5d63686466388926ab6e
Jen Nguyen <jenniferannnguyen@live.com>
2023-08-27 17:53:45 -0600

    Add getIngredientNames function

--- src/recipes.js
+++ src/recipes.js
@@ -1,19 +1,33 @@
 const filterByTag = (tag, recipes, status) => {
   if (status === true) {
-    return recipes.filter((recipe) => {
+    return recipes.filter(recipe => {
       return recipe.tags.includes(tag);
     });
   } else return recipes;
 };
 
 const searchRecipes = (searchTerm, recipes) => {
-  return recipes.filter((recipe) => {
-    console.log("getting here");
+  return recipes.filter(recipe => {
     return recipe.name.toLowerCase().includes(searchTerm.toLowerCase());
   });
+};
+
+const getIngredientNames = (recipe, ingredients) => {
+  let ingredientIds = recipe.ingredients.map(ingr => {
+    return ingr.id;
+  });
+
+  let ingredientNames = ingredients.reduce((acc, cv) => {
+    if (ingredientIds.includes(cv.id)) {
+      acc.push(cv.name);
+    }
+    return acc;
+  }, []);
+  return ingredientNames;
 };
 
 module.exports = {
   filterByTag,
   searchRecipes,
+  getIngredientNames,
 };
