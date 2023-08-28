f9eb9a8a751dedf2dcfa9f01e25f2dc47a4b46ea
Logan Matheny <loganpaulmatheny@gmail.com>
2023-08-28 09:17:17 -0400

    Create function to find the right recipe card to then be built

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,21 +1,29 @@
-const recipeArea = document.querySelector('.recipe-area');
+const recipeArea = document.querySelector(".recipe-area");
 
-const createRecipeCards = recipes => {
-  recipeArea.innerHTML = '';
-  recipes.forEach(recipe => {
-    let recipeCard = document.createElement('div');
-    recipeCard.classList.add('recipe-card');
-    recipeCard.setAttribute('id', recipe.id);
-    let recipeTitle = document.createElement('h2');
-    recipeTitle.classList.add('recipe-title');
+const createRecipeCards = (recipes) => {
+  recipeArea.innerHTML = "";
+  recipes.forEach((recipe) => {
+    let recipeCard = document.createElement("div");
+    recipeCard.classList.add("recipe-card");
+    recipeCard.setAttribute("id", recipe.id);
+    let recipeTitle = document.createElement("h2");
+    recipeTitle.classList.add("recipe-title");
     recipeCard.appendChild(recipeTitle);
     recipeTitle.innerText = recipe.name;
-    let recipeImage = document.createElement('img');
-    recipeImage.classList.add('recipe-image');
-    recipeImage.setAttribute('src', recipe.image);
+    let recipeImage = document.createElement("img");
+    recipeImage.classList.add("recipe-image");
+    recipeImage.setAttribute("src", recipe.image);
     recipeCard.appendChild(recipeImage);
     recipeArea.appendChild(recipeCard);
   });
 };
 
-export { createRecipeCards };
+const locateRecipe = (recipeId, recipes) => {
+  let formatRecipeId = Number(recipeId);
+  let foundRecipe = recipes.find((recipe) => {
+    return recipe.id === formatRecipeId;
+  });
+  return foundRecipe;
+};
+
+export { createRecipeCards, locateRecipe };
