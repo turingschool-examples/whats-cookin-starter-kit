fab6666b41335cebd4372632e683e8691e94c8ad
Patrick Eitel <patrickeitel12@gmail.com>
2023-08-29 13:56:34 -0700

    update a displayTags function

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -71,7 +71,7 @@ export function displayTags(array) {
   const tagsArray = returnListOfUniqueTags(array);
   let tagsHtml = "";
   tagsArray.forEach((tagEl) => {
-    tagsHtml += `<button class="btn" id="${tagEl}">${tagEl}</button>
+    tagsHtml += `<div><button class="btn" id="${tagEl}"></button><p>${tagEl}</p></div>
     `;
   });
   tagButtons.innerHTML = tagsHtml;
