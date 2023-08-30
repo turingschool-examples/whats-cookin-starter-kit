484767efca44adfdf8582c09aaf1c459d83f1e60
Patrick Eitel <patrickeitel12@gmail.com>
2023-08-29 18:32:52 -0700

    Add a robust test for returnFilteredTag function. Remove an unnecessary test

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -3,82 +3,76 @@ const expect = chai.expect;
 
 // const { createFunction } = require("../src/scripts.js");
 import {
-  createFunction,
   returnFilteredListName,
   returnIngredientNames,
   returnFilteredTag,
   returnRecipeCost,
   returnRecipeDirections,
-} from "../src/functions.js";
+} from "../src/functions.js"
 //from functions
-
 // const ingredientsData = require("../src/data/ingredients.js");
-import ingredientsData from "../src/data/ingredients.js";
+import ingredientsTestData from "../src/data/ingredients-test-data.js";
 
-import recipeData from "../src/data/recipes.js";
+import recipeTestData from "../src/data/recipes-test-data.js";
 
-import usersData from "../src/data/users.js";
-
-describe("Testing", () => {
-  it("Should be a function", () => {
-    expect(createFunction).to.be.a("function");
-  });
-
-  it("should return an array", () => {
-    const result = createFunction(recipeData);
-    expect(result).to.be.an("array");
-  });
-});
+import usersTestData from "../src/data/users-test-data.js";
 
 describe("Filtered List Based on Tag", () => {
   it("should return a filtered list based on a tag", () => {
-    const result = returnFilteredTag(recipeData, "starter");
-    expect(result.length).to.equal(9);
-  });
-  
-  it("should work with a different tag", () => {
-    const result = returnFilteredTag(recipeData, "sauce");
-    expect(result.length).to.equal(1)
-  })
-});
-
-describe("Cost of Recipe", () => {
-  it("should calculate the cost of a given recipe's ingredients", () => {
-    const result = returnRecipeCost(recipeData, ingredientsData, 595736);
-    expect(result).to.equal(177.76000000000002);
-  });
-});
-
-describe("Filtered List Based on Name", () => {
-  it("should return a filtered list based on a name", () => {
-    const result = returnFilteredListName(
-      recipeData,
-      "Loaded Chocolate Chip Pudding Cookie Cups"
-    );
+    const result = returnFilteredTag(recipeTestData, "starter");
+    expect(result[0].id).to.equal(595736);
     expect(result.length).to.equal(1);
   });
-});
 
-describe("Directions for Recipe", () => {
-  it("should return the directions for a given recipe", () => {
-    const result = returnRecipeDirections(recipeData, 595736);
-    expect(result.length).to.equal(6);
-  });
-});
-
-describe("Filtered List Based on Name", () => {
-  it("should return a filtered list based on a name", () => {
-    const result = returnFilteredListName(
-      recipeData,
-      "Loaded Chocolate Chip Pudding Cookie Cups"
-    );
+  it("should return a filtered list based on a tag", () => {
+    const result = returnFilteredTag(recipeTestData, "lunch");
+    expect(result[0].id).to.equal(678353);
     expect(result.length).to.equal(1);
   });
-});
 
-describe("Names of Ingredients", () => {
-  it("should determine the names of ingredients based on a recipe name", () => {
-    const result = returnIngredientNames(recipeData, ingredientsData, 595736);
-    expect(result.length).to.equal(11);
+  it("should return an empty array if no matches", () => {
+    const result = returnFilteredTag(recipeTestData, "second breakfeast");
+    expect(result.length).to.equal(0);
   });
 });
+
+// describe("Cost of Recipe", () => {
+//   it("should calculate the cost of a given recipe's ingredients", () => {
+//     const result = returnRecipeCost(recipeData, ingredientsData, 595736);
+//     expect(result).to.equal(177.76000000000002);
+//   });
+// });
+
+// describe("Filtered List Based on Name", () => {
+//   it("should return a filtered list based on a name", () => {
+//     const result = returnFilteredListName(
+//       recipeData,
+//       "Loaded Chocolate Chip Pudding Cookie Cups"
+//     );
+//     expect(result.length).to.equal(1);
+//   });
+// });
+
+// describe("Directions for Recipe", () => {
+//   it("should return the directions for a given recipe", () => {
+//     const result = returnRecipeDirections(recipeData, 595736);
+//     expect(result.length).to.equal(6);
+//   });
+// });
+
+// describe("Filtered List Based on Name", () => {
+//   it("should return a filtered list based on a name", () => {
+//     const result = returnFilteredListName(
+//       recipeData,
+//       "Loaded Chocolate Chip Pudding Cookie Cups"
+//     );
+//     expect(result.length).to.equal(1);
+//   });
+// });
+
+// describe("Names of Ingredients", () => {
+//   it("should determine the names of ingredients based on a recipe name", () => {
+//     const result = returnIngredientNames(recipeData, ingredientsData, 595736);
+//     expect(result.length).to.equal(11);
+//   });
+// });
