887b98c325defbdd2dcaae289bde7661eea22ba2
Zen McMillan <zenmcmillan@gmail.com>
2023-08-31 18:32:03 -0600

    Create function to show all recipes and their names when the main page in loaded

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,24 @@
 //NOTE: Your DOM manipulation will occur in this file
 
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-function exampleFunction1(person) {
-  console.log(`oh hi there ${person}`)
-}
-
-function exampleFunction2(person) {
-  console.log(`bye now ${person}`)
-}
 
 
-export {
-  exampleFunction1,
-  exampleFunction2,
+const recipesContainer = document.querySelector('.recipe-container')
+
+const renderRecipes = (recipeData) => {
+
+  recipesContainer.innerHTML = '';
+
+  for (let i = 0; i < recipeData.length; i++) {
+    recipesContainer.innerHTML += `
+    <div class="${recipeData[i].id}">
+      <p class ="recipe-name">${recipeData[i].name}</p>
+      <img class="image-styling" src="${recipeData[i].image}">
+      </div>
+    `
+  }
 }
+
+
+ 
+
