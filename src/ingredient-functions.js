2b09f0a602662c22cb25707da60c12352438225d
William Lavery <lavery_william@yahoo.com>
2023-09-11 22:09:20 -0400

    Move recipe tests into recipe test files

---
+++ src/ingredient-functions.js
@@ -0,0 +1,46 @@
+const { ingredientsData } = require("./data/ingredients-test-data")
+
+// Determine the names of ingredients needed for a given recipe.
+
+const findRecipeIngredients = (recipeData, ingredientsData, id) => {
+  let givenRecipe = recipeData.find(recipe => {
+    return recipe["id"] == id
+  })
+
+  let ingredientIds = givenRecipe["ingredients"].map(ingredient => {
+    return ingredient["id"]
+  })
+
+  let recipeIngredients = ingredientsData.filter(ingredient => {
+    return ingredientIds.includes(ingredient["id"])
+  })
+
+  return recipeIngredients.map(ingredient => {
+    let ingredientNames = ingredient["name"]
+    return ingredientNames
+  } )
+}
+
+const calculateCost = (recipeData, ingredientsData, clickedId) => {
+  const clickedRecipe = recipeData.find(recipe => recipe.id == clickedId);
+  let reducedIngredients = clickedRecipe.ingredients.reduce((accumulator, currentValue) => {
+    let ingredientPrice = ingredientsData.find(ingredientDetail => ingredientDetail.id === currentValue.id);
+    accumulator += ingredientPrice.estimatedCostInCents * currentValue.quantity.amount;
+    return accumulator;
+  }, 0);
+  const costInDollars = (reducedIngredients / 100).toFixed(2); // Convert to dollars with 2 decimal places
+  return `$${costInDollars}`;
+};
+
+const findDirections = (recipeData, recipeName) => {
+  let chosenRecipe = recipeData.find(recipe => {
+    return recipeName === recipe.name
+  })
+    return chosenRecipe.instructions
+}
+
+export {
+  findRecipeIngredients,
+  calculateCost,
+  findDirections
+}
