cb00ac66e1f1f6b222b289f104615749b1ce374f
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-09-12 15:17:03 -0600

    Add sad path for fetch call using .catch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,7 +6,8 @@ const fetchData = (type, link, fn) => {
     .then(response => response.json())
     .then(data => {
       return fn(data[type]);
-    });
+    })
+    .catch(error => console.log("An error occurred. Oh dear!:", error)) //Fix sad path... what should happen if error
 };
 
 export {
