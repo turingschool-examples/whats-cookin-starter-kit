8e4abedadf1805e06f1461aed19de3d0a0001933
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-12-04 16:11:24 -0800

    Add sample recipe data and filter by tag function

--- src/recipes.js
+++ src/recipes.js
@@ -1,5 +1,12 @@
 //Here is an example demonstrating logic separated that can be imported into the scripts and test files. Feel free to update this later! 
 
-export const findRecipeIngredients = recipe => {
-  console.log(recipe)
+// export const findRecipeIngredients = recipe => {
+//   console.log(recipe)
+// }
+
+export function filterRecipesByTag(recipes, tag) {
+const filteredRecipesByTag = recipes.filter(recipe => {
+  return recipe.tags.includes(tag);
+});
+return filteredRecipesByTag;
 }
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,8 +1,17 @@
 import { expect } from 'chai';
-import { findRecipeIngredients } from '../src/recipes';
+import { findRecipeIngredients, filterRecipesByTag } from '../src/recipes';
+import { sampleRecipes } from '../src/data/sample-recipes';
 
-describe('Recipe', () => {
-  it('Should be a function', () => {
-    expect(findRecipeIngredients).to.be.a('function');
+// describe('Recipe', () => {
+//   it('Should be a function', () => {
+//     expect(findRecipeIngredients).to.be.a('function');
+//   });
+// })
+
+describe('filterRecipesByTag', () => {
+  it('should filter recipes by tag', () => {
+    const filteredRecipes = filterRecipesByTag(sampleRecipes, 'side dish');
+    const filteredRecipesIds = filteredRecipes.map(sampleRecipe => sampleRecipe.id);
+    expect(filteredRecipesIds).to.deep.equal([601216, 226562, 605132]);
   });
-})
+});
