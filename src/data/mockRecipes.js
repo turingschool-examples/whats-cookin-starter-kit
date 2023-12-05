3f896a64400b07a50a338dddee57033cfe635d6a
Eric Batiste <ericbatiste4@gmail.com>
2023-12-05 14:28:47 -0700

    Create mock data sets and begin unit tests for recipes.

---
+++ src/data/mockRecipes.js
@@ -0,0 +1,275 @@
+const recipeData = [
+  {
+    id: 1,
+    image: "url",
+    ingredients: [
+      {
+        id: 101,
+        quantity: {
+          amount: 2,
+          unit: "c"
+        }
+      },
+      {
+        id: 102,
+        quantity: {
+          amount: 1.5,
+          unit: "tbsp"
+        }
+      },
+      {
+        id: 104,
+        quantity: {
+          amount: 2,
+          unit: "slice"
+        }
+      }
+    ],
+    instructions: [
+      {
+        instruction: "This is instruction number 1",
+        number: 1
+      },
+      {
+        instruction: "This is instruction number 2",
+        number: 2
+      },
+      {
+        instruction: "This is instruction number 3",
+        number: 3
+      }
+    ],
+    name: "Peanut Butter and Jelly",
+    tags: [
+        "lunch",
+        'snack'
+    ]
+  },
+  {
+    id: 2,
+    image: "url",
+    ingredients: [
+      {
+        id: 103,
+        quantity: {
+          amount: 1,
+          unit: "one egg"
+        }
+      },
+      {
+        id: 105,
+        quantity: {
+          amount: 1,
+          unit: "cup"
+        }
+      },
+      {
+        id: 106,
+        quantity: {
+          amount: 2,
+          unit: "cup"
+        }
+      },
+      {
+        id: 110,
+        quantity: {
+          amount: 2,
+          unit: "tbsp"
+        }
+      },
+      {
+        id: 111,
+        quantity: {
+          amount: 1,
+          unit: "1/8 cup"
+        }
+      }
+    ],
+    instructions: [
+      {
+        instruction: "This is instruction number 1",
+        number: 1
+      },
+      {
+        instruction: "This is instruction number 2",
+        number: 2
+      },
+      {
+        instruction: "This is instruction number 3",
+        number: 3
+      }
+    ],
+    name: "Waffles",
+    tags: [
+      'breakfast',
+      'desert',
+      'snack'
+    ]
+  },
+  {
+    id: 3,
+    image: "url",
+    ingredients: [
+      {
+        id: 103,
+        quantity: {
+          amount: 1,
+          unit: "one egg"
+        }
+      },
+      {
+        id: 110,
+        quantity: {
+          amount: 1,
+          unit: "tbsp"
+        }
+      },
+      {
+        id: 118,
+        quantity: {
+          amount: 2,
+          unit: "slice"
+        }
+      }
+    ],
+    instructions: [
+      {
+        instruction: "This is instruction number 1",
+        number: 1
+      },
+      {
+        instruction: "This is instruction number 2",
+        number: 2
+      },
+      {
+        instruction: "This is instruction number 3",
+        number: 3
+      }
+    ],
+    name: "Bacon and Eggs",
+    tags: [
+      'breakfast'
+    ]
+  },
+  {
+    id: 4,
+    image: "url",
+    ingredients: [
+      {
+        id: 104,
+        quantity: {
+          amount: 2,
+          unit: "slice"
+        }
+      },
+      {
+        id: 107,
+        quantity: {
+          amount: 3,
+          unit: "slice"
+        }
+      },
+      {
+        id: 108,
+        quantity: {
+          amount: 1,
+          unit: "slice"
+        }
+      },
+      {
+        id: 115,
+        quantity: {
+          amount: 1,
+          unit: "tbsp"
+        }
+      }
+    ],
+    instructions: [
+      {
+        instruction: "This is instruction number 1",
+        number: 1
+      },
+      {
+        instruction: "This is instruction number 2",
+        number: 2
+      },
+      {
+        instruction: "This is instruction number 3",
+        number: 3
+      }
+    ],
+    name: "Turkey and Swiss Sandwich",
+    tags: [
+      'lunch'
+    ]
+  },
+  {
+    id: 5,
+    image: "url",
+    ingredients: [
+      {
+        id: 109,
+        quantity: {
+          amount: 1,
+          unit: "cup"
+        }
+      },
+      {
+        id: 112,
+        quantity: {
+          amount: 2,
+          unit: "cup"
+        }
+      },
+      {
+        id: 113,
+        quantity: {
+          amount: 1,
+          unit: "block"
+        }
+      },
+      {
+        id: 114,
+        quantity: {
+          amount: 2,
+          unit: "tbsp"
+        }
+      },
+      {
+        id: 116,
+        quantity: {
+          amount: 3,
+          unit: "clove"
+        }
+      },
+      {
+        id: 117,
+        quantity: {
+          amount: 2,
+          unit: "tbsp"
+        }
+      }
+    ],
+    instructions: [
+      {
+        instruction: "This is instruction number 1",
+        number: 1
+      },
+      {
+        instruction: "This is instruction number 2",
+        number: 2
+      },
+      {
+        instruction: "This is instruction number 3",
+        number: 3
+      }
+    ],
+    name: "Veggie Stir Fry",
+    tags: [
+      'lunch',
+      'dinner'
+    ]
+  }
+];
+
+export default recipeData 
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,8 +1,99 @@
 import { expect } from 'chai';
-import { findRecipeIngredients } from '../src/recipes';
+import { filterRecipesByTag, findRecipeByName, findRecipeIngredients, calcRecipeCost, returnRecipeInstructions } from '../src/recipes';
 
-describe('Recipe', () => {
+import recipeData from "../src/data/mockRecipes"
+import ingredientsData from "../src/data/mockIngredients"
+
+// beforeEach(() => {
+//   const recipeData = recipeData.map(data => data);
+//   const ingredientsData = ingredientsData.map(data => data);
+// });
+
+describe('Search by Tag', () => {
+  it('Should be a function', () => {
+    expect(filterRecipesByTag).to.be.a('function');
+  });
+
+  
+  // Test with valid tag and one matching recipe.
+  // Test with valid tag and multiple matching recipes
+  // Test with invalid tag.
+  // Test with empty string.
+  it('Should filter for multiple recipes with the same tag', () => {
+    const filteredRecipes = filterRecipesByTag('breakfast');
+    const result =  filteredRecipes.map(recipe => recipe.name);
+    
+    expect(result).to.deep.equal(['Waffles', 'Bacon and Eggs']); 
+  })
+  
+  it('Should...', () => {
+
+  })
+})
+
+describe('Search by Name', () => {
+  it('Should be a function', () => {
+    expect(findRecipeByName).to.be.a('function');
+  });
+  // Test with valid name and one matching recipe.
+  // Test with valid name and multiple matching recipes
+  // Test with invalid name.
+  // Test with empty string.
+  
+  it('Should...', () => {
+
+  })
+  
+  it('Should...', () => {
+
+  })
+})
+
+describe('Search by Ingredients', () => {
   it('Should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
   });
+  // Test with valid ingredient passed and one matching recipe
+  // Test with valid ingredient passed and multiple matching recipes
+  // Test with partially valid ingerdient passed
+  // Test with invalid ingredient passed
+  // Test with an empty string passed
+  
+  it('Should...', () => {
+
+  })
+  
+  it('Should...', () => {
+
+  })
+})
+
+describe('Calculate Cost', () => {
+  it('Should be a function', () => {
+    expect(calcRecipeCost).to.be.a('function');
+  });
+  // Test that it returns the total cost
+  //
+  
+  it('Should...', () => {
+
+  })
+  
+  it('Should...', () => {
+
+  })
+})
+
+describe('Return Cooking Instructions', () => {
+  it('Should be a function', () => {
+    expect(returnRecipeInstructions).to.be.a('function');
+  });
+  
+  it('Should...', () => {
+
+  })
+  
+  it('Should...', () => {
+
+  })
 })
