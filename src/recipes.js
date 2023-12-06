0eb967bc9dc099a28084b8572f1cda50564f9c82
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-12-05 18:18:26 -0800

    Add helper functions, refactor existing functions, and add more sad path tests

--- src/recipes.js
+++ src/recipes.js
@@ -5,11 +5,11 @@
 // }
 
 function filterRecipesByTag(recipes, tags) {
-const filteredRecipesByTag = recipes.filter(recipe => {
-  const allTagsMatch = tags.every(tag => recipe.tags.includes(tag));
-  return allTagsMatch;
-});
-return filteredRecipesByTag;
+  const filteredRecipesByTag = recipes.filter(recipe => {
+    const allTagsMatch = tags.every(tag => recipe.tags.includes(tag));
+    return allTagsMatch;
+  });
+  return filteredRecipesByTag;
 }
 
 function filterRecipesByName(recipes, name) {
@@ -18,40 +18,32 @@ function filterRecipesByName(recipes, name) {
     return upperCaseRecipeName.includes(name.toUpperCase());
   });
   return filteredRecipesByName;
-  }
+}
 
-  function getIngredientNames(recipe, ingredientsData) {
-    if (!recipe || !ingredientsData) {
-      return 'Error';
-    } else {
-      const ingredientNames = recipe.ingredients.map(ingredient => {
-        const ingredientObject = ingredientsData.find(ingredientData => {
-          return ingredientData.id === ingredient.id;
-        });
-        return ingredientObject.name;
-      }); 
-      return ingredientNames;
-    }
-  }
+function getIngredientNames(recipe, ingredientsData) {
+  return (!recipe || !ingredientsData) ? 'Error' :
+    recipe.ingredients.map(ingredient => getIngredientProperty(ingredient.id, ingredientsData, 'name') || 'Ingredient not found');
+}
 
-  function calculateRecipeCost(recipe, ingredientsData) {
-    const recipe = sampleRecipes.find(sampleRecipe => sampleRecipe.id === recipeId);
-    if (!recipe) {
-        return 0;
-    }
-    const totalCost = recipe.ingredients.reduce((acc, ingredient) => {
-        const currentIngredientObject = ingredientsData.find(ingredientData => ingredientData.id === ingredient.id);
-        if (currentIngredientObject) {
-            return acc + (currentIngredientObject.estimatedCostInCents * ingredient.quantity.amount);
-        }
-        return acc;
+function calculateRecipeCost(recipe, ingredientsData) {
+  const totalCost = (!recipe || !ingredientsData) ? 0 :
+    recipe.ingredients.reduce((acc, ingredient) => {
+      const ingredientCost = getIngredientProperty(ingredient.id, ingredientsData, 'estimatedCostInCents');
+      return acc + (ingredientCost ? ingredientCost * ingredient.quantity.amount : 0);
     }, 0);
 
-    return (totalCost / 100).toFixed(2);
+  return (totalCost / 100).toFixed(2);
+}
+
+function getIngredientProperty(ingredientId, ingredientsData, property) {
+  const ingredientObject = ingredientsData.find(ingredientData => ingredientData.id === ingredientId);
+  return ingredientObject ? ingredientObject[property] : null;
 }
 
 function getRecipeDirections(recipe) {
   return recipe.instructions;
 }
-  module.exports = { filterRecipesByTag, 
-  filterRecipesByName, getIngredientNames, calculateRecipeCost, getRecipeDirections }
+module.exports = {
+  filterRecipesByTag,
+  filterRecipesByName, getIngredientNames, calculateRecipeCost, getRecipeDirections, getIngredientProperty
+}
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { findRecipeIngredients, filterRecipesByTag, filterRecipesByName, getIngredientNames } from '../src/recipes';
+import { filterRecipesByTag, filterRecipesByName, getIngredientNames, calculateRecipeCost, getIngredientProperty } from '../src/recipes';
 import { sampleRecipes } from '../src/data/sample-recipes';
 import { sampleIngredients } from '../src/data/sample-ingredients';
 
@@ -15,12 +15,24 @@ describe('filterRecipesByTag', () => {
     const filteredRecipesIds = filteredRecipes.map(sampleRecipe => sampleRecipe.id);
     expect(filteredRecipesIds).to.deep.equal([601216, 226562, 605132, 618332]);
   });
+
   it('should filter recipes by multiple tags', () => {
     const filteredRecipes = filterRecipesByTag(sampleRecipes, ['main course', 'dinner']);
     const filteredRecipesIds = filteredRecipes.map(sampleRecipe => sampleRecipe.id);
     expect(filteredRecipesIds).to.deep.equal([991136]);
   });
+
+  it('should return an empty array if no recipes match the tag', () => {
+    const filteredRecipes = filterRecipesByTag(sampleRecipes, ['tag does not exist']);
+    expect(filteredRecipes).to.deep.equal([]);
+  });
+
+  it('should return an empty array if no recipes or tags are provided', () => {
+    const filteredRecipes = filterRecipesByTag([], []);
+    expect(filteredRecipes).to.deep.equal([]);
+  });
 });
+
 
 describe('filterRecipesByName', () => {
   it('should filter recipes by name', () => {
@@ -28,10 +40,21 @@ describe('filterRecipesByName', () => {
     const filteredRecipesIds = filteredRecipes.map(sampleRecipe => sampleRecipe.id);
     expect(filteredRecipesIds).to.deep.equal([583738, 618332]);
   });
+
   it('should still filter correctly even if name is not correct case', () => {
     const filteredRecipes = filterRecipesByName(sampleRecipes, 'cookies');
     const filteredRecipesIds = filteredRecipes.map(sampleRecipe => sampleRecipe.id);
     expect(filteredRecipesIds).to.deep.equal([583738, 618332]);
+  });
+
+  it('should return an empty array if no recipes match the name', () => {
+    const filteredRecipes = filterRecipesByName(sampleRecipes, 'non-existent name');
+    expect(filteredRecipes).to.deep.equal([]);
+  });
+
+  it('should return an empty array if no recipes or name is provided', () => {
+    const filteredRecipes = filterRecipesByName([], '');
+    expect(filteredRecipes).to.deep.equal([]);
   });
 });
 
@@ -41,9 +64,41 @@ describe('getIngredientNames', () => {
     const ingredientNames = getIngredientNames(sampleRecipe, sampleIngredients);
     expect(ingredientNames).to.deep.equal(['butter', 'hawaiian sweet rolls', 'runny honey', 'tabasco sauce', 'black pepper', 'boneless skinless chicken breasts'])
   });
+
   it('should give an error message if invalid recipe or ingredients data is passed', () => {
     const sampleRecipe = sampleRecipes[10];
     const ingredientNames = getIngredientNames(sampleRecipe, sampleIngredients);
     expect(ingredientNames).to.equal('Error')
+  });
+  
+  it('should return another message if an ingredient is not found in ingredients data', () => {
+    const sampleRecipe = { ingredients: [{ id: 'unknown' }] };
+    const ingredientNames = getIngredientNames(sampleRecipe, sampleIngredients);
+    expect(ingredientNames).to.deep.equal(['Ingredient not found']);
+  });
+});
+
+describe('calculateRecipeCost', () => {
+  it('should calculate the cost of a recipe correctly', () => {
+    const recipe = sampleRecipes.find(recipe => recipe.name === 'Buffalo Chicken Sliders');
+    const cost = calculateRecipeCost(recipe, sampleIngredients);
+    expect(cost).to.equal('94.85');
+  });
+
+  it('should return 0 if recipe or ingredients data is not provided', () => {
+    const recipe = null;
+    const cost = calculateRecipeCost(recipe, sampleIngredients);
+    expect(cost).to.equal('0.00');
+
+    const ingredientsData = null;
+    const sampleRecipe = sampleRecipes[0];
+    const cost2 = calculateRecipeCost(sampleRecipe, ingredientsData);
+    expect(cost2).to.equal('0.00');
+  });
+
+  it('should return 0 if recipe has no ingredients', () => {
+    const recipe = { ingredients: [] };
+    const cost = calculateRecipeCost(recipe, sampleIngredients);
+    expect(cost).to.equal('0.00');
   });
 });
