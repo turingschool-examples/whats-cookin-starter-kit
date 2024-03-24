fad537cf196dd3629f1d81846766338836bcf3c1
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-24 16:51:29 -0700

    refactor: Change tests to be more clear

--- test/cost-test.js
+++ test/cost-test.js
@@ -1,25 +1,20 @@
-import { expect } from 'chai';
-import { calculateRecipeCost, unitPriceLookup } from '../src/cost.js';
-import recipeData from '../src/data/recipes.js'
+import { expect } from "chai";
+import { calculateRecipeCost } from "../src/cost.js";
+import { recipe1, recipe2 } from "../src/data/mockRecipe.js";
 
-describe('calculateRecipeCost', function() {
-    const recipe = recipeData[0];
+describe("calculateRecipeCost", function () {
+  it("should default to zero dollars", function () {
+    const totalCost = calculateRecipeCost({});
+    expect(totalCost).to.be.equal(0);
+  });
 
-    it('should be able to calculate the total cost of a recipe using ingredients IDs', function() {
-        const totalCost = calculateRecipeCost(recipe, unitPriceLookup); 
-        const expectedCost = recipe.ingredients.reduce((acc, ingredient) => {
-            return acc + (ingredient.quantity.amount * unitPriceLookup[ingredient.id]);
-        }, 0);
+  it("should be able to calculate the total cost of recipe1", function () {
+    const totalCost = calculateRecipeCost(recipe1);
+    expect(totalCost).to.be.equal(177.76);
+  });
 
-        expect(totalCost).to.be.equal(+expectedCost.toFixed(2));
-    });
-
-    it('should be able to handle an ingredient with zero cost', function() {
-        const totalCost = calculateRecipeCost(recipe, unitPriceLookup);
-        const expectedCost = recipe.ingredients.reduce((acc, ingredient) => {
-            return acc + (ingredient.quantity.amount * unitPriceLookup[ingredient.id]);
-        }, 0);
-
-        expect(totalCost).to.be.equal(+expectedCost.toFixed(2));
-    });
+  it("should be able to calculate the total cost of recipe2", function () {
+    const totalCost = calculateRecipeCost(recipe2);
+    expect(totalCost).to.be.equal(1715.46);
+  });
 });
