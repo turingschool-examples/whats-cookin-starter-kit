fd8fd49bbdf5a1a4a57dafe93f6430073a3d8f2b
laurel Bonal <laurelbonal@gmail.com>
2024-03-25 16:28:51 -0600

    Display tags

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,11 +1,39 @@
 //NOTE: Your DOM manipulation will occur in this file
 
+import recipeData from "./data/recipes"
+
 //Here is an example function just to demonstrate one way you can export/import between the two js files. You'll want to delete this once you get your own code going.
-const displayRecipes = () => {
-  console.log(`Displaying recipes now`)
+
+const recipeTags = document.querySelectorAll('.recipe-tags')
+const recipeName = document.querySelectorAll('.recipe-name')
+const recipeIngredients = document.querySelectorAll('.recipe-ingredients')
+
+function displayRecipeTags(recipeData) {
+  recipeData.forEach((recipe, index) => {
+    const tagsElement = recipeTags[index];
+    tagsElement.innerText = '';
+
+    recipe.tags.forEach(tag => {
+      const tagSpan = document.createElement('span');
+      tagSpan.innerText = tag;
+      tagsElement.appendChild(tagSpan);
+    });
+  });
 }
+
+function displayRecipeNames(recipeData){
+  recipeData.forEach((recipe, index) => {
+    const nameElement = recipeName[index];
+    nameElement.innerText = recipe.name;
+  })
+}
+
+displayRecipeTags(recipeData)
+displayRecipeNames(recipeData)
+
+
 
 
 export {
-  displayRecipes,
+  displayRecipeTags,
 }
