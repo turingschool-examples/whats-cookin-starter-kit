6b95aaf42d9f5c7be3bee8a0679dc60c85cfdba4
Zach Wolek <zach.wolek@gmail.com>
2024-03-25 17:52:38 -0500

    Create the first functioning TDD

--- src/recipes.js
+++ src/recipes.js
@@ -1,5 +1,8 @@
 //Here is an example demonstrating logic separated that can be imported into the scripts and test files. Feel free to update this later! 
+import recipeData from './data/recipes';
 
-export const findRecipeIngredients = recipe => {
-  console.log(recipe)
+export const findRecipeIngredients = (recipeData, tag) => {
+  let recipeNames = recipeData.filter(recipe => recipe.tags.includes(tag)).map(recipe => recipe.name)
+  console.log(recipeNames)
+  return recipeNames
 }
