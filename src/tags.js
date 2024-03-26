49203cf9cbf1e61332a5dcdfe292e559d0921a28
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-25 17:12:21 -0700

    feature: Get available tags

--- src/tags.js
+++ src/tags.js
@@ -1,12 +1,22 @@
-import recipeData from '../src/data/recipes.js';
+import recipeData from "../src/data/recipes.js";
 
 export const filterRecipeByTag = (tags) => {
-    const filteredRecipes = [];
+  const filteredRecipes = [];
 
-    recipeData.forEach((recipe) => {
-        if (tags.every((tag) => recipe.tags.includes(tag))) {
-            filteredRecipes.push(recipe);
-        }
-    }); 
-    return filteredRecipes;
+  recipeData.forEach((recipe) => {
+    if (tags.every((tag) => recipe.tags.includes(tag))) {
+      filteredRecipes.push(recipe);
+    }
+  });
+  return filteredRecipes;
 };
+
+export function getAvailableTags(tags) {
+  return filterRecipeByTag(tags).reduce((list, recipe) => {
+    recipe.tags.forEach((tag) => {
+      if (!list.hasOwnProperty(tag)) list[tag] = 0;
+      list[tag]++;
+    });
+    return list;
+  }, {});
+}
--- test/tags-test.js
+++ test/tags-test.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
+import { recipe1, recipe2 } from "../src/data/mockRecipe";
 import recipeData from "../src/data/recipes";
-import { filterRecipeByTag } from "../src/tags";
+import { filterRecipeByTag, getAvailableTags } from "../src/tags";
 
 describe("filterRecipeByTag", function () {
   it("should find recipes by id tag", function () {
@@ -22,5 +23,51 @@ describe("filterRecipeByTag", function () {
 
     expect(recipesTag).to.be.an("array");
     expect(recipesTag).to.have.lengthOf(recipeData.length);
+  });
+});
+
+describe("Get available tags", () => {
+  it("Should return all tags in empty array", () => {
+    expect(getAvailableTags([])).to.deep.equal({
+      antipasti: 9,
+      starter: 9,
+      snack: 9,
+      appetizer: 9,
+      antipasto: 9,
+      "hor d'oeuvre": 9,
+      lunch: 12,
+      "main course": 12,
+      "main dish": 12,
+      dinner: 12,
+      sauce: 1,
+      "side dish": 22,
+      "morning meal": 1,
+      brunch: 1,
+      breakfast: 1,
+      salad: 4,
+      condiment: 1,
+      dip: 1,
+      spread: 1,
+    });
+  });
+
+  it("Should return the tags plus numbers given a set of tags #1", () => {
+    expect(getAvailableTags(["brunch"])).to.deep.equal({
+      breakfast: 1,
+      brunch: 1,
+      "morning meal": 1,
+    });
+  });
+
+  it("Should return the tags plus numbers given a set of tags #2", () => {
+    expect(getAvailableTags(["snack", "side dish"])).to.deep.equal({
+      antipasti: 1,
+      antipasto: 1,
+      appetizer: 1,
+      "hor d'oeuvre": 1,
+      "side dish": 1,
+      snack: 1,
+      starter: 1,
+    });
   });
 });
