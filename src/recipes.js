b53958b181b27b076a3019892aa65bb3d7716dff
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-26 16:11:12 -0600

    feat: Moves all functions into a single recipes file, and double checks they still pass all tests. fix: Adds in a new parameter to findRecipeName.

--- src/recipes.js
+++ src/recipes.js
@@ -10,5 +10,48 @@ function getIngredientsData(){
 function getRecipeInstructions(recipe){
   return recipe["instructions"];
 }
-export {getRecipeData, getIngredientsData, getRecipeInstructions};
+function findRecipeIngredients(recipe, ingredients){
+  const results = recipe['ingredients'].map((element) =>{
+      let match = ingredients.find(({id}) => id === element['id'])
+      if (match !== undefined){
+          return match
+      };
+  });
+  return results
+  };
+
+function estimatedCostInCents(recipe, ingredientList) {
+  const total = recipe.ingredients.reduce((acc, ingredient) => {
+      const matchingIngredient = ingredientList.find((item) => {
+          return item.id === ingredient.id
+      })
+      acc += matchingIngredient.estimatedCostInCents * ingredient.quantity.amount;
+      return acc
+  }, 0)
+  return total
+}
+const filterRecipeTag = (tag,recipeData) => { 
+  let filterRecipes = recipeData.filter((recipe) => {
+     return recipe['tags'].includes(tag);
+  });
+  let filteredRecipes = filterRecipes.reduce((acc, recipes) =>{
+      acc = recipes
+      return acc
+  }, {})
+  return filteredRecipes
+}
+
+const filterRecipeName = (name, recipeData) => {
+let findRecipe = recipeData.find((recipe) => {
+  return recipe['name'].includes(name)
+});
+return findRecipe
+}
+export {getRecipeData,
+  getIngredientsData,
+  getRecipeInstructions, 
+  estimatedCostInCents, 
+  filterRecipeName, 
+  filterRecipeTag,
+  findRecipeIngredients};
 
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -3,28 +3,28 @@ import { findRecipeIngredients , getRecipeData, getIngredientsData, getRecipeIns
 import {ingredients, recipes} from './mock-data'
 
 describe('getRecipeData', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(getRecipeData).to.be.a('function');
   });
-  it.skip('Should return a array of objects indentical  to the referenced data',()=>{
+  it('Should return a array of objects indentical  to the referenced data',()=>{
     const recipeData = getRecipeData()
     expect(recipeData).to.deep.equal(recipes)
   });
 })
 describe('getIngredientsData', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(getIngredientsData).to.be.a('function');
   });
-  it.skip('Should return a array of objects indentical  to the referenced data',()=>{
+  it('Should return a array of objects indentical  to the referenced data',()=>{
     const ingredientsData = getIngredientsData()
     expect(ingredientsData).to.deep.equal(ingredients)
   });
 })
 describe('filterRecipeTag', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(filterRecipeTag).to.be.a('function');
   });
-  it.skip('Should return an array of recipes that match a given tag',()=>{
+  it('Should return an array of recipes that match a given tag',()=>{
     const recipeData = getRecipeData();
     const tag = 'lunch'
     const filteredRecipes = filterRecipeTag(tag,recipeData);
@@ -33,22 +33,22 @@ describe('filterRecipeTag', () => {
   });
 })
 describe('filterRecipeName', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(filterRecipeName).to.be.a('function');
   });
   it('Should return a recipe when given a name',()=>{
     const recipeData = getRecipeData();
     const name = 'Pancakes';
-    const searchResult = filterRecipeName(name);
+    const searchResult = filterRecipeName(name, recipeData);
     const pancakes = recipeData[0];
     expect(searchResult).to.deep.equal(pancakes);
   });
 })
 describe('getRecipeInstructions', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(getRecipeInstructions).to.be.a('function');
   });
-  it.skip('Should return a array of instructions objects from selected recipe',()=>{
+  it('Should return a array of instructions objects from selected recipe',()=>{
     const recipeData = getRecipeData();
     const recipe = recipeData[0];
     const recipeInstructions = getRecipeInstructions(recipe);
@@ -56,10 +56,10 @@ describe('getRecipeInstructions', () => {
   });
 })
 describe('estimatedCostInCents', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(estimatedCostInCents).to.be.a('function');
   });
-  it.skip('Should take in a recipe object and ingredients list and return a cost in cents',()=>{
+  it('Should take in a recipe object and ingredients list and return a cost in cents',()=>{
     const pancakes = recipes[0]
     const ingredientList = [ingredients[0],ingredients[1],ingredients[2],ingredients[3]]
     const totalCost = estimatedCostInCents(pancakes,ingredientList);
@@ -67,10 +67,10 @@ describe('estimatedCostInCents', () => {
   })
 })
 describe('Recipe', () => {
-  it.skip('Should be a function', () => {
+  it('Should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
   });
-  it.skip('Should take in a recipe object and an ingredients object array, and return an array of matches', ()=>{
+  it('Should take in a recipe object and an ingredients object array, and return an array of matches', ()=>{
     const recipeData = getRecipeData();
     const recipe = recipeData[0];
     const ingredientsData = getIngredientsData()
