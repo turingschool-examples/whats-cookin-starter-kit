6f3205e650bb21d42f7ca2f2c1cdf56d8782ea5b
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-26 16:57:08 -0700

    fix: can search through dataset

--- test/cost-test.js
+++ test/cost-test.js
@@ -1,20 +1,21 @@
 import { expect } from "chai";
 import { calculateRecipeCost } from "../src/cost.js";
+import ingredientsData from "../src/data/ingredients.js";
 import { recipe1, recipe2 } from "../src/data/mockRecipe.js";
 
 describe("calculateRecipeCost", function () {
   it("should default to zero dollars", function () {
-    const totalCost = calculateRecipeCost({});
+    const totalCost = calculateRecipeCost({}, ingredientsData);
     expect(totalCost).to.be.equal(0);
   });
 
   it("should be able to calculate the total cost of recipe1", function () {
-    const totalCost = calculateRecipeCost(recipe1);
+    const totalCost = calculateRecipeCost(recipe1, ingredientsData);
     expect(totalCost).to.be.equal(177.76);
   });
 
   it("should be able to calculate the total cost of recipe2", function () {
-    const totalCost = calculateRecipeCost(recipe2);
+    const totalCost = calculateRecipeCost(recipe2, ingredientsData);
     expect(totalCost).to.be.equal(1715.46);
   });
 });
