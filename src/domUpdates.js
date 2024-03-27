e55de0d0031a3cf454a0db39cf354e8dcecab05b
Ben Wehrend <benwehrend@gmail.com>
2024-03-27 08:48:35 -0700

    adds working branch of recipe page

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -36,32 +36,6 @@ function displayRecipes(dataBase) {
   console.log(`Displaying recipes now`);
 }
 
-// function createRecipeHTML(recipe) {
-//   const article = document.createElement("article");
-//   article.classList.add("recipe-card");
-//   article.setAttribute("data-id", recipe.id);
-
-//   article.innerHTML = `
-//     <div class="recipe-image">
-//       <img src="${recipe.image}" alt="${recipe.name}">
-//     </div>
-//     <div class="recipe-info">
-//       <div class="tags-and-heart">
-//         <h3 class="recipe-tags">${recipe.tags.join(", ")}</h3>
-//         <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-//           <!-- SVG content omitted for brevity -->
-//         </svg>
-//       </div>
-//       <h2 class="recipe-name">${recipe.name}</h2>
-//       <h3 class="recipe-ingredients"><span class="label">Ingredients:</span> ${findRecipeIngredients(recipe).join(", ")}</h3>
-//     </div>`;
-
-//   article.addEventListener("click", () => {
-//     displayRecipes([recipe]);
-//   });
-
-//   return article;
-// }
 
 function createRecipeHTML(recipe) {
   const article = document.createElement("article");
@@ -75,61 +49,91 @@ function createRecipeHTML(recipe) {
     <div class="recipe-info">
       <div class="tags-and-heart">
         <h3 class="recipe-tags">${recipe.tags.join(", ")}</h3>
-        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <!-- SVG content omitted for brevity -->
+        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+          style="
+            fill: rgba(157, 150, 139, 1);
+            transform: scaleX(-1);
+            msFilter: 'progid:DXImageTransform.Microsoft.BasicImage(rotation=0, mirror=1)';
+          ">
+          <path d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z"></path>
         </svg>
       </div>
       <h2 class="recipe-name">${recipe.name}</h2>
-      <h3 class="recipe-ingredients"><span class="label">Ingredients:</span> ${findRecipeIngredients(
-        recipe
-      ).join(", ")}</h3>
-    </div>`;
-
-  article.addEventListener("click", () => {
-    console.log("recipe", recipe.instructions[0]);
-    const main = document.querySelector("main");
-    main.innerHTML = "";
-
-    const instructionsList = findRecipeInstructions(recipe).reduce(
-      (innerHTML, instruction) => {
-        innerHTML += `<li>${instruction}</li>`;
-        return innerHTML;
-      },
-      ""
-    );
-
-    const ingredientList = findRecipeIngredients(recipe);
-    const quantityList = findRecipeIngredientsQuantity(recipe);
-
-    let ingredientQuantityHTML = "";
-
-    ingredientList.forEach((ingredient, index) => {
-      ingredientQuantityHTML += `<li>
-      <div class="ingredient-name">${ingredient}</div>
-      <div class="ingredient-amount">${quantityList[index]}</div>
-       </li>`;
-    });
-
-    main.innerHTML = `
-    <div class="recipe-title">
-      <div class="image-container">
-        <img src="${recipe.image}" alt="${recipe.name} Image" />
-      </div>
-      <h1>${recipe.name}</h1>
+      <h3 class="recipe-ingredients">
+        <span class="label">Ingredients:</span> ${findRecipeIngredients(
+          recipe,
+        ).join(", ")}
+      </h3>
     </div>
+  `;
 
-    <div class="instructions">
-      <h1>Instructions</h1>
-      <ol>${instructionsList}</ol>
-    </div>
-
-    <div class="ingredients">
-      <h1>Ingredients</h1>
-      <ul>${ingredientQuantityHTML}</ul>
-    </div>`;
-  });
+  article.addEventListener("click", () => displayRecipeDetails(recipe));
 
   return article;
+}
+
+function displayRecipeDetails(recipe) {
+  const body = document.querySelector("body");
+  body.innerHTML = "";
+
+  const instructionsList = findRecipeInstructions(recipe).reduce(
+    (innerHTML, instruction) => {
+      innerHTML += `<li>${instruction}</li>`;
+      return innerHTML;
+    },
+    "",
+  );
+
+  const ingredientList = findRecipeIngredients(recipe);
+  const quantityList = findRecipeIngredientsQuantity(recipe);
+
+  let ingredientQuantityHTML = ingredientList
+    .map((ingredient, index) => {
+      return `<li>
+      <div class="ingredient-name">${ingredient}</div>
+      <div class="ingredient-amount">${quantityList[index]}</div>
+    </li>`;
+    })
+    .join("");
+
+  body.innerHTML = `
+    <header>
+      <h1 class="logo">What's Cookin'</h1>
+      <nav class="header-nav">
+        <button class="cookbook">COOKBOOK</button>
+        <button class="saved-recipes">SAVED RECIPES</button>
+      </nav>
+    </header>
+
+    <nav class="filter-container hidden">
+      <div class="filter-settings">
+        <input type="text" class="search-box" placeholder="Search ..."/>
+        <div class="tags-container">
+          <button class="tag">tag1</button>
+          <button class="tag">tag2</button>
+          <button class="tag">tag3</button>
+          <button class="tag">tag4</button>
+        </div>
+      </div>
+      <button class="random-recipe">RANDOM RECIPE</button>
+    </nav>
+
+    <main id="recipe-page">
+      <div class="recipe-title">
+        <div class="image-container">
+          <img src="${recipe.image}" alt="${recipe.name}"/>
+        </div>
+        <h1>${recipe.name}</h1>
+      </div>
+      <div class="instructions">
+        <h1>Instructions</h1>
+        <ol>${instructionsList}</ol>
+      </div>
+      <div class="ingredients">
+        <h1>Ingredients</h1>
+        <ul>${ingredientQuantityHTML}</ul>
+      </div>
+    </main>`;
 }
 
 function getActiveTags() {
